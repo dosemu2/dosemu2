d1e41bd41104d83595596a4fffbd25b1066e2070
Stas Sergeev <stsp@list.ru>
2004-10-12 18:51:38 +0000

    Making config.hogthreshold (and ocasionally a couple of other vars) signed - needed for my upcoming hogthreshold updates (using the negative thresholds sometimes).
    git-svn-id: https://dosemu.svn.sourceforge.net/svnroot/dosemu/trunk@724 7758b857-130e-0410-ab9c-d8735a528d18

--- src/include/emu.h
+++ src/include/emu.h
@@ -278,11 +278,11 @@ typedef struct vesamode_type_struct {
 
        int pktflags;		/* global flags for packet driver */
 
-       unsigned int update, freq;	/* temp timer magic */
+       int update, freq;	/* temp timer magic */
        unsigned long cpu_spd;		/* (1/speed)<<32 */
        unsigned long cpu_tick_spd;	/* (1.19318/speed)<<32 */
 
-       unsigned int hogthreshold;
+       int hogthreshold;
 
        int mem_size, xms_size, ems_size, dpmi, pm_dos_api, max_umb;
        unsigned int ems_frame;
