1aa5781b3cff5afe08b4c0df4200bfa12152af5e
Bart Oldeman <bartoldeman@users.sourceforge.net>
2005-08-28 00:05:03 +0000

    Re-add.
    git-svn-id: https://dosemu.svn.sourceforge.net/svnroot/dosemu/trunk@1283 7758b857-130e-0410-ab9c-d8735a528d18

---
+++ src/emu-i386/simx86/codegen-arch.h
@@ -0,0 +1,11 @@
+/* 
+ * (C) Copyright 1992, ..., 2005 the "DOSEMU-Development-Team".
+ *
+ * for details see file COPYING in the DOSEMU distribution
+ */
+
+#ifdef HOST_ARCH_SIM
+#include "codegne-sim.h"
+#else
+#include "codegen-x86.h"
+#endif
