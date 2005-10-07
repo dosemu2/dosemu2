17126391d04be92305d3b9d1549cd241438142dc
Stas Sergeev <stsp@list.ru>
2005-10-07 19:06:15 +0000

    Timer is not needed for sequencer.
    git-svn-id: https://dosemu.svn.sourceforge.net/svnroot/dosemu/trunk@1356 7758b857-130e-0410-ab9c-d8735a528d18

--- src/arch/linux/dosext/sound/midid/oss.c
+++ src/arch/linux/dosext/sound/midid/oss.c
@@ -22,47 +22,11 @@
 
 #define SEQUENCER_DEV   "/dev/sequencer"  /* Used device */
 
-#define TIME2TICK(t) ((long long)t.tv_sec * timebase + \
-    (((long long)t.tv_usec * timebase) / 1000000))
-
-static long long start_time;
-static int timebase = 100, timer_started = 0;
 static int seqfd;  /* Sequencer file handle */
 
 void oss_seqbuf_dump(void);
 
 SEQ_USE_EXTBUF();
-
-static void oss_start_timer(void)
-{
-  struct timeval time;
-  gettimeofday(&time, NULL);
-  start_time = TIME2TICK(time);
-  SEQ_START_TIMER();
-  SEQ_DUMPBUF();
-  timer_started = 1;
-}
-
-static void oss_stop_timer(void)
-{
-  SEQ_STOP_TIMER();
-  SEQ_DUMPBUF();
-  timer_started = 0;
-}
-
-static long oss_get_ticks(void)
-{
-  struct timeval time;
-  gettimeofday(&time, NULL);
-  return (TIME2TICK(time) - start_time);
-}
-
-static void oss_timestamp(void)
-{
-  if (!timer_started)
-    oss_start_timer();
-  SEQ_WAIT_TIME(oss_get_ticks());
-}
 
 static bool oss_detect(void)
 {
@@ -81,55 +45,48 @@ static bool oss_init(void)
 
 static void oss_done(void)
 {
-  oss_stop_timer();
+  close(seqfd);
+  seqfd = -1;
 }
 
 static void oss_noteon(int chn, int note, int vol)
 {
-  oss_timestamp();
   SEQ_START_NOTE(0, chn, note, vol);
 }
 
 static void oss_noteoff(int chn, int note, int vol)
 {
-  oss_timestamp();
   SEQ_STOP_NOTE(0, chn, note, vol);
 }
 
 static void oss_control(int chn, int control, int value)
 {
-  oss_timestamp();
   SEQ_CONTROL(0, chn, control, value);
 }
 
 static void oss_notepressure(int chn, int note, int pressure)
 {
-  oss_timestamp();
   SEQ_KEY_PRESSURE(0, chn, note, pressure);
 }
 
 static void oss_channelpressure(int chn, int pressure)
 {
-  oss_timestamp();
   SEQ_CHN_PRESSURE(0, chn, pressure);
 }
 
 static void oss_bender(int chn, int pitch)
 {
-  oss_timestamp();
   SEQ_BENDER(0, chn, pitch);
 }
 
 static void oss_program(int chn, int pgm)
 {
-  oss_timestamp();
   SEQ_PGM_CHANGE(0, chn, pgm);
 }
 
 static void oss_sysex(unsigned char *buf, int len)
 {
   int i;
-  oss_timestamp();
   for (i = 0; i < len; i += 6)
     SEQ_SYSEX(0, buf + i, MIN(len - i, 6));
 }
