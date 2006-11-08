dosemu2/dosemu2

commit c56a34442427439ee32fbc56d297784efd93c6ec

Use load/op/store for cpuemu (I)MUL, (I)DIV, NEG, and NOT.
git-svn-id: https://dosemu.svn.sourceforge.net/svnroot/dosemu/trunk@1670 7758b857-130e-0410-ab9c-d8735a528d18

src/emu-i386/simx86/codegen-sim.c
@@ -1076,13 +1076,13 @@ static void Gen_sim(int op, int mode, ...)
 	case O_NOT:		// no flags
 		GTRACE0("O_NOT");
 		if (mode & MBYTE) {
-			*AR1.pu = ~*AR1.pu;
+			DR1.b.bl = ~DR1.b.bl;
 		}
 		else if (mode & DATA16) {
-			*AR1.pwu = ~*AR1.pwu;
+			DR1.w.l = ~DR1.w.l;
 		}
 		else {
-			*AR1.pdu = ~*AR1.pdu;
+			DR1.d = ~DR1.d;
 		}
 		break;
 	case O_NEG:		// OSZAPC
@@ -1091,13 +1091,13 @@ static void Gen_sim(int op, int mode, ...)
 		RFL.valid = V_SUB;
 		RFL.S1 = 0;
 		if (mode & MBYTE) {
-			*AR1.pu = RFL.RES.d = -(RFL.S2=*AR1.pu);
+			DR1.b.bl = RFL.RES.d = -(RFL.S2=DR1.b.bl);
 		}
 		else if (mode & DATA16) {
-			*AR1.pwu = RFL.RES.d = -(RFL.S2=*AR1.pwu);
+			DR1.w.l = RFL.RES.d = -(RFL.S2=DR1.w.l);
 		}
 		else {
-			*AR1.pdu = RFL.RES.d = -(RFL.S2=*AR1.pdu);
+			DR1.d = RFL.RES.d = -(RFL.S2=DR1.d);
 		}
 		SET_CF(RFL.S2!=0);
 		break;
@@ -1190,20 +1190,20 @@ static void Gen_sim(int op, int mode, ...)
 		RFL.valid = V_GEN;
 		if (mode & MBYTE) {
 		    DR1.w.l =
-			(unsigned int)CPUBYTE(Ofs_AL) * (unsigned int)*AR1.pu;
+			(unsigned int)CPUBYTE(Ofs_AL) * (unsigned int)DR1.b.bl;
 		    CPUWORD(Ofs_AX) = DR1.w.l;
 		    of = (DR1.b.bh != 0);
 		}
 		else if (mode&DATA16) {
 		    DR1.d =
-			(unsigned int)CPUWORD(Ofs_AX) * (unsigned int)*AR1.pwu;
+			(unsigned int)CPUWORD(Ofs_AX) * (unsigned int)DR1.w.l;
 		    CPUWORD(Ofs_AX) = DR1.w.l;
 		    CPUWORD(Ofs_DX) = DR1.w.h;
 		    of = (DR1.w.h != 0);
 		}
 		else {
 		    u_int64_u v;
-		    v.td = (u_int64_t)CPULONG(Ofs_EAX) * (u_int64_t)*AR1.pdu;
+		    v.td = (u_int64_t)CPULONG(Ofs_EAX) * (u_int64_t)DR1.d;
 		    CPULONG(Ofs_EAX) = v.t.tl;
 		    CPULONG(Ofs_EDX) = v.t.th;
 		    of = (v.t.th != 0);
@@ -1226,7 +1226,7 @@ static void Gen_sim(int op, int mode, ...)
 			int b = va_arg(ap, int);
 			signed char o = Offs_From_Arg();
 			GTRACE3("O_IMUL",o,0xff,b);
-			DR1.ds = (int)*AR1.pws * b;
+			DR1.ds = (int)DR1.ws.l * b;
 			CPUWORD(o) = DR1.w.l;
 			DR1.ds >>= 15;
 			of = ((DR1.ds!=0) && (DR1.ds!=0xffffffff));
@@ -1236,7 +1236,7 @@ static void Gen_sim(int op, int mode, ...)
 			signed char o = Offs_From_Arg();
 			int64_u v;
 			GTRACE3("O_IMUL",o,0xff,b);
-			v.td = *AR1.pds;
+			v.td = DR1.ds;
 			v.td *= b;
 			CPULONG(o) = v.t.tl;
 			v.td >>= 31;
@@ -1256,7 +1256,7 @@ static void Gen_sim(int op, int mode, ...)
 			int b = va_arg(ap, int);
 			signed char o = Offs_From_Arg();
 			GTRACE3("O_IMUL",o,0xff,b);
-		    	DR1.ds = (int)*AR1.pws * b;
+		    	DR1.ds = (int)DR1.ws.l * b;
 		    	CPUWORD(o) = DR1.w.l;
 			DR1.ds >>= 15;
 			of = ((DR1.ds!=0) && (DR1.ds!=0xffffffff));
@@ -1264,7 +1264,7 @@ static void Gen_sim(int op, int mode, ...)
 		    else if (mode&MEMADR) {
 			signed char o = Offs_From_Arg();
 			GTRACE1("O_IMUL",o);
-			DR1.ds = (int)CPUWORD(o) * (int)*AR1.pws;
+			DR1.ds = (int)CPUWORD(o) * (int)DR1.ws.l;
 			CPUWORD(o) = DR1.w.l;
 			DR1.ds >>= 15;
 			of = ((DR1.ds!=0) && (DR1.ds!=0xffffffff));
@@ -1272,7 +1272,7 @@ static void Gen_sim(int op, int mode, ...)
 		    else {
 		    	GTRACE0("O_IMUL");
 			DR1.ds =
-			    (int)CPUWORD(Ofs_AX) * (int)*AR1.pws;
+			    (int)CPUWORD(Ofs_AX) * (int)DR1.ws.l;
 			CPUWORD(Ofs_AX) = DR1.w.l;
 			CPUWORD(Ofs_DX) = DR1.w.h;
 			DR1.ds = (DR1.ds >> 15) & 3;	// bits 15,16(ext)
@@ -1285,7 +1285,7 @@ static void Gen_sim(int op, int mode, ...)
 			signed char o = Offs_From_Arg();
 			int64_u v;
 			GTRACE3("O_IMUL",o,0xff,b);
-			v.td = (int64_t)*AR1.pds;
+			v.td = (int64_t)DR1.ds;
 			v.td *= b;
 			CPULONG(o) = v.t.tl;
 			v.td >>= 31;
@@ -1296,7 +1296,7 @@ static void Gen_sim(int op, int mode, ...)
 			int64_u v;
 			int vd = CPULONG(o);
 			GTRACE1("O_IMUL",o);
-			v.td = (int64_t)*AR1.pds;
+			v.td = (int64_t)DR1.ds;
 			v.td *= (int64_t)vd;
 			CPULONG(o) = v.t.tl;
 			v.td >>= 31;
@@ -1306,7 +1306,7 @@ static void Gen_sim(int op, int mode, ...)
 			int64_u v;
 			int vd = CPULONG(Ofs_EAX);
 		    	GTRACE0("O_IMUL");
-			v.td = (int64_t)vd * (int64_t)*AR1.pds;
+			v.td = (int64_t)vd * (int64_t)DR1.ds;
 			CPULONG(Ofs_EAX) = v.t.tl;
 			CPULONG(Ofs_EDX) = v.t.th;
 			v.td >>= 31;			// b31->b0
@@ -1330,7 +1330,7 @@ static void Gen_sim(int op, int mode, ...)
 			RFL.RES.w.h = 0;
 			/* exception trap: save current PC */
 			CPULONG(Ofs_CR2) = va_arg(ap,int);
-			RFL.S1 = *AR1.pu;
+			RFL.S1 = DR1.b.bl;
 			if (RFL.S1==0)
 			    TheCPU.err = EXCP00_DIVZ;
 			else {
@@ -1344,7 +1344,7 @@ static void Gen_sim(int op, int mode, ...)
 				RFL.RES.w.h = CPUWORD(Ofs_DX);
 				/* exception trap: save current PC */
 				CPULONG(Ofs_CR2) = va_arg(ap,int);
-				RFL.S1 = *AR1.pwu;
+				RFL.S1 = DR1.w.l;
 				if (RFL.S1==0)
 				    TheCPU.err = EXCP00_DIVZ;
 		    		else {
@@ -1359,7 +1359,7 @@ static void Gen_sim(int op, int mode, ...)
 				v.t.th = CPULONG(Ofs_EDX);
 				/* exception trap: save current PC */
 				CPULONG(Ofs_CR2) = va_arg(ap,int);
-				RFL.S1 = *AR1.pdu;
+				RFL.S1 = DR1.d;
 				if (RFL.S1==0)
 				    TheCPU.err = EXCP00_DIVZ;
 		    		else {
@@ -1378,7 +1378,7 @@ static void Gen_sim(int op, int mode, ...)
 			RFL.RES.d = (signed short)CPUWORD(Ofs_AX);
 			/* exception trap: save current PC */
 			CPULONG(Ofs_CR2) = va_arg(ap,int);
-			RFL.S1 = *AR1.ps;
+			RFL.S1 = DR1.bs.bl;
 			if (RFL.S1==0)
 			    TheCPU.err = EXCP00_DIVZ;
 	    		else {
@@ -1392,7 +1392,7 @@ static void Gen_sim(int op, int mode, ...)
 				RFL.RES.w.h = CPUWORD(Ofs_DX);
 				/* exception trap: save current PC */
 				CPULONG(Ofs_CR2) = va_arg(ap,int);
-				RFL.S1 = *AR1.pws;
+				RFL.S1 = DR1.ws.l;
 				if (RFL.S1==0)
 				    TheCPU.err = EXCP00_DIVZ;
 		    		else {
@@ -1407,7 +1407,7 @@ static void Gen_sim(int op, int mode, ...)
 				v.t.th = CPULONG(Ofs_EDX);
 				/* exception trap: save current PC */
 				CPULONG(Ofs_CR2) = va_arg(ap,int);
-				RFL.S1 = *AR1.pdu;
+				RFL.S1 = DR1.d;
 				if (RFL.S1==0)
 				    TheCPU.err = EXCP00_DIVZ;
 		    		else {

src/emu-i386/simx86/interp.c
@@ -641,11 +641,13 @@ intop3b:		{ int op = ArOpsFR[D_MO(opc)];
 			break; 
 /*69*/	case IMULwrm:
 			PC += ModRM(opc, PC, mode);
+			Gen(L_DI_R1, mode);		// mov (e)ax,[edi]
 			Gen(O_IMUL,mode|IMMED,DataFetchWL_S(mode,PC),REG1);
 			INC_WL_PC(mode, 0);
 			break;
 /*6b*/	case IMULbrm:
 			PC += ModRM(opc, PC, mode);
+			Gen(L_DI_R1, mode|MBYTE);		// mov al,[edi]
 			Gen(O_IMUL,mode|MBYTE|IMMED,(signed char)Fetch(PC),REG1);
 			PC++;
 			break;
@@ -1708,18 +1710,20 @@ repag0:
 			break;
 /*f6*/	case GRP1brm: {
 			PC += ModRM(opc, PC, mode|MBYTE);	// EDI=mem
+			Gen(L_DI_R1, mode|MBYTE);		// mov al,[edi]
 			switch(REG1) {
 			case Ofs_AL:	/*0*/	/* TEST */
 			case Ofs_CL:	/*1*/	/* undocumented */
-				Gen(L_DI_R1, mode|MBYTE);		// mov al,[edi]
 				Gen(O_AND_R, mode|MBYTE|IMMED, Fetch(PC));	// op al,#imm
 				PC++;
 				break;
 			case Ofs_DL:	/*2*/	/* NOT */
-				Gen(O_NOT, mode|MBYTE);			// not [edi]
+				Gen(O_NOT, mode|MBYTE);			// not al
+				Gen(S_DI, mode|MBYTE);
 				break;
 			case Ofs_BL:	/*3*/	/* NEG */
-				Gen(O_NEG, mode|MBYTE);			// neg [edi]
+				Gen(O_NEG, mode|MBYTE);			// neg al
+				Gen(S_DI, mode|MBYTE);
 				break;
 			case Ofs_AH:	/*4*/	/* MUL AL */
 				Gen(O_MUL, mode|MBYTE);			// al*[edi]->AX unsigned
@@ -1737,18 +1741,20 @@ repag0:
 			break;
 /*f7*/	case GRP1wrm: {
 			PC += ModRM(opc, PC, mode);			// EDI=mem
+			Gen(L_DI_R1, mode);		// mov (e)ax,[edi]
 			switch(REG1) {
 			case Ofs_AX:	/*0*/	/* TEST */
 			case Ofs_CX:	/*1*/	/* undocumented */
-				Gen(L_DI_R1, mode);		// mov al,[edi]
 				Gen(O_AND_R, mode|IMMED, DataFetchWL_U(mode,PC));	// op al,#imm
 				INC_WL_PC(mode,0);
 				break;
 			case Ofs_DX:	/*2*/	/* NOT */
-				Gen(O_NOT, mode);			// not [edi]
+				Gen(O_NOT, mode);			// not (e)ax
+				Gen(S_DI, mode);
 				break;
 			case Ofs_BX:	/*3*/	/* NEG */
-				Gen(O_NEG, mode);			// neg [edi]
+				Gen(O_NEG, mode);			// neg (e)ax
+				Gen(S_DI, mode);
 				break;
 			case Ofs_SP:	/*4*/	/* MUL AX */
 				Gen(O_MUL, mode);			// (e)ax*[edi]->(E)DX:(E)AX unsigned
@@ -2577,6 +2583,7 @@ repag0:
 			/* case 0xae:	Code Extension 24(MMX) */
 			case 0xaf: /* IMULregrm */
 				PC++; PC += ModRM(opc, PC, mode);
+				Gen(L_DI_R1, mode);		// mov (e)ax,[edi]
 				Gen(O_IMUL, mode|MEMADR, REG1);	// reg*[edi]->reg signed
 				break;
 			case 0xb0:

src/emu-i386/simx86/codegen-x86.c
@@ -673,25 +673,25 @@ arith1:
 		break;
 	case O_NOT:
 		if (mode & MBYTE) {
-			// notb (%%edi)
-			G2M(0xf6,0x17,Cp);
+			// notb %%al
+			G2M(0xf6,0xd0,Cp);
 		}
 		else {
-			// NOT{wl} (%%edi)
+			// NOT{wl} %%(e)ax
 			Gen66(mode,Cp);
-			G2M(0xf7,0x17,Cp);
+			G2M(0xf7,0xd0,Cp);
 		}
 		break;
 	case O_NEG:
 		G1(POPF,Cp);	// get flags from stack
 		if (mode & MBYTE) {
-			// negb (%%edi)
-			G2M(0xf6,0x1f,Cp);
+			// negb %%al
+			G2M(0xf6,0xd8,Cp);
 		}
 		else {
 			// neg{wl} (%%edi)
 			Gen66(mode,Cp);
-			G2M(0xf7,0x1f,Cp);
+			G2M(0xf7,0xd8,Cp);
 		}
 		G1(PUSHF,Cp);	// flags back on stack
 		break;
@@ -790,28 +790,22 @@ arith1:
 	case O_MUL:
 		G1(POPF,Cp);	// get flags from stack
 		if (mode & MBYTE) {
-			// movb Ofs_AL(%%ebx),%%al
-			G3M(0x8a,0x43,Ofs_AL,Cp);
-			// mulb (%%edi),%%al
-			G2M(0xf6,0x27,Cp);
+			// mulb Ofs_AL(%%ebx),%%al
+			G3M(0xf6,0x63,Ofs_AL,Cp);
 			// movw %%ax,Ofs_AX(%%ebx)
 			G4M(OPERoverride,0x89,0x43,Ofs_AX,Cp);
 		}
 		else if (mode&DATA16) {
-			// movw Ofs_AX(%%ebx),%%ax
-			G4M(OPERoverride,0x8b,0x43,Ofs_AX,Cp);
-			// mulw (%%edi),%%ax
-			G3M(OPERoverride,0xf7,0x27,Cp);
+			// mulw Ofs_AX(%%ebx),%%ax
+			G4M(OPERoverride,0xf7,0x63,Ofs_AX,Cp);
 			// movw %%ax,Ofs_AX(%%ebx)
 			G4M(OPERoverride,0x89,0x43,Ofs_AX,Cp);
 			// movw %%dx,Ofs_DX(%%ebx)
 			G4M(OPERoverride,0x89,0x53,Ofs_DX,Cp);
 		}
 		else {
-			// movl Ofs_EAX(%%ebx),%%eax
-			G3M(0x8b,0x43,Ofs_EAX,Cp);
-			// mull (%%edi),%%eax
-			G2M(0xf7,0x27,Cp);
+			// mull Ofs_EAX(%%ebx),%%eax
+			G3M(0xf7,0x63,Ofs_EAX,Cp);
 			// movl %%eax,Ofs_EAX(%%ebx)
 			G3M(0x89,0x43,Ofs_EAX,Cp);
 			// movl %%edx,Ofs_EDX(%%ebx)
@@ -823,52 +817,41 @@ arith1:
 		G1(POPF,Cp);	// get flags from stack
 		if (mode & MBYTE) {
 			if ((mode&(IMMED|DATA16))==(IMMED|DATA16)) {
-              			// movw (%%edi),%%dx
-				G3M(OPERoverride,0x8b,0x17,Cp);
-				// imul $immed,%%dx,%%ax
-				G3M(OPERoverride,0x6b,0xc2,Cp); G1(IG->p0,Cp);
+				// imul $immed,%%ax,%%ax
+				G3M(OPERoverride,0x6b,0xc0,Cp); G1(IG->p0,Cp);
 				// movw %%ax,offs(%%ebx)
 				G4M(OPERoverride,0x89,0x43,IG->p1,Cp);
 			}
 			else if ((mode&(IMMED|DATA16))==IMMED) {
-              			// movl (%%edi),%%edx
-				G2M(0x8b,0x17,Cp);
-				// imul $immed,%%edx,%%eax
-				G2M(0x6b,0xc2,Cp); G1(IG->p0,Cp);
+				// imul $immed,%%eax,%%eax
+				G2M(0x6b,0xc0,Cp); G1(IG->p0,Cp);
 				// movl %%eax,offs(%%ebx)
 				G3M(0x89,0x43,IG->p1,Cp);
 			}
 			else {
-				// movb Ofs_AL(%%ebx),%%al
-				G3M(0x8a,0x43,Ofs_AL,Cp);
-				// imul (%%edi),%%al
-				G2M(0xf6,0x2f,Cp);
+				// imul Ofs_AL(%%ebx),%%al
+				G3M(0xf6,0x6b,Ofs_AL,Cp);
 				// movw %%ax,Ofs_AX(%%ebx)
 				G4M(OPERoverride,0x89,0x43,Ofs_AX,Cp);
 			}
 		}
 		else if (mode&DATA16) {
 			if (mode&IMMED) {
-              			// movw (%%edi),%%dx
-				G3M(OPERoverride,0x8b,0x17,Cp);
-				// imul $immed,%%dx,%%ax
-				G3M(OPERoverride,0x69,0xc2,Cp); G2(IG->p0,Cp);
+				// imul $immed,%%ax,%%ax
+				G3M(OPERoverride,0x69,0xc0,Cp); G2(IG->p0,Cp);
 				// movw %%ax,offs(%%ebx)
 				G4M(OPERoverride,0x89,0x43,IG->p1,Cp);
 			}
 			else if (mode&MEMADR) {
-				// movw offs(%%ebx),%%ax
-				G4M(OPERoverride,0x8b,0x43,IG->p0,Cp);
-				// imul (%%edi),%%ax
-				G4M(OPERoverride,0x0f,0xaf,0x07,Cp);
+				// imul offs(%%ebx),%%ax
+				G4M(OPERoverride,0x0f,0xaf,0x43,Cp);
+				G1(IG->p0,Cp);
 				// movw %%ax,offs(%%ebx)
 				G4M(OPERoverride,0x89,0x43,IG->p0,Cp);
 			}
 			else {
-				// movw Ofs_AX(%%ebx),%%ax
-				G4M(OPERoverride,0x8b,0x43,Ofs_AX,Cp);
-				// imul (%%edi),%%ax
-				G3M(OPERoverride,0xf7,0x2f,Cp);
+				// imul Ofs_AX(%%ebx),%%ax
+				G4M(OPERoverride,0xf7,0x6b,Ofs_AX,Cp);
 				// movw %%ax,Ofs_AX(%%ebx)
 				G4M(OPERoverride,0x89,0x43,Ofs_AX,Cp);
 				// movw %%dx,Ofs_DX(%%ebx)
@@ -877,26 +860,20 @@ arith1:
 		}
 		else {
 			if (mode&IMMED) {
-              			// movl (%%edi),%%edx
-				G2M(0x8b,0x17,Cp);
-				// imul $immed,%%edx,%%eax
-				G2M(0x69,0xc2,Cp); G4(IG->p0,Cp);
+				// imul $immed,%%eax,%%eax
+				G2M(0x69,0xc0,Cp); G4(IG->p0,Cp);
 				// movl %%eax,offs(%%ebx)
 				G3M(0x89,0x43,IG->p1,Cp);
 			}
 			else if (mode&MEMADR) {
-				// movl offs(%%ebx),%%eax
-				G3M(0x8b,0x43,IG->p0,Cp);
-				// imul (%%edi),%%eax
-				G3M(0x0f,0xaf,0x07,Cp);
+				// imul offs(%%ebx),%%eax
+				G4M(0x0f,0xaf,0x43,IG->p0,Cp);
 				// movl %%eax,offs(%%ebx)
 				G3M(0x89,0x43,IG->p0,Cp);
 			}
 			else {
-				// movl Ofs_EAX(%%ebx),%%eax
-				G3M(0x8b,0x43,Ofs_EAX,Cp);
-				// imul (%%edi),%%eax
-				G2M(0xf7,0x2f,Cp);
+				// imul Ofs_EAX(%%ebx),%%eax
+				G3M(0xf7,0x6b,Ofs_EAX,Cp);
 				// movl %%eax,Ofs_EAX(%%ebx)
 				G3M(0x89,0x43,Ofs_EAX,Cp);
 				// movl %%edx,Ofs_EDX(%%ebx)
@@ -908,14 +885,15 @@ arith1:
 
 	case O_DIV: {
 		G1(POPF,Cp);	// get flags from stack
+		G2(0xc189,Cp);  // movl %%eax,%%ecx
 		if (mode & MBYTE) {
 			// movw Ofs_AX(%%ebx),%%ax
 			G4M(OPERoverride,0x8b,0x43,Ofs_AX,Cp);
 			/* exception trap: save current PC */
 			// movl $eip,Ofs_CR2(%%ebx)
 			G2M(0xc7,0x43,Cp); G1(Ofs_CR2,Cp); G4(IG->p0,Cp);
-			// div (%%edi),%%al
-			G2M(0xf6,0x37,Cp);
+			// div %%cl,%%al
+			G2M(0xf6,0xf1,Cp);
 			// movw %%ax,Ofs_AX(%%ebx)
 			G4M(OPERoverride,0x89,0x43,Ofs_AX,Cp);
 		}
@@ -927,8 +905,8 @@ arith1:
 			/* exception trap: save current PC */
 			// movl $eip,Ofs_CR2(%%ebx)
 			G2(0x43c7,Cp); G1(Ofs_CR2,Cp); G4(IG->p0,Cp);
-			// div (%%edi),%%ax
-			G3M(OPERoverride,0xf7,0x37,Cp);
+			// div %%cx,%%ax
+			G3M(OPERoverride,0xf7,0xf1,Cp);
 			// movw %%ax,Ofs_AX(%%ebx)
 			G4M(OPERoverride,0x89,0x43,Ofs_AX,Cp);
 			// movw %%dx,Ofs_DX(%%ebx)
@@ -942,8 +920,8 @@ arith1:
 			/* exception trap: save current PC */
 			// movl $eip,Ofs_CR2(%%ebx)
 			G2(0x43c7,Cp); G1(Ofs_CR2,Cp); G4(IG->p0,Cp);
-			// div (%%edi),%%eax
-			G2M(0xf7,0x37,Cp);
+			// div %%ecx,%%eax
+			G2M(0xf7,0xf1,Cp);
 			// movl %%eax,Ofs_EAX(%%ebx)
 			G3M(0x89,0x43,Ofs_EAX,Cp);
 			// movl %%edx,Ofs_EDX(%%ebx)
@@ -954,14 +932,15 @@ arith1:
 		break;
 	case O_IDIV: {
 		G1(POPF,Cp);	// get flags from stack
+		G2(0xc189,Cp);  // movw %%eax,%%ecx
 		if (mode & MBYTE) {
 			// movw Ofs_AX(%%ebx),%%ax
 			G4M(OPERoverride,0x8b,0x43,Ofs_AX,Cp);
 			/* exception trap: save current PC */
 			// movl $eip,Ofs_CR2(%%ebx)
 			G2(0x43c7,Cp); G1(Ofs_CR2,Cp); G4(IG->p0,Cp);
-			// idiv (%%edi),%%al
-			G2M(0xf6,0x3f,Cp);
+			// idiv %%cl,%%al
+			G2M(0xf6,0xf9,Cp);
 			// movw %%ax,Ofs_AX(%%ebx)
 			G4M(OPERoverride,0x89,0x43,Ofs_AX,Cp);
 		}
@@ -973,8 +952,8 @@ arith1:
 			/* exception trap: save current PC */
 			// movl $eip,Ofs_CR2(%%ebx)
 			G2(0x43c7,Cp); G1(Ofs_CR2,Cp); G4(IG->p0,Cp);
-			// idiv (%%edi),%%ax
-			G3M(OPERoverride,0xf7,0x3f,Cp);
+			// idiv %%cx,%%ax
+			G3M(OPERoverride,0xf7,0xf9,Cp);
 			// movw %%ax,Ofs_AX(%%ebx)
 			G4M(OPERoverride,0x89,0x43,Ofs_AX,Cp);
 			// movw %%dx,Ofs_DX(%%ebx)
@@ -988,8 +967,8 @@ arith1:
 			/* exception trap: save current PC */
 			// movl $eip,Ofs_CR2(%%ebx)
 			G2(0x43c7,Cp); G1(Ofs_CR2,Cp); G4(IG->p0,Cp);
-			// idiv (%%edi),%%eax
-			G2M(0xf7,0x3f,Cp);
+			// idiv %%ecx,%%eax
+			G2M(0xf7,0xf9,Cp);
 			// movl %%eax,Ofs_EAX(%%ebx)
 			G3M(0x89,0x43,Ofs_EAX,Cp);
 			// movl %%edx,Ofs_EDX(%%ebx)

commit 8ea75f0df57799e05a95b3772e8c72c87b5c4d5f

Fixed IMUL regressions from r1670.
git-svn-id: https://dosemu.svn.sourceforge.net/svnroot/dosemu/trunk@1673 7758b857-130e-0410-ab9c-d8735a528d18

src/emu-i386/simx86/codegen-sim.c
@@ -1239,7 +1239,7 @@ static void Gen_sim(int op, int mode, ...)
 		    else {
 		    	GTRACE0("O_IMUL");
 			DR1.ds =
-			    (int)CPUBYTE(Ofs_AL) * (int)*AR1.ps;
+			    (int)CPUBYTE(Ofs_AL) * (int)DR1.bs.bl;
 			CPUWORD(Ofs_AX) = DR1.w.l;
 			DR1.ds = (DR1.ds >> 7) & 3;	// bits 7,8(ext)
 			of = ((DR1.ds!=0) && (DR1.ds!=3));

src/emu-i386/simx86/interp.c
@@ -647,7 +647,7 @@ intop3b:		{ int op = ArOpsFR[D_MO(opc)];
 			break;
 /*6b*/	case IMULbrm:
 			PC += ModRM(opc, PC, mode);
-			Gen(L_DI_R1, mode|MBYTE);		// mov al,[edi]
+			Gen(L_DI_R1, mode);		// mov (e)ax,[edi]
 			Gen(O_IMUL,mode|MBYTE|IMMED,(signed char)Fetch(PC),REG1);
 			PC++;
 			break;