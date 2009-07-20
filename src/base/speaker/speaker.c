dosemu2/dosemu2

commit 06afb4d49a33c7adb0f2c41617ba229c8f2c88c5

Avoid turning the speaker off when it is already off. This avoids strange visual bells when compiz is active and keys are pressed, because the keyboard i/o ports overlap with the speaker i/o ports, and X gets a 0 length sound to turn off the speaker that still produces a visual effect.
git-svn-id: https://dosemu.svn.sourceforge.net/svnroot/dosemu/trunk@1899 7758b857-130e-0410-ab9c-d8735a528d18

src/base/speaker/speaker.c
@@ -88,6 +88,9 @@ struct speaker_info {
 	speaker_off_t off;
 };
 
+/* flag to avoid turning the speaker off if it is already off */
+static int speaker_is_on;
+
 /*
  * Generic speaker emulation
  * =============================================================================
@@ -135,6 +138,7 @@ void speaker_on(unsigned ms, unsigned short period)
 	if (config.speaker == SPKR_OFF)
 		return;
 	i_printf("SPEAKER: on, period=%d\n", period);
+	speaker_is_on = 1;
 	if (!speaker.on) {
 		speaker = dumb_speaker;
 	}
@@ -143,11 +147,14 @@ void speaker_on(unsigned ms, unsigned short period)
 
 void speaker_off(void)
 {
+	if (speaker_is_on)
+		return;
 	i_printf("SPEAKER: sound OFF!\n");
 	if (!speaker.off) {
 		speaker = dumb_speaker;
 	}
 	speaker.off(speaker.gp);
+	speaker_is_on = 0;
 }
 
 static int saved_port_val;

commit 4fa3937295140f20a93d842c0db8443cf03acbda

Fix speaker_is_on if statement regression in r1899.
git-svn-id: https://dosemu.svn.sourceforge.net/svnroot/dosemu/trunk@1900 7758b857-130e-0410-ab9c-d8735a528d18

src/base/speaker/speaker.c
@@ -147,7 +147,7 @@ void speaker_on(unsigned ms, unsigned short period)
 
 void speaker_off(void)
 {
-	if (speaker_is_on)
+	if (!speaker_is_on)
 		return;
 	i_printf("SPEAKER: sound OFF!\n");
 	if (!speaker.off) {