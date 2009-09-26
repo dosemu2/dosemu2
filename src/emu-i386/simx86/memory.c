bda259106b093b68f3e68e2a519ae449a45e53ad
Bart Oldeman <bartoldeman@users.sourceforge.net>
2009-09-26 20:56:17 +0000

    Make CPUEMU memory protection maps use integers which are the same for zero and non-zero mem_base.
    git-svn-id: https://dosemu.svn.sourceforge.net/svnroot/dosemu/trunk@1972 7758b857-130e-0410-ab9c-d8735a528d18

--- src/emu-i386/simx86/memory.c
+++ src/emu-i386/simx86/memory.c
@@ -51,19 +51,19 @@ typedef struct _mpmap {
 	struct _mpmap *next;
 	int mega;
 	unsigned char pagemap[32];	/* (32*8)=256 pages *4096 = 1M */
-	unsigned long subpage[0x100000>>(CGRAN+5)];	/* 16-byte granularity, 64k bits */
+	unsigned int subpage[0x100000>>(CGRAN+5)];	/* 16-byte granularity, 64k bits */
 } tMpMap;
 
-tMpMap *MpH = NULL;
+static tMpMap *MpH = NULL;
 long mMaxMem = 0;
 
 static tMpMap *LastMp = NULL;
 
 /////////////////////////////////////////////////////////////////////////////
 
-static inline tMpMap *FindM(caddr_t addr)
+static inline tMpMap *FindM(unsigned int addr)
 {
-	register long a2l = (long)addr >> (PAGE_SHIFT+8);
+	register int a2l = addr >> (PAGE_SHIFT+8);
 	register tMpMap *M = LastMp;
 
 	if (M && (M->mega==a2l)) return M;
@@ -78,14 +78,14 @@ static inline tMpMap *FindM(caddr_t addr)
 }
 
 
-static int AddMpMap(caddr_t addr, caddr_t aend, int onoff)
+static int AddMpMap(unsigned int addr, unsigned int aend, int onoff)
 {
 	int bs=0, bp=0;
-	register long page;
+	register int page;
 	tMpMap *M;
 
 	do {
-	    page = (long)addr >> PAGE_SHIFT;
+	    page = addr >> PAGE_SHIFT;
 	    M = MpH;
 	    while (M) {
 		if (M->mega==(page>>8)) break;
@@ -102,11 +102,11 @@ static int AddMpMap(caddr_t addr, caddr_t aend, int onoff)
 		bp++;
 	    }
 	    if (debug_level('e')) {
-		if ((long)addr > mMaxMem) mMaxMem = (long)addr;
+		if (addr > mMaxMem) mMaxMem = addr;
 		if (onoff)
-		  dbug_printf("MPMAP:   protect page=%08lx was %x\n",(long)addr,bs);
+		  dbug_printf("MPMAP:   protect page=%08x was %x\n",addr,bs);
 		else
-		  dbug_printf("MPMAP: unprotect page=%08lx was %x\n",(long)addr,bs);
+		  dbug_printf("MPMAP: unprotect page=%08x was %x\n",addr,bs);
 	    }
 	    addr += PAGE_SIZE;
 	} while (addr < aend);
@@ -114,19 +114,19 @@ static int AddMpMap(caddr_t addr, caddr_t aend, int onoff)
 }
 
 
-inline int e_querymprot(caddr_t addr)
+static inline int e_querymprot(unsigned int addr)
 {
-	register long a2 = (long)addr >> PAGE_SHIFT;
+	register int a2 = addr >> PAGE_SHIFT;
 	tMpMap *M = FindM(addr);
 
 	if (M==NULL) return 0;
 	return test_bit(a2&255, M->pagemap);
 }
 
-int e_querymprotrange(caddr_t al, caddr_t ah)
+int e_querymprotrange(unsigned int al, unsigned int ah)
 {
-	register long a2l = (long)al >> PAGE_SHIFT;
-	long a2h = (long)ah >> PAGE_SHIFT;
+	register int a2l = al >> PAGE_SHIFT;
+	int a2h = ah >> PAGE_SHIFT;
 	tMpMap *M = MpH;
 	int res = 0;
 
@@ -147,16 +147,17 @@ int e_querymprotrange(caddr_t al, caddr_t ah)
 /////////////////////////////////////////////////////////////////////////////
 
 
-int e_markpage(caddr_t addr, size_t len)
+int e_markpage(unsigned char *paddr, size_t len)
 {
 	unsigned int abeg, aend;
 	tMpMap *M;
+	unsigned int addr = paddr - mem_base;
 
-	abeg = ((long)addr >> CGRAN) & CGRMASK;
-	aend = ((((long)addr+len) >> CGRAN) + 1) & CGRMASK;
+	abeg = (addr >> CGRAN) & CGRMASK;
+	aend = (((addr+len) >> CGRAN) + 1) & CGRMASK;
 nextmega:
 	M = FindM(addr); if (M==NULL) return 0;
-	if (debug_level('e')>1) e_printf("MARK from %04x to %04x for %08lx\n",abeg,aend-1,(long)addr);
+	if (debug_level('e')>1) e_printf("MARK from %04x to %04x for %08x\n",abeg,aend-1,addr);
 	while (abeg != aend) {
 	    set_bit(abeg, M->subpage);
 	    abeg = (abeg+1) & CGRMASK;
@@ -165,34 +166,35 @@ nextmega:
 	return 1;
 }
 
-int e_querymark(caddr_t addr)
+int e_querymark(unsigned char *paddr)
 {
 	int idx;
 	tMpMap *M;
+	unsigned int addr = paddr - mem_base;
 
 	M = FindM(addr); if (M==NULL) return 0;
-	idx = ((long)addr >> CGRAN) & CGRMASK;
+	idx = (addr >> CGRAN) & CGRMASK;
 	return (test_bit(idx, M->subpage) & 1);
 }
 
-static void e_resetonepagemarks(caddr_t addr)
+static void e_resetonepagemarks(unsigned int addr)
 {
 	int i, idx;
 	tMpMap *M;
 
 	M = FindM(addr); if (M==NULL) return;
 	/* reset all 256 bits=8 longs for the page */
-	idx = (((long)addr >> PAGE_SHIFT) & 255) << 3;
-	if (debug_level('e')>1) e_printf("UNMARK 256 bits at %08lx (long=%x)\n",(long)addr,idx);
+	idx = ((addr >> PAGE_SHIFT) & 255) << 3;
+	if (debug_level('e')>1) e_printf("UNMARK 256 bits at %08x (long=%x)\n",addr,idx);
 	for (i=0; i<8; i++) M->subpage[idx++] = 0;
 }
 
-void e_resetpagemarks(caddr_t addr, size_t len)
+void e_resetpagemarks(unsigned char *paddr, size_t len)
 {
 	int i, pages;
-
-	pages = (((size_t)addr + len - 1) >> PAGE_SHIFT) -
-	  ((size_t)addr >> PAGE_SHIFT) + 1;
+	unsigned int addr = paddr - mem_base;
+	
+	pages = ((addr + len - 1) >> PAGE_SHIFT) - (addr >> PAGE_SHIFT) + 1;
 	for (i = 0; i < pages; i++)
 		e_resetonepagemarks(addr + i * PAGE_SIZE);
 }
@@ -200,39 +202,41 @@ void e_resetpagemarks(caddr_t addr, size_t len)
 /////////////////////////////////////////////////////////////////////////////
 
 
-int e_mprotect(caddr_t addr, size_t len)
+int e_mprotect(unsigned char *paddr, size_t len)
 {
 	int e;
-	caddr_t abeg, aend;
-	abeg = (caddr_t)((long)addr & PAGE_MASK);
+	unsigned int abeg, aend;
+	unsigned int addr = paddr - mem_base;
+	abeg = addr & PAGE_MASK;
 	if (len==0) {
 	    if (e_querymprot(abeg)) return 1;
 	    aend = abeg + PAGE_SIZE;
 	}
 	else {
-	    aend = (caddr_t)((long)(addr+len-1) & PAGE_MASK) + PAGE_SIZE;
+	    aend = ((addr+len-1) & PAGE_MASK) + PAGE_SIZE;
 	    if (((aend-abeg)<=PAGE_SIZE) && e_querymprot(abeg)) return 1;
 	}
-	e = mprotect(abeg, aend-abeg, PROT_READ|PROT_EXEC);
+	e = mprotect(&mem_base[abeg], aend-abeg, PROT_READ|PROT_EXEC);
 	if (e>=0) return AddMpMap(abeg, aend, 1);
 	e_printf("MPMAP: %s\n",strerror(errno));
 	return -1;
 }
 
-int e_munprotect(caddr_t addr, size_t len)
+int e_munprotect(unsigned char *paddr, size_t len)
 {
 	int e;
-	caddr_t abeg, aend;
-	abeg = (caddr_t)((long)addr & PAGE_MASK);
+	unsigned int abeg, aend;
+	unsigned int addr = paddr - mem_base;
+	abeg = addr & PAGE_MASK;
 	if (len==0) {
 	    if (!e_querymprot(abeg)) return 0;
 	    aend = abeg + PAGE_SIZE;
 	}
 	else {
-	    aend = (caddr_t)((long)(addr+len-1) & PAGE_MASK) + PAGE_SIZE;
+	    aend = ((addr+len-1) & PAGE_MASK) + PAGE_SIZE;
 	    if (((aend-abeg)<=PAGE_SIZE) && !e_querymprot(abeg)) return 0;
 	}
-	e = mprotect(abeg, aend-abeg, PROT_READ|PROT_WRITE|PROT_EXEC);
+	e = mprotect(&mem_base[abeg], aend-abeg, PROT_READ|PROT_WRITE|PROT_EXEC);
 	if (e>=0) return AddMpMap(abeg, aend, 0);
 	e_printf("MPUNMAP: %s\n",strerror(errno));
 	return -1;
@@ -240,7 +244,7 @@ int e_munprotect(caddr_t addr, size_t len)
 
 /* check if the address is aliased to a non protected page, and if it is,
    do not try to unprotect it */
-int e_check_munprotect(caddr_t addr)
+int e_check_munprotect(unsigned char *addr)
 {
 	if (lowmemp(addr) != addr)
 		return 0;
@@ -264,11 +268,11 @@ void mprot_end(void)
 	while (M) {
 	    tMpMap *M2 = M;
 	    for (i=0; i<32; i++) if ((b=M->pagemap[i])) {
-		caddr_t addr = (caddr_t)(uintptr_t)((M->mega<<20) | (i<<15));
+		unsigned int addr = (M->mega<<20) | (i<<15);
 		while (b) {
 		    if (b & 1) {
-			e_printf("MP_END %08lx = RWX\n",(long)addr);
-			(void)mprotect(addr, PAGE_SIZE, PROT_READ|PROT_WRITE|PROT_EXEC);
+			e_printf("MP_END %08x = RWX\n",addr);
+			(void)mprotect(&mem_base[addr], PAGE_SIZE, PROT_READ|PROT_WRITE|PROT_EXEC);
 		    }
 		    addr += PAGE_SIZE;
 	 	    b >>= 1;   	
