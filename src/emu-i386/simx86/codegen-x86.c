dosemu2/dosemu2

commit 9b5cb8da7bb89b231593917d9e552b33fcb92f8e

Fix pointer targets differ in signedness warnings by changing signed to unsigned pointers where applicable.
git-svn-id: https://dosemu.svn.sourceforge.net/svnroot/dosemu/trunk@2030 7758b857-130e-0410-ab9c-d8735a528d18

src/emu-i386/simx86/codegen-x86.c
@@ -195,7 +195,7 @@ static void CodeGen(IMeta *I, int j)
 {
 	/* evil hack, keeping state from MOVS_SavA to MOVS_SetA in
 	   a static variable */
-	static unsigned char * rep_retry_ptr = (char*)0xdeadbeef;
+	static unsigned char * rep_retry_ptr = (unsigned char*)0xdeadbeef;
 	IGen *IG = &(I->gen[j]);
 	register unsigned char *Cp = CodePtr;
 	unsigned char * CpTemp;
@@ -477,9 +477,11 @@ static void CodeGen(IMeta *I, int j)
 		    // movl %%edi,%%eax
 		    G2M(0x89,0xf8,Cp);
 		    // subl vga.bank_base(%%ebx),%%eax
-		    G2(0x832b,Cp); G4((char *)&vga.mem.bank_base-CPUOFFS(0),Cp);
+		    G2(0x832b,Cp);
+		    G4((unsigned char *)&vga.mem.bank_base-CPUOFFS(0),Cp);
 		    // cmpl vga.bank_len(%%ebx),%%eax
-		    G2(0x833b,Cp); G4((char *)&vga.mem.bank_len-CPUOFFS(0),Cp);
+		    G2(0x833b,Cp);
+		    G4((unsigned char *)&vga.mem.bank_len-CPUOFFS(0),Cp);
 		    // jnb normal_read
 		    // pushl mode
 	       	    G3(0x6a1073,Cp); G1(mode,Cp);
@@ -515,9 +517,11 @@ static void CodeGen(IMeta *I, int j)
 		    // movl %%edi,%%ecx
 		    G2M(0x89,0xf9,Cp);
 		    // subl vga.mem.bank_base(%%ebx),%%ecx
-		    G2(0x8b2b,Cp); G4((char *)&vga.mem.bank_base-CPUOFFS(0),Cp);
+		    G2(0x8b2b,Cp);
+		    G4((unsigned char *)&vga.mem.bank_base-CPUOFFS(0),Cp);
 		    // cmpl vga.mem.bank_len(%%ebx),%%ecx
-		    G2(0x8b3b,Cp); G4((char *)&vga.mem.bank_len-CPUOFFS(0),Cp);
+		    G2(0x8b3b,Cp);
+		    G4((unsigned char *)&vga.mem.bank_len-CPUOFFS(0),Cp);
 		    // jnb normal_write
 		    // pushl mode
 	       	    G3(0x6a1273,Cp); G1(mode,Cp);
@@ -1231,7 +1235,7 @@ shrot0:
 		} break;
 
 	case O_PUSH2: {		/* register push only */
-		static char pseq16[] = {
+		static unsigned char pseq16[] = {
 			// movl offs(%%ebx),%%eax
 /*00*/			0x8b,0x43,0x00,
 			// leal -2(%%ecx),%%ecx
@@ -1241,7 +1245,7 @@ shrot0:
 			// movw %%ax,(%%esi,%%ecx,1)
 			0x66,0x89,0x04,0x0e,
 		};
-		static char pseq32[] = {
+		static unsigned char pseq32[] = {
 			// movl offs(%%ebx),%%eax
 /*00*/			0x8b,0x43,0x00,
 			// leal -4(%%ecx),%%ecx
@@ -1251,7 +1255,7 @@ shrot0:
 			// movl %%eax,(%%esi,%%ecx,1)
 			0x89,0x04,0x0e,
 		};
-		register char *p, *q; int sz;
+		register unsigned char *p, *q; int sz;
 		if (mode&DATA16) p=pseq16,sz=sizeof(pseq16);
 			else p=pseq32,sz=sizeof(pseq32);
 		q=Cp; GNX(Cp, p, sz);
@@ -1264,7 +1268,7 @@ shrot0:
 		break;
 
 	case O_PUSH2F: {
-		static char pseqpre[] = {
+		static unsigned char pseqpre[] = {
 			// movl Ofs_XSS(%%ebx),%%esi
 			0x8b,0x73,Ofs_XSS,
 			// movl Ofs_ESP(%%ebx),%%ecx
@@ -1286,7 +1290,7 @@ shrot0:
 			// movl %%ecx,Ofs_ESP(%%ebx)
 			0x89,0x4b,Ofs_ESP
 		};
-		char *q=Cp; GNX(Cp, pseqpre, sizeof(pseqpre));
+		unsigned char *q=Cp; GNX(Cp, pseqpre, sizeof(pseqpre));
 		if (mode&DATA16) q[8] = 0xfe; /* use -2 in lea ins */
 		if (in_dpmi) {
 		    /* This solves the DOSX 'System test 8' error.
@@ -1308,7 +1312,7 @@ shrot0:
 			// bt $19,(_EFLAGS-TheCPU)(%ebx) (test for VIF)
 			G3M(0x0f,0xba,0xa3,Cp);
 			/* relative ebx offset works on x86-64 too */
-			G4((char *)&_EFLAGS-CPUOFFS(0),Cp);
+			G4((unsigned char *)&_EFLAGS-CPUOFFS(0),Cp);
 			// (19 from bt); rcl $10,%%eax
 			G4M(0x13,0xc1,0xd0,0x0a,Cp);
 		}
@@ -1324,7 +1328,7 @@ shrot0:
 		} break;
 
 	case O_PUSHI: {
-		static char pseq16[] = {
+		static unsigned char pseq16[] = {
 			// movw $immed,%%ax
 /*00*/			0xb8,0,0,0,0,
 			// movl Ofs_XSS(%%ebx),%%esi
@@ -1340,7 +1344,7 @@ shrot0:
 			// movl %%ecx,Ofs_ESP(%%ebx)
 			0x89,0x4b,Ofs_ESP
 		};
-		static char pseq32[] = {
+		static unsigned char pseq32[] = {
 			// movl $immed,%%eax
 /*00*/			0xb8,0,0,0,0,
 			// movl Ofs_XSS(%%ebx),%%esi
@@ -1356,7 +1360,7 @@ shrot0:
 			// movl %%ecx,Ofs_ESP(%%ebx)
 			0x89,0x4b,Ofs_ESP
 		};
-		register char *p, *q; int sz;
+		register unsigned char *p, *q; int sz;
 		if (mode&DATA16) {
 			p = pseq16,sz=sizeof(pseq16);
 		}
@@ -1505,7 +1509,7 @@ shrot0:
 		} break;
 
 	case O_POP2: {
-		static char pseq16[] = {
+		static unsigned char pseq16[] = {
 			// andl StackMask(%%ebx),%%ecx
 			0x23,0x4b,Ofs_STACKM,
 			// movw (%%esi,%%ecx,1),%%ax
@@ -1515,7 +1519,7 @@ shrot0:
 			// leal 2(%%ecx),%%ecx
 			0x8d,0x49,0x02
 		};
-		static char pseq32[] = {
+		static unsigned char pseq32[] = {
 			// andl StackMask(%%ebx),%%ecx
 			0x23,0x4b,Ofs_STACKM,
 			// movl (%%esi,%%ecx,1),%%eax
@@ -1535,7 +1539,7 @@ shrot0:
 			0x09,0xd1,
 #endif
 		};
-		register char *p, *q; int sz;
+		register unsigned char *p, *q; int sz;
 		if (mode&DATA16) p=pseq16,sz=sizeof(pseq16);
 			else p=pseq32,sz=sizeof(pseq32);
 		// for popping into memory the sequence is:
@@ -1551,7 +1555,7 @@ shrot0:
 		break;
 
 	case O_POPA: {
-		static char pseq16[] = {	// wrong if SP wraps!
+		static unsigned char pseq16[] = {	// wrong if SP wraps!
 			// movl Ofs_XSS(%%ebx),%%esi
 			0x8b,0x73,Ofs_XSS,
 			// movl Ofs_ESP(%%ebx),%%ecx
@@ -1591,7 +1595,7 @@ shrot0:
 			// movl %%ecx,Ofs_ESP(%%ebx)
 			0x89,0x4b,Ofs_ESP
 		};
-		static char pseq32[] = {
+		static unsigned char pseq32[] = {
 			// movl Ofs_XSS(%%ebx),%%esi
 			0x8b,0x73,Ofs_XSS,
 			// movl Ofs_ESP(%%ebx),%%ecx
@@ -1627,7 +1631,7 @@ shrot0:
 			// movl %%ecx,Ofs_ESP(%%ebx)
 			0x89,0x4b,Ofs_ESP
 		};
-		register char *p; int sz;
+		register unsigned char *p; int sz;
 		if (mode&DATA16) {
 			p = pseq16,sz=sizeof(pseq16);
 		}
@@ -2103,16 +2107,20 @@ shrot0:
 		G4(0xd789c189,Cp);
 		// subl	TimeStartExec.t.tl(%%ebx),%%eax
 		// sbbl	TimeStartExec.t.th(%%ebx),%%edx
-		G2(0x832b,Cp); G4((char *)&TimeStartExec.t.tl-CPUOFFS(0),Cp);
-		G2(0x931b,Cp); G4((char *)&TimeStartExec.t.th-CPUOFFS(0),Cp);
+		G2(0x832b,Cp);
+		G4((unsigned char *)&TimeStartExec.t.tl-CPUOFFS(0),Cp);
+		G2(0x931b,Cp);
+		G4((unsigned char *)&TimeStartExec.t.th-CPUOFFS(0),Cp);
 		// addl	TheCPU.EMUtime(%%ebx),%%eax
 		// adcl	TheCPU.EMUtime+4(%%ebx),%%edx
 		G3M(0x03,0x43,Ofs_ETIME,Cp);
 		G3M(0x13,0x53,Ofs_ETIME+4,Cp);
 		// movl	%%ecx,TimeStartExec.t.tl(%%ebx)
 		// movl	%%edi,TimeStartExec.t.th(%%ebx)
-		G2(0x8b89,Cp); G4((char *)&TimeStartExec.t.tl-CPUOFFS(0),Cp);
-		G2(0xbb89,Cp); G4((char *)&TimeStartExec.t.th-CPUOFFS(0),Cp);
+		G2(0x8b89,Cp);
+		G4((unsigned char *)&TimeStartExec.t.tl-CPUOFFS(0),Cp);
+		G2(0xbb89,Cp);
+		G4((unsigned char *)&TimeStartExec.t.th-CPUOFFS(0),Cp);
 		// movl	%%eax,TheCPU.EMUtime(%%ebx)
 		// movl	%%edx,TheCPU.EMUtime+4(%%ebx)
 		G3M(0x89,0x43,Ofs_ETIME,Cp);
@@ -2154,7 +2162,7 @@ shrot0:
 		break;
 
 	case JMP_LINK: {	// cond, dspt, retaddr, link
-		static char pseq16[] = {
+		static unsigned char pseq16[] = {
 			// movw $RA,%%ax
 /*00*/			0xb8,0,0,0,0,
 			// movl Ofs_XSS(%%ebx),%%esi
@@ -2170,7 +2178,7 @@ shrot0:
 			// movl %%ecx,Ofs_ESP(%%ebx)
 			0x89,0x4b,Ofs_ESP
 		};
-		static char pseq32[] = {
+		static unsigned char pseq32[] = {
 			// movl $RA,%%eax
 /*00*/			0xb8,0,0,0,0,
 			// movl Ofs_XSS(%%ebx),%%esi
@@ -2191,7 +2199,7 @@ shrot0:
 		int dspnt = IG->p2;
 		linkdesc *lt = IG->lt;
 		if (cond == 0x11) {	// call
-			register char *p, *q; int sz;
+			register unsigned char *p, *q; int sz;
 			if (mode&DATA16) {
 				p=pseq16,sz=sizeof(pseq16);
 			}
@@ -2797,7 +2805,7 @@ static void ProduceCode(unsigned int PC)
  */
 static void _nodelinker2(TNode *LG, TNode *G)
 {
-	int *lp;
+	unsigned int *lp;
 	linkdesc *T = &G->clink;
 	backref *B;
 
@@ -2938,7 +2946,7 @@ static void NodeLinker(TNode *G)
 
 void NodeUnlinker(TNode *G)
 {
-	int *lp;
+	unsigned int *lp;
 	linkdesc *T = &G->clink;
 	backref *B = T->bkr.next;
 #ifdef PROFILE
@@ -3118,8 +3126,8 @@ static unsigned int CloseAndExec_x86(unsigned int PC, TNode *G, int mode, int ln
 		    /* copy tail instructions to the end of the code block */
 		    memcpy(p, TailCode, TAILSIZE);
 		    p += TAILFIX;
-		    I0->clink.t_link.abs = (int *)p;
-		    *((int *)p) = PC;
+		    I0->clink.t_link.abs = (unsigned int *)p;
+		    *((unsigned int *)p) = PC;
 		    CodePtr += TAILSIZE;
 		}
 

src/arch/linux/debugger/mhpdbgc.c
@@ -1465,7 +1465,7 @@ int mhp_bpchk(unsigned int a1)
 
 int mhp_getcsip_value()
 {
-  int  seg, off, limit;
+  unsigned int  seg, off, limit;
   if (IN_DPMI) return mhp_getadr("cs:eip", &seg, &off, &limit);
   else return (LWORD(cs) << 4) + LWORD(eip);
 }

src/base/async/int.c
@@ -627,7 +627,7 @@ static int int15(void)
     unsigned src_addr, dst_addr;
     unsigned src_limit, dst_limit;
     unsigned int length;
-    lp = SEG_ADR((int*), es, si);
+    lp = SEG_ADR((unsigned int*), es, si);
     lp += 4;
     src_addr = (*lp >> 16) & 0x0000FFFF;
     src_limit = *lp & 0x0000FFFF;

src/base/bios/int10.c
@@ -708,7 +708,7 @@ static void return_state(unsigned int statebuf) {
 static void vga_RAM_to_RAM(unsigned height, unsigned char chr, unsigned count,
                            unsigned seg, unsigned ofs, int bank)
 {
-  char *dst;
+  unsigned char *dst;
   unsigned i;
   unsigned long src;
   unsigned bankofs;

src/base/init/init.c
@@ -180,7 +180,7 @@ void map_video_bios(void)
     memcheck_addtype('V', "Video BIOS");
     memcheck_reserve('V', VBIOS_START, VBIOS_SIZE);
     if (!config.vbios_post || config.chipset == VESA)
-      load_file("/dev/mem", 0, (char *)int_bios_area, sizeof(int_bios_area));
+      load_file("/dev/mem", 0, (unsigned char *)int_bios_area, sizeof(int_bios_area));
   }
 }
 
@@ -251,7 +251,7 @@ void device_init(void)
  */
 void low_mem_init(void)
 {
-  char *result;
+  unsigned char *result;
 
   open_mapping(MAPPING_INIT_LOWRAM);
   g_printf ("DOS+HMA memory area being mapped in\n");

src/base/misc/disks.c
@@ -52,7 +52,7 @@ static int disks_initiated = 0;
 #define FDISKS config.fdisks
 #define HDISKS config.hdisks
 
-static void set_part_ent(struct disk *dp, char *tmp_mbr);
+static void set_part_ent(struct disk *dp, unsigned char *tmp_mbr);
 
 #define USE_FSYNC 1
 
@@ -136,7 +136,7 @@ static void dump_disk_blks(unsigned char *tb, int count, int ssiz)
  */
 
 int
-read_sectors(struct disk *dp, char *buffer, long head, long sector,
+read_sectors(struct disk *dp, unsigned char *buffer, long head, long sector,
 	     long track, long count)
 {
   off64_t  pos;
@@ -235,7 +235,7 @@ read_sectors(struct disk *dp, char *buffer, long head, long sector,
 }
 
 int
-write_sectors(struct disk *dp, char *buffer, long head, long sector,
+write_sectors(struct disk *dp, unsigned char *buffer, long head, long sector,
 	      long track, long count)
 {
   off64_t  pos;
@@ -677,11 +677,11 @@ partition_setup(struct disk *dp)
  *       put in the dp->part_info.number'th entry in the part table.
  */
 
-static void set_part_ent(struct disk *dp, char *tmp_mbr)
+static void set_part_ent(struct disk *dp, unsigned char *tmp_mbr)
 {
   long	length;		/* partition length in sectors		*/
   long	end;		/* last sector number offset		*/
-  char	*p;		/* ptr to part table entry to create	*/
+  unsigned char	*p;	/* ptr to part table entry to create	*/
 
 #ifdef __linux__
   if (ioctl(dp->fdesc, BLKGETSIZE, &length)) {
@@ -1171,7 +1171,7 @@ int int13(void)
   unsigned int disk, head, sect, track, number;
   int res;
   off64_t  pos;
-  char *buffer;
+  unsigned char *buffer;
   struct disk *dp;
   int checkdp_val;
 
@@ -1226,7 +1226,7 @@ int int13(void)
       ((REG(ecx) & 0xc0) << 2);
     if (!checkdp_val && dp->diskcyl4096 && dp->heads <= 64 && (HI(dx) & 0xc0))
       track |= (HI(dx) & 0xc0) << 4;
-    buffer = SEG_ADR((char *), es, bx);
+    buffer = SEG_ADR((unsigned char *), es, bx);
     number = LO(ax);
     d_printf("DISK %d read [h:%d,s:%d,t:%d](%d)->%04x:%04x\n",
 	     disk, head, sect, track, number, REG(es), LWORD(ebx));
@@ -1282,7 +1282,7 @@ int int13(void)
       ((REG(ecx) & 0xc0) << 2);
     if (!checkdp_val && dp->diskcyl4096 && dp->heads <= 64 && (HI(dx) & 0xc0))
       track |= (HI(dx) & 0xc0) << 4;
-    buffer = SEG_ADR((char *), es, bx);
+    buffer = SEG_ADR((unsigned char *), es, bx);
     number = LO(ax);
     W_printf("DISK write [h:%d,s:%d,t:%d](%d)->%p\n",
 	     head, sect, track, number, (void *) buffer);

src/base/misc/dos2linux.c
@@ -683,11 +683,11 @@ void memmove_dos2dos(void *dest, const void *src, size_t n)
     MEMMOVE_DOSP2DOSP(dest, src, n);
 }
 
-int dos_read(int fd, char *data, int cnt)
+int dos_read(int fd, void *data, int cnt)
 {
   int ret;
   /* GW also reads or writes directly from a file to protected video memory. */
-  unsigned char *d = (unsigned char *)data;
+  unsigned char *d = data;
   if (vga.inst_emu && d >= &mem_base[0xa0000] && d < &mem_base[0xc0000]) {
     char buf[cnt];
     ret = RPT_SYSCALL(read(fd, buf, cnt));
@@ -701,17 +701,17 @@ int dos_read(int fd, char *data, int cnt)
   return (ret);
 }
 
-int dos_write(int fd, char *data, int cnt)
+int dos_write(int fd, const void *data, int cnt)
 {
   int ret;
-  char buf[cnt];
-  unsigned char *d = (unsigned char *)data;
+  const unsigned char *d = data;
+  unsigned char buf[cnt];
   if (vga.inst_emu && d >= &mem_base[0xa0000] && d < &mem_base[0xc0000]) {
-    memcpy_from_vga(buf, data, cnt);
-    data = buf;
+    memcpy_from_vga(buf, d, cnt);
+    d = buf;
   }
-  ret = RPT_SYSCALL(write(fd, data, cnt));
-  g_printf("Wrote %10.10s\n", data);
+  ret = RPT_SYSCALL(write(fd, d, cnt));
+  g_printf("Wrote %10.10s\n", d);
   return (ret);
 }
 

src/base/misc/fatfs.c
@@ -1167,6 +1167,7 @@ void build_boot_blk(fatfs_t *f)
   char *msg, *msg1;
 
   int i;
+  size_t msgsize;
   unsigned r_o, d_o, t_o;
   unsigned char *b;
   unsigned char *d0, *d1;
@@ -1181,11 +1182,12 @@ void build_boot_blk(fatfs_t *f)
   b[0x01] = 0x3e;
   b[0x02] = 0x90;
   memcpy(b + 0x40, boot_prog, boot_prog_end - boot_prog);
-  strcpy(b + 0x40 + (boot_prog_end - boot_prog), msg);
+  msgsize = strlen(msg) + 1;
+  memcpy(b + 0x40 + (boot_prog_end - boot_prog), msg, msgsize);
   b[0x1fe] = 0x55;
   b[0x1ff] = 0xaa;
 
-  t_o = (0x40 + boot_prog_end - boot_prog + strlen(msg) + 1 + 3) & ~3;
+  t_o = (0x40 + boot_prog_end - boot_prog + msgsize + 3) & ~3;
   d0 = b + t_o;
   d1 = d0 + 0x14;
 
@@ -1305,8 +1307,9 @@ void build_boot_blk(fatfs_t *f)
       break;
 
     default:			/* no system */
-      strcpy(b + 0x40 + (boot_prog_end - boot_prog), msg1);
-      t_o = (0x40 + (boot_prog_end - boot_prog) + strlen(msg1) + 1 + 3) & ~3;
+      msgsize = strlen(msg1) + 1;
+      memcpy(b + 0x40 + (boot_prog_end - boot_prog), msg1, msgsize);
+      t_o = (0x40 + (boot_prog_end - boot_prog) + msgsize + 3) & ~3;
       d0 = b + t_o;
 
       t_o += 0x7c00;

commit 878c7420fa7e8d58b450e5477a3e46b13eab8a91

Fix x86-64 build after broken in r2030 (thanks Reinhard).
git-svn-id: https://dosemu.svn.sourceforge.net/svnroot/dosemu/trunk@2031 7758b857-130e-0410-ab9c-d8735a528d18

src/emu-i386/simx86/codegen-x86.c
@@ -489,7 +489,7 @@ static void CodeGen(IMeta *I, int j)
 		    // pop %%rsi; push %%rdi
 		    G5(0x8b8d48575e,Cp);
 	    	    // lea e_VgaRead(%%rbx),%%rcx; call %%rcx; pop %%rdi
-		    G4((char *)e_VgaRead-CPUOFFS(0),Cp); G3(0x5fd1ff,Cp);
+		    G4((unsigned char *)e_VgaRead-CPUOFFS(0),Cp); G3(0x5fd1ff,Cp);
 		    // must be the same amount of ins bytes as i386!!
 #else
 		    // pushl %%edi
@@ -530,7 +530,7 @@ static void CodeGen(IMeta *I, int j)
 		    G4(0x57c6895a,Cp);
 	    	    // lea e_VgaWrite(%%ebx),%%ecx; call %%ecx; pop %%rdi
 		    G3(0x8b8d48,Cp);
-		    G4((char *)e_VgaWrite-CPUOFFS(0),Cp); G3(0x5fd1ff,Cp);
+		    G4((unsigned char *)e_VgaWrite-CPUOFFS(0),Cp); G3(0x5fd1ff,Cp);
 		    // must be the same amount of ins bytes as i386!!
 #else
 		    // pushl %%eax