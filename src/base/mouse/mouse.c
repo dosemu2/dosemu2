dosemu2/dosemu2

commit 699e95646e31ff44da134f95748a363cf2e03d74

Change vga memory functions to use integers instead of pointers for DOS memory.

src/base/mouse/mouse.c
@@ -1771,12 +1771,12 @@ mouse_update_cursor(int clipped)
 void
 text_cursor(void)
 {
-  unsigned short *p;
+  unsigned int p;
   int offset;
   int cx, cy;
   int co = READ_WORD(BIOS_SCREEN_COLUMNS);
   p = screen_adr(mouse.display_page);
-  offset = mouse_erase.x;
+  offset = mouse_erase.x*2;
   cx = MOUSE_RX >> mouse.xshift;
   cy = MOUSE_RY >> mouse.yshift;
 
@@ -1794,7 +1794,7 @@ text_cursor(void)
   	return;
 
   /* remember where we drew this. */
-  mouse_erase.x = offset = cx + cy * co;
+  mouse_erase.x = offset = 2 * (cx + cy * co);
   mouse_erase.backingstore.text[0] = vga_read_word(p + offset);
 
   /* draw it. */

src/base/async/int.c
@@ -1511,12 +1511,12 @@ static void do_print_screen(void) {
 int x_pos, y_pos;
 int li = READ_BYTE(BIOS_ROWS_ON_SCREEN_MINUS_1) + 1;
 int co = READ_WORD(BIOS_SCREEN_COLUMNS);
-ushort *base=screen_adr(READ_BYTE(BIOS_CURRENT_SCREEN_PAGE));
-    g_printf("PrintScreen: base=%p, lines=%i columns=%i\n", base, li, co);
+unsigned base=screen_adr(READ_BYTE(BIOS_CURRENT_SCREEN_PAGE));
+    g_printf("PrintScreen: base=%x, lines=%i columns=%i\n", base, li, co);
     if (printer_open(0) == -1) return;
     for (y_pos=0; y_pos < li; y_pos++) {
 	for (x_pos=0; x_pos < co; x_pos++) 
-	    printer_write(0, vga_read((unsigned char *)(base + y_pos*co + x_pos)));
+	    printer_write(0, vga_read(base + 2*(y_pos*co + x_pos)));
 	printer_write(0, 0x0d);
 	printer_write(0, 0x0a);
     }

src/base/bios/int10.c
@@ -82,7 +82,7 @@
 #define BIOS_CONFIG_SCREEN_MODE (READ_WORD(BIOS_CONFIGURATION) & 0x30)
 #define IS_SCREENMODE_MDA (BIOS_CONFIG_SCREEN_MODE == 0x30)
 
-unsigned short *screen_adr(int page)
+unsigned screen_adr(int page)
 {
   /* This is the text screen base, the DOS program actually has to use.
    * Programs that support simultaneous dual monitor support rely on
@@ -91,9 +91,8 @@ unsigned short *screen_adr(int page)
    * This is ugly, but there is no screen buffer address in the BIOS-DATA
    * at 0x400. (Hans)
    */
-  unsigned short *base = LINEAR2UNIX(IS_SCREENMODE_MDA ? MDA_VIRT_TEXT_BASE :
-				     VGA_VIRT_TEXT_BASE);
-  return base + page * READ_WORD(BIOS_VIDEO_MEMORY_USED) / 2;
+  unsigned base = IS_SCREENMODE_MDA ? MDA_VIRT_TEXT_BASE : VGA_VIRT_TEXT_BASE;
+  return base + page * READ_WORD(BIOS_VIDEO_MEMORY_USED);
 }
 
 /* this maps the cursor shape given by int10, fn1 to the actually
@@ -178,16 +177,16 @@ bios_scroll(int x0, int y0, int x1, int y1, int l, int att)
   int x, y, co, li;
   us blank = ' ' | (att << 8);
   us tbuf[MAX_COLUMNS];
-  us *sadr;
+  unsigned sadr;
 
   if (config.cardtype == CARD_NONE)
      return;
 
   li= READ_BYTE(BIOS_ROWS_ON_SCREEN_MINUS_1) + 1;
   co= READ_WORD(BIOS_SCREEN_COLUMNS);
-  sadr = screen_adr(0) + READ_WORD(BIOS_VIDEO_MEMORY_ADDRESS)/2;
+  sadr = screen_adr(0) + READ_WORD(BIOS_VIDEO_MEMORY_ADDRESS);
 
-  if ((sadr < (us*)VGA_PHYS_TEXT_BASE) && ((att & 7) != 0) && ((att & 7) != 7))
+  if (sadr < VGA_PHYS_TEXT_BASE && ((att & 7) != 0) && ((att & 7) != 7))
     {
       blank = ' ' | ((att | 7) << 8);
     }
@@ -221,26 +220,26 @@ bios_scroll(int x0, int y0, int x1, int y1, int l, int att)
   
   if (l == 0) {			/* Wipe mode */
     for (y = y0; y <= y1; y++)
-      memcpy_to_vga(&sadr[y * co + x0], tbuf, dx * sizeof(us));
+      memcpy_to_vga(sadr + 2 * (y * co + x0), tbuf, dx * 2);
     return;
   }
 
   if (l > 0) {
     if (dx == co)
-      vga_memcpy(&sadr[y0 * co], &sadr[(y0 + l) * co], (dy - l) * dx * sizeof(us));
+      vga_memcpy(sadr + 2 * y0 * co, sadr + 2 * (y0 + l) * co, (dy - l) * dx * 2);
     else
       for (y = y0; y <= (y1 - l); y++)
-	vga_memcpy(&sadr[y * co + x0], &sadr[(y + l) * co + x0], dx * sizeof(us));
+	vga_memcpy(sadr + 2 * (y * co + x0), sadr + 2 * ((y + l) * co + x0), dx * 2);
 
     for (y = y1 - l + 1; y <= y1; y++)
-      memcpy_to_vga(&sadr[y * co + x0], tbuf, dx * sizeof(us));
+      memcpy_to_vga(sadr + 2 * (y * co + x0), tbuf, dx * 2);
   }
   else {
     for (y = y1; y >= (y0 - l); y--)
-      vga_memcpy(&sadr[y * co + x0], &sadr[(y + l) * co + x0], dx * sizeof(us));
+      vga_memcpy(sadr + 2 * (y * co + x0), sadr + 2 * ((y + l) * co + x0), dx * 2);
 
     for (y = y0 - l - 1; y >= y0; y--)
-      memcpy_to_vga(&sadr[y * co + x0], tbuf, dx * sizeof(us));
+      memcpy_to_vga(sadr + 2 * (y * co + x0), tbuf, dx * 2);
   }
 }
 
@@ -265,7 +264,7 @@ void tty_char_out(unsigned char ch, int s, int attr)
   int newline_att = 7;
   int xpos, ypos, co, li;
   int gfx_mode = 0;
-  unsigned char *dst;
+  unsigned dst;
 
 /* i10_deb("tty_char_out: char 0x%02x, page %d, attr 0x%02x\n", ch, s, attr); */
 
@@ -291,7 +290,7 @@ void tty_char_out(unsigned char ch, int s, int attr)
   case '\n':         /* Newline */
     /* Color newline */
     newline_att = gfx_mode ? 0 :
-      vga_read((unsigned char *)(screen_adr(s) + ypos*co + xpos) + 1);
+      vga_read(screen_adr(s) + 2*(ypos*co + xpos) + 1);
     ypos++;
     xpos = 0;                  /* EDLIN needs this behavior */
     break;
@@ -318,7 +317,7 @@ void tty_char_out(unsigned char ch, int s, int attr)
     }
     else
     {
-      dst = (unsigned char *) (screen_adr(s) + ypos*co + xpos);
+      dst = screen_adr(s) + 2 * (ypos*co + xpos);
       vga_write(dst, ch);
       if(attr != -1) vga_write(dst + 1, attr);
       set_dirty(s);
@@ -346,16 +345,17 @@ void tty_char_out(unsigned char ch, int s, int attr)
  */
 static void clear_screen(void)
 {
-  u_short *schar, blank = ' ' | (7 << 8);
+  unsigned schar;
+  u_short blank = ' ' | (7 << 8);
   int lx, s;
 
   if (config.cardtype == CARD_NONE)
      return;
 
-  v_printf("INT10: cleared screen: screen_adr %p\n", screen_adr(0));
+  v_printf("INT10: cleared screen: screen_adr %x\n", screen_adr(0));
 
   for (schar = screen_adr(0), lx = 0; lx < 16*1024;
-       vga_write_word(schar++, blank), lx++);
+       vga_write_word(schar+=2, blank), lx++);
 
   for (s = 0; s < 8; s++) {
     set_dirty(s);
@@ -763,7 +763,7 @@ int int10(void) /* with dualmon */
   int x, y, co, li;
   unsigned page, page_size, address;
   u_char c;
-  us *sm;
+  unsigned sm;
 
 #if USE_DUALMON
   static int last_equip=-1;
@@ -917,8 +917,8 @@ int int10(void) /* with dualmon */
       }
       if(using_text_mode()) {
         sm = screen_adr(page);
-        LWORD(eax) = vga_read_word(sm + co * get_bios_cursor_y_position(page)
-				   + get_bios_cursor_x_position(page));
+        LWORD(eax) = vga_read_word(sm + (co * get_bios_cursor_y_position(page)
+				   + get_bios_cursor_x_position(page)) * 2);
       }
       else {
         LWORD(eax) = 0;
@@ -950,14 +950,14 @@ int int10(void) /* with dualmon */
         );
       }
       if(using_text_mode()) {
-        u_short *sadr;
+        unsigned sadr;
         u_short c_attr;
         int n;
 
         page = HI(bx);
-        sadr = (u_short *) screen_adr(page) 
-  	     + get_bios_cursor_y_position(page) * co
-  	     + get_bios_cursor_x_position(page);
+        sadr = screen_adr(page)
+	     + (get_bios_cursor_y_position(page) * co
+	        + get_bios_cursor_x_position(page)) * 2;
         n = LWORD(ecx);
         c = LO(ax);
 
@@ -969,10 +969,10 @@ int int10(void) /* with dualmon */
          */
         if(HI(ax) == 9) {		/* use attribute from BL */
   	 c_attr = c | (LO(bx) << 8);
-	 while(n--) vga_write_word(sadr++, c_attr);
+	 while(n--) vga_write_word(sadr+=2, c_attr);
         }
         else {				/* leave attribute as it is */
-  	 while(n--) vga_write((unsigned char *)(sadr++), c);
+	 while(n--) vga_write(sadr+=2, c);
         }
         set_dirty(page);
         break;

src/base/misc/dos2linux.c
@@ -671,7 +671,7 @@ int change_config(unsigned item, void *buf, int grab_active, int kbd_grab_active
 void memcpy_2unix(void *dest, unsigned src, size_t n)
 {
   if (vga.inst_emu && src >= 0xa0000 && src < 0xc0000)
-    memcpy_from_vga(dest, &mem_base[src], n);
+    memcpy_from_vga(dest, src, n);
   else
     MEMCPY_2UNIX(dest, src, n);
 }
@@ -679,7 +679,7 @@ void memcpy_2unix(void *dest, unsigned src, size_t n)
 void memcpy_2dos(unsigned dest, const void *src, size_t n)
 {
   if (vga.inst_emu && dest >= 0xa0000 && dest < 0xc0000)
-    memcpy_to_vga(&mem_base[dest], src, n);
+    memcpy_to_vga(dest, src, n);
   else
     MEMCPY_2DOS(dest, src, n);
 }
@@ -690,9 +690,9 @@ void memmove_dos2dos(unsigned dest, unsigned src, size_t n)
      TODO: worry about overlaps; could be a little cleaner
      using the memcheck.c mechanism */
   if (vga.inst_emu && src >= 0xa0000 && src < 0xc0000)
-    memcpy_from_vga(&mem_base[dest], &mem_base[src], n);
+    memcpy_dos_from_vga(dest, src, n);
   else if (vga.inst_emu && dest >= 0xa0000 && dest < 0xc0000)
-    memcpy_to_vga(&mem_base[dest], &mem_base[src], n);
+    memcpy_dos_to_vga(dest, src, n);
   else
     MEMMOVE_DOS2DOS(dest, src, n);
 }
@@ -706,7 +706,7 @@ int dos_read(int fd, void *data, int cnt)
     char buf[cnt];
     ret = RPT_SYSCALL(read(fd, buf, cnt));
     if (ret >= 0)
-      memcpy_to_vga(data, buf, ret);
+      memcpy_to_vga(d - mem_base, buf, ret);
   }
   else
     ret = RPT_SYSCALL(read(fd, lowmemp(data), cnt));
@@ -721,7 +721,7 @@ int dos_write(int fd, const void *data, int cnt)
   const unsigned char *d = data;
   unsigned char buf[cnt];
   if (vga.inst_emu && d >= &mem_base[0xa0000] && d < &mem_base[0xc0000]) {
-    memcpy_from_vga(buf, d, cnt);
+    memcpy_from_vga(buf, d - mem_base, cnt);
     d = buf;
   }
   ret = RPT_SYSCALL(write(fd, d, cnt));

src/base/mouse/gcursor.c
@@ -105,7 +105,7 @@ realize_cursor(short *scrmask,short *curmask,int org)
 }
 
 
-#define GRBASE &mem_base[GRAPH_BASE + mouse_current_video.offset]
+#define GRBASE (GRAPH_BASE + mouse_current_video.offset)
 
 static inline 
 unsigned char read_ega_reg(int port,int index)
@@ -170,7 +170,7 @@ cga2_bitblt(int x,int y,int width,int height,int toscr,int bpl,
 	unsigned char *bs)
 {
 	int byteWidth = ((x + width - 1) >> 3) - (x >> 3) + 1;
-	unsigned char *scr = GRBASE + cga_scanline(y) + (x >> 3);
+	unsigned scr = GRBASE + cga_scanline(y) + (x >> 3);
 
 	if (toscr)
 		while (height--) {
@@ -192,7 +192,7 @@ cga4_bitblt(int x,int y,int width,int height,int toscr,int bpl,
 	unsigned char *bs)
 {
 	int byteWidth = ((x + width - 1) >> 2) - (x >> 2) + 1;
-	unsigned char *scr = GRBASE + cga_scanline(y) + (x >> 2);
+	unsigned scr = GRBASE + cga_scanline(y) + (x >> 2);
 
 	if (toscr)
 		while (height--) {
@@ -215,13 +215,13 @@ ega16_bitblt(int x,int y,int width,int height,int toscr,int bpl,
 {
 	int byteWidth = ((x + width - 1) >> 3) - (x >> 3) + 1;
 	int plane;
-	unsigned char *scr = GRBASE + (y * bpl) + (x >> 3);
+	unsigned scr = GRBASE + (y * bpl) + (x >> 3);
 
 	SAVE_EGA_STATE
 
 	for (plane=0; plane<4; plane++) {
 		int h = height;
-		unsigned char *s = scr;
+		unsigned s = scr;
 
 		if (toscr) {
 			/* only enable writes to a single plane */
@@ -253,7 +253,7 @@ static void
 vga_bitblt(int x,int y,int width,int height,int toscr,int bpl,
 	unsigned char *bs)
 {
-	unsigned char *scr = GRBASE + (y * bpl) + x;
+	unsigned scr = GRBASE + (y * bpl) + x;
 
 	if (toscr)
 		while (height--) {
@@ -286,10 +286,10 @@ cga2_cursor(int x,int y,int width,int height,int xofs,int yofs,int bpl)
 {
 	int index = (xofs >> 3) + (yofs * 3) + (x & 7) * 48;
 	int bwidth = ((x + width - 1) >> 3) - (x >> 3);
-	unsigned char *scr = GRBASE + cga_scanline(y) + (x >> 3);
+	unsigned scr = GRBASE + cga_scanline(y) + (x >> 3);
 
 	while (height--) {
-		unsigned char *s = scr;
+		unsigned s = scr;
 		int i = index;
 		switch (bwidth) {
 			case 2: do_pixel
@@ -307,11 +307,11 @@ cga4_cursor(int x,int y,int width,int height,int xofs,int yofs,int bpl)
 {
 	int index = (xofs >> 2) + (yofs * 5) + (x & 3) * 80;
 	int bwidth = ((x + width - 1) >> 2) - (x >> 2);
-	unsigned char *scr = GRBASE + cga_scanline(y) + (x >> 2);
+	unsigned scr = GRBASE + cga_scanline(y) + (x >> 2);
 
 
 	while (height--) {
-		unsigned char *s = scr;
+		unsigned s = scr;
 		int i = index;
 		switch (bwidth) {
 			case 4: do_pixel
@@ -331,7 +331,7 @@ ega16_cursor(int x,int y,int width,int height,int xofs,int yofs,int bpl)
 {
 	int index = (xofs >> 3) + (yofs * 3) + (x & 7) * 48;
 	int bwidth = ((x + width - 1) >> 3) - (x >> 3);
-	unsigned char *scr = GRBASE + (y * bpl) + (x >> 3);
+	unsigned scr = GRBASE + (y * bpl) + (x >> 3);
 	int plane;
 
 	SAVE_EGA_STATE
@@ -341,7 +341,7 @@ ega16_cursor(int x,int y,int width,int height,int xofs,int yofs,int bpl)
 	for (plane = 0; plane < 4; plane++) {
 		int h = height;
 		int ii = index;
-		unsigned char *ss = scr;
+		unsigned ss = scr;
 
 		/* enable reads from this plane */
 		write_ega_reg(GRA_I,0x4,plane);
@@ -350,7 +350,7 @@ ega16_cursor(int x,int y,int width,int height,int xofs,int yofs,int bpl)
 		write_ega_reg(SEQ_I,0x2,1<<plane);
 
 		while (h--) {
-			unsigned char *s = ss;
+			unsigned s = ss;
 			int i = ii;
 			switch (bwidth) {
 				case 2: do_pixel
@@ -370,10 +370,10 @@ static void
 vga_cursor(int x,int y,int width,int height,int xofs,int yofs,int bpl)
 {
 	int index = xofs + (yofs << 3);
-	unsigned char *scr = GRBASE + (y * bpl) + x;
+	unsigned scr = GRBASE + (y * bpl) + x;
 
 	while (height--) {
-		unsigned char *s = scr;
+		unsigned s = scr;
 		int i = index;
 		switch (width) {
 			case 15: do_pixel

src/emu-i386/simx86/sigsegv.c
@@ -55,13 +55,13 @@ int TryMemRef = 0;
 unsigned e_VgaRead(unsigned a, int mode)
 {
   unsigned u;
-  unsigned char *addr = (unsigned char *)(uintptr_t)a;
+  unsigned int addr = (unsigned char *)(uintptr_t)a - mem_base;
   if (mode&(MBYTE|MBYTX))
     u = vga_read(addr);
   else {
-    u = vga_read_word((unsigned short *)addr);
+    u = vga_read_word(addr);
     if (!(mode&DATA16))
-      u |= vga_read_word((unsigned short *)addr+1) << 16;
+      u |= vga_read_word(addr+2) << 16;
   }
 #ifdef DEBUG_VGA
   e_printf("eVGAEmuFault: VGA read at %08x = %08x mode %x\n",addr,u,mode);
@@ -71,7 +71,7 @@ unsigned e_VgaRead(unsigned a, int mode)
 
 void e_VgaWrite(unsigned a, unsigned u, int mode)
 {
-  unsigned char *addr = (unsigned char *)(uintptr_t)a;
+  unsigned addr = (unsigned char *)(uintptr_t)a - mem_base;
 #ifdef DEBUG_VGA
   e_printf("eVGAEmuFault: VGA write %08x at %08x mode %x\n",u,addr,mode);
 #endif
@@ -79,9 +79,9 @@ void e_VgaWrite(unsigned a, unsigned u, int mode)
     vga_write(addr, u);
     return;
   }
-  vga_write_word((unsigned short *)addr, u);
+  vga_write_word(addr, u);
   if (mode&DATA16) return;
-  vga_write_word((unsigned short *)addr+1, u>>16);
+  vga_write_word(addr+2, u>>16);
 }
 
 void e_VgaMovs(struct sigcontext_struct *scp, char op, int w16, int dp)

src/env/video/instremu.c
@@ -189,7 +189,7 @@ static unsigned char it[0x100] = {
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 static unsigned seg, osp, asp, lock, rep;
 static int count;
-static unsigned char *vga_base, *vga_end;
+static unsigned vga_base, vga_end;
 
 static unsigned arg_len(unsigned char *);
 
@@ -396,16 +396,17 @@ unsigned arg_len(unsigned char *p)
  *
  */
 
-unsigned char instr_read_byte(const unsigned char *addr)
+unsigned char instr_read_byte(const unsigned char *address)
 {
   unsigned char u;
+  unsigned addr = address - mem_base;
 
   if(addr >= vga_base && addr < vga_end) {
     count = COUNT;
     u = vga_read(addr);
   }
   else {
-    u = *addr;
+    u = *address;
   }
 #if DEBUG_INSTR >= 2  
   instr_deb2("Read byte 0x%x", u);
@@ -415,7 +416,7 @@ unsigned char instr_read_byte(const unsigned char *addr)
   return u;
 }
 
-unsigned instr_read_word(const unsigned char *addr)
+unsigned instr_read_word(const unsigned char *address)
 {
   unsigned u;
 
@@ -423,13 +424,14 @@ unsigned instr_read_word(const unsigned char *addr)
    * segment wrap-arounds within a data word are not allowed since
    * at least i286, so no problems here
    */
+  unsigned addr = address - mem_base;
   if(addr >= vga_base && addr < vga_end) {
     count = COUNT;
     u = 0;
     R_LO(u) = vga_read(addr);
     R_HI(u) = vga_read(addr+1);
   } else 
-    u = *(unsigned short *)addr;
+    u = *(unsigned short *)address;
 
 #if DEBUG_INSTR >= 2  
   instr_deb2("Read word 0x%x", u);
@@ -438,7 +440,7 @@ unsigned instr_read_word(const unsigned char *addr)
   return u;
 }
 
-unsigned instr_read_dword(const unsigned char *addr)
+unsigned instr_read_dword(const unsigned char *address)
 {
   unsigned u;
   
@@ -446,6 +448,7 @@ unsigned instr_read_dword(const unsigned char *addr)
    * segment wrap-arounds within a data word are not allowed since
    * at least i286, so no problems here
    */
+  unsigned addr = address - mem_base;
   if(addr >= vga_base && addr < vga_end) {
     count = COUNT;
     R_LO(u) = vga_read(addr);
@@ -462,18 +465,20 @@ unsigned instr_read_dword(const unsigned char *addr)
   return u;
 }
 
-void instr_write_byte(unsigned char *addr, unsigned char u)
+void instr_write_byte(unsigned char *address, unsigned char u)
 {
+  unsigned addr = address - mem_base;
+
   if(addr >= vga_base && addr < vga_end) {
     count = COUNT;
     vga_write(addr, u);
   }
-  else if (ldt_buffer && addr >= ldt_buffer &&
-      addr < ldt_buffer + LDT_ENTRIES*LDT_ENTRY_SIZE) {
-    direct_ldt_write(addr - ldt_buffer, 1, (char*)&u);
+  else if (ldt_buffer && address >= ldt_buffer &&
+      address < ldt_buffer + LDT_ENTRIES*LDT_ENTRY_SIZE) {
+    direct_ldt_write(address - ldt_buffer, 1, (char*)&u);
   }
   else {
-    *addr = u;
+    *address = u;
   }
 #if DEBUG_INSTR >= 2  
   instr_deb2("Write byte 0x%x", u);
@@ -481,8 +486,9 @@ void instr_write_byte(unsigned char *addr, unsigned char u)
 #endif  
 }
 
-void instr_write_word(unsigned char *dst, unsigned u)
+void instr_write_word(unsigned char *address, unsigned u)
 {
+  unsigned dst = address - mem_base;
   /*
    * segment wrap-arounds within a data word are not allowed since
    * at least i286, so no problems here.
@@ -494,12 +500,12 @@ void instr_write_word(unsigned char *dst, unsigned u)
     vga_write(dst, R_LO(u));
     vga_write(dst+1, R_HI(u));
   }
-  else if (ldt_buffer && dst >= ldt_buffer &&
-      dst < ldt_buffer + LDT_ENTRIES*LDT_ENTRY_SIZE) {
-    direct_ldt_write(dst - ldt_buffer, 2, (char*)&u);
+  else if (ldt_buffer && address >= ldt_buffer &&
+      address < ldt_buffer + LDT_ENTRIES*LDT_ENTRY_SIZE) {
+    direct_ldt_write(address - ldt_buffer, 2, (char*)&u);
   }
   else
-    *(unsigned short *)dst = u;
+    *(unsigned short *)address = u;
 
 #if DEBUG_INSTR >= 2  
   instr_deb2("Write word 0x%x", u);
@@ -507,8 +513,10 @@ void instr_write_word(unsigned char *dst, unsigned u)
 #endif  
 }
 
-void instr_write_dword(unsigned char *dst, unsigned u)
+void instr_write_dword(unsigned char *address, unsigned u)
 {
+  unsigned dst = address - mem_base;
+
   /*
    * segment wrap-arounds within a data word are not allowed since
    * at least i286, so no problems here.
@@ -522,9 +530,9 @@ void instr_write_dword(unsigned char *dst, unsigned u)
     vga_write(dst+2, ((unsigned char *) &u)[2]);
     vga_write(dst+3, ((unsigned char *) &u)[3]);
   }
-  else if (ldt_buffer && dst >= ldt_buffer &&
-      dst < ldt_buffer + LDT_ENTRIES*LDT_ENTRY_SIZE) {
-    direct_ldt_write(dst - ldt_buffer, 4, (char*)&u);
+  else if (ldt_buffer && address >= ldt_buffer &&
+      address < ldt_buffer + LDT_ENTRIES*LDT_ENTRY_SIZE) {
+    direct_ldt_write(address - ldt_buffer, 4, (char*)&u);
   }
   else 
     *(unsigned *)dst = u;
@@ -2440,7 +2448,7 @@ void instr_emu(struct sigcontext_struct *scp, int pmode, int cnt)
 #endif
 
   count = cnt ? : COUNT + 1;
-  vga_base = &mem_base[vga.mem.bank_base];
+  vga_base = vga.mem.bank_base;
   vga_end =  vga_base + vga.mem.bank_len;
 
   x86.prefixes = 1;

src/env/video/vgaemu.c
@@ -764,72 +764,94 @@ static void Logical_VGA_write(unsigned offset, unsigned char value)
   
 }
 
-unsigned char vga_read(const unsigned char *addr)
+unsigned char vga_read(unsigned addr)
 {
   if (!vga.inst_emu)
-    return READ_BYTEP(addr);
-  return Logical_VGA_read(addr - mem_base - vga.mem.bank_base);
+    return READ_BYTE(addr);
+  return Logical_VGA_read(addr - vga.mem.bank_base);
 }
 
-unsigned short vga_read_word(const unsigned short *addr)
+unsigned short vga_read_word(unsigned addr)
 {
   if (!vga.inst_emu)
-    return READ_WORDP(addr);
-  return vga_read((const unsigned char *)addr) |
-	 vga_read((const unsigned char *)addr + 1) << 8;
+    return READ_WORD(addr);
+  return vga_read(addr) | vga_read(addr + 1) << 8;
 }
 
-void vga_write(unsigned char *addr, unsigned char val)
+void vga_write(unsigned addr, unsigned char val)
 {
   if (!vga.inst_emu) {
-    WRITE_BYTEP(addr, val);
+    WRITE_BYTE(addr, val);
     return;
   }
-  Logical_VGA_write(addr - mem_base - vga.mem.bank_base, val);
+  Logical_VGA_write(addr - vga.mem.bank_base, val);
 }
 
-void vga_write_word(unsigned short *addr, unsigned short val)
+void vga_write_word(unsigned addr, unsigned short val)
 {
   if (!vga.inst_emu) {
-    WRITE_WORDP(addr, val);
+    WRITE_WORD(addr, val);
     return;
   }
-  vga_write((unsigned char *)addr, val & 0xff);
-  vga_write((unsigned char *)addr + 1, val >> 8);
+  vga_write(addr, val & 0xff);
+  vga_write(addr + 1, val >> 8);
 }
 
-void memcpy_to_vga(void *dst, const void *src, size_t len)
+void memcpy_to_vga(unsigned dst, const void *src, size_t len)
 {
   int i;
   if (!vga.inst_emu) {
-    MEMCPY_2DOSP(dst, src, len);
+    MEMCPY_2DOS(dst, src, len);
     return;
   }
   for (i = 0; i < len; i++)
-    vga_write((unsigned char *)dst + i, READ_BYTEP((unsigned char *)src + i));
+    vga_write(dst + i, ((unsigned char *)src)[i]);
 }
 
-void memcpy_from_vga(void *dst, const void *src, size_t len)
+void memcpy_dos_to_vga(unsigned dst, unsigned src, size_t len)
 {
   int i;
   if (!vga.inst_emu) {
-    MEMCPY_P2UNIX(dst, src, len);
+    MEMCPY_DOS2DOS(dst, src, len);
+    return;
+  }
+  for (i = 0; i < len; i++)
+    vga_write(dst + i, READ_BYTE(src + i));
+}
+
+void memcpy_from_vga(void *dst, unsigned src, size_t len)
+{
+  int i;
+  if (!vga.inst_emu) {
+    MEMCPY_2UNIX(dst, src, len);
     return;
   }
   for (i = 0; i < len; i++) {
-    WRITE_BYTEP((unsigned char *)dst + i, vga_read((unsigned char *)src + i));
+    ((unsigned char *)dst)[i] = vga_read(src + i);
   }
 }
 
-void vga_memcpy(void *dst, const void *src, size_t len)
+void memcpy_dos_from_vga(unsigned dst, unsigned src, size_t len)
 {
   int i;
   if (!vga.inst_emu) {
-    MEMMOVE_DOSP2DOSP(dst, src, len);
+    MEMCPY_DOS2DOS(dst, src, len);
+    return;
+  }
+  for (i = 0; i < len; i++) {
+    WRITE_BYTE(dst + i, vga_read(src + i));
+  }
+}
+
+void vga_memcpy(unsigned dst, unsigned src, size_t len)
+{
+  int i;
+  if (!vga.inst_emu) {
+    MEMMOVE_DOS2DOS(dst, src, len);
     return;
   }
   for (i = 0; i < len; i++)
-    vga_write((unsigned char *)dst + i, vga_read((unsigned char *)src + i));
+    vga_write(dst + i, vga_read(src + i));
 }
 
 /*

commit aafcd151b9af18ffe49eb5e05ce388bf088be5c1

Mouse text cursor screen corruption fix.
This fixes a regression from git commit 699e9564:
When a mouse grab is in effect in a DOSEMU window under X,
and the native text mouse cursor is activated, the text screen
gets corrupted by moving the mouse. This fixes the screen corruption.
Patch modified from Eric Larouche: SF patch 3539932.

src/base/mouse/mouse.c
@@ -1794,7 +1794,8 @@ text_cursor(void)
   	return;
 
   /* remember where we drew this. */
-  mouse_erase.x = offset = 2 * (cx + cy * co);
+  mouse_erase.x = cx + cy * co;
+  offset = mouse_erase.x*2;
   mouse_erase.backingstore.text[0] = vga_read_word(p + offset);
 
   /* draw it. */