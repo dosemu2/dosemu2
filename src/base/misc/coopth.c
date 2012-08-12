46a9a077266266b9d695bf6cc9af05da8a9cab39
Stas Sergeev <stsp@users.sourceforge.net>
2012-08-12 19:55:47 +0400

    coopth: allow thread recursion of limited depth. Mainly for the sake of implementation completeness.

--- src/base/misc/coopth.c
+++ src/base/misc/coopth.c
@@ -31,20 +31,26 @@
 
 enum CoopthRet { COOPTH_NONE, COOPTH_INPR, COOPTH_WAIT,
 	COOPTH_SLEEP, COOPTH_DONE, COOPTH_MAX };
-enum CoopthState { COOPTHS_NONE, COOPTHS_RUNNING, COOPTHS_SLEEPING,
-	COOPTHS_DELETE };
+enum CoopthState { COOPTHS_NONE, COOPTHS_RUNNING, COOPTHS_SLEEPING };
 
 struct coopth_thr_t {
     coopth_func_t func;
     void *arg;
 };
 
+struct coopth_per_thread_t {
+    coroutine_t thread;
+    enum CoopthState state;
+};
+
+#define MAX_COOP_RECUR_DEPTH 5
+
 struct coopth_t {
     struct coopth_thr_t thr;
     char *name;
-    Bit32u hlt_off;
-    coroutine_t thread;
-    enum CoopthState state;
+    Bit16u hlt_off;
+    int cur_thr;
+    struct coopth_per_thread_t pth[MAX_COOP_RECUR_DEPTH];
 };
 
 #define MAX_COOPTHREADS 1024
@@ -59,28 +65,35 @@ void coopth_init(void)
     co_thread_init();
 }
 
-static void do_run_thread(struct coopth_t *thr)
+static void do_run_thread(struct coopth_t *thr,
+	struct coopth_per_thread_t *pth)
 {
     enum CoopthRet ret;
     int r;
-    co_call(thr->thread);
-    r = (long)co_get_data(thr->thread);
+    fake_retf(0);
+    co_call(pth->thread);
+    r = (long)co_get_data(pth->thread);
     if (r >= COOPTH_MAX) {
 	error("Coopthreads error, exiting\n");
 	leavedos(2);
     }
     ret = r;
+    if (ret != COOPTH_DONE)
+	fake_call_to(BIOS_HLT_BLK_SEG, thr->hlt_off);
     switch (ret) {
     case COOPTH_WAIT:
 	idle(0, 5, 0, INT2F_IDLE_USECS, thr->name);
 	break;
     case COOPTH_SLEEP:
-	thr->state = COOPTHS_SLEEPING;
+	pth->state = COOPTHS_SLEEPING;
 	break;
     case COOPTH_INPR:
 	break;
     case COOPTH_DONE:
-	thr->state = COOPTHS_DELETE;
+	pth->state = COOPTHS_NONE;
+	co_delete(pth->thread);
+	thr->cur_thr--;
+	thread_running--;
 	break;
     default:
 	error("Coopthreads error, exiting\n");
@@ -91,22 +104,17 @@ static void do_run_thread(struct coopth_t *thr)
 static void coopth_hlt(Bit32u offs, void *arg)
 {
     struct coopth_t *thr = (struct coopth_t *)arg + offs;
-    switch (thr->state) {
+    struct coopth_per_thread_t *pth = &thr->pth[thr->cur_thr - 1];
+    switch (pth->state) {
     case COOPTHS_NONE:
 	error("Coopthreads error switch to inactive thread, exiting\n");
 	leavedos(2);
 	break;
     case COOPTHS_RUNNING:
-	do_run_thread(thr);
+	do_run_thread(thr, pth);
 	break;
     case COOPTHS_SLEEPING:
 	idle(0, 5, 0, INT2F_IDLE_USECS, thr->name);
-	break;
-    case COOPTHS_DELETE:
-	fake_retf(0);
-	thr->state = COOPTHS_NONE;
-	co_delete(thr->thread);
-	thread_running--;
 	break;
     }
 }
@@ -144,8 +152,7 @@ int coopth_create(char *name)
     thr = &coopthreads[num];
     thr->hlt_off = register_handler(nm, thr, 1);
     thr->name = nm;
-    thr->state = COOPTHS_NONE;
-    thr->thread = NULL;
+    thr->cur_thr = 0;
 
     return num;
 }
@@ -168,9 +175,8 @@ int coopth_create_multi(char *name, int len)
     for (i = 0; i < len; i++) {
 	thr = &coopthreads[num + i];
 	thr->name = nm;
-	thr->state = COOPTHS_NONE;
-	thr->thread = NULL;
 	thr->hlt_off = hlt_off + i;
+	thr->cur_thr = 0;
     }
 
     return num;
@@ -179,6 +185,8 @@ int coopth_create_multi(char *name, int len)
 int coopth_start(int tid, coopth_func_t func, void *arg)
 {
     struct coopth_t *thr;
+    struct coopth_per_thread_t *pth;
+    int tn;
     if (tid < 0 || tid >= coopth_num) {
 	dosemu_error("Wrong tid\n");
 	leavedos(2);
@@ -186,26 +194,18 @@ int coopth_start(int tid, coopth_func_t func, void *arg)
     thr = &coopthreads[tid];
     thr->thr.func = func;
     thr->thr.arg = arg;
-
-    switch (thr->state) {
-    case COOPTHS_NONE:
-	break;
-    case COOPTHS_RUNNING:
-    case COOPTHS_DELETE:
-	/* We do not handle the threaded recursion yet... */
-	g_printf("Coopthreads recursion %i\n", thread_running);
-	thr->thr.func(thr->thr.arg);
-	return 0;
-    default:
-	error("Coopthreads error, exiting, state=%i\n", thr->state);
+    if (thr->cur_thr >= MAX_COOP_RECUR_DEPTH) {
+	error("Coopthreads recursion depth exceeded\n");
 	leavedos(2);
     }
-    thr->thread = co_create(coopth_thread, &thr->thr, NULL, COOP_STK_SIZE);
-    if (!thr->thread) {
+    tn = thr->cur_thr++;
+    pth = &thr->pth[tn];
+    pth->thread = co_create(coopth_thread, &thr->thr, NULL, COOP_STK_SIZE);
+    if (!pth->thread) {
 	error("Thread create failure\n");
 	leavedos(2);
     }
-    thr->state = COOPTHS_RUNNING;
+    pth->state = COOPTHS_RUNNING;
     thread_running++;
     fake_call_to(BIOS_HLT_BLK_SEG, thr->hlt_off);
     return 0;
@@ -230,12 +230,14 @@ void coopth_sleep(void)
 void coopth_wake_up(int tid)
 {
     struct coopth_t *thr;
+    struct coopth_per_thread_t *pth;
     if (tid < 0 || tid >= coopth_num) {
 	dosemu_error("Wrong tid\n");
 	leavedos(2);
     }
     thr = &coopthreads[tid];
-    thr->state = COOPTHS_RUNNING;
+    pth = &thr->pth[thr->cur_thr - 1];
+    pth->state = COOPTHS_RUNNING;
 }
 
 void coopth_done(void)
