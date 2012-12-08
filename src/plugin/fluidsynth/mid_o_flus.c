cafbec12f00dc5906011194ba93c69729ce8a8e1
Stas Sergeev <stsp@users.sourceforge.net>
2012-12-09 01:13:06 +0400

    fluidsynth: added sequencer code

--- src/plugin/fluidsynth/mid_o_flus.c
+++ src/plugin/fluidsynth/mid_o_flus.c
@@ -47,7 +47,7 @@ static fluid_sequencer_t* sequencer;
 static short synthSeqID;
 static fluid_midi_parser_t* parser;
 static int pcm_stream, pcm_running;
-static double mf_time_cur;
+static double mf_time_cur, mf_time_base;
 
 static int midoflus_init(void)
 {
@@ -89,28 +89,26 @@ static void midoflus_reset(void)
 static void midoflus_write(unsigned char val)
 {
     fluid_midi_event_t* event;
+
+    if (mf_time_cur == 0) {
+	mf_time_cur = GETusTIME(0);
+	S_printf("MIDI: starting fluidsynth\n");
+	mf_time_base = mf_time_cur;
+	fluid_sequencer_process(sequencer, 0);
+    }
+
     event = fluid_midi_parser_parse(parser, val);
     if (event != NULL) {
 	int ret;
+	unsigned long long now = GETusTIME(0);
+	int msec = (now - mf_time_base) / 1000;
+	fluid_sequencer_process(sequencer, msec);
 	if (debug_level('S') >= 5)
-	    S_printf("MIDI: sending event to fluidsynth\n");
+	    S_printf("MIDI: sending event to fluidsynth, msec=%i\n", msec);
 	ret = fluid_sequencer_add_midi_event_to_buffer(sequencer, event);
 	if (ret != FLUID_OK)
 	    S_printf("MIDI: failed sending midi event\n");
     }
-    if (mf_time_cur == 0) {
-	mf_time_cur = GETusTIME(0);
-	S_printf("MIDI: starting fluidsynth\n");
-    }
-}
-
-static void midoflus_stop(void)
-{
-    S_printf("MIDI: stoping fluidsynth\n");
-    if (pcm_running)
-	pcm_flush(pcm_stream);
-    pcm_running = 0;
-    mf_time_cur = 0;
 }
 
 static void mf_process_samples(int nframes)
@@ -126,24 +124,51 @@ static void mf_process_samples(int nframes)
 	    FLUS_CHANNELS, pcm_stream);
 }
 
-static void midoflus_timer(void)
+static void process_samples(long long now, int min_buf)
 {
-    int nframes;
+    int nframes, retry;
     double period;
-    long long now;
     if (mf_time_cur == 0)
 	return;
+    do {
+	retry = 0;
+	period = pcm_frame_period_us(flus_srate);
+	nframes = (now - mf_time_cur) / period;
+	if (nframes > FLUS_MAX_BUF) {
+	    nframes = FLUS_MAX_BUF;
+	    retry = 1;
+	}
+	if (nframes >= min_buf) {
+	    mf_process_samples(nframes);
+	    mf_time_cur += nframes * period;
+	    if (debug_level('S') >= 5)
+		S_printf("MIDI: processed %i samples with fluidsynth\n", nframes);
+	}
+    } while (retry);
+}
+
+static void midoflus_stop(void)
+{
+    long long now;
+    int msec;
     now = GETusTIME(0);
-    period = pcm_frame_period_us(flus_srate);
-    nframes = (now - mf_time_cur) / period;
-    if (nframes > FLUS_MAX_BUF)
-	nframes = FLUS_MAX_BUF;
-    if (nframes >= FLUS_MIN_BUF) {
-	mf_process_samples(nframes);
-	mf_time_cur += nframes * period;
-	if (debug_level('S') >= 5)
-	    S_printf("MIDI: processed %i samples with fluidsynth\n", nframes);
-    }
+    msec = (now - mf_time_base) / 1000;
+    S_printf("MIDI: stopping fluidsynth at msec=%i\n", msec);
+    /* advance past last event */
+    fluid_sequencer_process(sequencer, msec);
+    /* play remaining samples */
+    process_samples(now, 1);
+    /* shut down all active notes */
+    fluid_synth_system_reset(synth);
+    if (pcm_running)
+	pcm_flush(pcm_stream);
+    pcm_running = 0;
+    mf_time_cur = 0;
+}
+
+static void midoflus_timer(void)
+{
+    process_samples(GETusTIME(0), FLUS_MIN_BUF);
 }
 
 CONSTRUCTOR(static int midoflus_register(void))
