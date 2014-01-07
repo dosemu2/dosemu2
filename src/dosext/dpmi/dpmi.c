dosemu2/dosemu2

commit e19445634c9dad2f1ecdbe3b4e091b9055939139

made in_win31 flag private to dpmi to avoid confusion. Use win31_mode instead.

src/dosext/dpmi/dpmi.c
@@ -109,6 +109,7 @@ static unsigned char * cli_blacklist[CLI_BLACKLIST_LEN];
 static unsigned char * current_cli;
 static int cli_blacklisted = 0;
 static int return_requested = 0;
+static int in_win31;
 static unsigned long *emu_stack_ptr;
 #ifdef __x86_64__
 static unsigned int *iret_frame;
@@ -2596,6 +2597,7 @@ void dpmi_cleanup(void)
   }
   if (in_dpmi == 1) {
     in_win31 = 0;
+    win31_mode = 0;
     mprotect_mapping(MAPPING_DPMI, ldt_buffer,
       PAGE_ALIGN(LDT_ENTRIES*LDT_ENTRY_SIZE), PROT_READ | PROT_WRITE);
     if (!RSP_num)
@@ -3006,7 +3008,6 @@ void dpmi_init(void)
     dpmi_free_memory = dpmi_total_memory;
     DPMI_rm_procedure_running = 0;
     pm_block_handle_used = 1;
-    in_win31 = 0;
   }
 
   DPMI_CLIENT.private_data_segment = REG(es);

src/base/async/int.c
@@ -1781,8 +1781,8 @@ static int int2f(void)
     }
     switch (LO(ax)) {
       case 0x00:		/* WINDOWS ENHANCED MODE INSTALLATION CHECK */
-    if (in_dpmi && in_win31) {
-      D_printf("WIN: WINDOWS ENHANCED MODE INSTALLATION CHECK: %i\n", in_win31);
+    if (in_dpmi && win31_mode) {
+      D_printf("WIN: WINDOWS ENHANCED MODE INSTALLATION CHECK: %i\n", win31_mode);
       if (win31_mode == 3)
         LWORD(eax) = 0x0a03;
       else
@@ -1800,7 +1800,7 @@ static int int2f(void)
     return 1;
 
       case 0x0a:			/* IDENTIFY WINDOWS VERSION AND TYPE */
-    if(in_dpmi && in_win31) {
+    if(in_dpmi && win31_mode) {
       D_printf ("WIN: WINDOWS VERSION AND TYPE\n");
       LWORD(eax) = 0;
       LWORD(ebx) = 0x030a;	/* 3.10 */
@@ -1810,17 +1810,17 @@ static int int2f(void)
       break;
 
       case 0x83:
-        if (in_dpmi && in_win31)
+        if (in_dpmi && win31_mode)
             LWORD (ebx) = 0;	/* W95: number of virtual machine */
       case 0x81:		/* W95: enter critical section */
-        if (in_dpmi && in_win31) {
+        if (in_dpmi && win31_mode) {
 	    D_printf ("WIN: enter critical section\n");
 	    /* LWORD(eax) = 0;	W95 DDK says no return value */
 	    return 1;
   }
       break;
       case 0x82:		/* W95: exit critical section */
-        if (in_dpmi && in_win31) {
+        if (in_dpmi && win31_mode) {
 	    D_printf ("WIN: exit critical section\n");
 	    /* LWORD(eax) = 0;	W95 DDK says no return value */
 	    return 1;
@@ -2255,7 +2255,7 @@ static void update_xtitle(void)
       return;
   }
 
-  if (in_win31 && memcmp(cmd_ptr, "krnl", 4) == 0) {
+  if (win31_mode && memcmp(cmd_ptr, "krnl", 4) == 0) {
     cmd_ptr = win31_title;
     force_update = 1;
   }

src/dosext/dpmi/dpmi.h
@@ -141,7 +141,6 @@ struct RSP_s {
 };
 
 EXTERN volatile int in_dpmi INIT(0);/* Set to 1 when running under DPMI */
-EXTERN volatile int in_win31 INIT(0); /* Set to 1 when running Windows 3.1 */
 EXTERN volatile int in_dpmi_dos_int INIT(0);
 EXTERN volatile int dpmi_mhp_TF INIT(0);
 EXTERN unsigned char dpmi_mhp_intxxtab[256] INIT({0});

commit f3f238315a8c7f6ae19998884f0bad4a808689f2

init win31_mode to 0
This fixes carmageddon game
Looks like a regression from e19445634
Also remove in_win31

src/dosext/dpmi/dpmi.c
@@ -101,7 +101,6 @@ static unsigned char * cli_blacklist[CLI_BLACKLIST_LEN];
 static unsigned char * current_cli;
 static int cli_blacklisted = 0;
 static int return_requested = 0;
-static int in_win31;
 static unsigned long *emu_stack_ptr;
 #ifdef __x86_64__
 static unsigned int *iret_frame;
@@ -2579,7 +2578,6 @@ void dpmi_cleanup(void)
     DPMI_CLIENT.pm_block_root = NULL;
   }
   if (in_dpmi == 1) {
-    in_win31 = 0;
     win31_mode = 0;
     if (!RSP_num)
       dpmi_free_pool();
@@ -3033,7 +3031,7 @@ void dpmi_init(void)
     inherit_idt = DPMI_CLIENT.is_32 == PREV_DPMI_CLIENT.is_32
 #if WINDOWS_HACKS
 /* work around the disability of win31 in Standard mode to run the DPMI apps */
-	&& !(in_win31 && win31_mode == 2)
+	&& (win31_mode != 2)
 #endif
     ;
   else
@@ -3680,7 +3678,6 @@ int dpmi_fault(struct sigcontext_struct *scp)
           if (_LWORD(eax) == 0x0100) {
             _eax = DPMI_CLIENT.LDT_ALIAS;  /* simulate direct ldt access */
 	    _eflags &= ~CF;
-	    in_win31 = 1;
 	  } else
             _eflags |= CF;
 

src/include/int.h
@@ -13,7 +13,7 @@
 
 #define WINDOWS_HACKS 1
 #if WINDOWS_HACKS
-EXTERN int win31_mode INIT(3);
+EXTERN int win31_mode INIT(0);
 #endif
 
 EXTERN unsigned int  check_date INIT(0);