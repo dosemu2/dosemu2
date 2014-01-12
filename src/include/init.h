c48509d59588bf2a68075d597f2e3c53451c3c74
Stas Sergeev <stsp@users.sourceforge.net>
2014-01-12 17:08:56 +0400

    pcm: add demo plugin for sound input

--- src/include/init.h
+++ src/include/init.h
@@ -1,4 +1,5 @@
 #ifndef DOSEMU_INIT_H
 #define DOSEMU_INIT_H
 #define CONSTRUCTOR(X) X __attribute__((constructor)); X
+#define DESTRUCTOR(X) X __attribute__((destructor)); X
 #endif /* DOSEMU_INIT_H */
---
+++ src/plugin/midimisc/config/plugin_init.h
@@ -0,0 +1,16 @@
+/*
+ * (C) Copyright 1992, ..., 2007 the "DOSEMU-Development-Team".
+ *
+ * for details see file COPYING.DOSEMU in the DOSEMU distribution
+ */
+
+/*
+ * This is file plugin_init.h for use within the src/plugin/<name>/
+ *
+ * It should contain a valid call to the init function of the plug-in such as
+ *
+ *       my_plugin_init();
+ *
+ */
+
+sndpipe_plugin_init();
