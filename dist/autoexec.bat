4a7c43ed3f9b228b28c5ffafd28d655c34bbb377
Stas Sergeev <stsp@users.sourceforge.net>
2014-04-22 01:51:00 +0400

    loadhi doesnt exist
    Patch from Frantisek Hanzlik <franta@hanzlici.cz>

--- dist/autoexec.bat
+++ dist/autoexec.bat
@@ -11,7 +11,7 @@ lredir del d: > nul
 lredir d: linux\fs%DOSDRIVE_D%
 :nodrived
 rem uncomment to load another bitmap font
-rem loadhi display con=(vga,437,2)
+rem lh display con=(vga,437,2)
 rem mode con codepage prepare=((850) z:\cpi\ega.cpx)
 rem mode con codepage select 850
 rem chcp 850
