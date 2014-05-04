0d43d2c01c58ca5d2f6a0ee248f6d7747c23a451
Stas Sergeev <stsp@users.sourceforge.net>
2014-05-04 16:35:58 +0400

    vgabios: redice direct uses of vga struct

--- src/base/bios/vgabios.c
+++ src/base/bios/vgabios.c
@@ -15,11 +15,17 @@ static Bit32u color2pixels[16] = {0,0xff,0xff00,0xffff,0xff0000,0xff00ff,0xffff0
 			0x00ffffff,0xff000000,0xff0000ff,0xff00ff00,0xff00ffff,
 			0xffff0000,0xffff00ff,0xffffff00,0xffffffff};
 
+static vga_mode_info *get_vmi(void)
+{
+    return vga_emu_find_mode(READ_BYTE(BIOS_VIDEO_MODE), NULL);
+}
+
 static unsigned vgaemu_xy2ofs(unsigned x, unsigned y)
 {
   unsigned ofs;
+  vga_mode_info *vmi = get_vmi();
 
-  switch(vga.mode_type) {
+  switch(vmi->type) {
     case TEXT:
     case P15:
     case P16:
@@ -45,10 +51,10 @@ static unsigned vgaemu_xy2ofs(unsigned x, unsigned y)
       break;
 
     case CGA:
-      if(vga.color_bits == 1) {
+      if(vmi->color_bits == 1) {
         ofs = vga.scan_len * (y >> 1) + (x >> 3) + (y & 1) * 0x2000;
       }
-      else {	/* vga.color_bits == 2 */
+      else {	/* vmi->color_bits == 2 */
         ofs = vga.scan_len * (y >> 1) + (x >> 2) + (y & 1) * 0x2000;
       }
       break;
@@ -67,6 +73,7 @@ static unsigned vgaemu_xy2ofs(unsigned x, unsigned y)
 static void vgaemu_move_vga_mem(unsigned dst, unsigned src, unsigned len)
 {
   unsigned char *dp, *sp;
+  vga_mode_info *vmi = get_vmi();
 
   if(len == 0) return;
 
@@ -74,7 +81,7 @@ static void vgaemu_move_vga_mem(unsigned dst, unsigned src, unsigned len)
   sp = vga.mem.base + src;
 
   memcpy(dp, sp, len);
-  switch(vga.mode_type) {
+  switch(vmi->type) {
     case PL4:
       memcpy(dp + 0x20000, sp + 0x20000, len);
       memcpy(dp + 0x30000, sp + 0x30000, len);
@@ -86,9 +93,10 @@ static void vgaemu_move_vga_mem(unsigned dst, unsigned src, unsigned len)
 
 static void vgaemu_clear_vga_mem(unsigned dst, unsigned len, unsigned char attr)
 {
+  vga_mode_info *vmi = get_vmi();
   if(len == 0) return;
 
-  switch(vga.mode_type) {
+  switch(vmi->type) {
     case PL4:
       memset(vga.mem.base + dst + 0x20000, attr & 4 ? 0xff : 0, len);
       memset(vga.mem.base + dst + 0x30000, attr & 8 ? 0xff : 0, len);
@@ -98,9 +106,9 @@ static void vgaemu_clear_vga_mem(unsigned dst, unsigned len, unsigned char attr)
       memset(vga.mem.base + dst, attr & 1 ? 0xff : 0, len);
       break;
     case CGA:
-      if(vga.color_bits == 1)
+      if(vmi->color_bits == 1)
         attr = attr & 1 ? 0xff : 0;
-      else {  /* vga.color_bits == 2 */
+      else {  /* vmi->color_bits == 2 */
         attr &= 3;
         attr |= (attr<<2);
         attr |= (attr<<4);
@@ -115,22 +123,23 @@ void vgaemu_scroll(int x0, int y0, int x1, int y1, int n, unsigned char attr)
 {
   int dx, dy;
   unsigned height;
+  vga_mode_info *vmi = get_vmi();
 
   vga_msg(
     "vgaemu_scroll: %d lines, area %d.%d-%d.%d, attr 0x%02x\n",
     n, x0, y0, x1, y1, attr
   );
 
-  if(x1 >= vga.text_width || y1 >= vga.text_height) {
-    if(x1 >= vga.text_width) x1 = vga.text_width - 1;
-    if(y1 >= vga.text_height) y1 = vga.text_height - 1;
+  if(x1 >= vmi->text_width || y1 >= vmi->text_height) {
+    if(x1 >= vmi->text_width) x1 = vmi->text_width - 1;
+    if(y1 >= vmi->text_height) y1 = vmi->text_height - 1;
   }
   dx = x1 - x0 + 1;
   dy = y1 - y0 + 1;
   if(n >= dy || n <= -dy) n = 0;
   if(
     dx <= 0 || dy <= 0 || x0 < 0 ||
-    x1 >= vga.text_width || y0 < 0 || y1 >= vga.text_height
+    x1 >= vmi->text_width || y0 < 0 || y1 >= vmi->text_height
   ) {
     vga_msg("vgaemu_scroll: scroll parameters impossibly out of bounds\n");
     return;
@@ -171,6 +180,7 @@ void vgaemu_put_char(int x, int y, unsigned char c, unsigned char attr)
   unsigned src, ofs, height, u, page0, page1, start_x, start_y, m, mc, v;
   unsigned col = attr;
   unsigned char *font;
+  vga_mode_info *vmi = get_vmi();
 
   vga_msg(
     "vgaemu_put_char: x.y %d.%d, char 0x%02x, attr 0x%02x\n",
@@ -183,14 +193,14 @@ void vgaemu_put_char(int x, int y, unsigned char c, unsigned char attr)
   start_y = y * height;
 
   if(
-    vga.mode_type == P15 || vga.mode_type == P16 ||
-    vga.mode_type == P24 || vga.mode_type == P32
+    vmi->type == P15 || vmi->type == P16 ||
+    vmi->type == P24 || vmi->type == P32
   ) {
     // Maybe use default DAC table to look up color values?
     col = 0xffffffff;
   }
 
-  if(vga.mode_type == CGA && c >= 0x80) {
+  if(vmi->type == CGA && c >= 0x80) {
     /* use special 8x8 gfx chars in modes 4, 5, 6 */
     src = IVEC(0x1f);
     src -= 0x80 * 8;
@@ -217,9 +227,9 @@ void vgaemu_put_char(int x, int y, unsigned char c, unsigned char attr)
   for(u = 0; u < height; u++) {
     ofs = vgaemu_xy2ofs(start_x, start_y + u);
     if((ofs >> 12) > page1) page1 = ofs >> 12;
-    switch(vga.mode_type) {
+    switch(vmi->type) {
       case CGA:
-        if(vga.color_bits == 1) {
+        if(vmi->color_bits == 1) {
           if((attr & 0x80)) {
             vga.mem.base[ofs] ^= (attr & 1) ? font[u] : 0;
           }
@@ -227,7 +237,7 @@ void vgaemu_put_char(int x, int y, unsigned char c, unsigned char attr)
             vga.mem.base[ofs] = (attr & 1) ? font[u] : 0;
           }
         }
-        else {	/* vga.color_bits == 2 */
+        else {	/* vmi->color_bits == 2 */
           mc = (attr & 3) << 14;
           for(v = 0, m = 0x80; m; m >>= 1, mc >>= 2) {
             v |= (font[u] & m) ? mc : 0;
@@ -310,7 +320,7 @@ void vgaemu_put_char(int x, int y, unsigned char c, unsigned char attr)
   for(u = page0; u <= page1; u++) {
     vga.mem.dirty_map[u] = 1;
   }
-  switch(vga.mode_type) {
+  switch(vmi->type) {
     case PL1:
     case PL2:
     case PL4:
@@ -329,6 +339,7 @@ void vgaemu_put_pixel(int x, int y, unsigned char page, unsigned char attr)
 {
   unsigned ofs, u, page0, v;
   unsigned col = attr;
+  vga_mode_info *vmi = get_vmi();
 
   vga_msg(
     "vgaemu_put_pixel: x.y %d.%d, page 0x%02x, attr 0x%02x\n",
@@ -345,13 +356,13 @@ void vgaemu_put_pixel(int x, int y, unsigned char page, unsigned char attr)
 
   page0 = ofs >> 12;
 
-  switch(vga.mode_type) {
+  switch(vmi->type) {
       case CGA:
-        if(vga.color_bits == 1) {
+        if(vmi->color_bits == 1) {
           col = (attr & 1) << (7 - (x & 7));
 	   v = 1 << (7 - (x & 7));
         }
-        else {	/* vga.color_bits == 2 */
+        else {	/* vmi->color_bits == 2 */
           col = (attr & 3) << (2 * (3 - (x & 3)));
 	   v = 3 << (2 * (3 - (x & 3)));
         }
@@ -393,7 +404,7 @@ void vgaemu_put_pixel(int x, int y, unsigned char page, unsigned char attr)
 
   vga.mem.dirty_map[page0] = 1;
 
-  switch(vga.mode_type) {
+  switch(vmi->type) {
     case PL1:
     case PL2:
     case PL4:
@@ -408,6 +419,7 @@ void vgaemu_put_pixel(int x, int y, unsigned char page, unsigned char attr)
 unsigned char vgaemu_get_pixel(int x, int y, unsigned char page)
 {
   unsigned ofs, u = 0, v = 0;
+  vga_mode_info *vmi = get_vmi();
 
   vga_msg(
     "vgaemu_get_pixel: x.y %d.%d, page 0x%02x\n",
@@ -422,11 +434,11 @@ unsigned char vgaemu_get_pixel(int x, int y, unsigned char page)
     return 0;
   }
 
-  switch(vga.mode_type) {
+  switch(vmi->type) {
       case CGA:
-        if(vga.color_bits == 1)
+        if(vmi->color_bits == 1)
           return (vga.mem.base[ofs] >> (7 - (x & 7))) & 1;
-        else 	/* vga.color_bits == 2 */
+        else 	/* vmi->color_bits == 2 */
           return (vga.mem.base[ofs] >> (2 * (3 - (x & 3)))) & 3;
 
       case PL1:
