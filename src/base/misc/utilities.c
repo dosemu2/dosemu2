69fff26f5622f1566230f540774fc552314e3216
Stas Sergeev <stsp@users.sourceforge.net>
2014-09-24 23:21:08 +0400

    fix popen2() to handle also stderr

--- src/base/misc/utilities.c
+++ src/base/misc/utilities.c
@@ -753,6 +753,7 @@ int popen2(const char *cmdline, struct popen2 *childinfo)
         close(pipe_stdin[0]);
         close(pipe_stdout[0]);
         dup2(pipe_stdout[1], 1);
+        dup2(pipe_stdout[1], 2);
         close(pipe_stdout[1]);
         execl("/bin/sh", "sh", "-c", cmdline, NULL);
         perror("execl"); exit(99);
