6205cba79f6810878da3b75aa827bae56eabbdca
Stas Sergeev <stsp@users.sourceforge.net>
2014-12-15 23:53:23 +0300

    X: work around the mutter-3.14 bug
    mutter-3.14 has a bug: it calculates aspect-based size constraints
    for the entire window with decorators instead of for client area.
    https://bugzilla.gnome.org/show_bug.cgi?id=739573
    
    So for now we disable PAspect...

--- src/plugin/X/X.c
+++ src/plugin/X/X.c
@@ -2016,6 +2016,13 @@ static void lock_window_size(unsigned wx_res, unsigned wy_res)
   sh.flags = PSize  | PMinSize | PMaxSize | PBaseSize;
   if(config.X_fixed_aspect || config.X_aspect_43) sh.flags |= PAspect;
   if (use_bitmap_font) {
+#if 1
+    /* mutter-3.14 has a bug: it calculates aspect-based size constraints
+     * for the entire window with decorators instead of for client area.
+     * https://bugzilla.gnome.org/show_bug.cgi?id=739573
+     * So for now we disable PAspect... */
+    sh.flags &= ~PAspect;
+#endif
     sh.flags |= PResizeInc;
     sh.max_width = 32767;
     sh.max_height = 32767;
