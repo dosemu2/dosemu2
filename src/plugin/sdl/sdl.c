3d5a5f361e1b8fc8ee500c7ff3c4e26a8dabeae1
Stas Sergeev <stsp@users.sourceforge.net>
2015-01-04 00:02:39 +0300

    sdl: missing lock around X_handle_text_expose()

--- src/plugin/sdl/sdl.c
+++ src/plugin/sdl/sdl.c
@@ -792,9 +792,13 @@ static void SDL_handle_events(void)
      }
    }
 #ifdef X_SUPPORT
-   if (!use_bitmap_font && X_handle_text_expose())
-     /* need to check separately because SDL_VIDEOEXPOSE is eaten by SDL */
-     SDL_redraw_text_screen();
+   if (!use_bitmap_font) {
+      x11.lock_func();
+      X_handle_text_expose();
+      x11.unlock_func();
+      /* need to check separately because SDL_VIDEOEXPOSE is eaten by SDL */
+      SDL_redraw_text_screen();
+   }
 #endif
    do_mouse_irq();
 }
