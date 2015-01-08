a254511359bfd00e7e3710cd550bad769333fd21
Stas Sergeev <stsp@users.sourceforge.net>
2015-01-08 21:15:27 +0300

    fix regression of vgaemu

--- src/emu-i386/simx86/sigsegv.c
+++ src/emu-i386/simx86/sigsegv.c
@@ -173,7 +173,7 @@ vga2vgal:
   }
 }
 
-#if 0
+#if 1
 static int jitx86_instr_len(const unsigned char *rip)
 {
   const unsigned char *p = rip;
@@ -512,14 +512,15 @@ int e_emu_fault(struct sigcontext_struct *scp)
   if (_trapno==0x0e) {
 	if (Video->update_screen) {
 		if (!DPMIValidSelector(_cs)) {
-#if 0
-			dosaddr_t pf = DOSADDR_REL(LINP(_cr2));
-			if (e_vgaemu_fault(scp,pf >> 12) == 1) return 1;
-#else
-			/* e_vgaemu_fault misses mutex and I don't
-			 * see the need for such code duplication */
-			if (vga_emu_fault(scp, 0) == True) return 1;
-#endif
+			/* in vga.inst_emu mode, vga_emu_fault() can handle
+			 * only faults from DOS code, and here we are with
+			 * the fault from jit-compiled code */
+			if (!vga.inst_emu) {
+				if (vga_emu_fault(scp, 0) == True) return 1;
+			} else {
+				dosaddr_t pf = DOSADDR_REL(LINP(_cr2));
+				if (e_vgaemu_fault(scp,pf >> 12) == 1) return 1;
+			}
 		} else {
 			if(VGA_EMU_FAULT(scp,code,1)==True) {
 				dpmi_check_return(scp);
