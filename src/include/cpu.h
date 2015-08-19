56e2fab8c6909688251575c39331015833ebdc9e
Stas Sergeev <stsp@users.sourceforge.net>
2015-08-19 18:09:29 +0400

    cast regs to uint32_t to avoid gcc warnings

--- src/include/cpu.h
+++ src/include/cpu.h
@@ -29,7 +29,7 @@
 #ifdef __linux__
 #define REGS  vm86s.regs
 /* this is used like: REG(eax) = 0xFFFFFFF */
-#define REG(reg) (REGS.reg)
+#define REG(reg) (*(uint32_t *)&REGS.reg)
 #define READ_SEG_REG(reg) (REGS.reg)
 #define WRITE_SEG_REG(reg, val) REGS.reg = (val)
 #endif
