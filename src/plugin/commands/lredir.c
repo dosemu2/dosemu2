f6cc6be2a9f4b513ef14d0ebd0d29993bb09ff87
Stuart Axon <stuaxo2@yahoo.com>
2015-08-27 13:02:30 +0800

    Fix indent to 2 chars

--- src/plugin/commands/lredir.c
+++ src/plugin/commands/lredir.c
@@ -493,10 +493,10 @@ static uint16 CheckForDosc(void)
 static
 int ArgIsHelp(char *arg)
 {
-    /* Help can be HELP ? or /? */
-    return 1 ^ strncmpi(arg, KEYWORD_HELP, KEYWORD_HELP_COMPARE_LENGTH) &
-        strncmpi(arg, "/?", 2) &
-        strncmpi(arg, "?", 1);
+  /* Help can be HELP ? or /? */
+  return (1 ^ strncmpi(arg, KEYWORD_HELP, KEYWORD_HELP_COMPARE_LENGTH) &
+    strncmp(arg, "/?", 2) &
+    strncmp(arg, "?", 1));
 }
 
 int lredir_main(int argc, char **argv)
