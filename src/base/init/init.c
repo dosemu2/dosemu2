ce6c5e91315a87f7eeacdb44b3bc8a12318f232a
Stas Sergeev <stsp@users.sourceforge.net>
2015-11-21 19:53:14 +0300

    different fix for #111

--- src/base/init/init.c
+++ src/base/init/init.c
@@ -370,10 +370,8 @@ void print_version(void)
   warn(" -m64\n");
 #endif
 #ifdef CFLAGS_STR
-#define __S(x) #x+1
-/* extra () is needed if the string has commas: it otherwise expands to
- * multiple args, giving a compilation error. +1 above removes leading ( */
-#define _S(x) __S((x))
+#define __S(...) #__VA_ARGS__
+#define _S(x) __S(x)
   warn("CFLAGS: %s\n", _S(CFLAGS_STR));
 #endif
 }
