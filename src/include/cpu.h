dosemu2/dosemu2

commit 6aceba889f3103fb6147412934c2e43633bd1867

reduce users of LWORD(eflags)
This is almost never the right thing to do, unless you check
the single flag.

src/include/cpu.h
@@ -93,7 +93,7 @@ union dword {
 #define _FS      (vm86s.regs.fs)
 #define _GS      (vm86s.regs.gs)
 #define _EFLAGS  UDWORD(eflags)
-#define _FLAGS   LWORD(eflags)
+#define _FLAGS   REG(eflags)
 
 /* these are used like:  LO(ax) = 2 (sets al to 2) */
 #define LO(reg)  vm86u.b[offsetof(struct vm86_struct, regs.e##reg)]
@@ -138,11 +138,6 @@ static inline far_t rFAR_FARt(FAR_PTR far_ptr) {
 }
 
 #define peek(seg, off)	(READ_WORD(SEGOFF2LINEAR(seg, off)))
-
-#define WRITE_FLAGS(val)    LWORD(eflags) = (val)
-#define WRITE_FLAGSE(val)    REG(eflags) = (val)
-#define READ_FLAGS()        LWORD(eflags)
-#define READ_FLAGSE()        REG(eflags)
 
 extern struct _fpstate vm86_fpu_state;
 
@@ -317,9 +312,8 @@ static __inline__ void reset_revectored(int nr, struct revectored_struct * bitma
 #define isset_VIP()   ((_EFLAGS & VIP) != 0)
 
 #define set_EFLAGS(flgs, new_flgs) ({ \
-  int __nflgs = (new_flgs); \
-  (flgs)=(__nflgs) | IF | IOPL_MASK; \
-  ((__nflgs & IF) ? set_IF() : clear_IF()); \
+  int __nflgs = (new_flgs) & ~(VIP | VIF); \
+  (flgs) = (__nflgs) | IF | IOPL_MASK | ((__nflgs & IF) ? VIF : 0); \
 })
 #define set_FLAGS(flags) set_EFLAGS(_FLAGS, flags)
 #define get_EFLAGS(flags) ({ \

src/base/async/int.c
@@ -542,9 +542,6 @@ static int int15(void)
 
   if (HI(ax) != 0x4f)
     NOCARRY;
-  /* REG(eflags) |= VIF;
-  WRITE_FLAGSE(READ_FLAGSE() | VIF);
-  */
 
   switch (HI(ax)) {
   case 0x10:			/* TopView/DESQview */
@@ -597,7 +594,7 @@ static int int15(void)
   case 0x4f:			/* Keyboard intercept */
     HI(ax) = 0x86;
     /*k_printf("INT15 0x4f CARRY=%x AX=%x\n", (LWORD(eflags) & CF),LWORD(eax));*/
-    k_printf("INT15 0x4f CARRY=%x AX=%x\n", (READ_FLAGS() & CF),LWORD(eax));
+    k_printf("INT15 0x4f CARRY=%x AX=%x\n", (_FLAGS & CF),LWORD(eax));
     CARRY;
 /*
     if (LO(ax) & 0x80 )

src/tools/debugger/mhpdbgc.c
@@ -481,7 +481,7 @@ static unsigned long mhp_getreg(char * regn)
     case _BPr: return LWORD(ebp);
     case _SPr: return LWORD(esp);
     case _IPr: return LWORD(eip);
-    case _FLr: return LWORD(eflags);
+    case _FLr: return get_FLAGS(REG(eflags));
     case _EAXr: return REG(eax);
     case _EBXr: return REG(ebx);
     case _ECXr: return REG(ecx);
@@ -518,7 +518,7 @@ static void mhp_setreg(char * regn, unsigned long val)
     case _BPr: LWORD(ebp) = val; break;
     case _SPr: LWORD(esp) = val; break;
     case _IPr: LWORD(eip) = val; break;
-    case _FLr: LWORD(eflags) = val; break;
+    case _FLr: set_FLAGS(val); break;
     case _EAXr: REG(eax) = val; break;
     case _EBXr: REG(ebx) = val; break;
     case _ECXr: REG(ecx) = val; break;
@@ -539,7 +539,7 @@ static void mhp_go(int argc, char * argv[])
       mhp_printf("already in running state\n");
    } else {
       dpmi_mhp_setTF(0);
-      WRITE_FLAGS(READ_FLAGS() & ~TF);
+      clear_TF();
       mhpdbg.TFpendig = 0;
       mhpdbgc.stopped = 0;
       pic_sti();
@@ -572,7 +572,7 @@ static void mhp_trace(int argc, char * argv[])
       if (in_dpmi) {
         dpmi_mhp_setTF(1);
       }
-      WRITE_FLAGS(READ_FLAGS() | TF);
+      set_TF();
 
       if (!strcmp(argv[0], "ti")) {
 	mhpdbgc.trapcmd = 2;
@@ -604,7 +604,7 @@ static void mhp_trace_force(int argc, char * argv[])
       if (in_dpmi) {
         dpmi_mhp_setTF(1);
       }
-      WRITE_FLAGS(READ_FLAGS() | TF);
+      set_TF();
       mhpdbg.TFpendig = 1;
       mhpdbgc.trapcmd = 1;
       /* disable PIC: we want to trace the program, not the HW int handlers */
@@ -1368,8 +1368,8 @@ static void mhp_regs(int argc, char * argv[])
                 LWORD(eax), LWORD(ebx), LWORD(ecx), LWORD(edx));
     mhp_printf("  SI=%04x  DI=%04x  SP=%04x  BP=%04x",
                 LWORD(esi), LWORD(edi), LWORD(esp), LWORD(ebp));
-    mhp_printf("\nDS=%04x  ES=%04x  FS=%04x  GS=%04x  FL=%04x",
-                LWORD(ds), LWORD(es), LWORD(fs), LWORD(gs), LWORD(eflags));
+    mhp_printf("\nDS=%04x  ES=%04x  FS=%04x  GS=%04x  FL=%08x",
+                LWORD(ds), LWORD(es), LWORD(fs), LWORD(gs), REG(eflags));
     mhp_printf("\nCS:IP=%04x:%04x       SS:SP=%04x:%04x\n",
                 LWORD(cs), LWORD(eip), LWORD(ss), LWORD(esp));
   }

commit c4df86542640edd7fb7e1e64d80b06588602bd81

Fix set_EFLAGS macro regression from 6aceba889f3.
The set_EFLAGS change in commit 6aceba889f3 unconditionally clears
VIP. By reverting the change VIP is preserved if VIF is not set.
This fixes a hang in Jazz Jackrabbit.

src/include/cpu.h
@@ -312,8 +312,9 @@ static __inline__ void reset_revectored(int nr, struct revectored_struct * bitma
 #define isset_VIP()   ((_EFLAGS & VIP) != 0)
 
 #define set_EFLAGS(flgs, new_flgs) ({ \
-  int __nflgs = (new_flgs) & ~(VIP | VIF); \
-  (flgs) = (__nflgs) | IF | IOPL_MASK | ((__nflgs & IF) ? VIF : 0); \
+  int __nflgs = (new_flgs); \
+  (flgs)=(__nflgs) | IF | IOPL_MASK; \
+  ((__nflgs & IF) ? set_IF() : clear_IF()); \
 })
 #define set_FLAGS(flags) set_EFLAGS(_FLAGS, flags)
 #define get_EFLAGS(flags) ({ \