51fc50b74e60a87c3a20a02c05a220475177bf4a
Bart Oldeman <bartoldeman@users.sourceforge.net>
2015-12-15 09:28:40 -0500

    simx86: use L_DI_R1 for LODS without REP.

--- src/emu-i386/simx86/interp.c
+++ src/emu-i386/simx86/interp.c
@@ -1269,7 +1269,7 @@ intop3b:		{ int op = ArOpsFR[D_MO(opc)];
 			} break;
 /*ac*/	case LODSb: {	int m = mode|(MBYTE|MOVSSRC);
 			Gen(O_MOVS_SetA, m);
-			Gen(O_MOVS_LodD, m);
+			Gen(L_DI_R1, m);
 			Gen(S_REG, m, Ofs_AL); PC++;
 #ifndef SINGLESTEP
 			/* optimize common sequence LODSb-STOSb */
@@ -1284,7 +1284,7 @@ intop3b:		{ int op = ArOpsFR[D_MO(opc)];
 			} break;
 /*ad*/	case LODSw: {	int m = mode|MOVSSRC;
 			Gen(O_MOVS_SetA, m);
-			Gen(O_MOVS_LodD, m);
+			Gen(L_DI_R1, m);
 			Gen(S_REG, m, Ofs_EAX); PC++;
 #ifndef SINGLESTEP
 			/* optimize common sequence LODSw-STOSw */
@@ -1709,14 +1709,24 @@ repag0:
 				case LODSb:
 					repmod |= (MBYTE|MOVSSRC);
 					Gen(O_MOVS_SetA, repmod);
-					Gen(O_MOVS_LodD, repmod);
+					if (repmod & (MREPNE|MREP)) {
+						Gen(O_MOVS_LodD, repmod);
+					}
+					else {
+						Gen(L_DI_R1, repmod);
+					}
 					Gen(S_REG, repmod, Ofs_AL);
 					Gen(O_MOVS_SavA, repmod);
 					PC++; break;
 				case LODSw:
 					repmod |= MOVSSRC;
 					Gen(O_MOVS_SetA, repmod);
-					Gen(O_MOVS_LodD, repmod);
+					if (repmod & (MREPNE|MREP)) {
+						Gen(O_MOVS_LodD, repmod);
+					}
+					else {
+						Gen(L_DI_R1, repmod);
+					}
 					Gen(S_REG, repmod, Ofs_AX);
 					Gen(O_MOVS_SavA, repmod);
 					PC++; break;
