084a4b509b28f9768f7825e459ad6612661121a5
Stas Sergeev <stsp@users.sourceforge.net>
2015-12-18 20:13:27 +0300

    blaster.c: change copyrights to GPLv2+
    Acked-by: Ryan Underwood <nemesis@icequake.net>

--- src/plugin/commands/blaster.c
+++ src/plugin/commands/blaster.c
@@ -1,8 +1,21 @@
 /*
- * All modifications in this file to the original code are
- * (C) Copyright 1992, ..., 2014 the "DOSEMU-Development-Team".
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
  *
- * for details see file COPYING in the DOSEMU distribution
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+/*
+ * Authors: Ryan C. Underwood, Stas Sergeev
  */
 
 #include <string.h>
@@ -92,4 +105,3 @@ int blaster_main(int argc, char **argv) {
 
 	return 0;
 }
-
