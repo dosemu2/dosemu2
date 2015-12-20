53b361bca6dc484a5a7ed0e66ae6cdcf9150b2b5
Stas Sergeev <stsp@users.sourceforge.net>
2015-12-20 18:12:12 +0300

    msdos: fix compilation error

--- src/dosext/dpmi/msdoshlp.h
+++ src/dosext/dpmi/msdoshlp.h
@@ -9,7 +9,7 @@ extern void msdos_pm_call(struct sigcontext *scp, int is_32);
 
 int allocate_realmode_callbacks(void (*handler[])(struct sigcontext *,
 	const struct RealModeCallStructure *),
-	void (*ret_handler[])(const struct sigcontext *,
+	void (*ret_handler[])(struct sigcontext *,
 	struct RealModeCallStructure *),
 	int num, far_t *r_cbks);
 extern void free_realmode_callbacks(far_t *r_cbks, int num);
