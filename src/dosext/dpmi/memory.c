5e596e000424334c84a41bd4539fadda9284e7ab
Bart Oldeman <bartoldeman@users.sourceforge.net>
2015-12-21 08:31:45 -0500

    DPMI: allocate ldt_alias buffer from DPMI memory pool.

--- src/dosext/dpmi/memory.c
+++ src/dosext/dpmi/memory.c
@@ -139,7 +139,8 @@ int dpmi_alloc_pool(void)
 
     /* Create DPMI pool */
     num_pages = (config.dpmi >> 2) +
-      ((DPMI_pm_stack_size * DPMI_MAX_CLIENTS) >> PAGE_SHIFT);
+      ((DPMI_pm_stack_size * DPMI_MAX_CLIENTS) >> PAGE_SHIFT) +
+      (PAGE_ALIGN(LDT_ENTRIES*LDT_ENTRY_SIZE) >> PAGE_SHIFT);
     mpool_numpages = num_pages + 5;  /* 5 extra pages */
     memsize = mpool_numpages << PAGE_SHIFT;
 
