1bce5813b809947ef643490f2469645f0b7ab745
Stas Sergeev <stsp@users.sourceforge.net>
2016-01-06 04:43:52 +0300

    signal: set sigaltstack via uc_stack

--- src/arch/linux/async/signal.c
+++ src/arch/linux/async/signal.c
@@ -74,11 +74,10 @@ static struct sigchld_hndl chld_hndl[MAX_SIGCHLD_HANDLERS];
 static int chd_hndl_num;
 
 static sigset_t q_mask;
-#if SIGALTSTACK_WA
 static void *cstack;
+#if SIGALTSTACK_WA
 static void *backup_stack;
 #endif
-static stack_t sig_stk;
 
 static int sh_tid;
 static int in_handle_signals;
@@ -497,8 +496,6 @@ static void sigstack_init(void)
 {
 /* reserve 1024 uncommitted pages for stack */
 #define SIGSTACK_SIZE (1024 * getpagesize())
-  stack_t ss;
-
 #ifndef MAP_STACK
 #define MAP_STACK 0
 #endif
@@ -517,7 +514,7 @@ static void sigstack_init(void)
     return;
   }
 #else
-  void *cstack = mmap(NULL, SIGSTACK_SIZE, PROT_READ | PROT_WRITE,
+  cstack = mmap(NULL, SIGSTACK_SIZE, PROT_READ | PROT_WRITE,
 	MAP_PRIVATE | MAP_ANONYMOUS | MAP_STACK, -1, 0);
   if (cstack == MAP_FAILED) {
     error("Unable to allocate stack\n");
@@ -525,11 +522,6 @@ static void sigstack_init(void)
     return;
   }
 #endif
-  ss.ss_sp = cstack;
-  ss.ss_size = SIGSTACK_SIZE;
-  ss.ss_flags = SS_ONSTACK;
-
-  sigaltstack(&ss, NULL);
 }
 
 /* DANG_BEGIN_FUNCTION signal_pre_init
@@ -950,8 +942,8 @@ static void async_awake(void *arg)
 
 void signal_return_to_dosemu(void)
 {
-  stack_t stk;
   int err;
+  stack_t ss = {};
 
 #if SIGALTSTACK_WA
   jmp_buf hack;
@@ -970,8 +962,8 @@ void signal_return_to_dosemu(void)
   else
     return;
 #endif
-  stk.ss_flags = SS_DISABLE;
-  err = sigaltstack(&stk, &sig_stk);
+  ss.ss_flags = SS_DISABLE;
+  err = sigaltstack(&ss, NULL);
   if (err)
     perror("sigaltstack");
   fault_cnt--;
@@ -983,4 +975,11 @@ void signal_return_to_dosemu(void)
 void signal_return_to_dpmi(void)
 {
   fault_cnt++;
+}
+
+void signal_set_altstack(stack_t *stk)
+{
+  stk->ss_sp = cstack;
+  stk->ss_size = SIGSTACK_SIZE;
+  stk->ss_flags = SS_ONSTACK;
 }
