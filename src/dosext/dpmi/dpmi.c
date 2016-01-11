833b5c8e3289ba4ae7cdbb0bf37682964378e012
Stas Sergeev <stsp@users.sourceforge.net>
2016-01-11 15:44:44 +0300

    dpmi: fix race in run_dpmi_thr
    Need to re-check in_dpmi_pm() after coopth_yield(), because the
    IRQ processing code may switch us to RM in background.

--- src/dosext/dpmi/dpmi.c
+++ src/dosext/dpmi/dpmi.c
@@ -3089,7 +3089,7 @@ static void run_dpmi_thr(void *arg)
 #endif
     if (in_dpmi_pm())
       coopth_yield();
-    else
+    if (!in_dpmi_pm())		// re-check after coopth_yield()! not "else"
       break;
   }
 }
