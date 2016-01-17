13b5c5ffa931e6fb64fb211aec67b2c3986244d4
Stas Sergeev <stsp@users.sourceforge.net>
2016-01-18 01:26:58 +0300

    move ems and xms init funcs to iodev

--- src/emu.c
+++ src/emu.c
@@ -356,10 +356,7 @@ int main(int argc, char **argv)
       leavedos(0);
     }
 
-    ems_init();			/* initialize ems */
-    xms_init();			/* initialize xms */
     dpmi_setup();
-
     g_printf("EMULATE\n");
 
     fflush(stdout);
