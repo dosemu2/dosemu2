dosemu2/dosemu2

commit a1ae229f2601d797c60bbc1e6ed5e25d175ea9c6

Implement page fault exception handling in CPUEMU. For the simulator use sigsetjmp/siglongjmp; for the JIT lookup the corresponding eip and return similar to how divide by zero exceptions are handled. Refactored the CPUEMU page protection handling so it is in memory.c and only looks at protected memory areas.
git-svn-id: https://dosemu.svn.sourceforge.net/svnroot/dosemu/trunk@1981 7758b857-130e-0410-ab9c-d8735a528d18

src/emu-i386/simx86/sigsegv.c
@@ -36,6 +36,7 @@
 #include <stdlib.h>
 #include <stdarg.h>
 #include <string.h>
+#include <setjmp.h>
 #include "emu86.h"
 #include "codegen-arch.h"
 #include "trees.h"
@@ -406,6 +407,17 @@ badrw:
 /* this function is called from dosemu_fault */
 int e_emu_fault(struct sigcontext_struct *scp)
 {
+#ifdef __x86_64__
+  if (_trapno == 0x0e && _cr2 > 0xffffffff)
+#else
+  if (_trapno == 0x0e && _cr2 > getregister(esp))
+#endif
+  {
+    error("Accessing reserved memory at %08lx\n"
+	  "\tMaybe a null segment register\n",_cr2);
+    return 0;
+  }
+
   /* if config.cpuemu==3 (only vm86 emulated) then this function can
      be trapped from within DPMI, and we still must be prepared to
      reset permissions on code pages */
@@ -423,16 +435,7 @@ int e_emu_fault(struct sigcontext_struct *scp)
     }
   }
 
-  /*
-   * We are probably asked to stop the execution and run some int
-   * set up by the program... so it's a bad idea to just return back.
-   * Implemented solution:
-   *	for every "sensitive" instruction we store the current PC in
-   *	some place, let's say TheCPU.cr2. As we get the exception, we
-   *	use this address to return back to the interpreter loop, skipping
-   *	the rest of the sequence, and possibly with an error code in
-   *	TheCPU.err.
-   */
+  if (_trapno!=0x0e && _trapno != 0x00) return 0;
 
   if (_trapno==0x0e) {
 	if (Video->update_screen) {
@@ -444,8 +447,6 @@ int e_emu_fault(struct sigcontext_struct *scp)
 			}
 			/* VGAEMU may also access/protect the LFB */
 			if (e_vgaemu_fault(scp,pf >> 12) == 1) return 1;
-			if ((unsigned)(pf - vga.mem.graph_base) < 
-			    vga.mem.graph_size) goto verybad;
 		} else {
 			if(VGA_EMU_FAULT(scp,code,1)==True) {
 				dpmi_check_return(scp);
@@ -454,119 +455,60 @@ int e_emu_fault(struct sigcontext_struct *scp)
 		}
 	}
 
-#ifdef HOST_ARCH_X86
-    if (!CONFIG_CPUSIM) {
-
-        /* bit 0 = 1	page protect
-         * bit 1 = 1	writing
-         * bit 2 = 1	user mode
-         * bit 3 = 0	no reserved bit err
-         */
-#ifdef __x86_64__
-	if (_cr2 > 0xffffffff)
-#else
-	if (_cr2 > getregister(esp))
-#endif
-	{
-		error("Accessing reserved memory at %08lx\n"
-		      "\tMaybe a null segment register\n",_cr2);
-		goto verybad;
-	}
-	if (DPMIValidSelector(_cs) &&
-	    (unsigned char *)_cr2 >= ldt_buffer &&
-	    (unsigned char *)_cr2 < ldt_buffer + LDT_ENTRIES*LDT_ENTRY_SIZE)
-		/* LDT access emulation */
-		return 0;
-	if ((_err&0x0f)==0x07) {
-		unsigned int icr2 = _cr2 - TheCPU.mem_base;
-		unsigned char *p = (unsigned char *)_rip;
-		int codehit = 0;
-		register int v;
-		/* Got a fault in a write-protected memory page, that is,
-		 * a page _containing_code_. 99% of the time we are
-		 * hitting data or stack in the same page, NOT code.
-		 *
-		 * We assume that no other write protections exist and
-		 * that no other cause could return an error code of 7.
-		 * (this is a very weak assumption indeed)
-		 *
-		 * _cr2 keeps the address where the code tries to write
-		 * _rip keeps the address of the faulting instruction
-		 *	(in the code buffer or in the tree)
-		 *
-		 * Possible instructions we'll find here are (see above):
-		 *	8807	movb	%%al,(%%edi)
-		 *	(66)8907	mov{wl}	%%{e}ax,(%%edi)
-		 *	(f3)(66)a4,a5	movs
-		 *	(f3)(66)aa,ab	stos
+	if (CONFIG_CPUSIM && in_dpmi_emu) {
+		/* reflect DPMI page fault back to a DOSEMU crash or
+		   DPMI exception;
+		   vm86 faults will terminate DOSEMU via "return 0"
 		 */
-#ifdef PROFILE
-		PageFaults++;
-#endif
-		if (DPMIValidSelector(_cs))
-		    p = (unsigned char *) SEL_ADR(_cs, _rip);
-		if (debug_level('e') || (!InCompiledCode && !DPMIValidSelector(_cs))) {
-		    v = *((int *)p);
-		    __asm__("bswap %0" : "=r" (v) : "0" (v));
-		    e_printf("Faulting ops: %08x\n",v);
-
-		    if (!InCompiledCode) {
-			dbug_printf("*\tFault out of %scode, cs:eip=%x:%lx,"
-				    " cr2=%x, fault_cnt=%d\n",
-				    !DPMIValidSelector(_cs) ? "DOSEMU " : "",
-				    _cs, _rip, icr2, fault_cnt);
-		    }
-		    if (e_querymark(icr2)) {
-			e_printf("CODE node hit at %08x\n",icr2);
-		    }
-		    else if (InCompiledCode) {
-			e_printf("DATA node hit at %08x\n",icr2);
-		    }
-		}
-		/* the page is not unprotected here, the code
-		 * linked by Cpatch will do it */
-		/* ACH: we can set up a data patch for code
-		 * which has not yet been executed! */
-		if (InCompiledCode && !e_querymark(icr2) && Cpatch(scp))
-		    return 1;
-		/* We HAVE to invalidate all the code in the page
-		 * if the page is going to be unprotected */
-		InvalidateNodePage(icr2, 0, p, &codehit);
-		e_resetpagemarks(icr2, 1);
-		e_munprotect(icr2, 0);
-		/* now go back and perform the faulting op */
-		return 1;
+		TheCPU.err = EXCP0E_PAGE;
+		TheCPU.scp_err = _err;
+		TheCPU.cr2 = _cr2;
+		fault_cnt--;
+		siglongjmp(jmp_env, 0);
 	}
-    }
+
+#ifdef HOST_ARCH_X86
+	if (!CONFIG_CPUSIM && e_handle_pagefault(scp))
+		return 1;
 #endif
   }
+
 #ifdef HOST_ARCH_X86
-  else if (!CONFIG_CPUSIM && _trapno==0x00) {
+  /*
+   * We are probably asked to stop the execution and run some int
+   * set up by the program... so it's a bad idea to just return back.
+   * Implemented solution:
+   *	for every "sensitive" instruction we store the current PC in
+   *	some place, let's say TheCPU.cr2. As we get the exception, we
+   *	use this address to return back to the interpreter loop, skipping
+   *	the rest of the sequence, and possibly with an error code in
+   *	TheCPU.err.
+   *    For page faults the current PC is recovered from the tree.
+   */
+  if (!CONFIG_CPUSIM) {
 	if (InCompiledCode) {
-		TheCPU.err = EXCP00_DIVZ;
+		TheCPU.scp_err = _err;
 		/* save eip, eflags, and do a "ret" out of compiled code */
-		_eax = TheCPU.cr2;
-		_edx = _eflags;
+		if (_trapno == 0x00) {
+			TheCPU.err = EXCP00_DIVZ;
+			_eax = TheCPU.cr2;
+			_edx = _eflags;
+		} else {
+			TheCPU.err = EXCP0E_PAGE;
+			_eax = FindPC((unsigned char *)_rip);
+			e_printf("FindPC: found %x\n",_eax);
+			_edx = *(long *)_rsp; // flags
+			_rsp += sizeof(long);
+		}
+		TheCPU.cr2 = _cr2;
 		_rip = *(long *)_rsp;
 		_rsp += sizeof(long);
 		return 1;
 	}
+	return TryMemRef;
   }
-  return !CONFIG_CPUSIM && TryMemRef && _trapno != 0x0d;
-#else
-  return 0;
 #endif
-
-verybad:
-  /*
-   * Too bad, we have to give up.
-   */
-  dbug_printf("Unhandled CPUEMU exception %02x\n", _trapno);
-  flush_log();
-  if (in_vm86) in_vm86=0;	/* otherwise leavedos() complains */
-  fatalerr = 5;
-  leavedos(fatalerr);		/* shouldn't return */
-  return 1;
+  return 0;
 }
 
 /* ======================================================================= */

src/emu-i386/simx86/cpu-emu.c
@@ -89,6 +89,7 @@ int e_sigpa_count;
 
 int in_vm86_emu = 0;
 int in_dpmi_emu = 0;
+sigjmp_buf jmp_env;
 
 union SynCPU	TheCPU_union;
 
@@ -1286,7 +1287,13 @@ int e_dpmi(struct sigcontext_struct *scp)
     dt = (TheCPU.cs&4? LDT:GDT);
 
     /* ---- INNER LOOP: exit with error or code>0 (vm86 fault) ---- */
-    do {
+    if (CONFIG_CPUSIM && sigsetjmp(jmp_env, 1)) {
+      /* long jump to here from page fault */
+      xval = TheCPU.err;
+      return_addr = P0;
+      in_dpmi_emu = 0;
+    }
+    else do {
       /* switch to DPMI process */
       in_dpmi_emu = 1;
       e_printf("INTERP: enter=%08x mode=%04x\n",trans_addr,mode);

src/emu-i386/simx86/emu86.h
@@ -641,6 +641,9 @@ extern int eTimeCorrect;
 
 /////////////////////////////////////////////////////////////////////////////
 //
+extern unsigned int P0;
+extern sigjmp_buf jmp_env;
+extern int in_dpmi_emu;
 extern unsigned long eTSSMASK;
 extern int Running;		/* into interpreter loop */
 extern unsigned int mMaxMem;
@@ -671,6 +674,7 @@ int e_querymprotrange(unsigned int al, unsigned int ah);
 int e_markpage(unsigned int addr, size_t len);
 int e_querymark(unsigned int addr);
 void e_resetpagemarks(unsigned int addr, size_t len);
+int e_handle_pagefault(struct sigcontext_struct *scp);
 void mprot_init(void);
 void mprot_end(void);
 void InvalidateSegs(void);

src/emu-i386/simx86/interp.c
@@ -2901,10 +2901,10 @@ repag0:
 		    if (debug_level('e')>2) e_printf("  %s\n", ds);
 		}
 
-		P0 = PC;
 #ifndef SINGLESTEP
 		if (!(CEmuStat & CeS_TRAP)) continue;
 #endif
+		P0 = PC;
 		CloseAndExec(P0, NULL, mode, __LINE__);
 		e_printf("\n%s",e_print_regs());
 		NewNode = 0;

src/emu-i386/simx86/memory.c
@@ -43,6 +43,8 @@
 #include "dosemu_debug.h"
 #include "dlmalloc.h"
 #include "emu86.h"
+#include "codegen.h"
+#include "dpmi.h"
 
 #define CGRAN		4		/* 2^n */
 #define CGRMASK		(0xfffff>>CGRAN)
@@ -274,6 +276,83 @@ int e_check_munprotect(unsigned int addr)
 		return 0;
 	return e_munprotect(addr,0);
 }
+
+
+#ifdef HOST_ARCH_X86
+int e_handle_pagefault(struct sigcontext_struct *scp)
+{
+	int codehit;
+	register int v;
+	unsigned char *p;
+	unsigned int addr = _cr2 - TheCPU.mem_base;
+
+	/* _err:
+	 * bit 0 = 1	page protect
+	 * bit 1 = 1	writing
+	 * bit 2 = 1	user mode
+	 * bit 3 = 0	no reserved bit err
+	 */
+	if (!e_querymprot(addr) || (_err&0x0f) != 0x07) return 0;
+
+	/* Got a fault in a write-protected memory page, that is,
+	 * a page _containing_code_. 99% of the time we are
+	 * hitting data or stack in the same page, NOT code.
+	 *
+	 * We check using e_querymprot whether we protected the
+	 * page ourselves. Additionally an error code of 7 should
+	 * have been given.
+	 *
+	 * _cr2 keeps the address where the code tries to write
+	 * _rip keeps the address of the faulting instruction
+	 *	(in the code buffer or in the tree)
+	 *
+	 * Possible instructions we'll find here are (see sigsegv.v):
+	 *	8807	movb	%%al,(%%edi)
+	 *	(66)8907	mov{wl}	%%{e}ax,(%%edi)
+	 *	(f3)(66)a4,a5	movs
+	 *	(f3)(66)aa,ab	stos
+	 */
+#ifdef PROFILE
+	PageFaults++;
+#endif
+	if (DPMIValidSelector(_cs))
+		p = (unsigned char *) SEL_ADR(_cs, _rip);
+	else
+		p = (unsigned char *) _rip;
+	if (debug_level('e') || (!InCompiledCode && !DPMIValidSelector(_cs))) {
+		v = *((int *)p);
+		__asm__("bswap %0" : "=r" (v) : "0" (v));
+		e_printf("Faulting ops: %08x\n",v);
+
+		if (!InCompiledCode) {
+			dbug_printf("*\tFault out of %scode, cs:eip=%x:%lx,"
+				    " cr2=%x, fault_cnt=%d\n",
+				    !DPMIValidSelector(_cs) ? "DOSEMU " : "",
+				    _cs, _rip, addr, fault_cnt);
+		}
+		if (e_querymark(addr)) {
+			e_printf("CODE node hit at %08x\n",addr);
+		}
+		else if (InCompiledCode) {
+			e_printf("DATA node hit at %08x\n",addr);
+		}
+	}
+	/* the page is not unprotected here, the code
+	 * linked by Cpatch will do it */
+	/* ACH: we can set up a data patch for code
+	 * which has not yet been executed! */
+	if (InCompiledCode && !e_querymark(addr) && Cpatch(scp))
+		return 1;
+	/* We HAVE to invalidate all the code in the page
+	 * if the page is going to be unprotected */
+	codehit = 0;
+	InvalidateNodePage(addr, 0, p, &codehit);
+	e_resetpagemarks(addr, 1);
+	e_munprotect(addr, 0);
+	/* now go back and perform the faulting op */
+	return 1;
+}
+#endif
 
 /////////////////////////////////////////////////////////////////////////////
 

src/emu-i386/simx86/trees.c
@@ -628,6 +628,40 @@ quit:
 
 /////////////////////////////////////////////////////////////////////////////
 
+/*
+ * Given addr with translated code (from e.g., a fault) find the
+ * corresponding original PC. This is slow but it's only used for
+ * DPMI exceptions.
+ */
+unsigned int FindPC(unsigned char *addr)
+{
+  TNode *G = &CollectTree.root;
+  unsigned char *ahE;
+  Addr2Pc *AP;
+  unsigned int i;
+
+  for (;;) {
+      /* walk to next node */
+      NEXTNODE(G);
+      if (G == &CollectTree.root) break;
+      if (!G->addr || !G->pmeta || G->alive<=0) continue;
+      ahE = G->addr + G->len;
+      if (!ADDR_IN_RANGE(addr,G->addr,ahE)) continue;
+      e_printf("### FindPC: Found node %p->%p..%p", addr,G->addr,ahE);
+      AP = G->pmeta;
+      for (i=0; i<G->seqnum; i++) {
+	  e_printf("     %08x:%p",(G->key+AP->dnpc),G->addr+AP->daddr);
+	  if (addr < G->addr+AP->daddr) break;
+	  AP++;
+      }
+      e_printf("\nFindPC: PC=%x\n", G->key+(AP-1)->dnpc);
+      return G->key+(AP-1)->dnpc;
+  }
+  return 0;
+}
+
+/////////////////////////////////////////////////////////////////////////////
+
 #ifdef DEBUG_LINKER
 
 void CheckLinks (void)

src/emu-i386/simx86/trees.h
@@ -174,6 +174,7 @@ TNode *Move2Tree(void);
 void InitTrees(void);
 
 #ifdef HOST_ARCH_X86
+unsigned int FindPC(unsigned char *addr);
 int  FindCodeNode(int addr);
 int  InvalidateSingleNode (int addr, unsigned char *eip);
 int  InvalidateNodePage(int addr, int len, unsigned char *eip, int *codehit);

src/tests/test-i386.c
@@ -46,8 +46,6 @@ typedef unsigned long long uint64_t;
 #include <errno.h>
 #include <sys/mman.h>
 
-//#define TEST_PF 1
-
 #if !defined(__x86_64__)
 #define TEST_VM86
 #define TEST_SEGS
@@ -2039,7 +2037,6 @@ void test_exceptions(void)
     }
 #endif
 
-#ifdef TEST_PF /* CPUEMU can't do this yet */
     /* test SEGV reporting */
     printf("PF exception:\n");
     if (setjmp(jmp_env) == 0) {
@@ -2065,7 +2062,6 @@ void test_exceptions(void)
         v1 = *(char *)0x1234;
 #endif
     }
-#endif
 
     /* test illegal instruction reporting */
     printf("UD2 exception:\n");

commit 666345d77bba01a9ead5f8ac15303b6927f010b4

signal: ifdef the wrong sanity checks to x86_64-only
These checks were introduced by commit a1ae229f and were probably
just for debugging. However, the i386 part was entirely wrong and
caused crashes (fixed now in a61a4afa).

src/emu-i386/simx86/sigsegv.c
@@ -482,12 +482,14 @@ badrw:
 /* this function is called from dosemu_fault */
 int e_emu_fault(struct sigcontext *scp)
 {
+#ifdef __x86_64__
   if (_trapno == 0x0e && _cr2 > 0xffffffff)
   {
     error("Accessing reserved memory at %08lx\n"
 	  "\tMaybe a null segment register\n",_cr2);
     return 0;
   }
+#endif
 
   /* if config.cpuemu==3 (only vm86 emulated) then this function can
      be trapped from within DPMI, and we still must be prepared to