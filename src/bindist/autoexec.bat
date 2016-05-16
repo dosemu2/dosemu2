be546b567d726a69ce58e951affc80aaba5bd7db
Stas Sergeev <stsp@users.sourceforge.net>
2016-05-17 00:06:52 +0300

    switch to new commands: lredir2 and system.com

--- src/bindist/autoexec.bat
+++ src/bindist/autoexec.bat
@@ -5,17 +5,17 @@ set HELPPATH=z:\help
 set TEMP=c:\tmp
 sound /e
 prompt $P$G
-unix -s DOSDRIVE_D
+system -s DOSDRIVE_D
 if "%DOSDRIVE_D%" == "" goto nodrived
-lredir del d: > nul
-lredir d: linux\fs%DOSDRIVE_D%
+lredir2 -d d: > nul
+lredir2 d: linux\fs%DOSDRIVE_D%
 :nodrived
 rem uncomment to load another bitmap font
 rem lh display con=(vga,437,2)
 rem mode con codepage prepare=((850) z:\cpi\ega.cpx)
 rem mode con codepage select 850
 rem chcp 850
-lredir linux\fs/media/cdrom c
-unix -s DOSEMU_VERSION
+lredir2 -nC linux\fs/media/cdrom
+system -s DOSEMU_VERSION
 echo "Welcome to dosemu %DOSEMU_VERSION%!"
 system -e
