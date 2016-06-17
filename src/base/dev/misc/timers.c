3e956665fd3fdb736925df034df21025aa782011
Stas Sergeev <stsp@users.sourceforge.net>
2016-06-17 15:24:10 +0300

    speaker: avoid duplicate beeps again [#195]
    Dont call do_sound() when latch2 is written.
    We need to wait for GATE2 pulse instead.

--- src/base/dev/misc/timers.c
+++ src/base/dev/misc/timers.c
@@ -428,9 +428,11 @@ void pit_outp(ioport_t port, Bit8u val)
 	       pit[0].cntr, PIT_TICK_RATE/(double)pit[0].cntr, 10000.0/timer_div);
 #endif
     }
+#if 0
     else if (port == 2 && config.speaker == SPKR_EMULATED) {
       do_sound(pit[2].write_latch & 0xffff);
     }
+#endif
   }
 }
 
