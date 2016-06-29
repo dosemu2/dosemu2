dosemu2/dosemu2

commit 1820c4feb1ceb058c02c475be1533cbfc79cf4f7

use vm86plus for now as otherwise dosdebug doesn't work

src/emu-i386/do_vm86.c
@@ -295,6 +295,7 @@ static int handle_GP_hlt(void)
 }
 
 #ifdef __i386__
+#if 0
 static int true_vm86(struct vm86_struct *x)
 {
     int ret;
@@ -306,6 +307,19 @@ static int true_vm86(struct vm86_struct *x)
     loadregister(gs, gs);
     return ret;
 }
+#else
+static int true_vm86(struct vm86_struct *x)
+{
+    static struct vm86plus_struct p;		// static to not do memset
+    int ret;
+
+    /* need to use vm86_plus for now as otherwise dosdebug doesn't work */
+    memcpy(&p, x, sizeof(*x));
+    ret = vm86_plus(VM86_ENTER, &p);
+    memcpy(x, &p, sizeof(*x));
+    return ret;
+}
+#endif
 #endif
 
 static int do_vm86(struct vm86_struct *x)

commit 1c3dbd5434c6915dfc4ef805b6ca168e913bc7ff

fix vm86 support broken by 1820c4f [fixes #130]
Doing memcpy() produces the race where the signal handler sees
the old image of realmode registers.
We need to have a single image used by both the syscall and signal
handler.

src/emu-i386/do_vm86.c
@@ -409,24 +409,21 @@ static int true_vm86(struct vm86_struct *x)
     return ret;
 }
 #else
-static int true_vm86(struct vm86_struct *x)
+static int true_vm86(union vm86_union *x)
 {
-    static struct vm86plus_struct p;		// static to not do memset
     int ret;
 
     /* need to use vm86_plus for now as otherwise dosdebug doesn't work */
-    memcpy(&p, x, sizeof(*x));
-    ret = vm86_plus(VM86_ENTER, &p);
-    memcpy(x, &p, sizeof(*x));
+    ret = vm86_plus(VM86_ENTER, &x->vm86compat);
     return ret;
 }
 #endif
 #endif
 
-static int do_vm86(struct vm86_struct *x)
+static int do_vm86(union vm86_union *x)
 {
     if (config.cpu_vm == CPUVM_KVM)
-	return kvm_vm86(x);
+	return kvm_vm86(&x->vm86ps);
 #ifdef __i386__
 #ifdef X86_EMULATOR
     if (config.cpu_vm == CPUVM_EMU)
@@ -449,7 +446,7 @@ static void _do_vm86(void)
     loadfpstate(vm86_fpu_state);
     in_vm86 = 1;
 again:
-    retval = do_vm86(&vm86s);
+    retval = do_vm86(&vm86u);
     vtype = VM86_TYPE(retval);
     /* optimize VM86_STI case that can return with ints disabled
      * if VIP is set */

src/include/emu.h
@@ -68,6 +68,7 @@ union vm86_union
   unsigned char b[sizeof(struct vm86_struct)];
   unsigned short w[sizeof(struct vm86_struct)/2];
   unsigned int d[sizeof(struct vm86_struct)/4];
+  struct vm86plus_struct vm86compat;
 };
 
 extern union vm86_union vm86u;