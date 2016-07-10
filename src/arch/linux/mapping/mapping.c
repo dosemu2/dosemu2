4e41b70d81843c5bb6cf64c11669fccee8ab115a
Stas Sergeev <stsp@users.sourceforge.net>
2016-07-10 18:54:45 +0300

    Revert "Don't use the alias map for vgaemu memory."
    This reverts commit 4823138a97c5b33c77f3c5b1784ec2fac8903810.
    
    This hack seems no longer needed.
    
    Conflicts:
    	src/arch/linux/mapping/mapping.c
    	src/emu-i386/simx86/cpatch.c

--- src/arch/linux/mapping/mapping.c
+++ src/arch/linux/mapping/mapping.c
@@ -63,18 +63,14 @@ static struct mappingdrivers *mappingdrv[] = {
 static struct mappingdrivers *mappingdriver;
 
 /* The alias map is used to track alias mappings from the first 1MB + HMA
-   to the corresponding addresses in Linux address space (either lowmem
-   or EMS). The DOS address (&mem_base[address]) may be r/w
-   protected by cpuemu, but the alias is never protected,
+   to the corresponding addresses in Linux address space (either lowmem,
+   vgaemu, or EMS). The DOS address (&mem_base[address]) may be r/w
+   protected by cpuemu or vgaemu, but the alias is never protected,
    so it can be used to write without needing to unprotect and reprotect
    afterwards.
    If the alias is not used (hardware RAM from /dev/mem, or DPMI memory
    (aliasing using fn 0x509 is safely ignored here)),
    the address is identity-mapped to &mem_base[address].
-
-   The alias is also not used for vgaemu memory to allow special traps to occur
-   in the C patches for cpuemu (LINEAR2UNIX in cpatch.c).
-   This should be cleaned up.
 */
 static unsigned char *aliasmap[(LOWMEM_SIZE+HMASIZE)/PAGE_SIZE];
 
@@ -217,7 +213,7 @@ void *alias_mapping(int cap, unsigned targ, size_t mapsize, int protect, void *s
   addr = mappingdriver->alias(cap, target, mapsize, protect, source);
   if (addr == MAP_FAILED)
     return addr;
-  update_aliasmap(addr, mapsize, (cap & MAPPING_VGAEMU) ? target : source);
+  update_aliasmap(target, mapsize, source);
   if (config.cpu_vm == CPUVM_KVM)
     mprotect_kvm(addr, mapsize, protect);
   Q__printf("MAPPING: %s alias created at %p\n", cap, addr);
