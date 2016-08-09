dosemu2/dosemu2

commit 619a60d0fb2c11f17ba4e0f10234080c625d1def

use single io_bitmap array [#3]
simx86 had its own io_bitmap array, never synced with the one
of dosemu.

src/include/cpu-emu.h
@@ -4,14 +4,8 @@
 #include <signal.h>
 #include "bitops.h"
 
-/*
- * Size of io_bitmap in longwords: 2048 is ports 0-0xffff.
- */
-#define IO_BITMAP_SIZE	2048
-
-extern unsigned long	io_bitmap[IO_BITMAP_SIZE+1];
 extern void e_priv_iopl(int);
-#define test_ioperm(a)	(test_bit((a),io_bitmap))
+#define test_ioperm(a)	(test_bit((a),emu_io_bitmap))
 
 /* ------------------------------------------------------------------------
  * if this is defined, cpuemu will only start when explicitly activated

src/emu-i386/ports.c
@@ -55,7 +55,7 @@ unsigned char port_handle_table[0x10000];
 unsigned char port_andmask[0x10000];
 unsigned char port_ormask[0x10000];
 static unsigned char portfast_map[0x10000/8];
-static unsigned char emu_io_bitmap[0x10000/8];
+unsigned char emu_io_bitmap[0x10000/8];
 static pid_t portserver_pid = 0;
 
 static unsigned char port_handles;	/* number of io_handler's */
@@ -790,9 +790,6 @@ int port_init(void)
 	  port_handler[i].irq = EMU_NO_IRQ;
 	  port_handler[i].fd = -1;
 	}
-#ifdef X86_EMULATOR
-	memset (io_bitmap, 0, sizeof(io_bitmap));
-#endif
 
   /* handle 0 maps to the unmapped IO device handler.  Basically any
      ports which don't map to any other device get mapped to this

src/emu-i386/simx86/cpu-emu.c
@@ -157,8 +157,6 @@ FILE *aLog = NULL;
  */
 struct sigcontext e_scp; /* initialized to 0 */
 
-unsigned long io_bitmap[IO_BITMAP_SIZE+1];
-
 /* ======================================================================= */
 
 unsigned long eTSSMASK = 0;

src/include/port.h
@@ -126,6 +126,8 @@ extern int port_rep_outd(ioport_t port, Bit32u *dest, int df, Bit32u count);
 
 extern void port_exit(void);
 
+extern unsigned char emu_io_bitmap[];
+
 /* avoid potential clashes with <sys/io.h> */
 #undef inb
 #undef inw

commit ac3f59b0fd0d1412428147a31c223bbcdb161455

Fix direct VGA regression (#222)
Console graphics was broken in commits 619a60d0fb2 (cpuemu only),
c43732e99, 4354909, and 43453825.

This is a minimal patch to fix those regressions.
* only initialize vgaemu if !config.vga (no console graphics).
* don't set vbase to -1 when graphics are unmapped. When switching
  otherwise it is lost forever.
* the io bitmap change for cpuemu (619a60d0fb2) activated some code
  controlled by CPUEMU_DIRECT_IO which does not work properly here
  (because the same i/o instruction writes to port 0 and VGA ports).

src/arch/linux/mapping/mapping.c
@@ -624,7 +624,6 @@ int unmap_hardware_ram(char type, int cap)
     }
     g_printf("unmapped hardware ram at 0x%08zx .. 0x%08zx at %#x\n",
 	     hw->base, hw->base+hw->size-1, hw->vbase);
-    hw->vbase = -1;
   }
   return 0;
 }

src/base/video/video.c
@@ -403,8 +403,10 @@ void video_post_init(void)
     }
   }
 
-  vga_emu_pre_init();
-  render_init();
+  if (!config.vga) {
+    vga_emu_pre_init();
+    render_init();
+  }
 
   if (Video && Video->init)
     Video->init();

src/include/cpu-emu.h
@@ -38,7 +38,7 @@ extern void e_priv_iopl(int);
  * untrapped port will be allowed to compile. This is not 100% safe
  * since DX can dynamically change.
  */
-#if 1
+#if 0
 #define CPUEMU_DIRECT_IO
 #endif
 