d5f7ff12669d7a7f72f243eb3f96af5941328a5b
Julius Schwartzenberg <julius.schwartzenberg@gmail.com>
2016-09-19 22:59:25 +0200

    WIP: Add support for MS-DOS v6.22

--- src/bindist/autoemu.bat
+++ src/bindist/autoemu.bat
@@ -1,20 +1,21 @@
 @echo off
-rem generic autoexec.bat for DOSEMU + any DOS
-rem must be edited in most circumstances
-path c:\dos;c:\windows\command;e:\dosemu
-lredir d: linux\fs\${DOSEMU_LIB_DIR}/freedos ro
+set PATH=d:\dos;e:\dosemu
 set TEMP=c:\tmp
 sound /e
 prompt $P$G
-unix -s DOSDRIVE_EXTRA
+system -s CDROM_PATH
+if "%CDROM_PATH%" == "" goto nocdrom
+lredir2 -nC linux\fs%CDROM_PATH%
+:nocdrom
+system -s DOSDRIVE_EXTRA
 if "%DOSDRIVE_EXTRA%" == "" goto nodrived
 lredir2 -n linux\fs%DOSDRIVE_EXTRA%
 :nodrived
-rem uncomment to load another bitmap font
-rem mode con codepage prepare=((850) c:\dos\ega.cpi)
-rem mode con codepage select 850
-rem chcp 850
-lredir2 -nC linux\fs/media/cdrom
-unix -s DOSEMU_VERSION
-echo "Welcome to dosemu2 %DOSEMU_VERSION%!"
+mode con codepage prepare=((850) d:\dos\ega.cpi)
+mode con codepage select=850
+LH nlsfunc
+chcp 850
+echo Welcome to dosemu2!
+system -s DOSEMU_VERSION
+echo     Build %DOSEMU_VERSION%
 system -e
