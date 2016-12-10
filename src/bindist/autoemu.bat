e025d792644dc0a4a1ce3a4ed33cd7a58dd8db17
Julius Schwartzenberg <julius.schwartzenberg@gmail.com>
2016-12-10 13:10:54 +0100

    Prevent problems when using paths containing spaces. This allows -cdrom to work when it points to a path with spaces.

--- src/bindist/autoemu.bat
+++ src/bindist/autoemu.bat
@@ -5,11 +5,11 @@ sound /e
 prompt $P$G
 system -s CDROM_PATH
 if "%CDROM_PATH%" == "" goto nocdrom
-lredir2 -nC linux\fs%CDROM_PATH%
+lredir2 -nC "linux\fs%CDROM_PATH%"
 :nocdrom
 system -s DOSDRIVE_EXTRA
 if "%DOSDRIVE_EXTRA%" == "" goto nodrived
-lredir2 -n linux\fs%DOSDRIVE_EXTRA%
+lredir2 -n "linux\fs%DOSDRIVE_EXTRA%"
 :nodrived
 mode con codepage prepare=((850) d:\dos\ega.cpi)
 mode con codepage select=850
