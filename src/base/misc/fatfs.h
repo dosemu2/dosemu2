a6ff2dd0dc10425b757c42b0acdfa2f14538d8ac
Andrew Bird <ajb@spheresystems.co.uk>
2017-01-10 16:53:52 +0000

    disks: Remove forgotten prototypes
    These should have been removed as part of the culling of fatfs_boot.S

--- src/base/misc/fatfs.h
+++ src/base/misc/fatfs.h
@@ -74,10 +74,4 @@ int fatfs_write(fatfs_t *, unsigned, unsigned, int);
 int fatfs_is_bootable(const fatfs_t *);
 int fatfs_get_part_type(const fatfs_t *);
 
-/* boot sector */
-extern const unsigned char boot_prog[];
-extern const unsigned char boot_prog_end[];
-extern unsigned char boot_txfr[];
-extern unsigned char boot_message[];
-
 #endif
