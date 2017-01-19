da3bf4b7cf5ec561ba2ea1ef121f50e25d7e28ea
Stas Sergeev <stsp@users.sourceforge.net>
2017-01-19 14:38:44 +0300

    video: add fall-backs from sdl to X or slang

--- src/base/video/video.c
+++ src/base/video/video.c
@@ -197,7 +197,7 @@ static void init_video_none(void)
  */
 static int video_init(void)
 {
-  if ((config.vga == -1 || config.console_video == -1) && using_kms())
+  if (!config.term && using_kms())
   {
     config.vga = config.console_video = config.mapped_bios = config.pci_video = 0;
     warn("KMS detected: using SDL mode.\n");
@@ -412,24 +412,49 @@ void video_post_init(void)
     int err = Video->init();
     if (err) {
       if (config.sdl) {
-        /* silly fall-back from SDL to X
-         * Can work because X does not have priv_init */
+        /* silly fall-back from SDL to X or slang.
+         * Can work because X/slang do not have priv_init */
         config.sdl = 0;
-        Video = video_get("X");
-        if (Video) {
-          config.X = 1;
-          config.mouse.type = MOUSE_X;
-          err = Video->init();
-          if (err) {
-            error("Unable to initialize X and SDL video\n");
-            leavedos(3);
+        if (using_kms()) {
+#ifdef USE_SLANG
+          load_plugin("term");
+          Video = video_get("term");
+          config.term = 1;
+#endif
+          if (!Video)
+            init_video_none();
+          if (Video) {
+            err = Video->init();
+            if (err) {
+              error("Unable to initialize SDL and terminal video\n");
+              leavedos(3);
+            }
           }
         }
+#ifdef X_SUPPORT
+        else {
+          load_plugin("X");
+          Video = video_get("X");
+          if (Video) {
+            config.X = 1;
+            config.mouse.type = MOUSE_X;
+            err = Video->init();
+            if (err) {
+              error("Unable to initialize X and SDL video\n");
+              leavedos(3);
+            }
+          }
+        }
+#endif
       } else {
         error("Unable to initialize video subsystem %s\n", Video->name);
         leavedos(3);
       }
     }
+  }
+  if (!Video) {
+    error("Unable to initialize video subsystem\n");
+    leavedos(3);
   }
 }
 
