0d863ab6b6b1b02c76fac9b77865a2a2ed357f2c
Stas Sergeev <stsp@users.sourceforge.net>
2017-03-05 18:13:49 +0300

    fix build regression [fixes #324]

---
+++ git-rev.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+SYM=`git rev-parse --symbolic-full-name HEAD`
+REV=`git rev-parse --git-path $SYM`
+if [ "${REV:0:1}" != "/" ]; then
+    CDUP=`git rev-parse --show-cdup`
+    if [ -n "$CDUP" ]; then
+	REV=$CDUP$REV
+    fi
+fi
+echo $REV
