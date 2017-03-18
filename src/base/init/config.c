dosemu2/dosemu2

commit 386549e36fc3c7bc72ab2c67cda31ad047322470

vc: move console switching code to console plugin

src/base/video/video.c
@@ -494,7 +494,8 @@ int on_console(void)
 }
 
 void
-vt_activate(int con_num)
+vt_activate(int num)
 {
-    ioctl(console_fd, VT_ACTIVATE, con_num);
+    if (Video && Video->vt_activate)
+        Video->vt_activate(num);
 }

src/include/video.h
@@ -76,6 +76,7 @@ struct video_system {
    void (*handle_events)(void);    /* event handler (not strictly video but
                                   related, optional) */
    const char *name;
+   void (*vt_activate)(int num);
 };
 
 extern struct video_system *Video;

src/plugin/console/console.c
@@ -178,6 +178,11 @@ static void console_close(void)
   fprintf(stdout,"\033[?25h\r");      /* Turn on the cursor */
 }
 
+static void console_switch(int num)
+{
+  ioctl(console_fd, VT_ACTIVATE, num);
+}
+
 #define console_setmode NULL
 
 static struct video_system Video_console = {
@@ -190,7 +195,8 @@ static struct video_system Video_console = {
    console_update_screen,
    NULL,
    NULL,              /* handle_events */
-   "console"
+   "console",
+   console_switch,
 };
 
 CONSTRUCTOR(static void init(void))

src/plugin/console/vga.c
@@ -668,6 +668,11 @@ static void vga_close(void)
   sem_destroy(&cpy_sem);
 }
 
+static void vga_vt_activate(int num)
+{
+  Video_console->vt_activate(num);
+}
+
 static struct video_system Video_graphics = {
    vga_initialize,
    vga_init,
@@ -678,7 +683,8 @@ static struct video_system Video_graphics = {
    NULL,             /* update_screen */
    NULL,
    NULL,
-   .name = "graphics"
+   .name = "graphics",
+   vga_vt_activate,
 };
 
 /* init_vga_card - Initialize a VGA-card */

commit d60afe95e55087fb8860ca661e6695418d7296dd

kbd: fix raw mode with video term under console
It was broken by commit 386549e3.

src/base/init/config.c
@@ -599,6 +599,7 @@ static void config_post_process(void)
 #endif
 	}
     }
+#ifdef USE_CONSOLE_PLUGIN
     if (on_console()) {
 	if (!can_do_root_stuff && config.console_video) {
 	    /* force use of Slang-terminal on console too */
@@ -611,7 +612,9 @@ static void config_post_process(void)
 	    register_speaker((void *)(uintptr_t)console_fd,
 			     console_speaker_on, console_speaker_off);
 	}
-    } else {
+    } else
+#endif
+    {
 	if (config.console_keyb == -1) {
 	    config.console_keyb =
 #ifdef USE_SLANG

src/base/video/video.c
@@ -21,13 +21,14 @@
 #include "bios.h"
 #include "port.h"
 #include "memory.h"
-#include "video.h"
 #include "render.h"
 #include "vgaemu.h"
 #include "vc.h"
 #include "mapping.h"
 #include "utilities.h"
 #include "pci.h"
+#include "keyb_clients.h"
+#include "video.h"
 
 struct video_system *Video = NULL;
 #define MAX_VID_CLIENTS 16
@@ -208,8 +209,8 @@ static int video_init(void)
 #endif
   }
 
-#if defined(USE_DL_PLUGINS)
-  if (!config.X)
+#ifdef USE_CONSOLE_PLUGIN
+  if (config.console_video || config.console_keyb == KEYB_RAW)
     load_plugin("console");
 #endif
   /* figure out which video front end we are to use */
@@ -496,6 +497,9 @@ int on_console(void)
 void
 vt_activate(int num)
 {
-    if (Video && Video->vt_activate)
-        Video->vt_activate(num);
+    struct video_system *v = video_get("console");
+    if (v && v->vt_activate)
+	v->vt_activate(num);
+    else
+	error("VID: Console plugin unavailable\n");
 }

src/plugin/console/config/plugin_config.h
@@ -1,0 +1,1 @@
+#define USE_CONSOLE_PLUGIN 1

src/plugin/kbd_unicode/dosemu_keys.c
@@ -16,7 +16,7 @@
 
 static int switch_to_console(int vc_num)
 {
-	if (config.console_video) {
+	if (config.console_keyb == KEYB_RAW || config.console_video) {
 		t_shiftstate shiftstate = get_modifiers_r(input_keyboard_state.shiftstate);
 		k_printf("KBD: switching to console #%d\n",vc_num);
 		shiftstate &= ~(MODIFIER_ALT|MODIFIER_CTRL);