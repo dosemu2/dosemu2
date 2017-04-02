0846186c4f88d03cfa81b486f3ab14a3097f2c8d
Stas Sergeev <stsp@users.sourceforge.net>
2017-04-02 19:58:14 +0300

    remove console_video check before install_dos()

--- src/base/async/int.c
+++ src/base/async/int.c
@@ -277,8 +277,7 @@ int dos_helper(void)
     }
 
   case DOS_HELPER_SHOW_BANNER:		/* show banner */
-    if (!config.console_video)
-      install_dos();
+    install_dos();
     if (!config.dosbanner)
       break;
     p_dos_str(PACKAGE_NAME " "VERSTR "\nConfigured: " CONFIG_TIME "\n");
