4f41b09c2915685943da894f02f0f0a0349e43e6
Stas Sergeev <stsp@users.sourceforge.net>
2017-04-20 22:24:35 +0300

    build: add some support for old git [fixes #356]
    Old git doesn't support worktrees.

--- git-rev.sh
+++ git-rev.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 SYM=`git rev-parse --symbolic-full-name HEAD`
-REV=`git rev-parse --git-path $SYM`
+GIT_MAJ=`git --version | cut -d "." -f 1 | cut -d " " -f 3`
+if [ $GIT_MAJ -lt 2 ]; then
+    echo "git version too old" >&2
+    REV=".git/$SYM"
+else
+    REV=`git rev-parse --git-path $SYM`
+fi
 if [ "${REV:0:1}" != "/" ]; then
     CDUP=`git rev-parse --show-cdup`
     if [ -n "$CDUP" ]; then
