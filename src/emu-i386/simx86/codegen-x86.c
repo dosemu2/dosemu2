dosemu2/dosemu2

commit f24a0266e91497ab0c118e76ba171c3d65ec9049

various: stop using dpmi_base [#385]

src/emu-i386/simx86/codegen-x86.c
@@ -110,7 +110,7 @@
 #include <string.h>
 #include "emu86.h"
 #include "dlmalloc.h"
-
+#include "mapping.h"
 #ifdef HOST_ARCH_X86
 #include "codegen-x86.h"
 
@@ -162,7 +162,7 @@ static void _test_(void)
 static int goodmemref(unsigned int m)
 {
 	if (m < 0x110000) return 1;
-	if (m >= config.dpmi_base && m <= mMaxMem) return 1;
+	if (mapping_find_hole(m, mMaxMem, 1) == MAP_FAILED) return 1;
 	return 0;
 }
 

src/base/dev/vga/vgaemu.c
@@ -1025,8 +1025,7 @@ int vga_emu_fault(struct sigcontext *scp, int pmode)
     cs_ip = SEL_ADR_CLNT(_cs, _eip, dpmi_mhp_get_selector_size(_cs));
     if (
 	  (cs_ip >= &mem_base[0] && cs_ip < &mem_base[0x110000]) ||
-	  (cs_ip > MEM_BASE32(config.dpmi_base) &&
-	   cs_ip + 20 < MEM_BASE32(config.dpmi_base) + config.dpmi * 1024 &&
+	  ((mapping_find_hole((uintptr_t)cs_ip, (uintptr_t)cs_ip + 20, 1) == MAP_FAILED) &&
 	   dpmi_is_valid_range(daddr, 15)))
      vga_deb_map(
       "vga_emu_fault: cs:eip = %04x:%04x, instr: %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x\n",

src/base/misc/dump.c
@@ -13,6 +13,7 @@
 #include "dpmi.h"
 #include "msdos_ex.h"
 #include "int.h"
+#include "mapping.h"
 #ifdef X86_EMULATOR
 #include "cpu-emu.h"
 #endif
@@ -220,8 +221,7 @@ char *DPMI_show_state(struct sigcontext *scp)
       pos += sprintf(buf + pos, "OPS  : ");
       if (!(_cs & 0x0004) ||
 	  (csp2 >= &mem_base[0] && csp2 + 20 < &mem_base[0x110000]) ||
-	  (csp2 > MEM_BASE32(config.dpmi_base) &&
-	   csp2 + 20 < MEM_BASE32(config.dpmi_base) + config.dpmi * 1024 &&
+	  ((mapping_find_hole((uintptr_t)csp2, (uintptr_t)csp2 + 20, 1) == MAP_FAILED) &&
 	   dpmi_is_valid_range(daddr - 10, 20))) {
 	for (i = 0; i < 10; i++)
 	  pos += sprintf(buf + pos, "%02x ", *csp2++);
@@ -244,8 +244,7 @@ char *DPMI_show_state(struct sigcontext *scp)
       }
       pos += sprintf(buf + pos, "STACK: ");
       if ((ssp2 >= &mem_base[0] && ssp2 + 20 < &mem_base[0x110000]) ||
-	  (ssp2 > MEM_BASE32(config.dpmi_base) &&
-	   ssp2 + 20 < MEM_BASE32(config.dpmi_base) + config.dpmi * 1024 &&
+	  ((mapping_find_hole((uintptr_t)ssp2, (uintptr_t)ssp2 + 20, 1) == MAP_FAILED) &&
 	   dpmi_is_valid_range(saddr - 10, 20))) {
 	for (i = 0; i < 10; i++)
 	  pos += sprintf(buf + pos, "%02x ", *ssp2++);

src/emu-i386/simx86/cpu-emu.c
@@ -46,6 +46,7 @@
 #include "emu86.h"
 #include "codegen-arch.h"
 #include "dpmi.h"
+#include "mapping.h"
 #include "dis8086.h"
 #include "sig.h"
 
@@ -251,9 +252,7 @@ char *e_print_regs(void)
 		int i;
 		unsigned char *st = MEM_BASE32(LONG_SS+TheCPU.esp);
 		if ((st >= mem_base && st < (unsigned char *)MEM_BASE32(0x110000)) ||
-		    (st > MEM_BASE32(config.dpmi_base) &&
-		     st <= MEM_BASE32(config.dpmi_base) +
-		     config.dpmi * 1024)) {
+		    (mapping_find_hole((uintptr_t)st, (uintptr_t)st + 4094, 1) == MAP_FAILED)) {
 			unsigned short *stk = (unsigned short *)st;
 			for (i=(ERB_L5+ERB_LEFTM); i<(ERB_L6-2); i+=5) {
 			   exprintw(*stk++,buf,i);

src/env/builtins/commands.c
@@ -69,10 +69,12 @@ int dpmi_main(int argc, char **argv)
 		else
 			com_printf("%7s%n", "off", &len);
 		com_printf("%*s| -m | DPMI memory size in Kbytes      |\n", 11-len, "");
+#if 0
 		if (config.dpmi_base == -1)
 			com_printf("|$_dpmi_base               |    auto   | -b | Address of the DPMI memory pool |\n");
 		else
 			com_printf("|$_dpmi_base               |0x%08" PRIxPTR " | -b | Address of the DPMI memory pool |\n", config.dpmi_base);
+#endif
 		com_printf("|$_pm_dos_api              |    %s    | -p | Protected mode DOS API support  |\n",
 			    config.pm_dos_api ? "on " : "off");
 		com_printf("|$_ignore_djgpp_null_derefs|    %s    | -n | Disable DJGPP NULL-deref protec.|\n",
@@ -84,18 +86,20 @@ int dpmi_main(int argc, char **argv)
 		int c = 0;
 		optind = 0;
 		while (c != -1) {
-		    c = getopt(argc, argv, "m:b:p:n:t:");
+		    c = getopt(argc, argv, "m:p:n:t:");
 		    switch (c) {
 			case 'm':
 			    if (optarg) {
 				config.dpmi = strtoll(optarg, NULL, 0);
 			    }
 			    break;
+#if 0
 			case 'b':
 			    if (optarg) {
 				config.dpmi_base = strtoll(optarg, NULL, 0);
 			    }
 			    break;
+#endif
 			case 'p':
 			    if (optarg) {
 				if (strequalDOS(optarg, "ON"))

commit 0ce451d1b53528aae1f59322ea181755fdc9fb59

simx86: fix goodmemref() broken by f24a0266 [fixes #409]

src/emu-i386/simx86/codegen-x86.c
@@ -162,7 +162,9 @@ static void _test_(void)
 static int goodmemref(unsigned int m)
 {
 	if (m < 0x110000) return 1;
-	if (mapping_find_hole(m, mMaxMem, 1) == MAP_FAILED) return 1;
+	if (mapping_find_hole((uintptr_t)MEM_BASE32(m),
+			(uintptr_t)MEM_BASE32(mMaxMem), 1) == MAP_FAILED)
+		return 1;
 	return 0;
 }
 