dosemu2/dosemu2

commit 5f00cc2d91294e67eed2eb38914dfaaabb2186b9

signal: unblock async signals before calling instremu [fixes #477]
instremu needs them to interrupt the long runs.
We can safely unblock them even in presence of sas wa, as
the threat is only that the signal can come after the stack
switch but before disabling sas. But for instremu we do not
do the stack switch, we only do it for dpmi_fault().

src/arch/linux/async/signal.c
@@ -1166,3 +1166,15 @@ void signal_set_altstack(stack_t *stk)
   stk->ss_flags = SS_ONSTACK | SS_AUTODISARM;
 #endif
 }
+
+void signal_unblock_async_sigs(void)
+{
+  /* unblock only nonfatal, fatals should already be unblocked */
+  sigprocmask(SIG_UNBLOCK, &nonfatal_q_mask, NULL);
+}
+
+void signal_restore_async_sigs(void)
+{
+  if (need_sas_wa)
+    sigprocmask(SIG_BLOCK, &nonfatal_q_mask, NULL);
+}

src/arch/linux/async/sigsegv.c
@@ -76,8 +76,13 @@ static int dosemu_fault1(int signal, struct sigcontext *scp)
 #endif
 
   if (in_vm86) {
-    if ( _trapno == 0x0e && VGA_EMU_FAULT(scp, code, 0) == True)
-      return 0;
+    if ( _trapno == 0x0e) {
+      /* we can get to instremu from here, so unblock SIGALRM & friends.
+       * It is needed to interrupt instremu when it runs for too long. */
+      signal_unblock_async_sigs();
+      if (VGA_EMU_FAULT(scp, code, 0) == True)
+        return 0;
+    }
     return vm86_fault(scp);
   }
 
@@ -94,6 +99,7 @@ static int dosemu_fault1(int signal, struct sigcontext *scp)
  *  but if only the protection needs to be adjusted (no instructions emulated)
  *  we should be able to handle it in DOSEMU
  */
+    signal_unblock_async_sigs();
     if(VGA_EMU_FAULT(scp,code,1)==True) {
       v_printf("BUG: dosemu touched protected video mem, but trying to recover\n");
       return 0;
@@ -112,8 +118,13 @@ static int dosemu_fault1(int signal, struct sigcontext *scp)
       goto bad;	/* well, this goto is unnecessary but I like gotos:) */
     } /*!DPMIValidSelector(_cs)*/
     else {
-      if (_trapno == 0x0e && VGA_EMU_FAULT(scp, code, 1) == True)
-        return dpmi_check_return(scp);
+      if (_trapno == 0x0e) {
+        signal_unblock_async_sigs();
+        if (VGA_EMU_FAULT(scp, code, 1) == True)
+          return dpmi_check_return(scp);
+        /* going for dpmi_fault(), careful with async signals and sas_wa */
+        signal_restore_async_sigs();
+      }
       /* Not in dosemu code: dpmi_fault() will handle that */
       return dpmi_fault(scp);
     }

src/include/sig.h
@@ -54,6 +54,8 @@ extern void signal_switch_to_dpmi(void);
 extern void signal_return_to_dosemu(void);
 extern void signal_return_to_dpmi(void);
 extern void signal_set_altstack(stack_t *stk);
+extern void signal_unblock_async_sigs(void);
+extern void signal_restore_async_sigs(void);
 
 extern pthread_t dosemu_pthread_self;
 

commit ebdc6e9b9f5a2771f75092bc6791eb0fe0e7b97c

signal: fix race introduced by 5f00cc2
We need to block signals when returning to DPMI regardless
of sas_wa because the race is possible in deinit_handler():
it should not be interrupted after it changed %fs.

src/arch/linux/async/signal.c
@@ -1200,6 +1200,7 @@ void signal_unblock_async_sigs(void)
 
 void signal_restore_async_sigs(void)
 {
-  if (need_sas_wa)
-    sigprocmask(SIG_BLOCK, &nonfatal_q_mask, NULL);
+  /* block sigs even for !sas_wa because if deinit_handler is
+   * interrupted after changing %fs, we are in troubles */
+  sigprocmask(SIG_BLOCK, &nonfatal_q_mask, NULL);
 }

src/arch/linux/async/sigsegv.c
@@ -119,11 +119,14 @@ static int dosemu_fault1(int signal, struct sigcontext *scp)
     } /*!DPMIValidSelector(_cs)*/
     else {
       if (_trapno == 0x0e) {
+        int rc;
         signal_unblock_async_sigs();
-        if (VGA_EMU_FAULT(scp, code, 1) == True)
-          return dpmi_check_return(scp);
-        /* going for dpmi_fault(), careful with async signals and sas_wa */
+        rc = vga_emu_fault(scp, 1);
+        /* going for dpmi_fault() or deinit_handler(),
+         * careful with async signals and sas_wa */
         signal_restore_async_sigs();
+        if (rc == True)
+          return dpmi_check_return(scp);
       }
       /* Not in dosemu code: dpmi_fault() will handle that */
       return dpmi_fault(scp);