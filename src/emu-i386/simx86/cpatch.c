dosemu2/dosemu2

commit c159328f4cd2d843e3d938b3769a478ed29e5a1a

Changed interfaces and callers to simx86 memory protection functions to use integers instead of pointers for consistency between zero and non-zero bases.
git-svn-id: https://dosemu.svn.sourceforge.net/svnroot/dosemu/trunk@1978 7758b857-130e-0410-ab9c-d8735a528d18

src/emu-i386/simx86/cpatch.c
@@ -46,58 +46,61 @@
 #define asmlinkage static __attribute__((unused))
 #endif
 
-int s_munprotect(caddr_t addr)
+int s_munprotect(unsigned int addr)
 {
-	if (debug_level('e')>3) e_printf("\tS_MUNPROT %p\n",addr);
+	if (debug_level('e')>3) e_printf("\tS_MUNPROT %08x\n",addr);
 	return e_check_munprotect(addr);
 }
 
-int s_mprotect(caddr_t addr)
+int s_mprotect(unsigned int addr)
 {
-	if (debug_level('e')>3) e_printf("\tS_MPROT   %p\n",addr);
+	if (debug_level('e')>3) e_printf("\tS_MPROT   %08x\n",addr);
 	return e_mprotect(addr,0);
 }
 
 #ifdef HOST_ARCH_X86
 
-static int m_mprotect(caddr_t addr)
+static int m_mprotect(unsigned int addr)
 {
 	if (debug_level('e')>3)
-	    e_printf("\tM_MPROT   %p\n",addr);
+	    e_printf("\tM_MPROT   %08x\n",addr);
 	return e_mprotect(addr,0);
 }
 
 /*
  * Return address of the stub function is passed into eip
  */
-static int m_munprotect(caddr_t addr, unsigned char *eip)
+static int m_munprotect(unsigned int addr, unsigned char *eip)
 {
-	if (debug_level('e')>3) e_printf("\tM_MUNPROT %p:%p [%08x]\n",
+	if (debug_level('e')>3) e_printf("\tM_MUNPROT %08x:%p [%08x]\n",
 		addr,eip,*((int *)(eip-3)));
 	/* verify that data, not code, has been hit */
 	if (!e_querymark(addr))
 	    return e_check_munprotect(addr);
 	/* Oops.. we hit code, maybe the stub was set up before that
 	 * code was parsed. Ok, undo the patch and clear that code */
-	e_printf("CODE %p hit in DATA %p patch\n",addr,eip);
+	e_printf("CODE %08x hit in DATA %p patch\n",addr,eip);
 /*	if (UnCpatch((void *)(eip-3))) leavedos(0); */
-	InvalidateSingleNode((long)addr, eip);
+	InvalidateSingleNode(TheCPU.mem_base + addr, eip);
 	return e_check_munprotect(addr);
 }
 
-asmlinkage int r_munprotect(caddr_t addr, long len, unsigned char *eip)
+asmlinkage int r_munprotect(unsigned char *paddr, unsigned int len,
+			    unsigned char *eip)
 {
+	unsigned int addr;
 	if (*eip == 0xf3) /* skip rep */
 		eip++;
 	if (*eip == 0x66)
 		len *= 2;
 	else if (*eip & 1)
 		len *= 4;
+	addr = paddr - mem_base;
 	if (EFLAGS & EFLAGS_DF) addr -= len;
 	if (debug_level('e')>3)
-	    e_printf("\tR_MUNPROT %p:%p %s\n",
+	    e_printf("\tR_MUNPROT %08x:%08x %s\n",
 		addr,addr+len,(EFLAGS&EFLAGS_DF?"back":"fwd"));
-	InvalidateNodePage((long)addr,len,eip,NULL);
+	InvalidateNodePage(TheCPU.mem_base + addr,len,eip,NULL);
 	e_resetpagemarks(addr,len);
 	e_munprotect(addr,len);
 	return 0;
@@ -105,28 +108,31 @@ asmlinkage int r_munprotect(caddr_t addr, long len, unsigned char *eip)
 
 /* ======================================================================= */
 
-asmlinkage void stk_16(caddr_t addr, Bit16u value)
+asmlinkage void stk_16(unsigned char *paddr, Bit16u value)
 {
+	unsigned int addr = paddr - mem_base;
 	int ret = s_munprotect(addr);
-	WRITE_WORDP(addr, value);
+	WRITE_WORD(addr, value);
 	if (ret & 1)
 		s_mprotect(addr);
 }
 
-asmlinkage void stk_32(caddr_t addr, Bit32u value)
+asmlinkage void stk_32(unsigned char *paddr, Bit32u value)
 {
+	unsigned int addr = paddr - mem_base;
 	int ret = s_munprotect(addr);
-	WRITE_DWORDP(addr, value);
+	WRITE_DWORD(addr, value);
 	if (ret & 1)
 		s_mprotect(addr);
 }
 
-asmlinkage void wri_8(caddr_t addr, Bit8u value, unsigned char *eip)
+asmlinkage void wri_8(unsigned char *paddr, Bit8u value, unsigned char *eip)
 {
+	unsigned int addr = paddr - mem_base;
 	int ret;
 	Bit8u *p;
 	ret = m_munprotect(addr, eip);
-	p = lowmemp(addr);
+	p = LINEAR2UNIX(addr);
 	/* there is a slight chance that this stub hits VGA memory.
 	   For that case there is a simple instruction decoder but
 	   we must use mov %al,(%edi) (%rdi for x86_64) */
@@ -135,23 +141,25 @@ asmlinkage void wri_8(caddr_t addr, Bit8u value, unsigned char *eip)
 		m_mprotect(addr);
 }
 
-asmlinkage void wri_16(caddr_t addr, Bit16u value, unsigned char *eip)
+asmlinkage void wri_16(unsigned char *paddr, Bit16u value, unsigned char *eip)
 {
+	unsigned int addr = paddr - mem_base;
 	int ret;
 	Bit16u *p;
 	ret = m_munprotect(addr, eip);
-	p = lowmemp(addr);
+	p = LINEAR2UNIX(addr);
 	asm("movw %1,(%2)" : "=m"(*p) : "a"(value), "D"(p));
 	if (ret & 1)
 		m_mprotect(addr);
 }
 
-asmlinkage void wri_32(caddr_t addr, Bit32u value, unsigned char *eip)
+asmlinkage void wri_32(unsigned char *paddr, Bit32u value, unsigned char *eip)
 {
+	unsigned int addr = paddr - mem_base;
 	int ret;
 	Bit32u *p;
 	ret = m_munprotect(addr, eip);
-	p = lowmemp(addr);
+	p = LINEAR2UNIX(addr);
 	asm("movl %1,(%2)" : "=m"(*p) : "a"(value), "D"(p));
 	if (ret & 1)
 		m_mprotect(addr);

src/emu-i386/simx86/codegen-x86.c
@@ -3132,8 +3132,8 @@ static unsigned char *CloseAndExec_x86(unsigned char *PC, TNode *G, int mode, in
 		/* mprotect the page here; a page fault will be triggered
 		 * if some other code tries to write over the page including
 		 * this node */
-		e_markpage((void *)(uintptr_t)G->seqbase, G->seqlen);
-		e_mprotect((void *)(uintptr_t)G->seqbase, G->seqlen);
+		e_markpage(G->seqbase - TheCPU.mem_base, G->seqlen);
+		e_mprotect(G->seqbase - TheCPU.mem_base, G->seqlen);
 		SeqStart = G->addr;
 	}
 	else {

src/emu-i386/simx86/codegen.h
@@ -219,15 +219,15 @@ static __inline__ int FastLog2(register int v)
 static __inline__ void PUSH(int m, void *w)
 {
 	unsigned int sp;
-	caddr_t addr;
+	unsigned int addr;
 	int v;
 	sp = (TheCPU.esp-BT24(BitDATA16, m)) & TheCPU.StackMask;
-	addr = (caddr_t)(uintptr_t)(LONG_SS + sp);
+	addr = LONG_SS - TheCPU.mem_base + sp;
 	v = e_check_munprotect(addr);
 	if (m&DATA16)
-		WRITE_WORDP(addr, *(short *)w);
+		WRITE_WORD(addr, *(short *)w);
 	else
-		WRITE_DWORDP(addr, *(int *)w);
+		WRITE_DWORD(addr, *(int *)w);
 	if (v) e_mprotect(addr, 0);
 #ifdef KEEP_ESP
 	TheCPU.esp = (sp&TheCPU.StackMask) | (TheCPU.esp&~TheCPU.StackMask);

src/emu-i386/simx86/emu86.h
@@ -662,13 +662,15 @@ void GCPrint(unsigned char *cp, unsigned char *cbase, int len);
 char *showreg(signed char r);
 char *showmode(unsigned int m);
 int e_debug_check(unsigned char *PC);
-int e_mprotect(unsigned char *addr, size_t len);
-int e_munprotect(unsigned char *addr, size_t len);
-int e_check_munprotect(unsigned char *addr);
+int s_munprotect(unsigned int addr);
+int s_mprotect(unsigned int addr);
+int e_mprotect(unsigned int addr, size_t len);
+int e_munprotect(unsigned int addr, size_t len);
+int e_check_munprotect(unsigned int addr);
 int e_querymprotrange(unsigned int al, unsigned int ah);
-int e_markpage(unsigned char *addr, size_t len);
-int e_querymark(unsigned char *addr);
-void e_resetpagemarks(unsigned char *addr, size_t len);
+int e_markpage(unsigned int addr, size_t len);
+int e_querymark(unsigned int addr);
+void e_resetpagemarks(unsigned int addr, size_t len);
 void mprot_init(void);
 void mprot_end(void);
 void InvalidateSegs(void);

src/emu-i386/simx86/memory.c
@@ -147,11 +147,10 @@ int e_querymprotrange(unsigned int al, unsigned int ah)
 /////////////////////////////////////////////////////////////////////////////
 
 
-int e_markpage(unsigned char *paddr, size_t len)
+int e_markpage(unsigned int addr, size_t len)
 {
 	unsigned int abeg, aend;
 	tMpMap *M;
-	unsigned int addr = paddr - mem_base;
 
 	abeg = (addr >> CGRAN) & CGRMASK;
 	aend = (((addr+len) >> CGRAN) + 1) & CGRMASK;
@@ -166,11 +165,10 @@ nextmega:
 	return 1;
 }
 
-int e_querymark(unsigned char *paddr)
+int e_querymark(unsigned int addr)
 {
 	int idx;
 	tMpMap *M;
-	unsigned int addr = paddr - mem_base;
 
 	M = FindM(addr); if (M==NULL) return 0;
 	idx = (addr >> CGRAN) & CGRMASK;
@@ -189,10 +187,9 @@ static void e_resetonepagemarks(unsigned int addr)
 	for (i=0; i<8; i++) M->subpage[idx++] = 0;
 }
 
-void e_resetpagemarks(unsigned char *paddr, size_t len)
+void e_resetpagemarks(unsigned int addr, size_t len)
 {
 	int i, pages;
-	unsigned int addr = paddr - mem_base;
 	
 	pages = ((addr + len - 1) >> PAGE_SHIFT) - (addr >> PAGE_SHIFT) + 1;
 	for (i = 0; i < pages; i++)
@@ -202,11 +199,10 @@ void e_resetpagemarks(unsigned char *paddr, size_t len)
 /////////////////////////////////////////////////////////////////////////////
 
 
-int e_mprotect(unsigned char *paddr, size_t len)
+int e_mprotect(unsigned int addr, size_t len)
 {
 	int e;
 	unsigned int abeg, aend;
-	unsigned int addr = paddr - mem_base;
 	unsigned int abeg1 = (unsigned)-1;
 	unsigned a;
 	int ret = 1;
@@ -236,11 +232,10 @@ int e_mprotect(unsigned char *paddr, size_t len)
 	return ret;
 }
 
-int e_munprotect(unsigned char *paddr, size_t len)
+int e_munprotect(unsigned int addr, size_t len)
 {
 	int e;
 	unsigned int abeg, aend;
-	unsigned int addr = paddr - mem_base;
 	unsigned int abeg1 = (unsigned)-1;
 	unsigned a;
 	int ret = 0;
@@ -273,9 +268,9 @@ int e_munprotect(unsigned char *paddr, size_t len)
 
 /* check if the address is aliased to a non protected page, and if it is,
    do not try to unprotect it */
-int e_check_munprotect(unsigned char *addr)
+int e_check_munprotect(unsigned int addr)
 {
-	if (lowmemp(addr) != addr)
+	if (LINEAR2UNIX(addr) != &mem_base[addr])
 		return 0;
 	return e_munprotect(addr,0);
 }

src/emu-i386/simx86/sigsegv.c
@@ -478,6 +478,7 @@ int e_emu_fault(struct sigcontext_struct *scp)
 		/* LDT access emulation */
 		return 0;
 	if ((_err&0x0f)==0x07) {
+		unsigned int icr2 = _cr2 - TheCPU.mem_base;
 		unsigned char *p = (unsigned char *)_rip;
 		int codehit = 0;
 		register int v;
@@ -515,24 +516,24 @@ int e_emu_fault(struct sigcontext_struct *scp)
 				    !DPMIValidSelector(_cs) ? "DOSEMU " : "",
 				    _cs, _rip, _cr2, fault_cnt);
 		    }
-		    if (e_querymark((void *)_cr2)) {
-			e_printf("CODE node hit at %08lx\n",_cr2);
+		    if (e_querymark(icr2)) {
+			e_printf("CODE node hit at %08x\n",icr2);
 		    }
 		    else if (InCompiledCode) {
-			e_printf("DATA node hit at %08lx\n",_cr2);
+			e_printf("DATA node hit at %08x\n",icr2);
 		    }
 		}
 		/* the page is not unprotected here, the code
 		 * linked by Cpatch will do it */
 		/* ACH: we can set up a data patch for code
 		 * which has not yet been executed! */
-		if (InCompiledCode && !e_querymark((void *)_cr2) && Cpatch(scp))
+		if (InCompiledCode && !e_querymark(icr2) && Cpatch(scp))
 		    return 1;
 		/* We HAVE to invalidate all the code in the page
 		 * if the page is going to be unprotected */
 		InvalidateNodePage(_cr2, 0, p, &codehit);
-		e_resetpagemarks((void *)_cr2, 1);
-		e_munprotect((void *)_cr2, 0);
+		e_resetpagemarks(icr2, 1);
+		e_munprotect(icr2, 0);
 		/* now go back and perform the faulting op */
 		return 1;
 	}

src/emu-i386/simx86/trees.c
@@ -1335,14 +1335,20 @@ quit:
 
 /////////////////////////////////////////////////////////////////////////////
 
-void e_invalidate(unsigned char *data, int cnt)
+void e_invalidate(unsigned char *pdata, int cnt)
 {
+	unsigned int data;
 	if (config.cpuemu <= 1)
 		return;
+#ifdef __x86_64__
+	if ((unsigned long)(pdata - mem_base) > 0xffffffff)
+		return;
+#endif
+	data = pdata - mem_base;
 	e_munprotect(data, cnt);
 #ifdef HOST_ARCH_X86
 	if (!CONFIG_CPUSIM)
-        	InvalidateNodePage((uintptr_t)data, cnt, 0, NULL);
+        	InvalidateNodePage((uintptr_t)pdata, cnt, 0, NULL);
 #endif
 	e_resetpagemarks(data, cnt);
 }

src/include/cpu-emu.h
@@ -85,9 +85,6 @@ void avltr_destroy(void);
 
 #define FLUSH_TREE	if (config.cpuemu>1) avltr_destroy()
 
-int s_munprotect(caddr_t addr);
-int s_mprotect(caddr_t addr);
-
 /* called from dpmi.c */
 void emu_mhp_SetTypebyte (unsigned short selector, int typebyte);
 unsigned short emu_do_LAR (unsigned short selector);

commit e12ee58fac659f3407d91766db4d30e5ed48755f

simx86: fix cpatch vga accesses
This was broken by commit c159328f.
Asms that write to vga mem should not use LINEAR2UNIX
or the fault never happens.
However taking the fault is too expensive and defeats the
entire purpose of cpatch, so use vga_write_xxx() funcs.

src/emu-i386/simx86/cpatch.c
@@ -46,6 +46,8 @@
 
 #ifdef HOST_ARCH_X86
 
+static int in_cpatch;
+
 /*
  * Return address of the stub function is passed into eip
  */
@@ -101,6 +103,7 @@ asmlinkage void rep_movs_stos(struct rep_stack *stack)
 	unsigned char op;
 	unsigned int size;
 
+	in_cpatch++;
 	assert(InCompiledCode);
 	InCompiledCode--;
 	addr = DOSADDR_REL(paddr);
@@ -189,6 +192,7 @@ done:
 	stack->edi = MEM_BASE32(addr);
 	stack->ecx = ecx;
 	InCompiledCode++;
+	in_cpatch--;
 }
 
 /* ======================================================================= */
@@ -197,69 +201,73 @@ asmlinkage void stk_16(unsigned char *paddr, Bit16u value)
 {
 	dosaddr_t addr;
 
+	in_cpatch++;
 	assert(InCompiledCode);
 	InCompiledCode--;
 	addr = DOSADDR_REL(paddr);
 	e_invalidate(addr, 2);
 	WRITE_WORD(addr, value);
 	InCompiledCode++;
+	in_cpatch--;
 }
 
 asmlinkage void stk_32(unsigned char *paddr, Bit32u value)
 {
 	dosaddr_t addr;
 
+	in_cpatch++;
 	assert(InCompiledCode);
 	InCompiledCode--;
 	addr = DOSADDR_REL(paddr);
 	e_invalidate(addr, 4);
 	WRITE_DWORD(addr, value);
 	InCompiledCode++;
+	in_cpatch--;
 }
 
 asmlinkage void wri_8(unsigned char *paddr, Bit8u value, unsigned char *eip)
 {
 	dosaddr_t addr;
-	Bit8u *p;
 
+	in_cpatch++;
 	assert(InCompiledCode);
 	InCompiledCode--;
 	addr = DOSADDR_REL(paddr);
 	m_munprotect(addr, 1, eip);
-	p = LINEAR2UNIX(addr);
 	InCompiledCode++;
-	/* there is a slight chance that this stub hits VGA memory.
-	   For that case there is a simple instruction decoder but
-	   we must use mov %al,(%edi) (%rdi for x86_64) */
-	asm("movb %1,(%2)" : "=m"(*p) : "a"(value), "D"(p));
+	/* there is a slight chance that this stub hits VGA memory. */
+	vga_write(addr, value);
+	in_cpatch--;
 }
 
 asmlinkage void wri_16(unsigned char *paddr, Bit16u value, unsigned char *eip)
 {
 	dosaddr_t addr;
-	Bit16u *p;
 
+	in_cpatch++;
 	assert(InCompiledCode);
 	InCompiledCode--;
 	addr = DOSADDR_REL(paddr);
 	m_munprotect(addr, 2, eip);
-	p = LINEAR2UNIX(addr);
 	InCompiledCode++;
-	asm("movw %1,(%2)" : "=m"(*p) : "a"(value), "D"(p));
+	/* there is a slight chance that this stub hits VGA memory. */
+	vga_write_word(addr, value);
+	in_cpatch--;
 }
 
 asmlinkage void wri_32(unsigned char *paddr, Bit32u value, unsigned char *eip)
 {
 	dosaddr_t addr;
-	Bit32u *p;
 
+	in_cpatch++;
 	assert(InCompiledCode);
 	InCompiledCode--;
 	addr = DOSADDR_REL(paddr);
 	m_munprotect(addr, 4, eip);
-	p = LINEAR2UNIX(addr);
 	InCompiledCode++;
-	asm("movl %1,(%2)" : "=m"(*p) : "a"(value), "D"(p));
+	/* there is a slight chance that this stub hits VGA memory. */
+	vga_write_dword(addr, value);
+	in_cpatch--;
 }
 
 #ifdef __i386__
@@ -450,6 +458,9 @@ int Cpatch(struct sigcontext *scp)
     int w16;
     unsigned int v;
     unsigned char *eip = (unsigned char *)_rip;
+
+    if (in_cpatch)
+	return 0;
 
     p = eip;
     if (*p==0xf3 && p[-1] == 0x90 && p[-2] == 0x90) {	// rep movs, rep stos