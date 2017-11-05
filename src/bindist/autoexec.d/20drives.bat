4946f393dd98c1dcc160a39cec7cec3d1a1d2021
Julius Schwartzenberg <julius.schwartzenberg@gmail.com>
2017-11-05 22:28:57 +0100

    Allow spaces in host path to extra drives

--- src/bindist/autoexec.d/20drives.bat
+++ src/bindist/autoexec.d/20drives.bat
@@ -1,9 +1,9 @@
 @rem Set up drives specified with -cdrom and DOSDRIVE_EXTRA environment variable (see dosemu start-up script)
 system -s CDROM_PATH
 if "%CDROM_PATH%" == "" goto nocdrom
-lredir2 -nC \\linux\fs%CDROM_PATH%
+lredir2 -nC "\\linux\fs%CDROM_PATH%"
 :nocdrom
 system -s DOSDRIVE_EXTRA
 if "%DOSDRIVE_EXTRA%" == "" goto nodrived
-lredir2 -n \\linux\fs%DOSDRIVE_EXTRA%
+lredir2 -n "\\linux\fs%DOSDRIVE_EXTRA%"
 :nodrived
