fd3e3b185e95fc99a7289348f50553efc12d8cb3
Julius Schwartzenberg <julius.schwartzenberg@gmail.com>
2017-11-05 21:29:41 +0100

    Separate batch file to print welcome message

--- src/bindist/autoexec.bat
+++ src/bindist/autoexec.bat
@@ -18,7 +18,4 @@ rem lh display con=(vga,437,2)
 rem mode con codepage prepare=((850) d:\cpi\ega.cpx)
 rem mode con codepage select 850
 rem chcp 850
-echo Welcome to dosemu2!
-system -s DOSEMU_VERSION
-echo     Build %DOSEMU_VERSION%
 if exist e:\autoexec.d\*.bat for %%B in (e:\autoexec.d\*.bat) do call %%B
---
+++ src/bindist/autoexec.d/98welcom.bat
@@ -0,0 +1,4 @@
+@rem Print dosemu's welcome message
+echo Welcome to dosemu2!
+system -s DOSEMU_VERSION
+echo     Build %DOSEMU_VERSION%
