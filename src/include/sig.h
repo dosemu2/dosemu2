920c44653fbfe98b6d4b5062534fbf59128d4486
Stas Sergeev <stsp@users.sourceforge.net>
2017-12-23 18:19:08 +0300

    fix 32bit build regression [#544]

--- src/include/sig.h
+++ src/include/sig.h
@@ -3,12 +3,12 @@
 
 #include <unistd.h>
 #include <signal.h>
+#include <syscall.h>
 
 /* reserve 1024 uncommitted pages for stack */
 #define SIGSTACK_SIZE (1024 * getpagesize())
 
 #if defined(__x86_64__) && defined(__linux__)
-#include <syscall.h>
 #define ARCH_SET_GS 0x1001
 #define ARCH_SET_FS 0x1002
 #define ARCH_GET_FS 0x1003
