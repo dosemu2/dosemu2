42b57d8b0a3be479edba1d2a0cbd12f09279c522
Stas Sergeev <stsp@users.sourceforge.net>
2018-04-16 14:11:21 +0300

    smalloc: sync with fdpp

--- src/base/misc/smalloc.c
+++ src/base/misc/smalloc.c
@@ -28,7 +28,9 @@
 #include "smalloc.h"
 
 #define POOL_USED(p) (p->mn.used || p->mn.next)
+#ifndef min
 #define min(x, y) ((x) < (y) ? (x) : (y))
+#endif
 
 static void smerror_dummy(int prio, const char *fmt, ...) FORMAT(printf, 2, 3);
 
@@ -47,15 +49,23 @@ void do_smerror(int prio, struct mempool *mp, const char *fmt, ...)
     char buf[1024];
     int pos;
     va_list al;
+    struct memnode *mn;
 
     assert(prio != -1);
     va_start(al, fmt);
     pos = vsnprintf(buf, sizeof(buf), fmt, al);
     va_end(al);
-#define DO_PRN(...) pos += snprintf(buf + pos, sizeof(buf) - pos, __VA_ARGS__)
+#define DO_PRN(...) do { \
+    assert(pos < sizeof(buf)); \
+    pos += snprintf(buf + pos, sizeof(buf) - pos, __VA_ARGS__); \
+} while (0)
     DO_PRN("Total size: %zi\n", mp->size);
     DO_PRN("Available space: %zi\n", mp->avail);
     DO_PRN("Largest free area: %zi\n", smget_largest_free_area(mp));
+    DO_PRN("Memory pool dump:\n");
+    for (mn = &mp->mn; mn; mn = mn->next)
+        DO_PRN("\tarea: %zi bytes, %s\n",
+                mn->size, mn->used ? "used" : "free");
 
     mp->smerr(prio, "%s", buf);
 }
