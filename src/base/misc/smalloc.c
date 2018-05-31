dosemu2/dosemu2

commit 5cf19a4dfdf679e0c1deb3730dc09eeb23a2eb95

dpmi: reserve address space for linear allocs [fixes #385]
This diverts config.dpmi_base. Perhaps it should be renamed.
Patches will follow soon.

src/dosext/dpmi/memory.c
@@ -134,14 +134,24 @@ static int uncommit(void *ptr, size_t size)
 
 int dpmi_alloc_pool(void)
 {
+    void *dpmi_base;
+    memsize = dpmi_mem_size();
+    /* some DPMI clients don't like negative memory pointers,
+     * i.e. over 0x80000000. In fact, Screamer game won't work
+     * with anything above 0x40000000 */
+    dpmi_base = mapping_find_hole((uintptr_t)MEM_BASE32(LOWMEM_SIZE),
+        (uintptr_t)MEM_BASE32(0x40000000), memsize);
+    if (dpmi_base == MAP_FAILED) {
+      error("MAPPING: cannot find mem hole for DPMI pool\n");
+      dpmi_base = (void *)-1;
+    }
     /* Create DPMI pool */
-    if (config.dpmi_base == -1) {
+    mpool_ptr = mmap_mapping_ux(MAPPING_DPMI | MAPPING_SCRATCH |
+        MAPPING_NOOVERLAP, dpmi_base, memsize, PROT_NONE);
+    if (mpool_ptr == MAP_FAILED) {
       error("MAPPING: cannot create mem pool for DPMI\n");
       return -1;
     }
-
-    memsize = dpmi_mem_size();
-    mpool_ptr = MEM_BASE32(config.dpmi_base);
     c_printf("DPMI: mem init, mpool is %ld bytes at %p\n", memsize, mpool_ptr);
     sminit_com(&mem_pool, mpool_ptr, memsize, commit, uncommit);
     dpmi_total_memory = config.dpmi * 1024;
@@ -297,6 +307,7 @@ dpmi_pm_block * DPMI_mallocLinear(dpmi_pm_block_root *root,
     dpmi_pm_block *block;
     unsigned char *realbase;
     int i;
+    int cap = MAPPING_DPMI | MAPPING_SCRATCH;
 
    /* aligned size to PAGE size */
     size = PAGE_ALIGN(size);
@@ -304,6 +315,9 @@ dpmi_pm_block * DPMI_mallocLinear(dpmi_pm_block_root *root,
 	return NULL;
     if (base == 0)
 	base = -1;
+    else if (base < config.dpmi_base || base >= config.dpmi_base +
+	    config.dpmi_lin_size * 1024)
+	cap |= MAPPING_NOOVERLAP;
     if (committed && size > dpmi_free_memory)
 	return NULL;
     if ((block = alloc_pm_block(root, size)) == NULL)
@@ -311,7 +325,7 @@ dpmi_pm_block * DPMI_mallocLinear(dpmi_pm_block_root *root,
 
     /* base is just a hint here (no MAP_FIXED). If vma-space is
        available the hint will be block->base */
-    realbase = mmap_mapping(MAPPING_DPMI | MAPPING_SCRATCH | MAPPING_NOOVERLAP,
+    realbase = mmap_mapping(cap,
 	base, size, committed ? PROT_READ | PROT_WRITE | PROT_EXEC : PROT_NONE);
     if (realbase == MAP_FAILED) {
 	free_pm_block(root, block);

src/base/init/config.c
@@ -175,8 +175,8 @@ void dump_config_status(void (*printfunc)(const char *, ...))
 	config.mem_size, config.ext_mem);
     (*print)("ems_size 0x%x\nems_frame 0x%x\n",
         config.ems_size, config.ems_frame);
-    (*print)("umb_a0 %i\numb_b0 %i\numb_f0 %i\ndpmi 0x%x\ndpmi_base 0x%x\npm_dos_api %i\nignore_djgpp_null_derefs %i\n",
-        config.umb_a0, config.umb_b0, config.umb_f0, config.dpmi, config.dpmi_base, config.pm_dos_api, config.no_null_checks);
+    (*print)("umb_a0 %i\numb_b0 %i\numb_f0 %i\ndpmi 0x%x\ndpmi_base 0x%x\ndpmi_lin_size 0x%x\npm_dos_api %i\nignore_djgpp_null_derefs %i\n",
+        config.umb_a0, config.umb_b0, config.umb_f0, config.dpmi, config.dpmi_base, config.dpmi_lin_size, config.pm_dos_api, config.no_null_checks);
     (*print)("mapped_bios %d\nvbios_file %s\n",
         config.mapped_bios, (config.vbios_file ? config.vbios_file :""));
     (*print)("vbios_copy %d\nvbios_seg 0x%x\nvbios_size 0x%x\n",

src/base/init/init.c
@@ -259,24 +259,8 @@ static void *mem_reserve_split(void *base, uint32_t size, uint32_t dpmi_size)
     return result;
   if (!config.dpmi)
     return result;
-  if (config.dpmi_base == (uintptr_t)-1) {
-    void *dpmi_base1;
-    /* some DPMI clients don't like negative memory pointers,
-     * i.e. over 0x80000000. In fact, Screamer game won't work
-     * with anything above 0x40000000 */
-    if ((uintptr_t)result + 0x40000000 <= 0xffffffff)
-      dpmi_base1 = mapping_find_hole((uintptr_t)result + size,
-        (uintptr_t)result + 0x40000000, dpmi_size);
-    else
-      dpmi_base1 = mapping_find_hole(0, 0x40000000, dpmi_size);
-    if (dpmi_base1 == MAP_FAILED)
-      error("MAPPING: cannot find mem hole for DPMI pool\n");
-      /* try mmap() below regardless of whether find_hole() succeeded or not*/
-    else
-      dpmi_base = dpmi_base1;
-  } else {
-    dpmi_base = (void*)(((uintptr_t)result + config.dpmi_base) & 0xffffffff);
-  }
+  assert(config.dpmi_base != (uintptr_t)-1);
+  dpmi_base = (void*)(((uintptr_t)result + config.dpmi_base) & 0xffffffff);
   dpmi_base = mmap_mapping_ux(MAPPING_DPMI | MAPPING_SCRATCH, dpmi_base,
       dpmi_size, PROT_NONE);
   if (dpmi_base == MAP_FAILED) {
@@ -305,14 +289,16 @@ static void *mem_reserve_split(void *base, uint32_t size, uint32_t dpmi_size)
 static void *mem_reserve(void)
 {
   void *result = MAP_FAILED;
-  size_t dpmi_size = dpmi_mem_size();
+  /* XXX */
+  config.dpmi_lin_size = 0x8000;
+  size_t dpmi_size = PAGE_ALIGN(config.dpmi_lin_size * 1024);
   size_t memsize = LOWMEM_SIZE + HMASIZE;
 
 #ifdef __i386__
   if (config.cpu_vm == CPUVM_VM86) {
     if (config.dpmi && config.dpmi_base == (uintptr_t)-1)
       result = mem_reserve_contig(0, memsize, dpmi_size);
-    if (result == MAP_FAILED)
+    else
       result = mem_reserve_split(0, memsize, dpmi_size);
     if (result == MAP_FAILED) {
       const char *msg =

src/include/emu.h
@@ -250,6 +250,7 @@ typedef struct config_info {
        unsigned int ems_frame;
        int ems_uma_pages, ems_cnv_pages;
        int dpmi, pm_dos_api, no_null_checks;
+       uint32_t dpmi_lin_size;
        uintptr_t dpmi_base;
 
        int sillyint;            /* IRQ numbers for Silly Interrupt Generator

commit 4eb2dc83ce8b33dfaa004143bd7a9ca83549a2c0

dpmi: use smalloc for allocs from reserved space [fixes #612]
Without this the allocations from linear pool are uncontrolable
and can corrupt each other. Also freeing was wrongly done.

Regression since 5cf19a4d.

src/base/misc/smalloc.c
@@ -23,6 +23,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
+#include <stdint.h>
 #include <string.h>
 #include <assert.h>
 #include "smalloc.h"
@@ -176,6 +177,18 @@ static struct memnode *find_mn(struct mempool *mp, unsigned char *ptr,
   return NULL;
 }
 
+static struct memnode *find_mn_at(struct mempool *mp, unsigned char *ptr)
+{
+  struct memnode *mn;
+  for (mn = &mp->mn; mn; mn = mn->next) {
+    if (mn->mem_area > ptr)
+      return NULL;
+    if (mn->mem_area + mn->size > ptr)
+      return mn;
+  }
+  return NULL;
+}
+
 static struct memnode *smfind_free_area(struct mempool *mp, size_t size)
 {
   struct memnode *mn;
@@ -207,11 +220,57 @@ static struct memnode *sm_alloc_mn(struct mempool *mp, size_t size)
   return mn;
 }
 
+static struct memnode *sm_alloc_fixed(struct mempool *mp, void *ptr,
+    size_t size)
+{
+  struct memnode *mn;
+  ptrdiff_t delta;
+  if (!size || !ptr) {
+    smerror(mp, "SMALLOC: zero-sized allocation attempted\n");
+    return NULL;
+  }
+  if (!(mn = find_mn_at(mp, ptr))) {
+    smerror(mp, "SMALLOC: invalid address %p on alloc_fixed\n", ptr);
+    return NULL;
+  }
+  if (mn->used) {
+    do_smerror(0, mp, "SMALLOC: address %p already allocated\n", ptr);
+    return NULL;
+  }
+  delta = (uint8_t *)ptr - mn->mem_area;
+  assert(delta >= 0);
+  if (size + delta > mn->size) {
+    smerror(mp, "SMALLOC: no space %zi at address %p\n", size, ptr);
+    return NULL;
+  }
+  if (delta) {
+    mntruncate(mn, delta);
+    mn = mn->next;
+    assert(!mn->used && mn->size >= size);
+  }
+  if (!sm_commit_simple(mp, mn->mem_area, size))
+    return NULL;
+  mn->used = 1;
+  mntruncate(mn, size);
+  assert(mn->size == size);
+  memset(mn->mem_area, 0, size);
+  return mn;
+}
+
 void *smalloc(struct mempool *mp, size_t size)
 {
   struct memnode *mn = sm_alloc_mn(mp, size);
   if (!mn)
     return NULL;
+  return mn->mem_area;
+}
+
+void *smalloc_fixed(struct mempool *mp, void *ptr, size_t size)
+{
+  struct memnode *mn = sm_alloc_fixed(mp, ptr, size);
+  if (!mn)
+    return NULL;
+  assert(mn->mem_area == ptr);
   return mn->mem_area;
 }
 

src/dosext/dpmi/dpmi.c
@@ -2199,6 +2199,8 @@ err:
 	  unsigned int base_address = _ebx;
 	  dpmi_pm_block block;
 	  unsigned long length = _ecx;
+	  D_printf("DPMI: allocate linear mem attempt for siz 0x%08x at 0x%08x (%s)\n",
+		   _ecx, base_address, _edx ? "committed" : "uncommitted");
 	  if (!length) {
 	      _eflags |= CF;
 	      _LWORD(eax) = 0x8021;
@@ -2220,8 +2222,6 @@ err:
 	  }
 	  _ebx = block.base;
 	  _esi = block.handle;
-	  D_printf("DPMI: allocate linear mem attempt for siz 0x%08x at 0x%08x (%s)\n",
-		   _ecx, base_address, _edx ? "committed" : "uncommitted");
 	  D_printf("      malloc returns address %#x\n", block.base);
 	  D_printf("                using handle 0x%08x\n",block.handle);
 	  break;

src/dosext/dpmi/memory.c
@@ -45,9 +45,22 @@ unsigned long dpmi_free_memory;           /* how many bytes memory client */
 unsigned long pm_block_handle_used;       /* tracking handle */
 
 static smpool mem_pool;
+static smpool lin_pool;
 static void *dpmi_lin_rsv_base;
 static void *dpmi_base;
 
+static int in_rsv_pool(dosaddr_t base, unsigned int size)
+{
+    if (base >= DOSADDR_REL(dpmi_lin_rsv_base) &&
+	    base < DOSADDR_REL(dpmi_lin_rsv_base) +
+	    config.dpmi_lin_rsv_size * 1024) {
+	if (base + size <= DOSADDR_REL(dpmi_lin_rsv_base) +
+		config.dpmi_lin_rsv_size * 1024)
+	    return 1;
+	return -1;
+    }
+    return 0;
+}
 
 void dpmi_set_mem_bases(void *rsv_base, void *main_base)
 {
@@ -167,6 +180,8 @@ int dpmi_alloc_pool(void)
     c_printf("DPMI: mem init, mpool is %d bytes at %p\n", memsize, dpmi_base);
     /* Create DPMI pool */
     sminit_com(&mem_pool, dpmi_base, memsize, commit, uncommit);
+    sminit_com(&lin_pool, dpmi_lin_rsv_base, config.dpmi_lin_rsv_size * 1024,
+	    commit, uncommit);
     dpmi_total_memory = config.dpmi * 1024;
 
     D_printf("DPMI: dpmi_free_memory available 0x%lx\n", dpmi_total_memory);
@@ -319,6 +334,7 @@ dpmi_pm_block * DPMI_mallocLinear(dpmi_pm_block_root *root,
     dpmi_pm_block *block;
     unsigned char *realbase;
     int i;
+    int inp = 0;
     int cap = MAPPING_DPMI | MAPPING_SCRATCH;
 
    /* aligned size to PAGE size */
@@ -327,22 +343,33 @@ dpmi_pm_block * DPMI_mallocLinear(dpmi_pm_block_root *root,
 	return NULL;
     if (base == 0)
 	base = -1;
-    else if (base < DOSADDR_REL(dpmi_lin_rsv_base) ||
-	    base >= DOSADDR_REL(dpmi_lin_rsv_base) +
-	    config.dpmi_lin_rsv_size * 1024)
-	cap |= MAPPING_NOOVERLAP;
+    else {
+	inp = in_rsv_pool(base, size);
+	if (inp == -1)
+	    return NULL;
+	if (!inp)
+	    cap |= MAPPING_NOOVERLAP;
+    }
     if (committed && size > dpmi_free_memory)
 	return NULL;
     if ((block = alloc_pm_block(root, size)) == NULL)
 	return NULL;
 
-    /* base is just a hint here (no MAP_FIXED). If vma-space is
-       available the hint will be block->base */
-    realbase = mmap_mapping(cap,
-	base, size, committed ? PROT_READ | PROT_WRITE | PROT_EXEC : PROT_NONE);
-    if (realbase == MAP_FAILED) {
-	free_pm_block(root, block);
-	return NULL;
+    if (inp) {
+	realbase = smalloc_fixed(&lin_pool, MEM_BASE32(base), size);
+	if (realbase == NULL) {
+	    free_pm_block(root, block);
+	    return NULL;
+	}
+    } else {
+	/* base is just a hint here (no MAP_FIXED). If vma-space is
+	   available the hint will be block->base */
+	realbase = mmap_mapping(cap,
+	    base, size, committed ? PROT_READ | PROT_WRITE | PROT_EXEC : PROT_NONE);
+	if (realbase == MAP_FAILED) {
+	    free_pm_block(root, block);
+	    return NULL;
+	}
     }
     block->base = DOSADDR_REL(realbase);
     block->linear = 1;
@@ -364,7 +391,12 @@ int DPMI_free(dpmi_pm_block_root *root, unsigned int handle)
 	return -1;
     e_invalidate_full(block->base, block->size);
     if (block->linear) {
-	munmap(MEM_BASE32(block->base), block->size);
+	int inp = in_rsv_pool(block->base, block->size);
+	assert(inp != -1);
+	if (inp)
+	    smfree(&lin_pool, MEM_BASE32(block->base));
+	else
+	    munmap(MEM_BASE32(block->base), block->size);
     } else {
 	smfree(&mem_pool, MEM_BASE32(block->base));
     }
@@ -441,6 +473,7 @@ dpmi_pm_block * DPMI_reallocLinear(dpmi_pm_block_root *root,
 {
     dpmi_pm_block *block;
     unsigned char *ptr;
+    int inp;
 
     if (!newsize)	/* DPMI spec. says resize to 0 is an error */
 	return NULL;
@@ -469,11 +502,21 @@ dpmi_pm_block * DPMI_reallocLinear(dpmi_pm_block_root *root,
     e_invalidate_full(block->base, block->size);
     mprotect_mapping(MAPPING_DPMI, block->base, block->size,
       PROT_READ | PROT_WRITE | PROT_EXEC);
-    ptr = mremap(MEM_BASE32(block->base), block->size, newsize,
-      MREMAP_MAYMOVE);
-    if (ptr == MAP_FAILED) {
-	restore_page_protection(block);
-	return NULL;
+    inp = in_rsv_pool(block->base, block->size);
+    assert(inp != -1);
+    if (inp) {
+	ptr = smrealloc(&lin_pool, MEM_BASE32(block->base), newsize);
+	if (ptr == NULL) {
+	    restore_page_protection(block);
+	    return NULL;
+	}
+    } else {
+	ptr = mremap(MEM_BASE32(block->base), block->size, newsize,
+	    MREMAP_MAYMOVE);
+	if (ptr == MAP_FAILED) {
+	    restore_page_protection(block);
+	    return NULL;
+	}
     }
 
     finish_realloc(block, newsize, committed);

src/include/smalloc.h
@@ -30,6 +30,7 @@ typedef struct mempool {
 } smpool;
 
 extern void *smalloc(struct mempool *mp, size_t size);
+extern void *smalloc_fixed(struct mempool *mp, void *ptr, size_t size);
 extern void smfree(struct mempool *mp, void *ptr);
 extern void *smrealloc(struct mempool *mp, void *ptr, size_t size);
 extern int sminit(struct mempool *mp, void *start, size_t size);