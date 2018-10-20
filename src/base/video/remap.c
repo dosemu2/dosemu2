2f24d74dfc36fcc0dad4e8844d12aa8ce80148b6
Andrew Bird <ajb@spheresystems.co.uk>
2018-10-20 11:02:26 +0100

    Video: Sort remapper gen_2to8_all

--- src/base/video/remap.c
+++ src/base/video/remap.c
@@ -1495,6 +1495,14 @@ static RemapFuncDesc remap_gen_list[] = {
     NULL
   ),
 
+  REMAP_DESC(
+    RFF_SCALE_ALL | RFF_REMAP_LINES,
+    MODE_VGA_2,
+    MODE_TRUE_8,
+    gen_2to8_all,
+    NULL
+  ),
+
   // sort position (temporary comment)
 
   REMAP_DESC(
@@ -1726,14 +1734,6 @@ static RemapFuncDesc remap_gen_list[] = {
     MODE_VGA_2,
     MODE_PSEUDO_8,
     gen_2to8p_all,
-    NULL
-  ),
-
-  REMAP_DESC(
-    RFF_SCALE_ALL | RFF_REMAP_LINES,
-    MODE_VGA_2,
-    MODE_TRUE_8,
-    gen_2to8_all,
     NULL
   ),
 
@@ -1997,6 +1997,49 @@ void gen_1to32_all(RemapObject *ro)
     for (s_x = d_x = 0, bre_x = ro->bre_x; d_x < d_x_len;) {
       c0 = (src[s_x >> 3] >> ((s_x & 7) ^ 7)) & 1;
       dst[d_x++] = ro->true_color_lut[c0];
+      s_x += *(bre_x++);
+    }
+  }
+}
+
+/*
+ * 2 bit VGA pseudo color --> 8 bit true color (shared color map)
+ * supports arbitrary scaling
+ *
+ * -- very basic and slow --
+ */
+void gen_2to8_all(RemapObject *ro)
+{
+  int k;
+  int d_x_len;
+  int s_x, d_x, d_y;
+  int d_scan_len = ro->dst_scan_len;
+  int *bre_x;
+  int *bre_y = ro->bre_y;
+  const unsigned char *src, *src0;
+  unsigned char *dst;
+  unsigned char *lut = (unsigned char *)ro->true_color_lut;
+  unsigned char c0, c1;
+  int i;
+
+  src0 = ro->src_image + ro->src_start;
+  dst = ro->dst_image + ro->dst_start + ro->dst_offset;
+  d_x_len = ro->dst_width;
+
+  for (d_y = ro->dst_y0; d_y < ro->dst_y1; dst += d_scan_len) {
+    src = src0 + bre_y[d_y++];
+    k = (d_y & 1) << 1;
+    for (s_x = d_x = 0, bre_x = ro->bre_x; d_x < d_x_len;) {
+      i = s_x >> 3;
+      c0 = src[i];
+      c1 = src[i + 0x20000];
+      i = (s_x & 7) ^ 7;
+      c0 >>= i;
+      c1 >>= i;
+      c0 &= 1;
+      c1 &= 1;
+      c0 |= c1 << 1;
+      dst[d_x++] = lut[4 * c0 + (k ^= 1)];
       s_x += *(bre_x++);
     }
   }
@@ -3073,47 +3116,6 @@ void gen_2to8p_all(RemapObject *ro)
       c0 &= 1; c1 &= 1;
       c0 |= c1 << 1;
       dst[d_x++] = c0;
-      s_x += *(bre_x++);
-    }
-  }
-}
-
-
-/*
- * 2 bit VGA pseudo color --> 8 bit true color (shared color map)
- * supports arbitrary scaling
- *
- * -- very basic and slow --
- */
-void gen_2to8_all(RemapObject *ro)
-{
-  int k;
-  int d_x_len;
-  int s_x, d_x, d_y;
-  int d_scan_len = ro->dst_scan_len;
-  int *bre_x;
-  int *bre_y = ro->bre_y;
-  const unsigned char *src, *src0;
-  unsigned char *dst;
-  unsigned char *lut = (unsigned char*) ro->true_color_lut;
-  unsigned char c0, c1;
-  int i;
-
-  src0 = ro->src_image + ro->src_start;
-  dst = ro->dst_image + ro->dst_start + ro->dst_offset;
-  d_x_len = ro->dst_width;
-
-  for(d_y = ro->dst_y0; d_y < ro->dst_y1; dst += d_scan_len) {
-    src = src0 + bre_y[d_y++];
-    k = (d_y & 1) << 1;
-    for(s_x = d_x = 0, bre_x = ro->bre_x; d_x < d_x_len; ) {
-      i = s_x >> 3;
-      c0 = src[i]; c1 = src[i + 0x20000];
-      i = (s_x & 7) ^ 7;
-      c0 >>= i; c1 >>= i;
-      c0 &= 1; c1 &= 1;
-      c0 |= c1 << 1;
-      dst[d_x++] = lut[4 * c0 + (k ^= 1)];
       s_x += *(bre_x++);
     }
   }
