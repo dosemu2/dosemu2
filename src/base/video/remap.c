3ad7571600dede0b4da734efd35ebf30ef15502a
Andrew Bird <ajb@spheresystems.co.uk>
2018-10-20 11:15:56 +0100

    Video: Sort remapper gen_c2to8p_all

--- src/base/video/remap.c
+++ src/base/video/remap.c
@@ -1543,6 +1543,14 @@ static RemapFuncDesc remap_gen_list[] = {
     NULL
   ),
 
+  REMAP_DESC(
+    RFF_SCALE_ALL | RFF_REMAP_LINES,
+    MODE_CGA_2,
+    MODE_PSEUDO_8,
+    gen_c2to8p_all,
+    NULL
+  ),
+
   // sort position (temporary comment)
 
   REMAP_DESC(
@@ -1766,14 +1774,6 @@ static RemapFuncDesc remap_gen_list[] = {
     MODE_TRUE_16,
     MODE_TRUE_32,
     gen_16to32_all,
-    NULL
-  ),
-
-  REMAP_DESC(
-    RFF_SCALE_ALL | RFF_REMAP_LINES,
-    MODE_CGA_2,
-    MODE_PSEUDO_8,
-    gen_c2to8p_all,
     NULL
   ),
 
@@ -2247,6 +2247,43 @@ void gen_c2to8_all(RemapObject *ro)
       c0 >>= i;
       c0 &= 3;
       dst[d_x++] = lut[4 * c0 + (k ^= 1)];
+      s_x += *(bre_x++);
+    }
+  }
+}
+
+/*
+ * 2 bit CGA pseudo color --> 8 bit pseudo color (private color map)
+ * supports arbitrary scaling
+ *
+ * -- very basic and slow --
+ */
+void gen_c2to8p_all(RemapObject *ro)
+{
+  int d_x_len;
+  int s_x, d_x, d_y;
+  int d_scan_len = ro->dst_scan_len;
+  int *bre_x;
+  int *bre_y = ro->bre_y;
+  unsigned char c0;
+  int i;
+
+  const unsigned char *src, *src0;
+  unsigned char *dst;
+
+  src0 = ro->src_image + ro->src_start;
+  dst = ro->dst_image + ro->dst_start + ro->dst_offset;
+  d_x_len = ro->dst_width;
+
+  for (d_y = ro->dst_y0; d_y < ro->dst_y1; dst += d_scan_len) {
+    src = src0 + bre_y[d_y++];
+    for (s_x = d_x = 0, bre_x = ro->bre_x; d_x < d_x_len;) {
+      i = s_x >> 2;
+      c0 = src[i];
+      i = 2 * ((s_x & 3) ^ 3);
+      c0 >>= i;
+      c0 &= 3;
+      dst[d_x++] = c0;
       s_x += *(bre_x++);
     }
   }
@@ -3289,42 +3326,6 @@ void gen_16to32_all(RemapObject *ro)
     }
   }
 }
-
-/*
- * 2 bit CGA pseudo color --> 8 bit pseudo color (private color map)
- * supports arbitrary scaling
- *
- * -- very basic and slow --
- */
-void gen_c2to8p_all(RemapObject *ro)
-{
-  int d_x_len;
-  int s_x, d_x, d_y;
-  int d_scan_len = ro->dst_scan_len;
-  int *bre_x;
-  int *bre_y = ro->bre_y;
-  unsigned char c0;
-  int i;
-
-  const unsigned char *src, *src0;
-  unsigned char *dst;
-
-  src0 = ro->src_image + ro->src_start;
-  dst = ro->dst_image + ro->dst_start + ro->dst_offset;
-  d_x_len = ro->dst_width;
-
-  for(d_y = ro->dst_y0; d_y < ro->dst_y1; dst += d_scan_len) {
-    src = src0 + bre_y[d_y++];
-    for(s_x = d_x = 0, bre_x = ro->bre_x; d_x < d_x_len; ) {
-      i = s_x >> 2;
-      c0 = src[i];
-      i = 2 * ((s_x & 3) ^ 3); c0 >>= i; c0 &= 3;
-      dst[d_x++] = c0;
-      s_x += *(bre_x++);
-    }
-  }
-}
-
 
 /*
  * 2 bit CGA pseudo color --> 15/16 bit true color
