6e3c9b96e92419d88fe83c089d793ebf868b9ca9
Andrew Bird <ajb@spheresystems.co.uk>
2018-10-20 11:05:52 +0100

    Video: Sort remapper gen_2to16_all

--- src/base/video/remap.c
+++ src/base/video/remap.c
@@ -1511,6 +1511,14 @@ static RemapFuncDesc remap_gen_list[] = {
     NULL
   ),
 
+  REMAP_DESC(
+    RFF_SCALE_ALL | RFF_REMAP_LINES,
+    MODE_VGA_2,
+    MODE_TRUE_15 | MODE_TRUE_16,
+    gen_2to16_all,
+    NULL
+  ),
+
   // sort position (temporary comment)
 
   REMAP_DESC(
@@ -1734,14 +1742,6 @@ static RemapFuncDesc remap_gen_list[] = {
     MODE_TRUE_16,
     MODE_TRUE_32,
     gen_16to32_all,
-    NULL
-  ),
-
-  REMAP_DESC(
-    RFF_SCALE_ALL | RFF_REMAP_LINES,
-    MODE_VGA_2,
-    MODE_TRUE_15 | MODE_TRUE_16,
-    gen_2to16_all,
     NULL
   ),
 
@@ -2081,6 +2081,47 @@ void gen_2to8p_all(RemapObject *ro)
       c1 &= 1;
       c0 |= c1 << 1;
       dst[d_x++] = c0;
+      s_x += *(bre_x++);
+    }
+  }
+}
+
+/*
+ * 2 bit VGA pseudo color --> 15/16 bit true color
+ * supports arbitrary scaling
+ *
+ * -- very basic and slow --
+ */
+void gen_2to16_all(RemapObject *ro)
+{
+  int d_x_len;
+  int s_x, d_x, d_y;
+  int d_scan_len = ro->dst_scan_len >> 1;
+  int *bre_x;
+  int *bre_y = ro->bre_y;
+  unsigned char c0, c1;
+  int i;
+
+  const unsigned char *src, *src0;
+  unsigned short *dst;
+
+  src0 = ro->src_image + ro->src_start;
+  dst = (unsigned short *)(ro->dst_image + ro->dst_start + ro->dst_offset);
+  d_x_len = ro->dst_width;
+
+  for (d_y = ro->dst_y0; d_y < ro->dst_y1; dst += d_scan_len) {
+    src = src0 + bre_y[d_y++];
+    for (s_x = d_x = 0, bre_x = ro->bre_x; d_x < d_x_len;) {
+      i = s_x >> 3;
+      c0 = src[i];
+      c1 = src[i + 0x20000];
+      i = (s_x & 7) ^ 7;
+      c0 >>= i;
+      c1 >>= i;
+      c0 &= 1;
+      c1 &= 1;
+      c0 |= (c1 << 1);
+      dst[d_x++] = ro->true_color_lut[c0];
       s_x += *(bre_x++);
     }
   }
@@ -3123,45 +3164,6 @@ void gen_16to32_all(RemapObject *ro)
     }
   }
 }
-
-/*
- * 2 bit VGA pseudo color --> 15/16 bit true color
- * supports arbitrary scaling
- *
- * -- very basic and slow --
- */
-void gen_2to16_all(RemapObject *ro)
-{
-  int d_x_len;
-  int s_x, d_x, d_y;
-  int d_scan_len = ro->dst_scan_len >> 1;
-  int *bre_x;
-  int *bre_y = ro->bre_y;
-  unsigned char c0, c1;
-  int i;
-
-  const unsigned char *src, *src0;
-  unsigned short *dst;
-
-  src0 = ro->src_image + ro->src_start;
-  dst = (unsigned short *) (ro->dst_image + ro->dst_start + ro->dst_offset);
-  d_x_len = ro->dst_width;
-
-  for(d_y = ro->dst_y0; d_y < ro->dst_y1; dst += d_scan_len) {
-    src = src0 + bre_y[d_y++];
-    for(s_x = d_x = 0, bre_x = ro->bre_x; d_x < d_x_len; ) {
-      i = s_x >> 3;
-      c0 = src[i]; c1 = src[i + 0x20000];
-      i = (s_x & 7) ^ 7;
-      c0 >>= i; c1 >>= i;
-      c0 &= 1; c1 &= 1;
-      c0 |= (c1 << 1);
-      dst[d_x++] = ro->true_color_lut[c0];
-      s_x += *(bre_x++);
-    }
-  }
-}
-
 
 /*
  * 2 bit VGA pseudo color --> 24 bit true color
