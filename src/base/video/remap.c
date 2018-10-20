14d7bc7334186a2699a143440cbf283442a88820
Andrew Bird <ajb@spheresystems.co.uk>
2018-10-20 12:09:33 +0100

    Video: Sort remapper gen_24to32_all

--- src/base/video/remap.c
+++ src/base/video/remap.c
@@ -1767,6 +1767,14 @@ static RemapFuncDesc remap_gen_list[] = {
     NULL
   ),
 
+  REMAP_DESC(
+    RFF_SCALE_ALL  | RFF_REMAP_LINES,
+    MODE_TRUE_24,
+    MODE_TRUE_32,
+    gen_24to32_all,
+    NULL
+  ),
+
   // sort position (temporary comment)
 
   REMAP_DESC(
@@ -1790,14 +1798,6 @@ static RemapFuncDesc remap_gen_list[] = {
     MODE_TRUE_24,
     MODE_TRUE_32,
     gen_24to32_1,
-    NULL
-  ),
-
-  REMAP_DESC(
-    RFF_SCALE_ALL  | RFF_REMAP_LINES,
-    MODE_TRUE_24,
-    MODE_TRUE_32,
-    gen_24to32_all,
     NULL
   ),
 
@@ -3289,6 +3289,37 @@ void gen_24to24_1(RemapObject *ro)
   }
 }
 
+/*
+ * 24 bit true color --> 32 bit true color
+ * supports arbitrary scaling
+ */
+void gen_24to32_all(RemapObject *ro)
+{
+  int d_x_len;
+  int s_x, d_x, d_y;
+  int d_scan_len = ro->dst_scan_len;
+  int *bre_x;
+  int *bre_y = ro->bre_y;
+
+  const unsigned char *src, *src0;
+  unsigned char *dst;
+  unsigned *dst_4;
+
+  src0 = ro->src_image + ro->src_start;
+  dst = ro->dst_image + ro->dst_start + ro->dst_offset;
+  d_x_len = ro->dst_width;
+
+  for(d_y = ro->dst_y0; d_y < ro->dst_y1; dst += d_scan_len) {
+    src = src0 + bre_y[d_y++];
+    dst_4 = (unsigned *) dst;
+    for(s_x = d_x = 0, bre_x = ro->bre_x; d_x < d_x_len; ) {
+      RGBColor c = { src[s_x * 3 + 2], src[s_x * 3 + 1], src[s_x * 3] };
+      dst_4[d_x++] = rgb_color_2int(ro->dst_color_space, 8, 8, 8, c);
+      s_x += *(bre_x++);
+    }
+  }
+}
+
 // sort position (temporary comment)
 
 /*
@@ -3371,37 +3402,6 @@ void gen_24to32_1(RemapObject *ro)
 
     src += ro->src_scan_len;
     dst += ro->dst_scan_len;
-  }
-}
-
-/*
- * 24 bit true color --> 32 bit true color
- * supports arbitrary scaling
- */
-void gen_24to32_all(RemapObject *ro)
-{
-  int d_x_len;
-  int s_x, d_x, d_y;
-  int d_scan_len = ro->dst_scan_len;
-  int *bre_x;
-  int *bre_y = ro->bre_y;
-
-  const unsigned char *src, *src0;
-  unsigned char *dst;
-  unsigned *dst_4;
-
-  src0 = ro->src_image + ro->src_start;
-  dst = ro->dst_image + ro->dst_start + ro->dst_offset;
-  d_x_len = ro->dst_width;
-
-  for(d_y = ro->dst_y0; d_y < ro->dst_y1; dst += d_scan_len) {
-    src = src0 + bre_y[d_y++];
-    dst_4 = (unsigned *) dst;
-    for(s_x = d_x = 0, bre_x = ro->bre_x; d_x < d_x_len; ) {
-      RGBColor c = { src[s_x * 3 + 2], src[s_x * 3 + 1], src[s_x * 3] };
-      dst_4[d_x++] = rgb_color_2int(ro->dst_color_space, 8, 8, 8, c);
-      s_x += *(bre_x++);
-    }
   }
 }
 
