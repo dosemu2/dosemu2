dosemu2/dosemu2

commit 20ba2a7d66200495250a1e189b8fada11f18b1ec

cpu: fix ubsan error

src/include/cpu.h
@@ -54,6 +54,11 @@ union dword {
   struct { Bit8u l, h, b2, b3; } b;
 };
 
+union word {
+  Bit16u w;
+  struct { Bit8u l, h; } b;
+};
+
 union g_reg {
   greg_t reg;
 #ifdef __x86_64__
@@ -76,8 +81,8 @@ union g_reg {
 #define LO_WORD(wrd)		LO_WORD_(wrd,)
 #define HI_WORD(wrd)		HI_WORD_(wrd,)
 
-#define LO_BYTE_(wrd, c)	(((c union dword *)&(wrd))->b.l)
-#define HI_BYTE_(wrd, c)	(((c union dword *)&(wrd))->b.h)
+#define LO_BYTE_(wrd, c)	(((c union word *)&(wrd))->b.l)
+#define HI_BYTE_(wrd, c)	(((c union word *)&(wrd))->b.h)
 #define LO_BYTE(wrd)		LO_BYTE_(wrd,)
 #define HI_BYTE(wrd)		HI_BYTE_(wrd,)
 

commit 5e8fdb7abc3127e518b9d8275121d6a3390f3e7a

various: fix strict-aliasing problems caused by 20ba2a7d6

src/base/async/int.c
@@ -1645,7 +1645,7 @@ static int msdos_chainrevect(int stk_offs)
 static int msdos_xtra(int old_ax)
 {
     di_printf("int_rvc 0x21 call for ax=0x%04x %x\n", LWORD(eax), old_ax);
-    switch (HI_BYTE(old_ax)) {
+    switch (HI_BYTE_d(old_ax)) {
     case 0x71:
 	CARRY;
 	if (LWORD(eax) != 0x7100)

src/base/mouse/mouse.c
@@ -39,9 +39,9 @@
 #include "gcursor.h"
 #include "vgaemu.h"
 
-#define SETHIGH(x, v) HI_BYTE(x) = (v)
+#define SETHIGH(x, v) HI_BYTE_d(x) = (v)
 #define SETLO_WORD(x, v) LO_WORD(x) = (v)
-#define SETLO_BYTE(x, v) LO_BYTE(x) = (v)
+#define SETLO_BYTE(x, v) LO_BYTE_d(x) = (v)
 #define SETWORD(x, v) SETLO_WORD(x, v)
 
 #define MOUSE_RX mouse_roundx(get_mx())
@@ -227,7 +227,7 @@ mouse_helper(struct vm86_regs *regs)
 
   SETWORD(regs->eax, 0);		/* Set successful completion */
 
-  switch (LO_BYTE(regs->ebx)) {
+  switch (LO_BYTE_d(regs->ebx)) {
   case 0:				/* Reset iret for mouse */
     m_printf("MOUSE move iret !\n");
     mouse_enable_internaldriver();
@@ -254,21 +254,21 @@ mouse_helper(struct vm86_regs *regs)
     SETLO_BYTE(regs->edx, mice->ignorevesa);
     break;
   case 4:				/* Set vertical speed */
-    if (LO_BYTE(regs->ecx) < 1) {
+    if (LO_BYTE_d(regs->ecx) < 1) {
       m_printf("MOUSE Vertical speed out of range. ERROR!\n");
       SETWORD(regs->eax, 1);
     } else
-      mice->init_speed_y = LO_BYTE(regs->ecx);
+      mice->init_speed_y = LO_BYTE_d(regs->ecx);
     break;
   case 5:				/* Set horizontal speed */
-    if (LO_BYTE(regs->ecx) < 1) {
+    if (LO_BYTE_d(regs->ecx) < 1) {
       m_printf("MOUSE Horizontal speed out of range. ERROR!\n");
       SETWORD(regs->eax, 1);
     } else
-      mice->init_speed_x = LO_BYTE(regs->ecx);
+      mice->init_speed_x = LO_BYTE_d(regs->ecx);
     break;
   case 6:				/* Ignore vesa modes */
-    mice->ignorevesa = LO_BYTE(regs->ecx);
+    mice->ignorevesa = LO_BYTE_d(regs->ecx);
     break;
   case 7:				/* get minimum internal resolution */
     SETWORD(regs->ecx, mouse.min_max_x);

src/base/video/instremu.c
@@ -67,8 +67,8 @@
 #define COUNT  150	/* bail out when this #instructions were simulated
 			   after a VGA r/w access */
 
-#define R_LO(a) LO_BYTE(a)
-#define R_HI(a) HI_BYTE(a)
+#define R_LO(a) LO_BYTE_d(a)
+#define R_HI(a) HI_BYTE_d(a)
 #define R_WORD(a) LO_WORD(a)
 #define R_DWORD(a) (*((unsigned *) &(a)))
 #define AL (R_LO(x86->eax))

src/dosext/misc/emm.c
@@ -236,9 +236,9 @@ static void set_map_registers(const struct emm_reg *buf, int pages);
 
 #define SET_HANDLE_NAME(nameptr, name) \
 	{ memmove((nameptr), (name), 8); nameptr[8]=0; }
-#define SETHI_BYTE(x, v) HI_BYTE(x) = (v)
+#define SETHI_BYTE(x, v) HI_BYTE_d(x) = (v)
 #define SETLO_WORD(x, v) LO_WORD(x) = (v)
-#define SETLO_BYTE(x, v) LO_BYTE(x) = (v)
+#define SETLO_BYTE(x, v) LO_BYTE_d(x) = (v)
 #define UNCHANGED       2
 #define CHECK_HANDLE(handle) \
   if ((handle < 0) || (handle >= MAX_HANDLES)) { \
@@ -723,8 +723,8 @@ partial_map_registers(struct vm86_regs * state)
 {
   int ret;
   Kdebug0((dbg_fd, "partial_map_registers %d called\n",
-	   (int) LO_BYTE(state->eax)));
-  switch (LO_BYTE(state->eax)) {
+	   (int) LO_BYTE_d(state->eax)));
+  switch (LO_BYTE_d(state->eax)) {
   case PARTIAL_GET:
     ret = emm_get_partial_map_registers(Addr(state, es, edi),
 					Addr(state, ds, esi));
@@ -813,7 +813,7 @@ map_unmap_multiple(struct vm86_regs * state)
   int handle = LO_WORD(state->edx);
   int map_len;
   unsigned int array;
-  int method = LO_BYTE(state->eax);
+  int method = LO_BYTE_d(state->eax);
   int ret;
 
   Kdebug0((dbg_fd, "map_unmap_multiple %d called\n", method));
@@ -963,7 +963,7 @@ reallocate_pages(struct vm86_regs * state)
 static int
 handle_attribute(struct vm86_regs * state)
 {
-  switch (LO_BYTE(state->eax)) {
+  switch (LO_BYTE_d(state->eax)) {
   case GET_ATT:{
       int handle = LO_WORD(state->edx);
 
@@ -1000,7 +1000,7 @@ handle_attribute(struct vm86_regs * state)
 static void
 handle_name(struct vm86_regs * state)
 {
-  switch (LO_BYTE(state->eax)) {
+  switch (LO_BYTE_d(state->eax)) {
   case GET_NAME:{
       int handle = LO_WORD(state->edx);
       u_char *array = (u_char *) Addr(state, es, edi);
@@ -1035,7 +1035,7 @@ handle_name(struct vm86_regs * state)
     }
   default:
     Kdebug0((dbg_fd, "bad handle_name function %d\n",
-	     (int) LO_BYTE(state->eax)));
+	     (int) LO_BYTE_d(state->eax)));
     SETHI_BYTE(state->eax, EMM_FUNC_NOSUP);
     return;
   }
@@ -1044,9 +1044,9 @@ handle_name(struct vm86_regs * state)
 static void
 handle_dir(struct vm86_regs * state)
 {
-  Kdebug0((dbg_fd, "handle_dir %d called\n", (int) LO_BYTE(state->eax)));
+  Kdebug0((dbg_fd, "handle_dir %d called\n", (int) LO_BYTE_d(state->eax)));
 
-  switch (LO_BYTE(state->eax)) {
+  switch (LO_BYTE_d(state->eax)) {
   case GET_DIR:{
       int handle, count = 0;
       u_char *array = (u_char *) Addr(state, es, edi);
@@ -1099,7 +1099,7 @@ handle_dir(struct vm86_regs * state)
 
   default:
       Kdebug0((dbg_fd, "bad handle_dir function %d\n",
-	       (int) LO_BYTE(state->eax)));
+	       (int) LO_BYTE_d(state->eax)));
       SETHI_BYTE(state->eax, EMM_FUNC_NOSUP);
       return;
   }
@@ -1141,7 +1141,7 @@ struct __attribute__ ((__packed__)) alter_map_jmp_struct {
 static void
 alter_map_and_jump(struct vm86_regs * state)
 {
-  int method = LO_BYTE(state->eax);
+  int method = LO_BYTE_d(state->eax);
 
   Kdebug0((dbg_fd, "alter_map_and_jump %d called\n", method));
 
@@ -1192,7 +1192,7 @@ struct __attribute__ ((__packed__)) alter_map_call_struct {
 static void
 alter_map_and_call(struct vm86_regs * state)
 {
-  int method = LO_BYTE(state->eax);
+  int method = LO_BYTE_d(state->eax);
 
   Kdebug0((dbg_fd, "alter_map_and_call %d called\n", method));
 
@@ -1244,7 +1244,7 @@ alter_map_and_call(struct vm86_regs * state)
 
   default:
     Kdebug0((dbg_fd, "bad alter_map_and_call function %d\n",
-	     (int) LO_BYTE(state->eax)));
+	     (int) LO_BYTE_d(state->eax)));
     SETHI_BYTE(state->eax, EMM_FUNC_NOSUP);
     break;
   }
@@ -1484,7 +1484,7 @@ exchange_memory_region(struct vm86_regs * state)
 static int
 get_mpa_array(struct vm86_regs * state)
 {
-  switch (LO_BYTE(state->eax)) {
+  switch (LO_BYTE_d(state->eax)) {
   case GET_MPA:{
       u_short *ptr = (u_short *) Addr(state, es, edi);
       int i;
@@ -1558,7 +1558,7 @@ get_ems_hardinfo(struct vm86_regs * state)
 {
   if (os_allow)
      {
-    switch (LO_BYTE(state->eax)) {
+    switch (LO_BYTE_d(state->eax)) {
       case GET_ARRAY:{
         u_short *ptr = (u_short *) Addr(state, es, edi);
 
@@ -1676,7 +1676,7 @@ alternate_map_register(struct vm86_regs * state)
     Kdebug1((dbg_fd, "bios_emm: Illegal alternate_map_register\n"));
     return;
   }
-  switch (LO_BYTE(state->eax)) {
+  switch (LO_BYTE_d(state->eax)) {
     case 0:{			/* Get Alternate Map Register */
         if (save_es){
 	  Kdebug1((dbg_fd, "bios_emm: Get Alternate Map Registers\n"));
@@ -1709,15 +1709,15 @@ alternate_map_register(struct vm86_regs * state)
       return;
     case 5:			/* Allocate DMA */
       SETHI_BYTE(state->eax, EMM_NO_ERR);
-      Kdebug1((dbg_fd, "bios_emm: alternate_map_register Enable DMA bl=0x%x\n", (unsigned int)LO_BYTE(state->ebx)));
+      Kdebug1((dbg_fd, "bios_emm: alternate_map_register Enable DMA bl=0x%x\n", (unsigned int)LO_BYTE_d(state->ebx)));
       return;
     }
-    if (LO_BYTE(state->ebx)) {
+    if (LO_BYTE_d(state->ebx)) {
       SETHI_BYTE(state->eax, 0x8f);
-      Kdebug1((dbg_fd, "bios_emm: Illegal alternate_map_register request for reg set 0x%x\n", (unsigned int)LO_BYTE(state->ebx)));
+      Kdebug1((dbg_fd, "bios_emm: Illegal alternate_map_register request for reg set 0x%x\n", (unsigned int)LO_BYTE_d(state->ebx)));
       return;
       }
-  switch (LO_BYTE(state->eax)) {
+  switch (LO_BYTE_d(state->eax)) {
     case 1:{			/* Set Alternate Map Register */
 
 	 Kdebug1((dbg_fd, "bios_emm: Set Alternate Registers\n"));
@@ -1737,10 +1737,10 @@ alternate_map_register(struct vm86_regs * state)
       Kdebug1((dbg_fd, "bios_emm: alternate_map_register Deallocate Register\n"));
       break;
     case 6:			/* Enable DMA */
-      if (LO_BYTE(state->ebx) > 0)
+      if (LO_BYTE_d(state->ebx) > 0)
         {
 	  SETHI_BYTE(state->eax, EMM_INVALID_SUB);
-	  Kdebug1((dbg_fd, "bios_emm: alternate_map_register Enable DMA not allowed bl=0x%x\n", (unsigned int)LO_BYTE(state->ebx)));
+	  Kdebug1((dbg_fd, "bios_emm: alternate_map_register Enable DMA not allowed bl=0x%x\n", (unsigned int)LO_BYTE_d(state->ebx)));
 	}
       else
        {
@@ -1749,10 +1749,10 @@ alternate_map_register(struct vm86_regs * state)
        }
       break;
     case 7:			/* Disable DMA */
-      if (LO_BYTE(state->ebx) > 0)
+      if (LO_BYTE_d(state->ebx) > 0)
         {
 	  SETHI_BYTE(state->eax, EMM_INVALID_SUB);
-	  Kdebug1((dbg_fd, "bios_emm: alternate_map_register Disable DMA not allowed bl=0x%x\n", (unsigned int)LO_BYTE(state->ebx)));
+	  Kdebug1((dbg_fd, "bios_emm: alternate_map_register Disable DMA not allowed bl=0x%x\n", (unsigned int)LO_BYTE_d(state->ebx)));
 	}
       else
        {
@@ -1761,10 +1761,10 @@ alternate_map_register(struct vm86_regs * state)
        }
       break;
     case 8:			/* Deallocate DMA */
-      if (LO_BYTE(state->ebx) > 0)
+      if (LO_BYTE_d(state->ebx) > 0)
         {
 	  SETHI_BYTE(state->eax, EMM_INVALID_SUB);
-	  Kdebug1((dbg_fd, "bios_emm: alternate_map_register Deallocate DMA not allowed bl=0x%x\n", (unsigned int)LO_BYTE(state->ebx)));
+	  Kdebug1((dbg_fd, "bios_emm: alternate_map_register Deallocate DMA not allowed bl=0x%x\n", (unsigned int)LO_BYTE_d(state->ebx)));
 	}
       else
        {
@@ -1782,7 +1782,7 @@ alternate_map_register(struct vm86_regs * state)
 static void
 os_set_function(struct vm86_regs * state)
 {
-  switch (LO_BYTE(state->eax)) {
+  switch (LO_BYTE_d(state->eax)) {
     case 00:			/* Open access to EMS / Get OS key */
       if (os_inuse)
 	 {
@@ -1856,7 +1856,7 @@ os_set_function(struct vm86_regs * state)
 int
 ems_fn(struct vm86_regs *state)
 {
-  switch (HI_BYTE(state->eax)) {
+  switch (HI_BYTE_d(state->eax)) {
   case GET_MANAGER_STATUS:{	/* 0x40 */
       Kdebug1((dbg_fd, "bios_emm: Get Manager Status\n"));
 
@@ -1907,7 +1907,7 @@ ems_fn(struct vm86_regs *state)
       break;
     }
   case MAP_UNMAP:{		/* 0x44 */
-      int physical_page = LO_BYTE(state->eax);
+      int physical_page = LO_BYTE_d(state->eax);
       int logical_page = LO_WORD(state->ebx);
       int handle = LO_WORD(state->edx);
       int ret;
@@ -2087,7 +2087,7 @@ ems_fn(struct vm86_regs *state)
   case PAGE_MAP_REGISTERS:{	/* 0x4e */
       Kdebug1((dbg_fd, "bios_emm: Page Map Registers Function.\n"));
 
-      switch (LO_BYTE(state->eax)) {
+      switch (LO_BYTE_d(state->eax)) {
       case GET_REGISTERS:{
 	  Kdebug1((dbg_fd, "bios_emm: Get Registers\n"));
 	  emm_get_map_registers((char *) Addr(state, es, edi));
@@ -2160,7 +2160,7 @@ ems_fn(struct vm86_regs *state)
     break;
 
   case MOD_MEMORY_REGION:	/* 0x57 */
-    switch (LO_BYTE(state->eax)) {
+    switch (LO_BYTE_d(state->eax)) {
     case 0x00:
       SETHI_BYTE(state->eax, move_memory_region(state));
       break;
@@ -2170,7 +2170,7 @@ ems_fn(struct vm86_regs *state)
     default:
       NOFUNC();
     }
-    E_printf("EMS: MOD returns %x\n", (u_short)HI_BYTE(state->eax));
+    E_printf("EMS: MOD returns %x\n", (u_short)HI_BYTE_d(state->eax));
     break;
 
   case GET_MPA_ARRAY:		/* 0x58 */
@@ -2182,7 +2182,7 @@ ems_fn(struct vm86_regs *state)
     break;
 
   case ALLOCATE_STD_PAGES:	/* 0x5a */
-    switch (LO_BYTE(state->eax)) {
+    switch (LO_BYTE_d(state->eax)) {
     case 00:			/* Allocate Standard Pages */
     case 01:			/* Allocate Raw Pages */
       allocate_std_pages(state);

src/emu-i386/simx86/sigsegv.c
@@ -291,9 +291,9 @@ int e_vgaemu_fault(sigcontext_t *scp, unsigned page_fault)
 /*8a*/	case MOVbtrm:
 		if (_err&2) goto badrw;
 		if (p[1]==0x07)
-		    LO_BYTE(_eax) = e_VgaRead(LINP(_edi),MBYTE);
+		    _LO(ax) = e_VgaRead(LINP(_edi),MBYTE);
 		else if (p[1]==0x17)
-		    LO_BYTE(_edx) = e_VgaRead(LINP(_edi),MBYTE);
+		    _LO(dx) = e_VgaRead(LINP(_edi),MBYTE);
 		else goto unimp;
 		_rip = (long)(p+2); break;
 /*8b*/	case MOVwtrm:
@@ -344,7 +344,7 @@ int e_vgaemu_fault(sigcontext_t *scp, unsigned page_fault)
 /*ac*/	case LODSb: {
 		int d = (_eflags & EFLAGS_DF? -1:1);
 		if (_err&2) goto badrw;
-		LO_BYTE(_eax) = e_VgaRead(LINP(_esi),MBYTE);
+		_LO(ax) = e_VgaRead(LINP(_esi),MBYTE);
 		_esi+=d;
 		_rip = (long)(p+1); } break;
 /*ad*/	case LODSw: {

src/env/builtins/emumouse.c
@@ -36,9 +36,9 @@
 #include "emumouse.h"
 
 #define printf  com_printf
-#define SETHIGH(x, v) HI_BYTE(x) = (v)
+#define SETHIGH(x, v) HI_BYTE_d(x) = (v)
 #define SETLO_WORD(x, v) LO_WORD(x) = (v)
-#define SETLO_BYTE(x, v) LO_BYTE(x) = (v)
+#define SETLO_BYTE(x, v) LO_BYTE_d(x) = (v)
 #define SETWORD(x, v) SETLO_WORD(x, v)
 
 /* Show help screen */
@@ -133,13 +133,13 @@ int emumouse_main(int argc, char *argv[])
 	printf("\nCurrent mouse setting:\n");
 	SETWORD(regs.ebx, 0x0003);
 	mouse_helper(&regs);
-	if (HI_BYTE(regs.ebx) == 0x10)
+	if (HI_BYTE_d(regs.ebx) == 0x10)
 	  printf("  2 button mouse mode (Microsoft)\n");
 	else
 	  printf("  3 button mouse mode (e.g. Mousesystems, PS2)\n");
-	printf  ("  Horizontal Speed (X) - %d\n", LO_BYTE(regs.ecx));
-	printf  ("  Vertical Speed   (Y) - %d\n", HI_BYTE(regs.ecx));
-	printf  ("  Ignore VESA modes    - %s\n\n", LO_BYTE(regs.edx) ? "yes" : "no");
+	printf  ("  Horizontal Speed (X) - %d\n", LO_BYTE_d(regs.ecx));
+	printf  ("  Vertical Speed   (Y) - %d\n", HI_BYTE_d(regs.ecx));
+	printf  ("  Ignore VESA modes    - %s\n\n", LO_BYTE_d(regs.edx) ? "yes" : "no");
 	SETWORD(regs.ebx, 0x0007);
 	mouse_helper(&regs);
 	if (LO_WORD(regs.eax) == 0) {
@@ -152,7 +152,7 @@ int emumouse_main(int argc, char *argv[])
 	printf("Selecting 3 button mouse mode (e.g. Mousesystems, PS2).\n");
 	SETWORD(regs.ebx, 0x0002);
 	mouse_helper(&regs);
-	if (LO_BYTE(regs.eax) == 0xff) {
+	if (LO_BYTE_d(regs.eax) == 0xff) {
 	  printf("ERROR! Cannot select 3 button mouse mode, \"emulate3buttons\" not set\n");
 	  printf("       in /etc/dosemu.conf, try e.g.\n");
 	  printf("       'mouse { ps2 device /dev/mouse internaldriver emulate3buttons }'\n");

src/include/cpu.h
@@ -80,11 +80,17 @@ union g_reg {
 #define HI_WORD_(wrd, c)	(((c union dword *)&(wrd))->w.h)
 #define LO_WORD(wrd)		LO_WORD_(wrd,)
 #define HI_WORD(wrd)		HI_WORD_(wrd,)
-
+#if 0
+#define LO_BYTE_(wrd, c)	(((c union word *)({static_assert(sizeof(wrd)==2, "bad reg"); &(wrd);}))->b.l)
+#define HI_BYTE_(wrd, c)	(((c union word *)({static_assert(sizeof(wrd)==2, "bad reg"); &(wrd);}))->b.h)
+#else
 #define LO_BYTE_(wrd, c)	(((c union word *)&(wrd))->b.l)
 #define HI_BYTE_(wrd, c)	(((c union word *)&(wrd))->b.h)
+#endif
 #define LO_BYTE(wrd)		LO_BYTE_(wrd,)
 #define HI_BYTE(wrd)		HI_BYTE_(wrd,)
+#define LO_BYTE_d(wrd)	(((union dword *)&(wrd))->b.l)
+#define HI_BYTE_d(wrd)	(((union dword *)&(wrd))->b.h)
 
 #define _AL      LO(ax)
 #define _BL      LO(bx)
@@ -125,8 +131,8 @@ union g_reg {
 #define LO(reg)  vm86u.b[offsetof(struct vm86_struct, regs.e##reg)]
 #define HI(reg)  vm86u.b[offsetof(struct vm86_struct, regs.e##reg)+1]
 
-#define _LO(reg) LO_BYTE(_##e##reg)
-#define _HI(reg) HI_BYTE(_##e##reg)
+#define _LO(reg) LO_BYTE_d(_##e##reg)
+#define _HI(reg) HI_BYTE_d(_##e##reg)
 
 /* these are used like: LWORD(eax) = 65535 (sets ax to 65535) */
 #define LWORD(reg)	(*({ static_assert(sizeof(REGS.reg) == 4, "bad reg"); \