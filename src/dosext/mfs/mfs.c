d8851412174dbe88f79c3f4599c56f5e13380720
Stas Sergeev <stsp@users.sourceforge.net>
2019-03-18 00:58:39 +0300

    mfs: disable redirector stacking
    This now causes infinite loops as FDPP passes the int back,
    and it goes to MFS again.

--- src/dosext/mfs/mfs.c
+++ src/dosext/mfs/mfs.c
@@ -2455,12 +2455,16 @@ GetRedirection(struct vm86_regs *state, u_short index)
       }
     }
   }
-
+#if 0
   /* if we dont own this index, pass down */
   redirected_drives = WORD(state->ebx) - index;
   SETWORD(&(state->ebx), index);
   Debug0((dbg_fd, "GetRedirect passing index of %d, Total redirected=%d\n", index, redirected_drives));
   return (REDIRECT);
+#else
+  SETWORD(&(state->eax), NO_MORE_FILES);
+  return (FALSE);
+#endif
 }
 
 /*****************************
