f957461ffafec72eaf9e2974d7fe924a8b57ef50
Stas Sergeev <stsp@users.sourceforge.net>
2019-06-06 15:19:20 +0300

    keyb_SDL.c: update copyrights
    Since this file is rewritten, lets proclaim myself as an author. :)

--- src/plugin/sdl/keyb_SDL.c
+++ src/plugin/sdl/keyb_SDL.c
@@ -1,12 +1,23 @@
 /*
- * (C) Copyright 1992, ..., 2014 the "DOSEMU-Development-Team".
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
  *
- * for details see file COPYING in the DOSEMU distribution
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
 
 /*
- * Authors: Emmanuel Jeandel, Bart Oldeman
- * SDL2 and XKB support by Stas Sergeev
+ * Purpose: SDL keyboard events handling
+ *
+ * Author: Stas Sergeev
  */
 
 #include <SDL.h>
