6123e163104a8d0210cdff9bcd1fade40091df8b
Stas Sergeev <stsp@users.sourceforge.net>
2019-08-11 01:35:59 +0300

    reenable kbd delay hack to help MOS

--- src/base/dev/misc/kbd.c
+++ src/base/dev/misc/kbd.c
@@ -52,8 +52,8 @@
  */
 #define KEYBUF_HACK 0
 
-/* the below hacks are disabled in favour of bios-assisted hack in serv_8042.c */
-#define USE_KBD_DELAY 0
+/* the below hack helps MOS to read arrow keys */
+#define USE_KBD_DELAY 1
 #define KBD_PIC_HACK 1
 
 /********** QUEUE ***********/
