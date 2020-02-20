dosemu2/dosemu2

commit f712d54d84be8cf1933b74a6fe68e5973140d231

mfs: FindFirst should return PATH_NOT_FOUND [fixes #360]
  If a directory specified in the path to search doesn't exist, the
proper return code is PATH_NOT_FOUND.

  c:\notexist\filename.ext  # should return PATH_NOT_FOUND
  c:\exists\notexist.???    # should return NO_MORE_FILES

  Use the simple patch from @stsp as per
https://github.com/stsp/dosemu2/pull/980#issuecomment-573414778

src/dosext/mfs/mfs.c
@@ -1330,13 +1330,14 @@ static struct dir_list *get_dir(char *name, char *mname, char *mext, int drive)
   struct mfs_dirent *cur_ent;
   struct dir_list *dir_list;
   struct dir_ent *entry;
-  struct stat sbuf;
   char buf[256];
   char fname[8];
   char fext[3];
 
-  if(is_dos_device(name) || !find_file(name, &sbuf, drive, NULL))
+  if (is_dos_device(name)) {
+    Debug0((dbg_fd, "get_dir(): is_dos_device() returned true for '%s'\n", name));
     return NULL;
+  }
 
   if ((cur_dir = dos_opendir(name)) == NULL) {
     Debug0((dbg_fd, "get_dir(): couldn't open '%s' errno = %s\n", name, strerror(errno)));
@@ -1372,6 +1373,8 @@ static struct dir_list *get_dir(char *name, char *mname, char *mext, int drive)
   /* for efficiency we don't read everything if there are no wildcards */
   else if (mname && !memchr(mname, '?', 8) && !memchr(mext, '?', 3))
   {
+    struct stat sbuf;
+
     dos83_to_ufs(buf, mname, mext);
     if (exists(name, buf, &sbuf, drive))
     {
@@ -4133,21 +4136,18 @@ do_create_truncate:
 
       bs_pos = getbasename(fpath);
       *bs_pos = '\0';
-      /* for efficiency we don't read everything if there are no wildcards */
-      if (!memchr(sdb_template_name(sdb), '?', 8) && !memchr(sdb_template_ext(sdb), '?', 3)) {
-        hlist = get_dir(fpath, sdb_template_name(sdb), sdb_template_ext(sdb), drive);
-        if (hlist == NULL) {
-          SETWORD(&(state->eax), PATH_NOT_FOUND);
-          return FALSE;
-        }
-        bs_pos = NULL;
-      } else {
-        hlist = get_dir(fpath, NULL, NULL, drive);
-        if (hlist == NULL) {
-          SETWORD(&(state->eax), NO_MORE_FILES);
-          return FALSE;
-        }
+
+      if (!find_file(fpath, &st, drive, NULL)) {
+        Debug0((dbg_fd, "get_dir(): find_file() returned false for '%s'\n", fpath));
+        SETWORD(&(state->eax), PATH_NOT_FOUND);
+        return FALSE;
       }
+      hlist = get_dir(fpath, sdb_template_name(sdb), sdb_template_ext(sdb), drive);
+      if (hlist == NULL) {
+        SETWORD(&(state->eax), NO_MORE_FILES);
+        return FALSE;
+      }
+
       if (long_path) {
         set_long_path_on_dirs(hlist);
       }

commit 6c5c88603f2754b50cfcbfe00f672397ff9adced

MFS: fix regressions introduced by commit f712d54
The PATH_NOT_FOUND/NO_MORE_FILES logic is correct but there were multiple
issues:
1. Single-file searches (without wildcards) could now be marked as duplicate
(that's what the deleted line with "bs_pos = 0" prevented before).
This broke "wcl *.c" in FreeCOM compilation by Open Watcom.
2. Wildcard searches were no longer as optimal. What happened before is that
if there is any wildcard than the whole directory is obtained but files are
not stat-ed yet, to delay the expensive part to findnext (which may never
happen for 2nd and subsequent matches).
3. There are two other places that also call get_dir(): wildcard rename and
wildcard delete. Those are not as widely used (mostly via FCBs) but they lost
their necessary calls to find_file().

(The duplication logic is part of a general garbage collection strategy,
as DOS has no findclose() for short file names.)

src/dosext/mfs/mfs.c
@@ -1324,18 +1324,24 @@ static int convert_compare(const char *d_name, char *fname, char *fext,
    mext = DOS (uppercase) extension to match (can have wildcards)
    drive = drive on which the directory lives
 */
-static struct dir_list *get_dir(char *name, char *mname, char *mext, int drive)
+static struct dir_list *get_dir(char *name, char *mname, char *mext, int drive, int *doserrno)
 {
   struct mfs_dir *cur_dir;
   struct mfs_dirent *cur_ent;
   struct dir_list *dir_list;
   struct dir_ent *entry;
+  struct stat sbuf;
   char buf[256];
   char fname[8];
   char fext[3];
 
+  *doserrno = PATH_NOT_FOUND;
   if (is_dos_device(name)) {
     Debug0((dbg_fd, "get_dir(): is_dos_device() returned true for '%s'\n", name));
+    return NULL;
+  }
+  if (!find_file(name, &sbuf, drive, NULL)) {
+    Debug0((dbg_fd, "get_dir(): find_file() returned false for '%s'\n", name));
     return NULL;
   }
 
@@ -1346,6 +1352,7 @@ static struct dir_list *get_dir(char *name, char *mname, char *mext, int drive)
 
   Debug0((dbg_fd, "get_dir() opened '%s'\n", name));
 
+  *doserrno = NO_MORE_FILES;
   dir_list = NULL;
 
 /* DANG_BEGIN_REMARK
@@ -3784,9 +3791,9 @@ static int dos_fs_redirect(struct vm86_regs *state)
       build_ufs_path(fpath, filename1, drive);
       bs_pos = getbasename(fpath);
       *bs_pos = '\0';
-      dir_list = get_dir(fpath, fname, fext, drive);
+      dir_list = get_dir(fpath, fname, fext, drive, &doserrno);
       if (!dir_list) {
-        SETWORD(&(state->eax), PATH_NOT_FOUND);
+        SETWORD(&(state->eax), doserrno);
         return FALSE;
       }
 
@@ -3827,7 +3834,7 @@ static int dos_fs_redirect(struct vm86_regs *state)
 
       bs_pos = getbasename(fpath);
       *bs_pos = '\0';
-      dir_list = get_dir(fpath, fname, fext, drive);
+      dir_list = get_dir(fpath, fname, fext, drive, &doserrno);
 
       if (dir_list == NULL) {
         build_ufs_path(fpath, filename1, drive);
@@ -4139,14 +4146,18 @@ do_create_truncate:
       bs_pos = getbasename(fpath);
       *bs_pos = '\0';
 
-      if (!find_file(fpath, &st, drive, NULL)) {
-        Debug0((dbg_fd, "get_dir(): find_file() returned false for '%s'\n", fpath));
-        SETWORD(&(state->eax), PATH_NOT_FOUND);
-        return FALSE;
+      if (!memchr(sdb_template_name(sdb), '?', 8) && !memchr(sdb_template_ext(sdb), '?', 3)) {
+        /* for efficiency we don't read everything if there are no wildcards */
+        hlist = get_dir(fpath, sdb_template_name(sdb), sdb_template_ext(sdb), drive, &doserrno);
+	/* make sure this single-file search is not marked as a duplicate */
+        bs_pos = NULL;
+      } else {
+        /* if there are any wildcards, get the list of ALL files and
+           delay the stat()ing of files to prove existence to findnext  */
+        hlist = get_dir(fpath, NULL, NULL, drive, &doserrno);
       }
-      hlist = get_dir(fpath, sdb_template_name(sdb), sdb_template_ext(sdb), drive);
       if (hlist == NULL) {
-        SETWORD(&(state->eax), NO_MORE_FILES);
+        SETWORD(&(state->eax), doserrno);
         return FALSE;
       }
 