e340b209d84d093d1877ec3d7f81d47bcaa4077c
Bart Oldeman <bartoldeman@users.noreply.github.com>
2020-02-20 20:48:37 -0500

    KVM: preserve RF as safe flag
    KVM is sometimes interrupted with X86_EFLAGS_RF set, and that bit was
    unconditionally reset, which caused unnecessary register sets. It is now
    copied back and forth from DOSEMU's eflags. This is consistent with how the
    Linux kernel handles signals (using FIX_EFLAGS).

--- src/base/emu-i386/kvm.c
+++ src/base/emu-i386/kvm.c
@@ -45,7 +45,8 @@
 #define SAFE_MASK (X86_EFLAGS_CF|X86_EFLAGS_PF| \
                    X86_EFLAGS_AF|X86_EFLAGS_ZF|X86_EFLAGS_SF| \
                    X86_EFLAGS_TF|X86_EFLAGS_DF|X86_EFLAGS_OF| \
-                   X86_EFLAGS_NT|X86_EFLAGS_AC|X86_EFLAGS_ID) // 0x244dd5
+                   X86_EFLAGS_RF| \
+                   X86_EFLAGS_NT|X86_EFLAGS_AC|X86_EFLAGS_ID) // 0x254dd5
 #define RETURN_MASK (SAFE_MASK | 0x28 | X86_EFLAGS_FIXED) // 0x244dff
 
 extern char kvm_mon_start[];
