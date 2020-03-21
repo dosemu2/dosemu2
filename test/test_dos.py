1752e8f0cddf4d5797762026cb95d7a96f653411
Andrew Bird <ajb@spheresystems.co.uk>
2020-03-21 10:47:28 +0000

    Travis: add lock file twice test

--- test/test_dos.py
+++ test/test_dos.py
@@ -4838,6 +4838,124 @@ $_floppy_a = ""
         """FAT DOSv2 set file time"""
         self._test_ds2_set_ftime("FAT")
 
+    def _test_ds3_lock_twice(self, fstype):
+        testdir = "test-imagedir/dXXXXs/d"
+
+        mkfile("testit.bat", """\
+d:\r
+%s\r
+c:\\lcktwice primary\r
+rem end\r
+""" % ("rem Internal share" if self.version == "FDPP kernel" else "c:\\share"))
+
+        # compile sources
+        mkexe("lcktwice", r"""
+/* Most of this was copied from DJGPP docs at
+   http://www.delorie.com/djgpp/doc/libc/libc_181.html */
+
+#include <dos.h>
+#include <dir.h>
+#include <fcntl.h>
+#include <io.h>
+#include <process.h>
+#include <stdio.h>
+#include <string.h>
+
+#define FNAME "FOO.DAT"
+
+int main(int argc, char *argv[]) {
+
+  int handle;
+  int ret;
+  int primary = -1;
+
+  if (argc < 2) {
+    printf("FAIL: Missing argument (primary|secondary)\n");
+    return -2;
+  }
+  if (strcmp(argv[1], "primary") == 0)
+    primary = 1;
+  if (strcmp(argv[1], "secondary") == 0)
+    primary = 0;
+  if (primary < 0) {
+    printf("FAIL: Invalid argument (primary|secondary)\n");
+    return -2;
+  }
+
+  if (primary) {
+    int rc;
+    ret = _dos_creat(FNAME, _A_NORMAL, &handle);
+    if (ret == 0)
+      _dos_write(handle, "hello\n", 6, &rc);  // something to lock on
+  } else {
+    ret = _dos_open(FNAME, O_RDWR, &handle);
+  }
+  if (ret != 0) {
+    printf("FAIL: %s: File '%s' not created or opened\n", argv[1], FNAME);
+    return -1;
+  }
+
+  ret = _dos_lock(handle, 0,  1); // just the first byte
+  if (ret != 0) {
+    if (primary) {
+      printf("FAIL: %s: Could not get lock on file '%s'\n", argv[1], FNAME);
+      _dos_close(handle);
+      return -1;
+    } else {
+      printf("PASS: %s: Could not get lock on file '%s'\n", argv[1], FNAME);
+      _dos_close(handle);
+      return 0;
+    }
+  } else {
+    if (primary) {
+      printf("OKAY: %s: Acquired lock on file '%s'\n", argv[1], FNAME);
+
+      // Now start second copy
+      spawnlp(P_WAIT, argv[0], argv[0], "secondary", NULL);
+
+    } else {
+      printf("FAIL: %s: Acquired lock on file '%s'\n", argv[1], FNAME);
+      _dos_unlock(handle, 0,  1);
+      _dos_close(handle);
+      return -1;
+    }
+  }
+
+  _dos_unlock(handle, 0,  1);
+  _dos_close(handle);
+  return 0;
+}
+""")
+
+        makedirs(testdir)
+
+        if fstype == "MFS":
+            config="""\
+$_hdimage = "dXXXXs/c:hdtype1 dXXXXs/d:hdtype1 +1"
+$_floppy_a = ""
+"""
+        else:       # FAT
+            files = [(x, 0) for x in listdir(testdir)]
+
+            name = self.mkimage("12", files, bootblk=False, cwd=testdir)
+            config="""\
+$_hdimage = "dXXXXs/c:hdtype1 %s +1"
+$_floppy_a = ""
+""" % name
+
+        results = self.runDosemu("testit.bat", config=config)
+
+        self.assertNotIn("FAIL:", results)
+        self.assertIn("PASS:", results)
+
+#    def test_mfs_ds3_lock_twice(self):
+#        """MFS DOSv3 lock file twice"""
+#        self._test_ds3_lock_twice("MFS")
+
+    def test_fat_ds3_lock_twice(self):
+        """FAT DOSv3 lock file twice"""
+        self._test_ds3_lock_twice("FAT")
+
     def _test_cpu(self, cpu_vm, cpu_vm_dpmi, cpu_emu):
         testdir = "test-imagedir/dXXXXs/d"
         makedirs(testdir)
@@ -4914,6 +5032,7 @@ class FRDOS120TestCase(OurTestCase, unittest.TestCase):
         cls.files = [
             ("kernel.sys", "0709f4e7146a8ad9b8acb33fe3fed0f6da9cc6e0"),
             ("command.com", "0733db7babadd73a1b98e8983c83b96eacef4e68"),
+            ("share.com", "cadc29d49115cb3a250f90921cca345e7c427464"),
         ]
         cls.systype = SYSTYPE_FRDOS_NEW
         cls.bootblocks = [
@@ -4964,6 +5083,7 @@ class MSDOS622TestCase(OurTestCase, unittest.TestCase):
             ("io.sys", "d697961ca6edaf9a1aafe8b7eb949597506f7f95"),
             ("msdos.sys", "d6a5f54006e69c4407e56677cd77b82395acb60a"),
             ("command.com", "c2179d2abfa241edd388ab875cfabbac89fec44d"),
+            ("share.exe", "9e7385cfa91a012638520e89b9884e4ce616d131"),
         ]
         cls.systype = SYSTYPE_MSDOS_INTERMEDIATE
         cls.bootblocks = [
