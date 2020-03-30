7b64546b405561aabcb10b3e69f8da2abdfe6364
Stas Sergeev <stsp@users.sourceforge.net>
2020-03-30 18:22:33 +0300

    conf: implement --with-plugin-options [#1070]

--- scripts/plugctl.sh
+++ scripts/plugctl.sh
@@ -36,7 +36,8 @@ if [ "$on" = "yes" -a -f $SRCDIR/$dir/$CONFIGURE ]; then
 	if [ ! -f ./configure ]; then
 		on="no"
 	else
-		if ! ./configure ; then
+		eval OPTS=\$${dir}_OPTS
+		if ! eval ./configure $OPTS; then
 			echo "Configuration for $dir failed, disabling"
 			on="no"
 		fi
