e8d43fe46cc5f7b6cbbf8792bb6c91275c4f7ddc
Bart Oldeman <bartoldeman@users.sourceforge.net>
2020-04-11 15:35:21 -0400

    Avoid FetchL(PC)==0 in debug statement that can potentially crash

--- src/base/emu-i386/simx86/interp.c
+++ src/base/emu-i386/simx86/interp.c
@@ -437,7 +437,7 @@ static unsigned int _Interp86(unsigned int PC, int basemode)
 			}
 		}
 		P0 = PC;	// P0 changes on instruction boundaries
-		if (PC==0 || FetchL(PC)==0 || debug_level('e')) {
+		if (PC==0 || debug_level('e')>1) {
 			e_printf("\n%s\nFetch %08x at %08x mode %x\n",
 				e_print_regs(),FetchL(PC),PC,mode);
 		}
