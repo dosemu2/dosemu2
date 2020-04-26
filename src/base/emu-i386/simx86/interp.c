dosemu2/dosemu2

commit a1174847fce7adbdf189b846f952f90bdb135329

simx86: generate code for direct vm86 far call/jmp.
Those can be linked too. Keep interpreting for protected mode, as that is
more complex.

src/base/emu-i386/simx86/interp.c
@@ -155,8 +155,15 @@ static unsigned int _JumpGen(unsigned int P2, int mode, int cond,
 	 *	eb ff	dsp=1	illegal or tricky
 	 *	eb fe	dsp=0	loop forever
 	 */
-	dsp = 0;
-	if (cond != 0x40) {	// not indirect jump
+	if (cond == 0x40) {	// indirect jump
+		dsp = 0;
+	}
+	else if (pskip == 3 + BT24(BitDATA16,mode)) { // jar jmp/call
+		d_t = DataFetchWL_U(mode, P2+1);
+		j_t = d_t + (FetchW(P2 + pskip - 2) << 4);
+		dsp = j_t - P2;
+	}
+	else {
 		dsp = pskip;
 		if (pskip == 2)	// short branch (byte)
 			dsp += (signed char)Fetch(P2+1);
@@ -1509,7 +1516,31 @@ intop3b:		{ int op = ArOpsFR[D_MO(opc)];
 		   break;
 
 /*9a*/	case CALLl:
-/*ea*/	case JMPld: {
+/*ea*/	case JMPld:
+		    if (REALADDR()) {
+			int len = 3 + BT24(BitDATA16,mode);
+			unsigned short jcs = FetchW(PC + len - 2);
+			dosaddr_t oip = 0;
+			if (opc==CALLl) {
+			    /* ok, now push old cs ({e}ip pushed in JumpGen) */
+			    ocs = TheCPU.cs;
+			    oip = PC + len - LONG_CS;
+			    Gen(L_REG, mode, Ofs_CS);
+			    Gen(O_PUSH, mode);
+			}
+			Gen(L_IMM, mode, Ofs_CS, jcs);
+			AddrGen(A_SR_SH4, mode, Ofs_CS, Ofs_XCS);
+			PC = JumpGen(PC, mode, 0x10+(opc==CALLl), len);
+			if (debug_level('e')>2) {
+			    if (opc==CALLl)
+				e_printf("CALL_FAR: ret=%04x:%08x\n  calling:	   %04x:%08x\n",
+					 ocs,oip,TheCPU.cs,PC-LONG_CS);
+			    else
+				e_printf("JMP_FAR: %04x:%08x\n",TheCPU.cs,PC-LONG_CS);
+			}
+			if (TheCPU.err) return PC;
+		    }
+		    else {
 			unsigned short jcs;
 			unsigned long oip,xcs,jip=0;
 			CODE_FLUSH();

src/base/emu-i386/simx86/tables.c
@@ -242,7 +242,7 @@ unsigned char InterOps[256] =
 	0x00,	// XCHGdi		0x97
 	0x00,	// CBW			0x98
 	0x00,	// CWD			0x99
-	0x01,	// CALLl		0x9a
+	0x00,	// CALLl		0x9a
 	0x00,	// WAIT			0x9b
 	0x00,	// PUSHF		0x9c
 	0x21,	// POPF			0x9d
@@ -322,7 +322,7 @@ unsigned char InterOps[256] =
 	0x01,	// OUTw			0xe7
 	0x80,	// CALLd		0xe8
 	0x82,	// JMPd			0xe9
-	0x81,	// JMPld		0xea
+	0x80,	// JMPld		0xea
 	0x82,	// JMPsid		0xeb
 #ifdef CPUEMU_DIRECT_IO
 	0x00,	// INvb			0xec

commit d7a1bafc698b1e41b6c1299c2c16717fdc2c8461

simx86: fix sim mode regression from a1174847
The issue with call far seg:ofs in sim mode is that CS is immediately set to
the new value but JumpGen still expected the old value, and pushed the
wrong return address. Fix this by generating everything in JumpGen and by
making it remember the old CS base.

src/base/emu-i386/simx86/interp.c
@@ -147,6 +147,8 @@ static unsigned int _JumpGen(unsigned int P2, int mode, int cond,
 	unsigned int P1;
 	int dsp;
 	unsigned int d_t, d_nt, j_t, j_nt;
+	/* must use the old cs for far jumps! */
+	dosaddr_t old_long_cs = LONG_CS;
 
 	/* pskip points to start of next instruction
 	 * dsp is the displacement relative to this jump instruction,
@@ -159,8 +161,17 @@ static unsigned int _JumpGen(unsigned int P2, int mode, int cond,
 		dsp = 0;
 	}
 	else if (pskip == 3 + BT24(BitDATA16,mode)) { // jar jmp/call
+		unsigned short jcs = FetchW(P2 + pskip - 2);
+		if (cond == 0x11) { /* call, push old cs first */
+			Gen(L_REG, mode, Ofs_CS);
+			Gen(O_PUSH, mode);
+		}
+		/* This immediately changes TheCPU.cs in sim mode but not
+		   yet in jit mode! */
+		Gen(L_IMM, mode, Ofs_CS, jcs);
+		AddrGen(A_SR_SH4, mode, Ofs_CS, Ofs_XCS);
 		d_t = DataFetchWL_U(mode, P2+1);
-		j_t = d_t + (FetchW(P2 + pskip - 2) << 4);
+		j_t = SEGOFF2LINEAR(jcs, d_t);
 		dsp = j_t - P2;
 	}
 	else {
@@ -180,11 +191,11 @@ static unsigned int _JumpGen(unsigned int P2, int mode, int cond,
 	}
 
 	/* displacement for not taken branch */
-	d_nt = P2 - LONG_CS + pskip;
+	d_nt = P2 - old_long_cs + pskip;
 	if (mode&DATA16) d_nt &= 0xffff;
 
 	/* jump address for not taken branch, usually next instruction */
-	j_nt = d_nt + LONG_CS;
+	j_nt = d_nt + old_long_cs;
 	*r_P0 = j_nt;
 
 	P1 = P2 + pskip;
@@ -1519,17 +1530,8 @@ intop3b:		{ int op = ArOpsFR[D_MO(opc)];
 /*ea*/	case JMPld:
 		    if (REALADDR()) {
 			int len = 3 + BT24(BitDATA16,mode);
-			unsigned short jcs = FetchW(PC + len - 2);
-			dosaddr_t oip = 0;
-			if (opc==CALLl) {
-			    /* ok, now push old cs ({e}ip pushed in JumpGen) */
-			    ocs = TheCPU.cs;
-			    oip = PC + len - LONG_CS;
-			    Gen(L_REG, mode, Ofs_CS);
-			    Gen(O_PUSH, mode);
-			}
-			Gen(L_IMM, mode, Ofs_CS, jcs);
-			AddrGen(A_SR_SH4, mode, Ofs_CS, Ofs_XCS);
+			dosaddr_t oip = PC + len - LONG_CS;
+			ocs = TheCPU.cs;
 			PC = JumpGen(PC, mode, 0x10+(opc==CALLl), len);
 			if (debug_level('e')>2) {
 			    if (opc==CALLl)