dosemu2/dosemu2

commit 507e05fef486b33f5c5d14cad19dd3ad13ad7b55

simx86: for far jmp/calls group all Gen calls together.
A cleaner way to implement d7a1baf since LONG_CS stays at the old value then.

src/base/emu-i386/simx86/interp.c
@@ -147,8 +147,6 @@ static unsigned int _JumpGen(unsigned int P2, int mode, int cond,
 	unsigned int P1;
 	int dsp;
 	unsigned int d_t, d_nt, j_t, j_nt;
-	/* must use the old cs for far jumps! */
-	dosaddr_t old_long_cs = LONG_CS;
 
 	/* pskip points to start of next instruction
 	 * dsp is the displacement relative to this jump instruction,
@@ -160,18 +158,9 @@ static unsigned int _JumpGen(unsigned int P2, int mode, int cond,
 	if (cond == 0x40) {	// indirect jump
 		dsp = 0;
 	}
-	else if (pskip == 3 + BT24(BitDATA16,mode)) { // jar jmp/call
-		unsigned short jcs = FetchW(P2 + pskip - 2);
-		if (cond == 0x11) { /* call, push old cs first */
-			Gen(L_REG, mode, Ofs_CS);
-			Gen(O_PUSH, mode);
-		}
-		/* This immediately changes TheCPU.cs in sim mode but not
-		   yet in jit mode! */
-		Gen(L_IMM, mode, Ofs_CS, jcs);
-		AddrGen(A_SR_SH4, mode, Ofs_CS, Ofs_XCS);
+	else if (pskip == 3 + BT24(BitDATA16,mode)) { // far jmp/call
 		d_t = DataFetchWL_U(mode, P2+1);
-		j_t = SEGOFF2LINEAR(jcs, d_t);
+		j_t = SEGOFF2LINEAR(FetchW(P2 + pskip - 2), d_t);
 		dsp = j_t - P2;
 	}
 	else {
@@ -191,11 +180,11 @@ static unsigned int _JumpGen(unsigned int P2, int mode, int cond,
 	}
 
 	/* displacement for not taken branch */
-	d_nt = P2 - old_long_cs + pskip;
+	d_nt = P2 - LONG_CS + pskip;
 	if (mode&DATA16) d_nt &= 0xffff;
 
 	/* jump address for not taken branch, usually next instruction */
-	j_nt = d_nt + old_long_cs;
+	j_nt = d_nt + LONG_CS;
 	*r_P0 = j_nt;
 
 	P1 = P2 + pskip;
@@ -280,6 +269,15 @@ static unsigned int _JumpGen(unsigned int P2, int mode, int cond,
 	if (dsp < 0) mode |= CKSIGN;
 	/* no break */
 	case 0x11:    /* call, unfortunately also uses JMP_LINK */
+		if (pskip == 3 + BT24(BitDATA16,mode)) { // far jmp/call
+		    unsigned short jcs = FetchW(P2 + pskip - 2);
+		    if (cond == 0x11) { /* call, push old cs first */
+			Gen(L_REG, mode, Ofs_CS);
+			Gen(O_PUSH, mode);
+		    }
+		    Gen(L_IMM, mode, Ofs_CS, jcs);
+		    AddrGen(A_SR_SH4, mode, Ofs_CS, Ofs_XCS);
+		}
 		if (CONFIG_CPUSIM)
 		    Gen(JMP_LINK, mode, cond, j_t, d_nt);
 		else

commit 95766f5aec15fc701461788e1ec57a1132ed813a

simx86: fix SIM regression from 507e05f in e.g. Windows
The skip jmp optimization is not valid for far jumps, so just skip it for
those. See also #1128.

src/base/emu-i386/simx86/interp.c
@@ -253,6 +253,7 @@ static unsigned int _JumpGen(unsigned int P2, int mode, int cond,
 		}
 #if !defined(SINGLESTEP)
 		if (CONFIG_CPUSIM && !(EFLAGS & TF) &&
+		    pskip != 3 + BT24(BitDATA16,mode) && // not far jmp
 		    ((P2 ^ j_t) & PAGE_MASK)==0) {	// same page
 		    if (debug_level('e')>1) dbug_printf("** JMP: ignored\n");
 		    TheCPU.mode |= SKIPOP;