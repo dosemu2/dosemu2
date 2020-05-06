dosemu2/dosemu2

commit a7e3612d4b02f8baffc15618249503a86138b205

simx86: IOPL=3 in eflags + code generation for v86 pushf
Using IOPL=3 simplifies IF flag processing by PUSHF/POPF etc, since
shifting to VIF is more work; also using VIF in PUSHF/POPF for
DPMI is cheating a little since the CPU doesn't trap them natively
and doesn't use VIF/VIP even with PVI unfortunately.

Programs shouldn't see much difference except that now for DPMI
IOPL=3 in the stack image of PUSHF (it was already 3 for vm86, no
changes there). There may be some DOS extenders sensitive to this.

src/base/emu-i386/simx86/cpu-emu.c
@@ -484,13 +484,17 @@ static void Reg2Cpu (int mode)
   */
 
  /* From now on we'll work on the cpuemu eflags (BUT vm86s eflags can be
-  * changed asynchronously by signals) */
-  TheCPU.eflags = vm86s.regs.eflags & (SAFE_MASK | EFLAGS_VIF);
+  * changed asynchronously by signals)
+  * Note that IOPL=3 in the emulated flags so cpuemu works directly with
+    IF, not VIF */
+  TheCPU.eflags = (vm86s.regs.eflags & SAFE_MASK) | IOPL_MASK;
+  if (isset_IF())
+    TheCPU.eflags |= EFLAGS_IF;
   /* get the protected mode flags. Note that RF and VM are cleared
    * by pushfd (but not by ints and traps). Equivalent to regs32->eflags
    * in vm86.c */
   flg = getflags();
-  TheCPU.eflags |= (flg & notSAFE_MASK & ~EFLAGS_VIF); // which VIP do we get here?
+  TheCPU.eflags |= (flg & notSAFE_MASK & ~EFLAGS_IF); // which VIP do we get here?
   TheCPU.eflags |= (VM | RF);	// RF is cosmetic...
   TheCPU.df_increments = (TheCPU.eflags&DF)?0xfcfeff:0x040201;
 
@@ -559,6 +563,11 @@ void Cpu2Reg (void)
    */
   REG(eflags) = (REG(eflags) & VIP) |
 			(TheCPU.eflags & ~VIP);
+  if (TheCPU.eflags & EFLAGS_IF)
+    set_IF();
+  else
+    clear_IF();
+  REG(eflags) |= EFLAGS_IF;
 
   if (debug_level('e')>1) e_printf("Cpu2Reg< vm86=%08x dpm=%08x emu=%08x\n",
 	REG(eflags),get_FLAGS(TheCPU.eflags),TheCPU.eflags);
@@ -646,6 +655,11 @@ static void Cpu2Scp (sigcontext_t *scp, int trapno)
 
   /* push running flags - same as eflags, RF is cosmetic */
   _eflags = (TheCPU.eflags & (eTSSMASK|0xfd5)) | 0x10002;
+  if (TheCPU.eflags & EFLAGS_IF)
+    set_IF();
+  else
+    clear_IF();
+  _eflags |= EFLAGS_IF;
   if (debug_level('e')>1) e_printf("Cpu2Scp< scp=%08x vm86=%08x dpm=%08x fl=%08x\n",
 	_eflags,REG(eflags),get_FLAGS(TheCPU.eflags),TheCPU.eflags);
 }

src/base/emu-i386/simx86/interp.c
@@ -645,6 +645,8 @@ intop3b:		{ int op = ArOpsFR[D_MO(opc)];
 			    else CODE_FLUSH();
 #endif
 			    /* virtual-8086 monitor */
+			    if (!(TheCPU.cr[4] & CR4_VME))
+				goto not_permitted;	/* GPF */
 			    temp = (EFLAGS|IOPL_MASK) & RETURN_MASK;
 			    if (EFLAGS & VIF) temp |= EFLAGS_IF;
 			    PUSH(mode, temp);
@@ -1789,30 +1791,40 @@ intop3b:		{ int op = ArOpsFR[D_MO(opc)];
 			temp=0; POP(mode, &temp);
 			if (CONFIG_CPUSIM) RFL.valid = V_INVALID;
 			if (V86MODE()) {
+			    int is_tf;
 stack_return_from_vm86:
 			    if (debug_level('e')>1)
 				e_printf("Popped flags %08x fl=%08x\n",
 					temp,EFLAGS);
+			    is_tf = !!(EFLAGS & TF);
 			    if (IOPL==3) {	/* Intel manual */
+				/* keep reserved bits + IOPL,VIP,VIF,VM,RF */
 				if (mode & DATA16)
-				    FLAGS = temp;	/* oh,really? */
+				    FLAGS &= ~(SAFE_MASK|EFLAGS_IF);
 				else
-				    EFLAGS = (temp&~0x1b3000) | (EFLAGS&0x1b3000);
+				    EFLAGS &= ~(SAFE_MASK|EFLAGS_IF);
+				EFLAGS |= temp & (SAFE_MASK|EFLAGS_IF);
 			    }
 			    else {
-				int is_tf = !!(EFLAGS & TF);
 				/* virtual-8086 monitor */
+				if (!(TheCPU.cr[4] & CR4_VME))
+				    goto not_permitted;	/* GPF */
 				/* move mask from pop{e}flags to regs->eflags */
-				EFLAGS = (EFLAGS & ~SAFE_MASK) | (temp & SAFE_MASK);
-				if (temp & EFLAGS_IF) {
+				if (mode & DATA16)
+				    FLAGS &= ~SAFE_MASK;
+				else
+				    EFLAGS &= ~SAFE_MASK;
+				EFLAGS |= temp & SAFE_MASK;
+				if (temp & EFLAGS_IF)
 				    EFLAGS |= EFLAGS_VIF;
-				    if (vm86s.regs.eflags & VIP) {
-					if (debug_level('e')>1)
-					    e_printf("Return for STI fl=%08x\n",
-						EFLAGS);
-					TheCPU.err = (is_tf ? EXCP01_SSTP : EXCP_STISIGNAL);
-					return PC + (opc==POPF);
-				    }
+			    }
+			    if (temp & EFLAGS_IF) {
+				if (vm86s.regs.eflags & VIP) {
+				    if (debug_level('e')>1)
+					e_printf("Return for STI fl=%08x\n",
+						 EFLAGS);
+				    TheCPU.err = (is_tf ? EXCP01_SSTP : EXCP_STISIGNAL);
+				    return PC + (opc==POPF);
 				}
 			    }
 			}
@@ -1825,13 +1837,15 @@ stack_return_from_vm86:
 			    else
 				EFLAGS = (EFLAGS&amask) |
 					 ((temp&(eTSSMASK|0xfd7))&~amask);
-//			    if (in_dpmi) {
+			    // unused "extended PVI" since real PVI does not
+			    // affect POPF
+			    if (IOPL<3 && (TheCPU.cr[4]&CR4_PVI)) {
 				if (temp & EFLAGS_IF)
 				    set_IF();
 				else {
 				    clear_IF();
 				}
-//			    }
+			    }
 			    if (debug_level('e')>1)
 				e_printf("Popped flags %08x->{r=%08x v=%08x}\n",temp,EFLAGS,_EFLAGS);
 			}
@@ -2136,22 +2150,25 @@ repag0:
 			break;
 /*fa*/	case CLI:
 			if (REALMODE() || (CPL <= IOPL) || (IOPL==3)) {
-				CODE_FLUSH();
-				EFLAGS &= ~EFLAGS_IF;
+				Gen(O_SETFL, mode, CLI);
 			}
 			else {
+			    CODE_FLUSH();
 			    /* virtual-8086 monitor */
 			    if (V86MODE()) {
 				if (debug_level('e')>2) e_printf("Virtual VM86 CLI\n");
-				Gen(O_SETFL, mode, CLI);
+				if (TheCPU.cr[4] & CR4_VME)
+				    EFLAGS &= ~EFLAGS_VIF;
+				else
+				    goto not_permitted;	/* GPF */
 			    }
-			    else/* if (in_dpmi)*/ {
+			    else if (TheCPU.cr[4] & CR4_PVI) {
 				CODE_FLUSH();
 				if (debug_level('e')>2) e_printf("Virtual DPMI CLI\n");
 				clear_IF();
 			    }
-//			    else
-//				goto not_permitted;	/* GPF */
+			    else
+				goto not_permitted;	/* GPF */
 			}
 			PC++;
 			break;
@@ -2160,7 +2177,12 @@ repag0:
 			if (V86MODE()) {    /* traps always (Intel man) */
 				/* virtual-8086 monitor */
 				if (debug_level('e')>2) e_printf("Virtual VM86 STI\n");
-				EFLAGS |= EFLAGS_VIF;
+				if (IOPL==3)
+				    EFLAGS |= EFLAGS_IF;
+				else if (TheCPU.cr[4]&CR4_VME)
+				    EFLAGS |= EFLAGS_VIF;
+				else
+				    goto not_permitted;	/* GPF */
 				if (vm86s.regs.eflags & VIP) {
 				    if (debug_level('e')>1)
 					e_printf("Return for STI fl=%08x\n",
@@ -2173,12 +2195,12 @@ repag0:
 			    if (REALMODE() || (CPL <= IOPL) || (IOPL==3)) {
 				EFLAGS |= EFLAGS_IF;
 			    }
-			    else/* if (in_dpmi)*/ {
+			    else if (TheCPU.cr[4] & CR4_PVI) {
 				if (debug_level('e')>2) e_printf("Virtual DPMI STI\n");
 				set_IF();
 			    }
-//			    else
-//				goto not_permitted;	/* GPF */
+			    else
+				goto not_permitted;	/* GPF */
 			}
 			PC++;
 			break;

src/base/emu-i386/simx86/codegen-sim.c
@@ -2107,20 +2107,23 @@ void Gen_sim(int op, int mode, ...)
 		int ftmp;
 		GTRACE0("O_PUSHF");
 		FlagSync_All();
-		ftmp = CPULONG(Ofs_EFLAGS);
-/*?*///		if (in_dpmi) ftmp = (ftmp & ~0x200) | (get_FLAGS(TheCPU.eflags) & 0x200);
+#if 0		// unused "extended PVI", if used should move to separate op
+		if (!V86MODE() && IOPL < 3 && (TheCPU.cr[4] & CR4_PVI))
+			ftmp = (ftmp & ~EFLAGS_IF) | (isset_IF() ? EFLAGS_IF : 0);
+#endif
+		ftmp = CPULONG(Ofs_EFLAGS) & (RETURN_MASK|EFLAGS_IF);
 		AR2.d = CPULONG(Ofs_XSS);
 		SR1.d = CPULONG(Ofs_ESP);
 		if (mode & DATA16) {
 			SR1.d = (SR1.d - 2) & CPULONG(Ofs_STACKM);
-			write_word(AR2.d + SR1.d, ftmp & 0x7eff);
+			write_word(AR2.d + SR1.d, ftmp);
 		}
 		else {
 			SR1.d = (SR1.d - 4) & CPULONG(Ofs_STACKM);
-			write_dword(AR2.d + SR1.d, ftmp & 0x3c7eff);
+			write_dword(AR2.d + SR1.d, ftmp);
 		}
 		CPULONG(Ofs_ESP) = SR1.d;
-		if (debug_level('e')>3) dbug_printf("(V) %08x\n",ftmp&0x3c7eff);
+		if (debug_level('e')>3) dbug_printf("(V) %08x\n",ftmp);
 		} break;
 
 	case O_PUSHI: {
@@ -2607,7 +2610,7 @@ void Gen_sim(int op, int mode, ...)
 			TheCPU.df_increments = 0xfcfeff;
 			break;
 		case CLI:
-			CPULONG(Ofs_EFLAGS) &= ~EFLAGS_VIF;
+			CPULONG(Ofs_EFLAGS) &= ~EFLAGS_IF;
 			break;
 		} }
 		break;

src/base/emu-i386/simx86/codegen-x86.c
@@ -1240,7 +1240,8 @@ shrot0:
 		};
 		unsigned char *q=Cp; GNX(Cp, pseqpre, sizeof(pseqpre));
 		if (mode&DATA16) q[8] = 0xfe; /* use -2 in lea ins */
-//		if (in_dpmi) {
+#if 0		// unused "extended PVI", if used should move to separate op
+		if (!V86MODE() && IOPL < 3 && (TheCPU.cr[4] & CR4_PVI)) {
 		    /* This solves the DOSX 'System test 8' error.
 		     * The virtualized IF is pushed instead of the
 		     * real one (which is always 1). This way, tests
@@ -1263,11 +1264,14 @@ shrot0:
 			G4((unsigned char *)&_EFLAGS-CPUOFFS(0),Cp);
 			// (19 from bt); rcl $10,%%eax
 			G4M(0x13,0xc1,0xd0,0x0a,Cp);
-//		}
+		}
+#endif
 		if (mode&DATA16) {
 			// movw %%ax,(%%esi,%%ecx,1)
 			G4M(0x66,0x89,0x04,0x0e,Cp);
 		} else {
+			// andl RETURN_MASK|EFLAGS_IF,%%eax
+			G1(0x25,Cp); G4(RETURN_MASK|EFLAGS_IF,Cp);
 			// movl %%eax,(%%esi,%%ecx,1)
 			G3M(0x89,0x04,0x0e,Cp);
 		}
@@ -1876,8 +1880,8 @@ shrot0:
 			G4(0xfcfeff,Cp);
 			break;
 		case CLI:
-			// andb $0xf8,EFLAGS+2(%%ebx)
-			G4M(0x80,0x63,Ofs_EFLAGS+2,0xf7u,Cp);
+			// andb $0xfd,EFLAGS+1(%%ebx)
+			G4M(0x80,0x63,Ofs_EFLAGS+1,~(uint8_t)(EFLAGS_IF>>8),Cp);
 			break;
 		} }
 		break;

src/base/emu-i386/simx86/tables.c
@@ -367,7 +367,7 @@ int GendBytesPerOp[128] =
 /* 50*/ 14,14,30,30, 8, 8, 8, 8, 8, 8,
 /* 60*/  8,35,19, 0, 6,11, 8, 5,12,13,
 /* 70*/  8, 9,10,24, 0, 9, 9,11, 9, 9,
-/* 80*/	19,24,22, 0, 0, 6,13,51, 3, 6,14, 3,24,
+/* 80*/	19,24,22, 0, 0, 6,13,41, 3, 6,14, 3,24,
 /* 93*/  0, 0, 0, 0, 0, 0, 0,
 /*100*/ 69,16,29,13,16,17,17,48, 7, 0, 8, 0,11,32,46,33,39,
 /*117*/  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0

commit bd1cc35f56b03f02b7953d9caf22f85d62d6b9b5

simx86: fix INT xx (performance regression from a7e3612d4)
Changing IOPL to 3 the INT instruction was now always trapped into a GPF instead
of quickly going back into the emulator when possible.

I've now fixed this and made the logic more like the Intel docs, in that
it uses the bitmap only for CR4.VME, and so I set TheCPU.cr[4]=CR4_VME at
init time, and it handles IF and VIF correctly depending on IOPL.

src/base/emu-i386/simx86/cpu-emu.c
@@ -723,6 +723,7 @@ erseg:
 void reset_emu_cpu(void)
 {
   TheCPU.cr[0] = 0x13;	/* valid bits: 0xe005003f */
+  TheCPU.cr[4] = CR4_VME;
   TheCPU.dr[4] = 0xffff1ff0;
   TheCPU.dr[5] = 0x400;
   TheCPU.dr[6] = 0xffff1ff0;

src/base/emu-i386/simx86/interp.c
@@ -1693,15 +1693,20 @@ intop3b:		{ int op = ArOpsFR[D_MO(opc)];
 #ifdef ASM_DUMP
 			fprintf(aLog,"%08x:\t\tint %02x\n", P0, inum);
 #endif
-			if (V86MODE() && IOPL<3) {
+			// V86: always #GP(0) if revectored or without VME
+			if (V86MODE() && !(TheCPU.cr[4] & CR4_VME) && IOPL<3)
+				goto not_permitted;
+			if (V86MODE() && (TheCPU.cr[4] & CR4_VME) &&
+			    !is_revectored(inum, &vm86s.int_revectored)) {
 				uint32_t segoffs;
-				/* V86: always #GP(0) if not revectored */
-				if (is_revectored(inum, &vm86s.int_revectored))
-					goto not_permitted;
 				segoffs = read_dword(inum << 2);
 				if (CONFIG_CPUSIM) FlagSync_All();
-				temp = (EFLAGS|IOPL_MASK) & RETURN_MASK;
-				if (EFLAGS & VIF) temp |= EFLAGS_IF;
+				temp = (EFLAGS|IOPL_MASK) & (RETURN_MASK|EFLAGS_IF);
+				if (IOPL<3) {
+					temp &= ~EFLAGS_IF;
+					if (EFLAGS & VIF)
+						temp |= EFLAGS_IF;
+				}
 				PUSH(mode, temp);
 				PUSH(mode, TheCPU.cs);
 				PUSH(mode, PC + 2 - LONG_CS);
@@ -1709,13 +1714,15 @@ intop3b:		{ int op = ArOpsFR[D_MO(opc)];
 				if (TheCPU.err) return P0;
 				TheCPU.eip = segoffs & 0xffff;
 				PC = LONG_CS + TheCPU.eip;
-				EFLAGS &= ~(EFLAGS_VIF|TF|RF|AC|NT);
+				EFLAGS &= ~(TF|RF|AC|NT);
+				EFLAGS &= ~(IOPL==3 ? EFLAGS_IF : EFLAGS_VIF);
 				if (debug_level('e')>1)
 					dbug_printf("EMU86: directly calling int %#x ax=%#x at %#x:%#x\n",
 						    inum, _AX, _CS, _IP);
 				break;
 			}
-			switch(inum) {
+			/* protected mode INT or revectored V86 with IOPL=3 */
+			if (PROTMODE()) switch(inum) {
 			case 0x03:
 				TheCPU.err=EXCP03_INT3;
 				PC += 2;