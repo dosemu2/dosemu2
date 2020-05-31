dosemu2/dosemu2

commit 6f16dbf307176ff15cd89f81ba18f4a9682d2e8a

simx86: generate code for INT (vm86 only)
This checks the revectored bitmap in the generated code since dosdebug can
change it dynamically, then either causes GPF or puts the IVT address
&mem_base[intno*4] in %edi after which pushf, turning off IF&TF and a far call
do the job.

src/base/emu-i386/simx86/codegen-x86.c
@@ -1513,6 +1513,26 @@ shrot0:
 		GNX(Cp, p, sz);
 		} break;
 
+	case O_INT: {
+		unsigned char intno = IG->p0;
+		int jpc = IG->p1;
+		// Check bitmap, GPF if revectored, else use mem_base+intno*4
+		// bt intno,Ofs_int_revectored(%ebx)
+		G3M(0x0f,0xba,0xa3,Cp);	G4(Ofs_int_revectored,Cp);
+		// (intno from bt);
+		G1(intno,Cp);
+		// jnc skip return
+		G2M(0x73,TAILSIZE+7,Cp);
+		// movb EXCP0D_GPF, Ofs_ERR(%%ebx)
+		G2M(0xc6,0x83,Cp); G4(Ofs_ERR,Cp); G1(EXCP0D_GPF,Cp);
+		// movl {exit_addr},%%eax; pop %%edx; ret
+		G1(0xb8,Cp); G4(jpc,Cp); G2M(0x5a,0xc3,Cp);
+		// address to call in edi
+		// movl $(MEM_BASE+inum*4), %edi
+		G1(0xbf,Cp); G4(TheCPU.mem_base+intno*4, Cp);
+		break;
+		}
+
 	case O_MOVS_SetA: {
 		/* use edi for loads unless MOVSDST or REP is set */
 		unsigned char modrm = mode&(MREP|MREPNE|MOVSDST) ? 0x73 : 0x7b;
@@ -1882,6 +1902,12 @@ shrot0:
 		case CLI:
 			// andb $0xfd,EFLAGS+1(%%ebx)
 			G4M(0x80,0x63,Ofs_EFLAGS+1,~(uint8_t)(EFLAGS_IF>>8),Cp);
+			break;
+		case INT:
+			// clear IF & TF
+			// andb $0xfc,EFLAGS+1(%%ebx)
+			G4M(0x80,0x63,Ofs_EFLAGS+1,
+			    ~(uint8_t)((EFLAGS_IF|EFLAGS_TF)>>8),Cp);
 			break;
 		} }
 		break;
@@ -2542,6 +2568,7 @@ static void Gen_x86(int op, int mode, ...)
 		}
 		break;
 
+	case O_INT:
 	case O_FOP: {
 		unsigned char exop = (unsigned char)va_arg(ap,int);
 		IG->p0 = exop;

src/base/emu-i386/simx86/codegen-sim.c
@@ -2248,6 +2248,18 @@ void Gen_sim(int op, int mode, ...)
 		} }
 		break;
 
+	case O_INT: {
+		unsigned char intno = va_arg(ap, int);
+		// Check bitmap, GPF if revectored
+		if (is_revectored(intno, &TheCPU.int_revectored)) {
+			P0 = va_arg(ap, dosaddr_t);
+			TheCPU.err = EXCP0D_GPF;
+		}
+		else {
+			AR1.d = intno * 4;
+		} }
+		break;
+
 	case O_MOVS_SetA:
 		GTRACE0("O_MOVS_SetA");
 		if (mode&ADDR16) {
@@ -2611,6 +2623,10 @@ void Gen_sim(int op, int mode, ...)
 			break;
 		case CLI:
 			CPULONG(Ofs_EFLAGS) &= ~EFLAGS_IF;
+			break;
+		case INT:
+			GTRACE0("O_INT");
+			CPUWORD(Ofs_FLAGS) &= ~(EFLAGS_IF|EFLAGS_TF);
 			break;
 		} }
 		break;

src/base/emu-i386/simx86/codegen.h
@@ -129,6 +129,7 @@
 #define O_POP2		90
 #define O_POP3		91
 #define O_LEAVE		92
+#define O_INT		93
 
 #define O_MOVS_SetA	100
 #define O_MOVS_MovD	101

src/base/emu-i386/simx86/cpu-emu.c
@@ -517,6 +517,8 @@ static void Reg2Cpu (int mode)
   TheCPU.err     = 0;
   TheCPU.eip     = vm86s.regs.eip&0xffff;
 
+  TheCPU.int_revectored = vm86s.int_revectored;
+
   SetSegReal(SREG(cs),Ofs_CS);
   SetSegReal(SREG(ss),Ofs_SS);
   SetSegReal(SREG(ds),Ofs_DS);

src/base/emu-i386/simx86/interp.c
@@ -149,7 +149,7 @@ static unsigned int _JumpGen(unsigned int P2, int mode, int opc,
 	 *	eb ff	dsp=1	illegal or tricky
 	 *	eb fe	dsp=0	loop forever
 	 */
-	if ((opc>>8) == GRP2wrm) {	// indirect jump
+	if ((opc>>8) == GRP2wrm || opc == INT) {	// indirect jump
 		dsp = 0;
 	}
 	else if (opc == JMPld || opc == CALLl) { // far jmp/call
@@ -304,6 +304,7 @@ static unsigned int _JumpGen(unsigned int P2, int mode, int opc,
 		    Gen(JLOOP_LINK, mode, opc, j_t, j_nt, &InstrMeta[0].clink);
 		break;
 	case RETl: case RETlisp: case JMPli: case CALLli: // far ret, indirect
+	case INT:
 		Gen(S_REG, mode, Ofs_CS);
 		AddrGen(A_SR_SH4, mode, Ofs_CS, Ofs_XCS);
 		Gen(L_REG, mode, Ofs_EIP);
@@ -1689,10 +1690,26 @@ intop3b:		{ int op = ArOpsFR[D_MO(opc)];
 			break;
 /*cd*/	case INT: {
 			int inum = Fetch(PC+1);
-			CODE_FLUSH();
 #ifdef ASM_DUMP
 			fprintf(aLog,"%08x:\t\tint %02x\n", P0, inum);
 #endif
+			if (V86MODE() && (TheCPU.cr[4] & CR4_VME) && IOPL == 3) {
+				Gen(O_INT, mode, inum, P0);
+				if (TheCPU.err) return P0;
+				Gen(O_PUSH2F, mode);
+				Gen(L_REG, mode, Ofs_CS);
+				Gen(O_PUSH, mode);
+				Gen(O_PUSHI, mode, PC + 2 - LONG_CS);
+				Gen(O_SETFL, mode, INT);
+				Gen(L_LXS1, mode, Ofs_EIP);
+				Gen(L_DI_R1, mode);
+				PC = JumpGen(PC, mode, opc, 2);
+				if (debug_level('e')>1)
+					dbug_printf("EMU86: directly called int %#x ax=%#x at %#x:%#x\n",
+						    inum, TheCPU.eax, TheCPU.cs, PC - LONG_CS);
+				break;
+			}
+			CODE_FLUSH();
 			// V86: always #GP(0) if revectored or without VME
 			if (V86MODE() && !(TheCPU.cr[4] & CR4_VME) && IOPL<3)
 				goto not_permitted;

src/base/emu-i386/simx86/syncpu.h
@@ -155,6 +155,9 @@ typedef struct {
 	void (*stub_read_8)(void);
 	void (*stub_read_16)(void);
 	void (*stub_read_32)(void);
+
+	/* should be moved to TSS once implemented */
+	struct revectored_struct int_revectored;
 } SynCPU;
 
 union _SynCPU {
@@ -234,6 +237,8 @@ extern union _SynCPU TheCPU_union;
 #define Ofs_stub_read_8	(unsigned int)(offsetof(SynCPU,stub_read_8)-SCBASE)
 #define Ofs_stub_read_16	(unsigned int)(offsetof(SynCPU,stub_read_16)-SCBASE)
 #define Ofs_stub_read_32	(unsigned int)(offsetof(SynCPU,stub_read_32)-SCBASE)
+#define Ofs_ERR		(unsigned int)(offsetof(SynCPU,err)-SCBASE)
+#define Ofs_int_revectored	(unsigned int)(offsetof(SynCPU,int_revectored)-SCBASE)
 
 #define rAX		CPUWORD(Ofs_AX)
 #define Ofs_AX		(Ofs_EAX)

src/base/emu-i386/simx86/tables.c
@@ -293,7 +293,7 @@ unsigned char InterOps[256] =
 	0x00,	// RETlisp		0xca
 	0x00,	// RETl			0xcb
 	0x01,	// INT3			0xcc
-	0x01,	// INT			0xcd
+	0x00,	// INT			0xcd
 	0x01,	// INTO			0xce
 	0x21,	// IRET			0xcf
 	0x38,	// SHIFTb		0xd0
@@ -367,8 +367,8 @@ int GendBytesPerOp[128] =
 /* 50*/ 14,14,30,30, 8, 8, 8, 8, 8, 8,
 /* 60*/  8,35,19, 0, 6,11, 8, 5,12,13,
 /* 70*/  8, 9,10,24, 0, 9, 9,11, 9, 9,
-/* 80*/	19,24,22, 0, 0, 6,13,41, 3, 6,14, 3,24,
-/* 93*/  0, 0, 0, 0, 0, 0, 0,
+/* 80*/	19,24,22, 0, 0, 6,13,41, 3, 6,14, 3,24,29,
+/* 94*/  0, 0, 0, 0, 0, 0,
 /*100*/ 69,16,29,13,16,17,17,48, 7, 0, 8, 0,11,32,46,33,39,
 /*117*/  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
 };

commit 698c0990ce3ffe5fa77c52425c070c39bace2e8e

simx86: fix int_revectored regression from 6f16dbf3 [fixes #1181]
btl encoding with immediate value is a bit tricky.
The value must be split to high and low order bits.
See http://x86.dapsen.com/html/file_module_x86_id_22.html:
"Some assemblers support immediate bit offsets larger than 31 by using
the immediate bit offset field in combination with the displacement
field of the memory operand. In this case, the loworder 3 or 5 bits
(3 for 16-bit operands, 5 for 32-bit operands) of the immediate bit
offset are stored in the immediate bit offset field, and the high-order
bits are shifted and combined with the byte displacement in the addressing
mode by the assembler. The processor will ignore the high order bits if
they are not zero."

And it indeed does ignore high order bits!
So lets follow the recommended split.

src/base/emu-i386/simx86/codegen-x86.c
@@ -1518,9 +1518,12 @@ shrot0:
 		int jpc = IG->p1;
 		// Check bitmap, GPF if revectored, else use mem_base+intno*4
 		// bt intno,Ofs_int_revectored(%ebx)
-		G3M(0x0f,0xba,0xa3,Cp);	G4(Ofs_int_revectored,Cp);
-		// (intno from bt);
-		G1(intno,Cp);
+		// Code offset of TheCPU.int_revectored.__map[intno>>5]
+		// as Ofs_int_revectored[intno>>3] aligned to 4.
+		// See http://x86.dapsen.com/html/file_module_x86_id_22.html
+		G3M(0x0f,0xba,0xa3,Cp);	G4(Ofs_int_revectored+((intno>>3)&~3),Cp);
+		// remaining 5 intno bits within __map dword from bt
+		G1(intno&0x1f,Cp);
 		// jnc skip return
 		G2M(0x73,TAILSIZE+7,Cp);
 		// movb EXCP0D_GPF, Ofs_ERR(%%ebx)