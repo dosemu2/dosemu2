e6a247040859acb32bd3a1c7178cee5d92e5d591
Stas Sergeev <stsp@users.sourceforge.net>
2020-08-28 22:47:06 +0300

    console_speaker: linux-specific [#506]

--- src/base/speaker/console_speaker.c
+++ src/base/speaker/console_speaker.c
@@ -12,11 +12,15 @@
 
 void console_speaker_on(void *gp, unsigned ms, unsigned short period)
 {
+#ifdef __linux__
 	ioctl((int)(uintptr_t)gp, KDMKTONE,
 		(unsigned) ((ms & 0xffff) << 16) | (period & 0xffff));
+#endif
 }
 
 void console_speaker_off(void *gp)
 {
+#ifdef __linux__
 	ioctl((int)(uintptr_t)gp, KDMKTONE, 0);
+#endif
 }
