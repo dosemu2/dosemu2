57a66f309657a2d5637c46c619fbdc4ae5ee3791
Andrew Bird <ajb@spheresystems.co.uk>
2020-09-04 15:40:57 +0100

    Travis: Add Japheth's DPMI tests
    Some of these tests are benchmarks, but we test for completion not
    performance as test hardware will be variable.

--- test/test_dos.py
+++ test/test_dos.py
@@ -5,7 +5,7 @@ import re
 from datetime import datetime
 from glob import glob
 from os import (makedirs, statvfs, listdir, symlink, uname, remove,
-                utime, environ, access, R_OK, W_OK)
+                utime, rename, environ, access, R_OK, W_OK)
 from os.path import exists, isdir, join
 from shutil import copy
 from subprocess import call
@@ -3549,6 +3549,116 @@ $_debug = "-D+d"
                     break
 
         self.assertIn(self.systype, systypeline)
+
+    def _test_memory_dpmi_japheth(self, switch):
+        self.unTarOrSkip("VARIOUS.tar", [
+            ("dpmihxrt218.exe", "65fda018f4422c39dbf36860aac2c537cfee466b"),
+        ])
+        rename(join(WORKDIR, "dpmihxrt218.exe"), join(WORKDIR, "dpmi.exe"))
+
+        mkfile("testit.bat", """\
+c:\\dpmi %s
+rem end
+""" % switch, newline="\r\n")
+
+        return self.runDosemu("testit.bat", config="""\
+$_hdimage = "dXXXXs/c:hdtype1 +1"
+$_floppy_a = ""
+""", timeout=60)
+
+    def test_memory_dpmi_japheth(self):
+        """Memory DPMI (Japheth) ''"""
+        results = self._test_memory_dpmi_japheth("")
+
+#Cpu is in V86-mode
+#Int 15h, ax=e801h, extended memory: 8256 kB
+#int 15h, ax=e820h, available memory at 000100000, size 8256 kB
+#XMS v3.0 host found, largest free block: 8192 kB
+#No VCPI host found
+#DPMI v0.90 host found, cpu: 05, support of 32-bit clients: 1
+#entry initial switch to protected-mode: F000:F500
+#size task-specific memory: 230 paragraphs
+#now in protected mode, client CS/SS/DS/FS/GS: A7/AF/AF/0/0
+#Eflags=206, ES (=PSP): B7 (environment: BF, parent PSP segm: 431)
+#GDTR: 17.A308100 IDTR: 7FF.A308200 LDTR: 0 TR: 0
+#DPMI version flags: 5
+#master/slave PICs base: 08/70
+#state save protected-mode: 97:1, real-mode: F000:F514
+#size state save buffer: 52 bytes
+#raw jump to real-mode: 97:0, protected-mode: F000:F513
+#largest free/lockable memory block (kB): 131004/131004
+#free unlocked (=virtual) memory (kB): 131004
+#total/free address space (kB): 32768/32768
+#total/free physical memory (kB): 131072/131004
+#Coprocessor status: 4D
+#vendor: DOSEMU Version 2.0, version: 0.90
+#capabilities: 78
+#vendor 'MS-DOS' API entry: 97:130
+#'MS-DOS' API, ax=100h (get LDT selector): E7
+
+        self.assertRegex(results, r"DPMI v0.90 host found, cpu: \d+, support of 32-bit clients: 1")
+        self.assertRegex(results, r"DPMI version flags: 5")
+        self.assertRegex(results, r"vendor: DOSEMU Version 2.0, version: 0.90")
+        self.assertRegex(results, r"capabilities: 78")
+
+    def test_memory_dpmi_japheth_c(self):
+        """Memory DPMI (Japheth) '-c'"""
+        results = self._test_memory_dpmi_japheth("-c")
+        self.assertRegex(results, r"time.*CLI/STI: \d+ ms")
+        self.assertRegex(results, r"time.*interrupts via DPMI: \d+ ms")
+
+    def test_memory_dpmi_japheth_i(self):
+        """Memory DPMI (Japheth) '-i'"""
+        results = self._test_memory_dpmi_japheth("-i")
+        self.assertRegex(results, r"time.*IN 21: \d+ ms")
+
+    def test_memory_dpmi_japheth_m(self):
+        """Memory DPMI (Japheth) '-m'"""
+        results = self._test_memory_dpmi_japheth("-m")
+        t = re.search(r"alloc largest mem block \(size=(\d+) kB\) returned handle [0-9a-fA-F]+, base \d+", results)
+        self.assertIsNotNone(t, "Unable to parse memory block size")
+        size = int(t.group(1))
+        self.assertGreater(size, 128000, results)
+
+    def test_memory_dpmi_japheth_r(self):
+        """Memory DPMI (Japheth) '-r'"""
+        results = self._test_memory_dpmi_japheth("-r")
+        self.assertRegex(results, r"time.*INT 69h: \d+ ms")
+        self.assertRegex(results, r"time.*INT 31h, AX=0300h \(Sim INT 69h\): \d+ ms")
+        self.assertRegex(results, r"time.*real-mode callback: \d+ ms")
+        self.assertRegex(results, r"time.*raw mode switches PM->RM->PM: \d+ ms")
+
+    def test_memory_dpmi_japheth_t(self):
+        """Memory DPMI (Japheth) '-t'"""
+        results = self._test_memory_dpmi_japheth("-t")
+
+#C:\>c:\dpmi -t
+#allocated rm callback FF10:214, rmcs=AF:20E4
+#calling rm proc [53A:8B6], rm cx=1
+#  inside rm proc, ss:sp=7A8:1FC, cx=1
+#  calling rm callback FF10:214
+#    inside rm callback, ss:esp=9F:EFF4, ds:esi=F7:1FC
+#    es:edi=AF:20E4, rm ss:sp=7A8:1FC, rm cx=1
+#    calling rm proc [53A:8B6]
+#      inside rm proc, ss:sp=7A8:1F8, cx=2
+#      calling rm callback FF10:214
+#        inside rm callback, ss:esp=9F:EFE0, ds:esi=F7:1F8
+#        es:edi=AF:20E4, rm ss:sp=7A8:1F8, rm cx=2
+#        exiting
+#      back in rm proc, ss:sp=7A8:1F8; exiting
+#    back in rm callback, rm ss:sp=7A8:1FC, rm cx=2; exiting
+#  back in rm proc, ss:sp=7A8:1FC; exiting
+#back in protected-mode, rm ss:sp=7A8:200, rm cx=2
+
+        self.assertRegex(results, re.compile(r"^allocated rm callback", re.MULTILINE))
+        self.assertRegex(results, re.compile(r"^  inside rm proc", re.MULTILINE))
+        self.assertRegex(results, re.compile(r"^    inside rm callback", re.MULTILINE))
+        self.assertRegex(results, re.compile(r"^      inside rm proc", re.MULTILINE))
+        self.assertRegex(results, re.compile(r"^        inside rm callback", re.MULTILINE))
+        self.assertRegex(results, re.compile(r"^      back in rm proc", re.MULTILINE))
+        self.assertRegex(results, re.compile(r"^    back in rm callback", re.MULTILINE))
+        self.assertRegex(results, re.compile(r"^  back in rm proc", re.MULTILINE))
+        self.assertRegex(results, re.compile(r"^back in protected-mode", re.MULTILINE))
 
     def test_memory_ems_borland(self):
         """Memory EMSTEST (Borland)"""
