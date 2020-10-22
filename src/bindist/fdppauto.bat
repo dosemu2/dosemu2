b361843807f5b95e0fe5d635fac51b1e216b371a
Stas Sergeev <stsp@users.sourceforge.net>
2020-10-22 14:45:09 +0300

    fdppauto.bat: split a bit more
    Create banner.bat and blaster.bat.

--- src/bindist/fdppauto.bat
+++ src/bindist/fdppauto.bat
@@ -5,9 +5,5 @@ if not "%SHELLDRV%" == "" path %PATH%;%SHELLDRV%:\
 if not "%FREEDOSDRV%" == "" path %PATH%;%FREEDOSDRV%:\bin;%FREEDOSDRV%:\gnu
 if not "%USERDRV%" == "" set TEMP=%USERDRV%:\tmp
 prompt $P$G
-emusound -e
-echo Welcome to dosemu2!
-system -s DOSEMU_VERSION
-echo     Build %DOSEMU_VERSION%
 for %%b in (%DOSEMUDRV%:\dosemu\dosrc.d\*.bat) do call %%b
 if exist %USERDRV%:\userhook.bat call %USERDRV%:\userhook.bat
