dosemu2/dosemu2

commit 8f1ac2e4e258cac3424a3166a3799c294d4b4fe7

mfs: qualify devices to C:/DEV notation [fixes #1359]

src/dosext/mfs/mfs.c
@@ -4697,14 +4697,28 @@ do_create_truncate:
       break;
     }
 
-    case QUALIFY_FILENAME: /* 0x23 */
+    case QUALIFY_FILENAME: { /* 0x23 */
+      char *name = (char *)Addr(state, ds, esi);
+      char *dst = (char *)Addr(state, es, edi);
+      char *slash;
       if (drive > PRINTER_BASE_DRIVE && drive < MAX_DRIVES) {
-        char *dst = (char *)Addr(state, es, edi);
         sprintf(dst, "%s\\%s", LINUX_PRN_RESOURCE, drives[drive].root);
+        Debug0((dbg_fd, "Qualify Filename: %s -> %s\n", name, dst));
         return TRUE;
       }
-      /* XXX SHOULD BE IMPLEMENTED PROPERLY! */
+      if (is_dos_device(name) && (slash = strrchr(name, '\\'))) {
+        /* translate to C:/DEV notation.
+         * DOS will not open such devices via redirector, and
+         * we would avoid the problem of opening NUL device,
+         * see https://github.com/dosemu2/dosemu2/issues/1359 */
+        int pos = slash - name;
+        strcpy(dst, name);
+        dst[pos] = '/';
+        Debug0((dbg_fd, "Qualify Filename: %s -> %s\n", name, dst));
+        return TRUE;
+      }
       break;
+    }
 
     case LOCK_FILE_REGION: { /* 0x0a */
       /* The following code only apply to DOS 4.0 and later */

commit cfd75f27fa135de9449839f1087b84dff3887e5c

mfs: support dir/dev notation
This fixes regression with freecom, introduced by 8f1ac2e4.
In fact, we can as well revert 8f1ac2e4 now as it was a result
of mft work-arounds which are not needed any more.

src/dosext/mfs/mfs.c
@@ -1948,6 +1948,10 @@ static void auspr(const char *filestring, char *name, char *ext)
   else
     bs_pos++;
   filestring = bs_pos;
+  /* support dir/dev notation */
+  bs_pos=strchr(filestring, '/');
+  if (bs_pos && is_dos_device(bs_pos + 1))
+    filestring = bs_pos + 1;
 
   extract_filename(filestring, name, ext);
   /* convert any * wildcards (from DRDOS 5.0) into ? */