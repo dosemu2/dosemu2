dosemu2/dosemu2

commit 9a15687d287f21e0d50fc7ca3990cf2e29af07d6

int: move reset_idle() from msdos to int10 [fixes #794]
msdos() may not be active w/o revectoring.

src/base/bios/int10.c
@@ -57,6 +57,7 @@
 #include "port.h"
 #include "speaker.h"
 #include "utilities.h"
+#include "timers.h"
 #include "vgaemu.h"
 #include "vgatext.h"
 
@@ -739,6 +740,8 @@ int int10(void) /* with dualmon */
     else Video->update_screen = Video_default->update_screen;
   }
 #endif
+
+  reset_idle(0);
 
   li= READ_BYTE(BIOS_ROWS_ON_SCREEN_MINUS_1) + 1;
   co= READ_WORD(BIOS_SCREEN_COLUMNS);

src/base/async/int.c
@@ -1382,6 +1382,8 @@ static int msdos(void)
 #endif
 
     switch (HI(ax)) {
+/* the below idle handling moved to int10 */
+#if 0
     case 0x06:
 	if (LO(dx) == 0xff)
 	    return 0;
@@ -1393,6 +1395,7 @@ static int msdos(void)
     case 0x40:			/* output functions: reset idle */
 	reset_idle(0);
 	return 0;
+#endif
 
     case 0x2C:{		/* get time & date */
 	    idle(2, 100, 0, "dos_time");

commit d7402eec84478c051d25e7b26dd8515514c186e2

int10: reset idle only on output funcs [#1545]
This fixes the CPU hog (in edit.com) regression from 9a15687d2.

src/base/bios/int10.c
@@ -750,8 +750,6 @@ int int10(void) /* with dualmon */
   }
 #endif
 
-  reset_idle(0);
-
   li= READ_BYTE(BIOS_ROWS_ON_SCREEN_MINUS_1) + 1;
   co= READ_WORD(BIOS_SCREEN_COLUMNS);
 
@@ -856,6 +854,7 @@ int int10(void) /* with dualmon */
 
 
     case 0x06:		/* scroll up */
+      reset_idle(0);
       i10_deb(
         "scroll up: %u lines, area %u.%u-%u.%u, attr 0x%02x\n",
         LO(ax), LO(cx), HI(cx), LO(dx), HI(dx), HI(bx)
@@ -870,6 +869,7 @@ int int10(void) /* with dualmon */
 
 
     case 0x07:		/* scroll down */
+      reset_idle(0);
       i10_deb(
         "scroll dn: %u lines, area %u.%u-%u.%u, attr 0x%02x\n",
         LO(ax), LO(cx), HI(cx), LO(dx), HI(dx), HI(bx)
@@ -910,6 +910,7 @@ int int10(void) /* with dualmon */
        * the difference is that 0xA ignores color for text modes
        */
     case 0x09:		/* write char & attr */
+      reset_idle(0);
       i10_deb(
           "rep char: page %u, char 0x%02x '%c', attr 0x%02x\n",
           HI(bx), LO(ax), LO(ax) > ' ' && LO(ax) < 0x7f ? LO(ax) : ' ', LO(bx)
@@ -918,6 +919,7 @@ int int10(void) /* with dualmon */
       break;
 
     case 0x0a:		/* write char */
+      reset_idle(0);
       i10_deb(
           "rep char: page %u, char 0x%02x '%c'\n",
           HI(bx), LO(ax), LO(ax) > ' ' && LO(ax) < 0x7f ? LO(ax) : ' '
@@ -947,6 +949,7 @@ int int10(void) /* with dualmon */
       break;
 
     case 0x0c:		/* write pixel */
+      reset_idle(0);
       if(!using_text_mode())
         vgaemu_put_pixel(LWORD(ecx), LWORD(edx), HI(bx), LO(ax));
       break;
@@ -961,6 +964,7 @@ int int10(void) /* with dualmon */
       break;
 
     case 0x0e:		/* print char */
+      reset_idle(0);
       if(using_text_mode()) {
         i10_deb(
           "tty put char: page %u, char 0x%02x '%c'\n",
@@ -1378,6 +1382,7 @@ int int10(void) /* with dualmon */
         unsigned int str = SEGOFF2LINEAR(SREG(es), LWORD(ebp));
         unsigned old_x, old_y;
 
+        reset_idle(0);
         old_x = get_bios_cursor_x_position(page);
         old_y = get_bios_cursor_y_position(page);
 