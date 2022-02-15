dosemu2/dosemu2

commit 62c6bbd51bafcb333ff8c97fb225b2d8f8c15422

mouse: apply text-mode rounding in event handler [fixes #1517]

src/base/mouse/mouse.c
@@ -2063,8 +2063,8 @@ static void call_int33_mouse_event_handler(void)
 {
     rm_stack_enter();
     LWORD(eax) = mouse_events;
-    LWORD(ecx) = get_mx();
-    LWORD(edx) = get_my();
+    LWORD(ecx) = MOUSE_RX;
+    LWORD(edx) = MOUSE_RY;
     LWORD(esi) = mickeyx();
     LWORD(edi) = mickeyy();
     LO(bx) = (mouse.rbutton ? 2 : 0) | (mouse.lbutton ? 1 : 0);

commit 0bbb4b595ed4787828d5152c90b5569ef2b59c6b

mouse: logging fix [#1590]
Got some regressions after 62c6bbd51

src/base/mouse/mouse.c
@@ -2078,9 +2078,9 @@ static void call_int33_mouse_event_handler(void)
     mouse.wmcount = 0;
 
     /* jump to mouse cs:ip */
-    m_printf("MOUSE: event %d, x %d, y %d, mx %d, my %d, b %x\n",
-	     mouse_events, get_mx(), get_my(), mickeyx(), mickeyy(),
-	     LWORD(ebx));
+    m_printf("MOUSE: event %d, x %d(%d), y %d(%d), mx %d, my %d, b %x\n",
+	     mouse_events, MOUSE_RX, get_mx(), MOUSE_RY, get_my(), mickeyx(),
+	     mickeyy(), LWORD(ebx));
     m_printf("MOUSE: .........jumping to %04x:%04x\n", mouse.cs, mouse.ip);
     SREG(ds) = mouse.cs;		/* put DS in user routine */
     do_call_back(mouse.cs, mouse.ip);