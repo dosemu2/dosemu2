f8b533c30b805347d859ae74b17bef1332e64135
Stas Sergeev <stsp@users.sourceforge.net>
2022-05-29 03:13:59 +0300

    fdppauto: fix plugins execution order [#1635]
    Needs to run external plugins first.
    That may include things like PATH to freedos binaries, that
    may be needed for -E (like `-E mem`).

--- src/bindist/fdppauto.bat
+++ src/bindist/fdppauto.bat
@@ -4,9 +4,11 @@ path %DOSEMUDRV%:\dosemu
 if not "%SHELLDRV%" == "" path %PATH%;%SHELLDRV%:\
 if not "%FREEDOSDRV%" == "" path %PATH%;%FREEDOSDRV%:\bin;%FREEDOSDRV%:\gnu
 if not "%USERDRV%" == "" set TEMP=%USERDRV%:\tmp
-for %%b in (%DOSEMUDRV%:\dosemu\dosrc.d\*.bat) do call %%b
+rem first run external plugins. -E commands may depend on them.
 if "%XBATDRV%" == "" goto noxbat
 path %PATH%;%XBATDRV%:\
 for %%b in (%XBATDRV%:\dosrc.d\*.bat) do call %%b
 :noxbat
+rem run dosemu2 plugins, including vars and -E commands.
+for %%b in (%DOSEMUDRV%:\dosemu\dosrc.d\*.bat) do call %%b
 if exist %USERDRV%:\userhook.bat call %USERDRV%:\userhook.bat
