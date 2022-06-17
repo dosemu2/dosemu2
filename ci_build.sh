abe433df9b70e40fc1888cf0768332591c877e3f
Andrew Bird <ajb@spheresystems.co.uk>
2022-06-17 19:12:52 +0100

    CI: Switch to compile with clang

--- ci_build.sh
+++ ci_build.sh
@@ -30,7 +30,7 @@ git clone --depth 1 --no-single-branch https://github.com/dosemu2/fdpp.git ${LOC
 mk-build-deps --install --root-cmd sudo
 
 export PKG_CONFIG_PATH=${LOCALFDPPINST}/lib/pkgconfig
-./default-configure -d
+CC=clang ./default-configure -d
 make
 
 # Install the FAT mount helper
