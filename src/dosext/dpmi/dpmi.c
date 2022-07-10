a11fcb3975c105b64ea81b188120ea64cef47edc
Stas Sergeev <stsp@users.sourceforge.net>
2022-07-10 21:30:41 +0300

    dpmi: add cli wa for pq4 [#1727]

--- src/dosext/dpmi/dpmi.c
+++ src/dosext/dpmi/dpmi.c
@@ -4784,13 +4784,17 @@ static int dpmi_gpf_simple(sigcontext_t *scp, uint8_t *lina, void *sp, int *rv)
 	break;
       }
       current_cli = lina;
-      /* look for "pushfd; pop eax; cli" (DOOM),
+      /* look for
+       * "pushfd; pop eax; cli" (DOOM),
        * "ret; pushfd; cli" (NFS-SE),
-       * "pusha; pushfd; cli" (drally) patterns */
+       * "pusha; pushfd; cli" (drally),
+       * "pushfw; cli" (PoliceQuest4)
+       *  patterns */
       if (!in_dpmi_irq && _eip >= 2 &&
           ((lina[-2] == 0x9c && lina[-1] == 0x58) ||
           (lina[-2] == 0xc3 && lina[-1] == 0x9c) ||
-          (lina[-2] == 0x60 && lina[-1] == 0x9c)
+          (lina[-2] == 0x60 && lina[-1] == 0x9c) ||
+          (lina[-2] == 0x66 && lina[-1] == 0x9c)
       )) {
         D_printf("DOOM cli work-around\n");
         dpmi_is_cli = 1;
