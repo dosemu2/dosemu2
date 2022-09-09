20a99e4f2e5199496a48e4750b2013eae76194be
Stas Sergeev <stsp@users.sourceforge.net>
2022-09-09 16:37:16 +0300

    fdpp: provide more heap to kernel
    Fixes https://github.com/dosemu2/fdpp/issues/203
    
    Had to enlarge dosemu2 heap by 1Kb as LFNs can get large
    chunks during boot.

--- src/plugin/fdpp/hooks.c
+++ src/plugin/fdpp/hooks.c
@@ -111,7 +111,7 @@ static int fdpp_pre_boot(unsigned char *boot_sec)
     if (!hndl)
         return -1;
     if (config.dos_up) {
-        heap_sz = 1024 * 3;
+        heap_sz = 1024 * 4;
         kptr = lowmem_alloc_aligned(16, krnl_len + heap_sz);
         daddr = DOSEMU_LMHEAP_OFFS_OF(kptr);
         assert(!(daddr & 15));
