a95edf19a09067578477a40c8ba6cf4ce110efe4
Stas Sergeev <stsp@users.sourceforge.net>
2022-10-06 23:50:18 +0500

    install automake scripts

--- autogen.sh
+++ autogen.sh
@@ -2,10 +2,18 @@
 
 DIR=$(dirname $0 | xargs realpath)
 echo "Generating toplevel configure script in $DIR..."
+
+check_scr() {
+  [ -f $DIR/$1 ] || ln -s scripts/$1 $DIR/$1
+}
+
 rm -f aclocal.m4
 if ! autoreconf -v -I m4 --install --force $DIR; then
 	echo "Failure!"
 	exit 1
 fi
+check_scr config.sub
+check_scr config.guess
+check_scr install-sh
 echo
 echo "Done, now run $DIR/default-configure"
