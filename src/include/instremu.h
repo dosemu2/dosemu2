ad2699b61a623e44dd39dc6572bbec6dca5b12b0
Stas Sergeev <stsp@users.sourceforge.net>
2022-12-12 19:02:05 +0500

    build: enable instremu only on x86 arches [#1833]
    Unfortunately this leaves msdos/instr_dec.c library functionally
    reduced.

--- src/include/instremu.h
+++ src/include/instremu.h
@@ -1,6 +1,7 @@
 #ifndef INSTREMU_H
 #define INSTREMU_H
 
+#ifdef INSTREMU
 int instr_len(unsigned char *, int);
 int instr_emu(sigcontext_t *scp, int pmode, int cnt);
 int decode_modify_segreg_insn(sigcontext_t *scp, int pmode,
@@ -11,5 +12,31 @@ unsigned instr_binary_word(unsigned op, unsigned op1,
                                    unsigned op2, unsigned *eflags);
 unsigned instr_binary_dword(unsigned op, unsigned op1,
                                    unsigned op2, unsigned *eflags);
+#else
+static inline int instr_len(unsigned char *, int)
+{
+    return 0;
+}
+static inline int instr_emu(sigcontext_t *scp, int pmode, int cnt)
+{
+    return 0;
+}
+static inline unsigned char instr_binary_byte(unsigned char op,
+                                   unsigned char op1,
+                                   unsigned char op2, unsigned *eflags)
+{
+    return 0;
+}
+static inline unsigned short instr_binary_word(unsigned op, unsigned op1,
+                                   unsigned op2, unsigned *eflags)
+{
+    return 0;
+}
+static inline unsigned instr_binary_dword(unsigned op, unsigned op1,
+                                   unsigned op2, unsigned *eflags)
+{
+    return 0;
+}
+#endif
 
 #endif
