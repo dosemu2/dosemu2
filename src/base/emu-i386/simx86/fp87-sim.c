dosemu2/dosemu2

commit aa7ff9dc5f0b00f3a33493db415c22b4f48be5f2

First phase of asm removal in fp87-sim.c
Replaces ftest() by plain C, and removes some unnecessary FPU inits, applying to
the real, not simulated FPU.

src/base/emu-i386/simx86/fp87-sim.c
@@ -64,19 +64,6 @@ static unsigned short WFRS;
 
 static long double _fparea[8];
 
-/*
- * This function is only here for looking at the generated binary code
- * with objdump.
- */
-static void _test_(void) __attribute__((used));
-
-static void _test_(void)
-{
-	__asm__ __volatile__ (" \
-		nop \
-		" : : : "memory" );
-}
-
 void init_emu_npu (void)
 {
 	int i;
@@ -93,18 +80,33 @@ void init_emu_npu (void)
 	TheCPU.fpstt = 0;
 	TheCPU.fpuc  = 0x37f;
 	TheCPU.fptag = 0xffff;
-	__asm__ __volatile__ ("fninit");
 	WFR0 = WFR1 = 0.0;
 }
 
 static void ftest(long double d)
 {
 	register unsigned short fps;
-	__asm__ __volatile__ (
-	"fldt	%1\n"
-	"fxam\n"
-	"fnstsw\n"
-	"fstp	%%st(0)" : "=a"(fps) : "m"(d) );
+	// https://www.felixcloutier.com/x86/fxam
+	// bits in status word: c0:8, c1:9, c2:10, c3:14
+	switch(fpclassify(d)) {
+	case FP_NAN:
+		fps = 0x100;
+		break;
+	case FP_INFINITE:
+		fps = 0x500;
+		break;
+	case FP_ZERO:
+		fps = 0x4000;
+		break;
+	case FP_SUBNORMAL:
+		fps = 0x4400;
+		break;
+	case FP_NORMAL:
+	default:
+		fps = 0x400;
+		break;
+	}
+	if (signbit(d)) fps |= 0x200;
 	TheCPU.fpus = (fps&0xc7df)|(TheCPU.fpstt<<11);
 }
 
@@ -649,14 +651,12 @@ fcom00:			TheCPU.fpus &= (~0x4500);	/* (C3,C2,C0) <-- 000 */
 //	63.3*	DB 11000011	FINIT
 		   case 2:		/* FCLEX */
 			TheCPU.fpus &= 0x7f00;
-			__asm__ __volatile__ ("fnclex");
 			break;
 		   case 3:		/* FINIT */
 			TheCPU.fpus  = 0;
 			TheCPU.fpstt = 0;
 			TheCPU.fpuc  = 0x37f;
 			TheCPU.fptag = 0xffff;
-			__asm__ __volatile__ ("fninit");
 			WFR0 = WFR1 = 0.0;
 			break;
 		   default: /* FNENI,FNDISI: 8087 */
@@ -996,7 +996,6 @@ fcom00:			TheCPU.fpus &= (~0x4500);	/* (C3,C2,C0) <-- 000 */
 			TheCPU.fpstt = 0;
 			TheCPU.fpuc  = 0x37f;
 			TheCPU.fptag = 0xffff;
-			__asm__ __volatile__ ("fninit");
 			WFR0 = WFR1 = 0.0;
 		    }
 		   }

commit b0718aeb7396e1bb65ab82c03dde9f491b016649

fp87-sim: fix mcpdiag regressions from aa7ff9dc5f0
The mcpdiag.exe checker from Intel shows a lot of failures, this brings it back
to less than before aa7ff9dc5f0 by checking for FPU exceptions in ftest().

ftest() should not set C0-C3 condition codes like FXAM for instructions other
than FXAM, so renamed ftest() to fxam().

src/base/emu-i386/simx86/fp87-sim.c
@@ -83,30 +83,43 @@ void init_emu_npu (void)
 	WFR0 = WFR1 = 0.0;
 }
 
-static void ftest(long double d)
+static void fxam(long double d)
 {
-	register unsigned short fps;
+	unsigned short fps = TheCPU.fpus & ~0x4700;
+
 	// https://www.felixcloutier.com/x86/fxam
 	// bits in status word: c0:8, c1:9, c2:10, c3:14
 	switch(fpclassify(d)) {
 	case FP_NAN:
-		fps = 0x100;
+		fps |= 0x100;
 		break;
 	case FP_INFINITE:
-		fps = 0x500;
+		fps |= 0x500;
 		break;
 	case FP_ZERO:
-		fps = 0x4000;
+		fps |= 0x4000;
 		break;
 	case FP_SUBNORMAL:
-		fps = 0x4400;
+		fps |= 0x4400;
 		break;
 	case FP_NORMAL:
 	default:
-		fps = 0x400;
+		fps |= 0x400;
 		break;
 	}
 	if (signbit(d)) fps |= 0x200;
+	TheCPU.fpus = (fps&0xc7df)|(TheCPU.fpstt<<11);
+}
+
+static void ftest(void)
+{
+	unsigned short fps = TheCPU.fpus & ~0x3f;
+	int exceptions = fetestexcept(FE_ALL_EXCEPT);
+	if (exceptions & FE_INVALID) fps |= 0x1;
+	if (exceptions & FE_DIVBYZERO) fps |= 0x4;
+	if (exceptions & FE_OVERFLOW) fps |= 0x8;
+	if (exceptions & FE_UNDERFLOW) fps |= 0x10;
+	if (exceptions & FE_INEXACT) fps |= 0x20;
 	TheCPU.fpus = (fps&0xc7df)|(TheCPU.fpstt<<11);
 }
 
@@ -293,7 +306,7 @@ static int Fp87_op_sim(int exop, int reg)
 		case 0x3c: WFR0 = (long double)read_double(AR1.d) / WFR0; break;
 		case 0x3e: WFR0 = (long double)(int16_t)read_word(AR1.d) / WFR0; break;
 		}
-		ftest(WFR0);
+		ftest();
 		*ST0 = WFR0;
 		break;
 
@@ -466,7 +479,7 @@ fcom00:			TheCPU.fpus &= (~0x4500);	/* (C3,C2,C0) <-- 000 */
 		case 0x70: WFR0 /= WFR1; break;
 		case 0x78: WFR0  = WFR1 / WFR0; break;
 		}
-		ftest(WFR0);
+		ftest();
 		*ST0 = WFR0;
 		break;
 
@@ -545,7 +558,7 @@ fcom00:			TheCPU.fpus &= (~0x4500);	/* (C3,C2,C0) <-- 000 */
 		}
 		*STn(reg) = WFR1;
 		if (exop&2) INCFSPP;
-		ftest(WFR1);
+		ftest();
 		break;
 
 /*64*/	case 0x64:
@@ -578,7 +591,7 @@ fcom00:			TheCPU.fpus &= (~0x4500);	/* (C3,C2,C0) <-- 000 */
 		}
 		*STn(reg) = WFR1;
 		if (exop&2) INCFSPP;
-		ftest(WFR1);
+		ftest();
 		break;
 
 /*41*/	case 0x41:
@@ -627,22 +640,24 @@ fcom00:			TheCPU.fpus &= (~0x4500);	/* (C3,C2,C0) <-- 000 */
 		WFR0 = *ST0;
 		switch(reg) {
 		   case 0:		/* FCHS */
+			TheCPU.fpus &= ~0x200;
 			WFR0 = -WFR0; break;
 		   case 1:		/* FABS */
+			TheCPU.fpus &= ~0x200;
 			WFR0 = fabsl(WFR0); break;
 		   case 4:		/* FTST */
-		   	TheCPU.fpus &= (~0x4500);
+			TheCPU.fpus &= (~0x4700);
 			if (WFR0 < 0.0) TheCPU.fpus |= 0x100;
 			  else if (WFR0 == 0.0) TheCPU.fpus |= 0x4000;
 			  else if (WFR0 > 0.0); /* do nothing; */
 			  else /* not comparable */ TheCPU.fpus |= 0x4500;
 			break;
 		   case 5:		/* FXAM */
+			fxam(WFR0);
 			break;
 		   default:
 			goto fp_notok;
 		}
-		if (reg != 4) ftest(WFR0);
 		*ST0 = WFR0;
 		break;
 
@@ -651,12 +666,14 @@ fcom00:			TheCPU.fpus &= (~0x4500);	/* (C3,C2,C0) <-- 000 */
 //	63.3*	DB 11000011	FINIT
 		   case 2:		/* FCLEX */
 			TheCPU.fpus &= 0x7f00;
+			feclearexcept(FE_ALL_EXCEPT);
 			break;
 		   case 3:		/* FINIT */
 			TheCPU.fpus  = 0;
 			TheCPU.fpstt = 0;
 			TheCPU.fpuc  = 0x37f;
 			TheCPU.fptag = 0xffff;
+			feclearexcept(FE_ALL_EXCEPT);
 			WFR0 = WFR1 = 0.0;
 			break;
 		   default: /* FNENI,FNDISI: 8087 */
@@ -684,7 +701,7 @@ fcom00:			TheCPU.fpus &= (~0x4500);	/* (C3,C2,C0) <-- 000 */
 			case 6: WFR0 = 0.0; break;
 			default: goto fp_notok;
 			}
-			ftest(WFR0);
+			ftest();
 			*ST0 = WFR0;
 		   }
 		   break;
@@ -839,7 +856,7 @@ fcom00:			TheCPU.fpus &= (~0x4500);	/* (C3,C2,C0) <-- 000 */
 		   case 4:		/* FRNDINT */
 	   		WFR0 = *ST0;
 			round_double();
-			ftest(WFR0);
+			ftest();
 			*ST0 = WFR0;
 			break;
 		   case 6:		/* FSIN */
@@ -996,6 +1013,7 @@ fcom00:			TheCPU.fpus &= (~0x4500);	/* (C3,C2,C0) <-- 000 */
 			TheCPU.fpstt = 0;
 			TheCPU.fpuc  = 0x37f;
 			TheCPU.fptag = 0xffff;
+			feclearexcept(FE_ALL_EXCEPT);
 			WFR0 = WFR1 = 0.0;
 		    }
 		   }