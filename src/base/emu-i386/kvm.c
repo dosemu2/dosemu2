dosemu2/dosemu2

commit e5ae21af1a663418d9e771dff1af8464e81a5528

KVM: map monitor directly after main pool map.
The use of DOSADDR_REL for the monitor was a little awkward since it assumes that
monitor is allocated within a 32bit distance of mem_base. In practice it would
wrap around (allocated below mem_base) and pick some random high address in 4G RAM.

It's cleaner to have a defined location, e.g. directly behind the main pool,
so just do that.

src/base/emu-i386/kvm.c
@@ -173,7 +173,7 @@ void kvm_set_idt_default(int i)
 {
     if (i < 0x11)
         return;
-    set_idt_default(DOSADDR_REL((unsigned char *)monitor), i);
+    set_idt_default(sregs.tr.base, i);
 }
 
 static void set_idt(int i, uint16_t sel, uint32_t offs, int is_32, int tg)
@@ -211,7 +211,7 @@ static void kvm_set_desc(Descriptor *desc, struct kvm_segment *seg)
 }
 
 /* initialize KVM virtual machine monitor */
-void init_kvm_monitor(void)
+void init_kvm_monitor(dosaddr_t monitor_dosaddr)
 {
   int ret, i;
 
@@ -224,7 +224,7 @@ void init_kvm_monitor(void)
   /* exclude special regions for KVM-internal TSS and identity page */
   mmap_kvm(MAPPING_SCRATCH|MAPPING_KVM, monitor,
 	offsetof(struct monitor, kvm_tss),
-	PROT_READ | PROT_WRITE, DOSADDR_REL((unsigned char *)monitor));
+	PROT_READ | PROT_WRITE, monitor_dosaddr);
   /* trap all I/O instructions with GPF */
   memset(monitor->io_bitmap, 0xff, TSS_IOPB_SIZE+1);
 
@@ -240,7 +240,7 @@ void init_kvm_monitor(void)
     return;
   }
 
-  sregs.tr.base = DOSADDR_REL((unsigned char *)monitor);
+  sregs.tr.base = monitor_dosaddr;
   sregs.tr.limit = offsetof(struct monitor, io_bitmap) + TSS_IOPB_SIZE - 1;
   sregs.tr.selector = 0x18;
   sregs.tr.unusable = 0;
@@ -362,13 +362,13 @@ static int init_kvm_vcpu(void)
      the kernel needs to emulate that using V86 mode, as is necessary
      on Nehalem and earlier Intel CPUs */
   ret = ioctl(vmfd, KVM_SET_TSS_ADDR,
-	      (unsigned long)DOSADDR_REL(monitor->kvm_tss));
+	      sregs.tr.base + offsetof(struct monitor, kvm_tss));
   if (ret == -1) {
     perror("KVM: KVM_SET_TSS_ADDR\n");
     return 0;
   }
 
-  uint64_t addr = DOSADDR_REL(monitor->kvm_identity_map);
+  uint64_t addr = sregs.tr.base + offsetof(struct monitor, kvm_identity_map);
   ret = ioctl(vmfd, KVM_SET_IDENTITY_MAP_ADDR, &addr);
   if (ret == -1) {
     perror("KVM: KVM_SET_IDENTITY_MAP_ADDR\n");

src/base/init/init.c
@@ -349,7 +349,7 @@ void low_mem_init(void)
 
   mem_base = mem_reserve(memsize, dpmi_size);
   if (config.cpu_vm == CPUVM_KVM || config.cpu_vm_dpmi == CPUVM_KVM) {
-    init_kvm_monitor();
+    init_kvm_monitor(memsize + dpmi_size);
     mmap_kvm(MAPPING_INIT_LOWRAM, mem_base, memsize + dpmi_size,
 	    PROT_READ | PROT_WRITE, 0);
   }

src/include/kvm.h
@@ -22,7 +22,7 @@
 #ifdef USE_KVM
 /* kvm functions */
 int init_kvm_cpu(void);
-void init_kvm_monitor(void);
+void init_kvm_monitor(dosaddr_t monitor_dosaddr);
 int kvm_vm86(struct vm86_struct *info);
 int kvm_dpmi(cpuctx_t *scp);
 void mprotect_kvm(int cap, dosaddr_t targ, size_t mapsize, int protect);
@@ -40,7 +40,7 @@ void kvm_done(void);
 
 #else
 static inline int init_kvm_cpu(void) { return -1; }
-static inline void init_kvm_monitor(void) {}
+static inline void init_kvm_monitor(dosaddr_t monitor_dosaddr) {}
 static inline int kvm_vm86(struct vm86_struct *info) { return -1; }
 static inline int kvm_dpmi(cpuctx_t *scp) { return -1; }
 static inline void mprotect_kvm(int cap, dosaddr_t targ, size_t mapsize, int protect) {}

commit b9ae3eae75f5e0b3f9177e4bf1f9b1abdcbd0034

KVM: don't use uninitialized sregs.tr.base in init_kvm_vcpu()
This fixes a regression from e5ae21a
Fixes #1897

src/base/emu-i386/kvm.c
@@ -150,7 +150,7 @@ static struct kvm_sregs sregs;
 #define MAXSLOT 400
 static struct kvm_userspace_memory_region maps[MAXSLOT];
 
-static int init_kvm_vcpu(void);
+static int init_kvm_vcpu(dosaddr_t monitor_dosaddr);
 
 static void set_idt_default(dosaddr_t mon, int i)
 {
@@ -228,7 +228,7 @@ void init_kvm_monitor(dosaddr_t monitor_dosaddr)
   /* trap all I/O instructions with GPF */
   memset(monitor->io_bitmap, 0xff, TSS_IOPB_SIZE+1);
 
-  if (!init_kvm_vcpu()) {
+  if (!init_kvm_vcpu(monitor_dosaddr)) {
     leavedos(99);
     return;
   }
@@ -336,7 +336,7 @@ void init_kvm_monitor(dosaddr_t monitor_dosaddr)
 }
 
 /* Initialize KVM and memory mappings */
-static int init_kvm_vcpu(void)
+static int init_kvm_vcpu(dosaddr_t monitor_dosaddr)
 {
   int ret, mmap_size;
 
@@ -346,13 +346,13 @@ static int init_kvm_vcpu(void)
      the kernel needs to emulate that using V86 mode, as is necessary
      on Nehalem and earlier Intel CPUs */
   ret = ioctl(vmfd, KVM_SET_TSS_ADDR,
-	      sregs.tr.base + offsetof(struct monitor, kvm_tss));
+	      monitor_dosaddr + offsetof(struct monitor, kvm_tss));
   if (ret == -1) {
     perror("KVM: KVM_SET_TSS_ADDR\n");
     return 0;
   }
 
-  uint64_t addr = sregs.tr.base + offsetof(struct monitor, kvm_identity_map);
+  uint64_t addr = monitor_dosaddr + offsetof(struct monitor, kvm_identity_map);
   ret = ioctl(vmfd, KVM_SET_IDENTITY_MAP_ADDR, &addr);
   if (ret == -1) {
     perror("KVM: KVM_SET_IDENTITY_MAP_ADDR\n");