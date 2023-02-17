6e972d6f724b15bd22af54fa88558d98620e29bc
Stas Sergeev <stsp@users.sourceforge.net>
2023-02-17 20:12:07 +0500

    int: use idle_enable() reading clock [fixes #1908]
    Reading time may re-enable interrupts.
    Also these idle() calls had non-zero first arg but trigger_idle()
    was not called for them. So they actually didn't work.

--- src/base/core/int.c
+++ src/base/core/int.c
@@ -1137,7 +1137,7 @@ increments AL so we *don't* lose a day if two consecutive midnights pass.
     case 0:			/* read time counter */
 	{
 	    int day_rollover;
-	    idle(50, 50, 0, "int1a:0");
+	    idle_enable(50, 0, "int1a:0");
 	    if (config.timemode == TM_LINUX) {
 		/* Set BIOS area flags to LINUX time computed values always */
 		last_ticks = get_linux_ticks(0, &day_rollover);
@@ -1449,7 +1449,7 @@ static int msdos(void)
 #endif
 
     case 0x2C:{		/* get time & date */
-	    idle(2, 100, 0, "dos_time");
+	    idle_enable(100, 0, "dos_time");
 	    return 0;
 	}
 
