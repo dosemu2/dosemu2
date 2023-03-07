dosemu2/dosemu2

commit 3476d321d9df69c62a39f0b99f8644944219d2a3

mapping: start adding non-identity map support
This patch adds alias_mapping_pa() and unalias_mapping_pa()
funcs that take pa as an argument.

init: create non-identity mapping for region from hma end to dpmi_base.
xms: use new API that allows to get rid of va.

src/arch/linux/mapping/mapping.c
@@ -179,10 +179,8 @@ static unsigned char *MEM_BASE32x(dosaddr_t a, int base)
 {
   if (mem_bases[base] == MAP_FAILED)
     return MAP_FAILED;
-  if (base == MEM_BASE)
+  if (base == MEM_BASE || a >= ALIAS_SIZE)
     return MEM_BASE32(a);
-  if (a >= ALIAS_SIZE)
-    return MAP_FAILED;
   return &mem_bases[base][a];
 }
 
@@ -415,7 +413,6 @@ void *mmap_mapping_huge_page_aligned(int cap, size_t mapsize, int protect)
       void *kvm_base = mmap_mapping_huge_page_aligned(cap, mapsize, protect);
       if (kvm_base == MAP_FAILED)
 	return kvm_base;
-      mmap_kvm(cap, 0, mapsize, kvm_base, 0, protect);
       mem_bases[KVM_BASE] = kvm_base;
     }
 #ifdef __i386__
@@ -695,13 +692,29 @@ void *realloc_mapping(int cap, void *addr, size_t oldsize, size_t newsize)
   return mappingdriver->realloc(cap, addr, oldsize, newsize);
 }
 
+static void populate_aliasmap(unsigned char **map, unsigned char *addr,
+	int size)
+{
+  int i;
+
+  for (i = 0; i < size >> PAGE_SHIFT; i++)
+    map[i] = addr ? addr + (i << PAGE_SHIFT) : NULL;
+}
+
+static unsigned char **alloc_aliasmap(unsigned char *addr, int size)
+{
+  unsigned char **ret = malloc((size >> PAGE_SHIFT) * sizeof(*ret));
+  populate_aliasmap(ret, addr, size);
+  return ret;
+}
+
 struct hardware_ram {
   size_t base;
   dosaddr_t default_vbase;
   dosaddr_t vbase;
   size_t size;
   int type;
-  void *uaddr;
+  unsigned char **aliasmap;
   struct hardware_ram *next;
 };
 
@@ -739,15 +752,17 @@ static int do_map_hwram(struct hardware_ram *hw)
 void init_hardware_ram(void)
 {
   struct hardware_ram *hw;
+  unsigned char *uaddr;
 
   for (hw = hardware_ram; hw != NULL; hw = hw->next) {
     int cap = MAPPING_KMEM;
-    if (hw->uaddr)  /* virtual hardware ram mapped later */
+    if (hw->vbase != (dosaddr_t)-1)  /* virtual hardware ram mapped later */
       continue;
     if (hw->default_vbase != (dosaddr_t)-1)
       cap |= MAPPING_LOWMEM;
 #ifdef __linux__
-    hw->uaddr = alloc_mapping_kmem(cap, hw->size, hw->base);
+    uaddr = alloc_mapping_kmem(cap, hw->size, hw->base);
+    populate_aliasmap(hw->aliasmap, uaddr, hw->size);
 #endif
     if (do_map_hwram(hw) == -1)
       return;
@@ -788,7 +803,7 @@ int unmap_hardware_ram(char type)
   return rc;
 }
 
-static int do_register_hwram(int type, dosaddr_t base, unsigned size,
+static int do_register_hwram(int type, unsigned base, unsigned size,
 	void *uaddr, dosaddr_t va)
 {
   struct hardware_ram *hw;
@@ -807,7 +822,7 @@ static int do_register_hwram(int type, dosaddr_t base, unsigned size,
   hw->vbase = va;
   hw->size = size;
   hw->type = type;
-  hw->uaddr = uaddr;
+  hw->aliasmap = alloc_aliasmap(uaddr, size);
   hw->next = hardware_ram;
   hardware_ram = hw;
   if (!uaddr && (base >= LOWMEM_SIZE || type == 'h'))
@@ -815,15 +830,27 @@ static int do_register_hwram(int type, dosaddr_t base, unsigned size,
   return 1;
 }
 
-int register_hardware_ram(int type, dosaddr_t base, unsigned int size)
+int register_hardware_ram(int type, unsigned base, unsigned int size)
 {
   return do_register_hwram(type, base, size, NULL, -1);
 }
 
-int register_hardware_ram_virtual(int type, unsigned base, unsigned int size,
+void register_hardware_ram_virtual2(int type, unsigned base, unsigned int size,
 	void *uaddr, dosaddr_t va)
 {
-  return do_register_hwram(type, base, size, uaddr, va);
+  do_register_hwram(type, base, size, MEM_BASE32(va), va);
+  if (config.cpu_vm_dpmi == CPUVM_KVM ||
+      (config.cpu_vm == CPUVM_KVM && base + size <= LOWMEM_SIZE + HMASIZE)) {
+    int prot = PROT_READ | PROT_WRITE | PROT_EXEC;
+    mmap_kvm(MAPPING_INIT_LOWRAM, base, size, uaddr, va, prot);
+  }
+}
+
+void register_hardware_ram_virtual(int type, unsigned base, unsigned int size,
+	dosaddr_t va)
+{
+  register_hardware_ram_virtual2(type, base, size,
+	MEM_BASE32x(va, KVM_BASE), va);
 }
 
 int unregister_hardware_ram_virtual(dosaddr_t base)
@@ -836,6 +863,7 @@ int unregister_hardware_ram_virtual(dosaddr_t base)
         phw->next = hw->next;
       else
         hardware_ram = hw->next;
+      free(hw->aliasmap);
       free(hw);
       return 0;
     }
@@ -865,14 +893,25 @@ dosaddr_t get_hardware_ram(unsigned addr, uint32_t size)
   return do_get_hardware_ram(addr, size, NULL);
 }
 
+static void hwram_update_aliasmap(struct hardware_ram *hw, unsigned addr,
+	int size, unsigned char *src)
+{
+  int off = addr - hw->base;
+  assert(!(off & (PAGE_SIZE - 1))); // page-aligned
+  assert(!(size & (PAGE_SIZE - 1))); // page-aligned
+  populate_aliasmap(&hw->aliasmap[off >> PAGE_SHIFT], src, size);
+}
+
 void *get_hardware_uaddr(unsigned addr)
 {
   struct hardware_ram *hw;
 
   for (hw = hardware_ram; hw != NULL; hw = hw->next) {
     if (hw->vbase != -1 &&
-	hw->base <= addr && addr < hw->base + hw->size)
-      return hw->uaddr + addr - hw->base;
+	hw->base <= addr && addr < hw->base + hw->size) {
+      int off = addr - hw->base;
+      return hw->aliasmap[off >> PAGE_SHIFT] + (off & (PAGE_SIZE - 1));
+    }
   }
   return MAP_FAILED;
 }
@@ -977,7 +1016,7 @@ int mcommit(void *ptr, size_t size)
 {
   int err;
   dosaddr_t targ = DOSADDR_REL(ptr);
-  int cap = (targ >= LOWMEM_SIZE + HMASIZE ? MAPPING_DPMI : MAPPING_LOWMEM);
+  int cap = MAPPING_INIT_LOWRAM;
   err = mprotect_mapping(cap, targ, size, PROT_READ | PROT_WRITE);
   if (err == -1)
     return 0;
@@ -992,7 +1031,7 @@ int mcommit(void *ptr, size_t size)
 int muncommit(void *ptr, size_t size)
 {
   dosaddr_t targ = DOSADDR_REL(ptr);
-  int cap = (targ >= LOWMEM_SIZE + HMASIZE ? MAPPING_DPMI : MAPPING_LOWMEM);
+  int cap = MAPPING_INIT_LOWRAM;
   if (mprotect_mapping(cap, targ, size, PROT_NONE) == -1)
     return 0;
   return 1;
@@ -1011,6 +1050,7 @@ int alias_mapping_pa(int cap, unsigned addr, size_t mapsize, int protect,
   if (addr2 == MAP_FAILED)
     return 0;
   assert(addr2 == MEM_BASE32(va));
+  hwram_update_aliasmap(hw, addr, mapsize, source);
   return 1;
 }
 
@@ -1022,5 +1062,6 @@ int unalias_mapping_pa(int cap, unsigned addr, size_t mapsize)
     return 0;
   assert(addr >= LOWMEM_SIZE + HMASIZE);
   restore_mapping(cap, va, mapsize);
+  hwram_update_aliasmap(hw, addr, mapsize, NULL);
   return 1;
 }

src/arch/linux/mapping/mapfile.c
@@ -93,7 +93,8 @@ static int commit(void *ptr, size_t size)
 
 static int open_mapping_f(int cap)
 {
-    int mapsize, estsize, padsize;
+    int mapsize = 0;
+    int estsize, padsize;
 
     if (cap) Q_printf("MAPPING: open, cap=%s\n",
 	  decode_mapping_cap(cap));
@@ -101,12 +102,11 @@ static int open_mapping_f(int cap)
     padsize = 4*1024;
 
     /* first estimate the needed size of the mapfile */
-    mapsize  = HMASIZE >> 10;	/* HMA */
- 				/* VGAEMU */
     mapsize += config.vgaemu_memsize;
     mapsize += config.ems_size;	/* EMS */
     mapsize += config.xms_size;	/* XMS */
-    mapsize += LOWMEM_SIZE >> 10; /* Low Mem */
+    mapsize += config.ext_mem;	/* extended mem */
+    mapsize += (LOWMEM_SIZE + HMASIZE) >> 10; /* Low Mem */
     estsize = mapsize;
 				/* keep heap fragmentation in mind */
     mapsize += (mapsize/4 < padsize ? padsize : mapsize/4);

src/base/dev/vga/vgaemu.c
@@ -1740,24 +1740,14 @@ int vga_emu_pre_init(void)
     if (addr) {
       vga.mem.lfb_base = DOSADDR_REL(addr);
       memcheck_addtype('e', "VGAEMU LFB");
-      register_hardware_ram_virtual('e', VGAEMU_PHYS_LFB_BASE, vga.mem.size,
-				    vga.mem.base, vga.mem.lfb_base);
+      register_hardware_ram_virtual2('e', VGAEMU_PHYS_LFB_BASE, vga.mem.size,
+				     vga.mem.base, vga.mem.lfb_base);
       if (!alias_mapping_pa(MAPPING_VGAEMU, VGAEMU_PHYS_LFB_BASE,
 			    vga.mem.size, VGA_EMU_RW_PROT, vga.mem.base))
 	addr = NULL;
     }
-    if (addr && config.cpu_vm_dpmi == CPUVM_KVM) {
-      /* create mapping:
-	 Linear vga.mem.lfb_base -> phys VGAEMU_PHYS_LFB_BASE=0xe0000000 ->
-	 addr (hugepage aligned) -> vga.mem.base */
-      addr = alias_mapping_huge_page_aligned(MAPPING_VGAEMU, vga.mem.size,
-					     VGA_EMU_RW_PROT, vga.mem.base);
-      if (addr != MAP_FAILED) {
-	mmap_kvm(MAPPING_VGAEMU, VGAEMU_PHYS_LFB_BASE, vga.mem.size, addr,
-		 vga.mem.lfb_base, VGA_EMU_RW_PROT);
+    if (addr && config.cpu_vm_dpmi == CPUVM_KVM)
 	kvm_set_dirty_log(VGAEMU_PHYS_LFB_BASE, vga.mem.size);
-      }
-    }
     if(addr == NULL || addr == MAP_FAILED) {
       error("vga_emu_init: not enough memory (%u k)\n", vga.mem.size >> 10);
       config.exitearly = 1;

src/base/emu-i386/kvm.c
@@ -609,7 +609,7 @@ void mprotect_kvm(int cap, dosaddr_t targ, size_t mapsize, int protect)
   unsigned int page;
   struct kvm_userspace_memory_region *p;
 
-  if (!(cap & (MAPPING_INIT_LOWRAM|MAPPING_LOWMEM|MAPPING_EMS|MAPPING_HMA|
+  if (!(cap & (MAPPING_LOWMEM|MAPPING_EMS|MAPPING_HMA|
 	       MAPPING_DPMI|MAPPING_VGAEMU|MAPPING_KVM|MAPPING_CPUEMU|
 	       MAPPING_EXTMEM))) return;
 

src/base/emu-i386/simx86/trees.c
@@ -1312,6 +1312,14 @@ void e_invalidate_pa(unsigned pa, int cnt)
     e_invalidate(addr, cnt);
 }
 
+void e_invalidate_full_pa(unsigned pa, int cnt)
+{
+    dosaddr_t addr = physaddr_to_dosaddr(pa, cnt);
+    if (addr == (dosaddr_t)-1)
+	return;
+    e_invalidate_full(addr, cnt);
+}
+
 /* invalidate and unprotect even if we hit only data.
  * Needed if we are about to destroy the page protection by other means.
  * Otherwise use e_invalidate() */

src/base/init/init.c
@@ -314,11 +314,6 @@ static void *mem_reserve(uint32_t memsize)
     perror ("LOWRAM mmap");
     exit(EXIT_FAILURE);
   }
-  if (config.cpu_vm_dpmi == CPUVM_KVM)
-    /* map only high memory here, rest is done with KVM_BASE in alias_mapping */
-    mmap_kvm(cap, LOWMEM_SIZE + HMASIZE, memsize - (LOWMEM_SIZE + HMASIZE),
-	     (unsigned char *)result + LOWMEM_SIZE + HMASIZE,
-	     LOWMEM_SIZE + HMASIZE, prot);
   return result;
 }
 
@@ -360,14 +355,15 @@ static void low_mem_init_config_scrub(void)
  */
 void low_mem_init(void)
 {
-  void *lowmem, *ptr;
+  unsigned char *lowmem, *ptr, *ptr2;
   int result;
   uint32_t memsize = config.dpmi ? LOWMEM_SIZE + HMASIZE + dpmi_lin_mem_rsv() : config.dpmi_base;
   int32_t dpmi_rsv_low, phys_rsv;
 
   open_mapping(MAPPING_INIT_LOWRAM);
   g_printf ("DOS+HMA memory area being mapped in\n");
-  lowmem = alloc_mapping(MAPPING_INIT_LOWRAM, LOWMEM_SIZE + HMASIZE);
+  lowmem = alloc_mapping(MAPPING_INIT_LOWRAM, LOWMEM_SIZE + HMASIZE +
+	EXTMEM_SIZE);
   if (lowmem == MAP_FAILED) {
     perror("LOWRAM alloc");
     leavedos(98);
@@ -378,6 +374,7 @@ void low_mem_init(void)
 #ifdef __x86_64__
   if (_MAP_32BIT) mem_base_mask = 0xffffffffu;
 #endif
+  register_hardware_ram_virtual('L', 0, LOWMEM_SIZE + HMASIZE, 0);
   result = alias_mapping(MAPPING_LOWMEM, 0, LOWMEM_SIZE + HMASIZE,
 			 PROT_READ | PROT_WRITE | PROT_EXEC, lowmem);
   if (result == -1) {
@@ -405,14 +402,34 @@ void low_mem_init(void)
   }
 
   if (config.ext_mem) {
-    /* LOWMEM_SIZE + HMASIZE == base
-     * Use dpmi_rsv_low as ext_mem. */
-    register_hardware_ram_virtual('X', LOWMEM_SIZE + HMASIZE, EXTMEM_SIZE,
-	    MEM_BASE32(LOWMEM_SIZE + HMASIZE), LOWMEM_SIZE + HMASIZE);
+    /* LOWMEM_SIZE + HMASIZE == base */
     memcheck_addtype('X', "EXT MEM");
     memcheck_reserve('X', LOWMEM_SIZE + HMASIZE, EXTMEM_SIZE);
     x_printf("Ext.Mem of size 0x%x at %#x\n", EXTMEM_SIZE, result + LOWMEM_SIZE + HMASIZE);
   }
+
+  ptr += phys_rsv + dpmi_rsv_low;
+  /* create non-identity mapping up to dpmi_base */
+  phys_rsv = config.dpmi_base - (LOWMEM_SIZE + HMASIZE);
+  ptr2 = smalloc_aligned_topdown(&main_pool, MEM_BASE32(memsize),
+	PAGE_SIZE, phys_rsv);
+  assert(ptr2);
+  /* establish alias access for int15 */
+  register_hardware_ram_virtual('X', LOWMEM_SIZE + HMASIZE, phys_rsv,
+	    DOSADDR_REL(ptr2));
+  register_hardware_ram_virtual('U',  DOSADDR_REL(ptr2), phys_rsv,
+	    LOWMEM_SIZE + HMASIZE);
+  /* map dpmi+uncommitted space to kvm */
+  if (config.cpu_vm_dpmi == CPUVM_KVM) {
+    int prot = PROT_READ | PROT_WRITE | PROT_EXEC;
+    mmap_kvm(MAPPING_INIT_LOWRAM, config.dpmi_base, ptr2 - ptr, ptr,
+	config.dpmi_base, prot);
+  }
+  /* create alias for dpmi */
+  result = alias_mapping(MAPPING_EXTMEM, DOSADDR_REL(ptr2), EXTMEM_SIZE,
+			 PROT_READ | PROT_WRITE,
+			 lowmem + LOWMEM_SIZE + HMASIZE);
+  assert(result != -1);
 
   /* R/O protect 0xf0000-0xf4000 */
   if (!config.umb_f0)

src/dosext/dpmi/memory.c
@@ -267,7 +267,6 @@ int dpmi_alloc_pool(void)
 
 void dpmi_free_pool(void)
 {
-    uint32_t memsize = dpmi_mem_size() + low_rsv;
     int leak = smdestroy(&mem_pool);
     if (leak)
 	error("DPMI: leaked %i bytes (main pool)\n", leak);
@@ -275,7 +274,7 @@ void dpmi_free_pool(void)
     if (leak)
 	error("DPMI: leaked %i bytes (lin pool)\n", leak);
     mprotect_mapping(MAPPING_DPMI, DOSADDR_REL(dpmi_lin_rsv_base),
-                memsize, PROT_READ | PROT_WRITE);
+                low_rsv, PROT_READ | PROT_WRITE);
 }
 
 static int SetAttribsForPage(unsigned int ptr, us attr, us *old_attr_p)

src/dosext/misc/xms.c
@@ -78,17 +78,12 @@ struct __attribute__ ((__packed__)) EMM {
    unsigned int DestOffset;
 };
 
-struct pava {
-  unsigned pa;
-  dosaddr_t va;
-};
-
 struct Handle {
   unsigned short int num;
   void *addr;
   unsigned int size;
   int lockcount;
-  struct pava dst;
+  unsigned dst;
 };
 
 static struct Handle handles[NUM_HANDLES];
@@ -253,36 +248,31 @@ static void xms_free(void *addr, unsigned osize)
   free_mapping(MAPPING_OTHER, addr, PAGE_ALIGN(osize));
 }
 
-static struct pava map_EMB(void *addr, unsigned size, unsigned handle)
+static unsigned map_EMB(void *addr, unsigned size, unsigned handle)
 {
   int page, rc;
-  struct pava ret = {};
+  unsigned ret = 0;
 
   page = pgaalloc(pgapool, PAGE_ALIGN(size) >> PAGE_SHIFT, handle);
   if (page < 0) {
     error("error allocating %i bytes for xms\n", size);
     return ret;
   }
-  ret.pa = xms_base + (page << PAGE_SHIFT);
-  rc = alias_mapping(MAPPING_EXTMEM, ret.pa, PAGE_ALIGN(size),
+  ret = xms_base + (page << PAGE_SHIFT);
+  rc = alias_mapping_pa(MAPPING_EXTMEM, ret, PAGE_ALIGN(size),
 		 PROT_READ | PROT_WRITE, addr);
   if (rc == -1) {
     error("failure to map xms\n");
     leavedos(2);
   }
-  ret.va = ret.pa; // identity mapped
-  register_hardware_ram_virtual('x', ret.pa, size, addr, ret.va);
   return ret;
 }
 
-static void unmap_EMB(struct pava base, unsigned size)
+static void unmap_EMB(unsigned base, unsigned size)
 {
-  int err = unregister_hardware_ram_virtual(base.pa);
-  if (err)
-    error("error unregistering hwram at %#x\n", base.pa);
-  e_invalidate_full(base.va, PAGE_ALIGN(size));
-  restore_mapping(MAPPING_DPMI, base.va, PAGE_ALIGN(size));
-  pgafree(pgapool, (base.pa - xms_base) >> PAGE_SHIFT);
+  e_invalidate_full_pa(base, PAGE_ALIGN(size));
+  unalias_mapping_pa(MAPPING_DPMI, base, PAGE_ALIGN(size));
+  pgafree(pgapool, (base - xms_base) >> PAGE_SHIFT);
 }
 
 #if 0
@@ -302,9 +292,9 @@ void *xms_resolve_physaddr(unsigned addr)
 
 static void do_free_EMB(int h)
 {
-    if (handles[h].dst.pa)
+    if (handles[h].dst)
       unmap_EMB(handles[h].dst, handles[h].size);
-    handles[h].dst.pa = 0;
+    handles[h].dst = 0;
     if (handles[h].addr)
       xms_free(handles[h].addr, handles[h].size);
     handles[h].addr = NULL;
@@ -906,8 +896,8 @@ xms_move_EMB(void)
     if (e.DestOffset + e.Length > handles[e.DestHandle].size) {
       return 0xa7;		/* invalid Length */
     }
-    if (handles[e.DestHandle].dst.pa) // .pa is set if mapped, invalidate on va
-      e_invalidate(handles[e.DestHandle].dst.va + e.DestOffset, e.Length);
+    if (handles[e.DestHandle].dst) // .pa is set if mapped, invalidate on va
+      e_invalidate_pa(handles[e.DestHandle].dst + e.DestOffset, e.Length);
     d = handles[e.DestHandle].addr + e.DestOffset;
   }
 
@@ -923,7 +913,7 @@ static unsigned char
 xms_lock_EMB(int flag)
 {
   int h = LWORD(edx);
-  struct pava addr;
+  unsigned addr;
 
   if (ValidHandle(h)) {
     /* flag=1 locks, flag=0 unlocks */
@@ -936,20 +926,20 @@ xms_lock_EMB(int flag)
         return 0xaa;		/* Block is not locked */
       }
       if (!handles[h].lockcount) {
-        x_printf("XMS unlock EMB %d --> %#x\n", h, handles[h].dst.pa);
+        x_printf("XMS unlock EMB %d --> %#x\n", h, handles[h].dst);
         unmap_EMB(handles[h].dst, handles[h].size);
-        handles[h].dst.pa = 0;
+        handles[h].dst = 0;
       }
       return 0;
     }
 
     addr = map_EMB(handles[h].addr, handles[h].size, h);
-    if (addr.pa) {
+    if (addr) {
       handles[h].lockcount++;
-      x_printf("XMS lock EMB %d --> %#x, va=%x\n", h, addr.pa, addr.va);
+      x_printf("XMS lock EMB %d --> %#x\n", h, addr);
       handles[h].dst = addr;
-      LWORD(edx) = addr.pa >> 16;
-      LWORD(ebx) = addr.pa & 0xffff;
+      LWORD(edx) = addr >> 16;
+      LWORD(ebx) = addr & 0xffff;
       return 0;
     }
     x_printf("XMS lock EMB %d failed\n", h);

commit be20f2d33595975aeaa08b5ef348e3fbbcd94242

mapping: fix XMS regression from 3476d32
the test:
test/test_dos.py PPDOSGITTestCase.test_memory_xms
fails with KVM since commit 3476d32

alias_mapping_pa() (similar to alias_mapping()) needs to forward the
new page protections to KVM's page tables, and similar for
unalias_mapping_pa() via restore_mapping().

src/arch/linux/mapping/mapping.c
@@ -459,6 +459,8 @@ int restore_mapping(int cap, dosaddr_t targ, size_t mapsize)
   assert((cap & MAPPING_DPMI) && (targ != (dosaddr_t)-1));
   target = MEM_BASE32(targ);
   addr = mmap_mapping(cap, target, mapsize, PROT_READ | PROT_WRITE);
+  if (is_kvm_map(cap))
+    mprotect_kvm(cap, targ, mapsize, PROT_READ | PROT_WRITE);
   return (addr == target ? 0 : -1);
 }
 
@@ -1069,6 +1071,8 @@ int alias_mapping_pa(int cap, unsigned addr, size_t mapsize, int protect,
     return 0;
   assert(addr2 == MEM_BASE32(va));
   hwram_update_aliasmap(hw, addr, mapsize, source);
+  if (is_kvm_map(cap))
+    mprotect_kvm(cap, va, mapsize, protect);
   return 1;
 }
 