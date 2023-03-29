f33a2e2dd662239105ef27d6d5fb728010f9c59e
Bart Oldeman <bartoldeman@users.noreply.github.com>
2023-03-29 10:11:24 -0400

    instremu: Remove parts that are no longer used.
    The remainder is still used by page faults on ROM (to determine
    instruction lengths), and the dpmi/msdos LDT code.

--- src/include/instremu.h
+++ src/include/instremu.h
@@ -2,7 +2,6 @@
 #define INSTREMU_H
 
 int instr_len(unsigned char *ptr, int is_32);
-int instr_emu(cpuctx_t *scp, int pmode, int cnt);
 int decode_modify_segreg_insn(cpuctx_t *scp, int pmode,
     unsigned int *new_val);
 unsigned char instr_binary_byte(unsigned char op, unsigned char op1,
@@ -11,5 +10,4 @@ unsigned instr_binary_word(unsigned op, unsigned op1,
                                    unsigned op2, unsigned *eflags);
 unsigned instr_binary_dword(unsigned op, unsigned op1,
                                    unsigned op2, unsigned *eflags);
-
 #endif
