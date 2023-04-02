dosemu2/dosemu2

commit 16195ac8567668a5aa6d62dd73f518aa2be72b84

mapping: hwram API extensions [#222]
This adds pin_hardware_ram() and unpin_hardware_ram() functions.
They switch the hwram and scratch mapping, supporting copyback.
map_hardware_ram() and unmap_hardware_ram() became unused, but can
be used for DPMI functions 0x800 and 0x801.

src/arch/linux/mapping/mapping.c
@@ -147,7 +147,7 @@ static int map_find(struct mem_map_struct *map, int max,
   return idx;
 }
 
-static void kmem_unmap_single(int idx)
+static dosaddr_t kmem_unmap_single(int idx)
 {
   kmem_map[idx].base = mmap(0, kmem_map[idx].len, PROT_NONE,
 	       MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
@@ -155,6 +155,7 @@ static void kmem_unmap_single(int idx)
       kmem_map[idx].len, MREMAP_MAYMOVE | MREMAP_FIXED, kmem_map[idx].base);
   kmem_map[idx].mapped = 0;
   update_aliasmap(kmem_map[idx].dst, kmem_map[idx].len, NULL);
+  return kmem_map[idx].dst;
 }
 
 static int kmem_unmap_mapping(dosaddr_t addr, int mapsize)
@@ -265,6 +266,36 @@ static void *mmap_mapping_kmem(int cap, dosaddr_t targ, size_t mapsize,
   kmem_map_single(i, targ);
 
   return target;
+}
+
+static void munmap_mapping_kmem(int cap, dosaddr_t addr, size_t mapsize)
+{
+  int i, rc;
+
+  Q__printf("MAPPING: unmap kmem, cap=%s, target=%x, size=%zx\n",
+	cap, addr, mapsize);
+
+  while ((i = map_find(kmem_map, kmem_mappings, addr, mapsize, 1)) != -1) {
+    dosaddr_t old_vbase = kmem_unmap_single(i);
+    if (!(cap & MAPPING_SCRATCH))
+      continue;
+    if (old_vbase < LOWMEM_SIZE) {
+      rc = alias_mapping(MAPPING_LOWMEM, old_vbase, mapsize,
+	    PROT_READ | PROT_WRITE, LOWMEM(old_vbase));
+    } else {
+      unsigned char *p;
+      p = mmap_mapping_ux(MAPPING_SCRATCH, MEM_BASE32(old_vbase), mapsize,
+          PROT_READ | PROT_WRITE);
+      if (p == MAP_FAILED)
+        rc = -1;
+    }
+    if (rc == -1) {
+      error("failure unmapping kmem region\n");
+      continue;
+    }
+    if (cap & MAPPING_COPYBACK)
+      memcpy_2dos(old_vbase, kmem_map[i].base, mapsize);
+  }
 }
 
 static void *do_mmap_mapping(int cap, void *target, size_t mapsize, int protect)
@@ -582,7 +613,7 @@ void init_hardware_ram(void)
   }
 }
 
-int map_hardware_ram(char type, int cap)
+int map_hardware_ram(char type)
 {
   struct hardware_ram *hw;
 
@@ -591,6 +622,31 @@ int map_hardware_ram(char type, int cap)
       continue;
     if (do_map_hwram(hw) == -1)
       return -1;
+  }
+  return 0;
+}
+
+int pin_hardware_ram(char type)
+{
+  struct hardware_ram *hw;
+  unsigned char *p;
+  int cap;
+
+  for (hw = hardware_ram; hw != NULL; hw = hw->next) {
+    if (hw->type != type)
+      continue;
+    if (hw->vbase == -1) {
+      dosemu_error("unable to pin hwram %c\n", type);
+      return -1;
+    }
+    cap = MAPPING_KMEM | MAPPING_COPYBACK;
+    p = mmap_mapping_kmem(cap, hw->vbase, hw->size, hw->base);
+    if (p == MAP_FAILED) {
+      error("mmap error in map_hardware_ram %s\n", strerror (errno));
+      return -1;
+    }
+    g_printf("pinned hardware ram at 0x%08zx .. 0x%08zx at %#x\n",
+	    hw->base, hw->base+hw->size-1, hw->vbase);
   }
   return 0;
 }
@@ -608,7 +664,7 @@ int map_hardware_ram_manual(size_t base, dosaddr_t vbase)
   return -1;
 }
 
-int unmap_hardware_ram(char type, int cap)
+int unmap_hardware_ram(char type)
 {
   struct hardware_ram *hw;
   int rc = 0;
@@ -616,14 +672,27 @@ int unmap_hardware_ram(char type, int cap)
   for (hw = hardware_ram; hw != NULL; hw = hw->next) {
     if (hw->type != type || hw->vbase == -1)
       continue;
-    kmem_unmap_mapping(hw->vbase, hw->size);
-    if (hw->vbase < LOWMEM_SIZE) {
-      rc = alias_mapping(cap, hw->vbase, hw->size,
-	    PROT_READ | PROT_WRITE, LOWMEM(hw->vbase));
-    }
+    munmap_mapping_kmem(MAPPING_KMEM, hw->vbase, hw->size);
     g_printf("unmapped hardware ram at 0x%08zx .. 0x%08zx at %#x\n",
 	    hw->base, hw->base+hw->size-1, hw->vbase);
     hw->vbase = -1;
+  }
+  return rc;
+}
+
+int unpin_hardware_ram(char type)
+{
+  struct hardware_ram *hw;
+  int rc = 0;
+  int cap;
+
+  for (hw = hardware_ram; hw != NULL; hw = hw->next) {
+    if (hw->type != type || hw->vbase == -1)
+      continue;
+    cap = MAPPING_KMEM | MAPPING_SCRATCH | MAPPING_COPYBACK;
+    munmap_mapping_kmem(cap, hw->vbase, hw->size);
+    g_printf("unpinned hardware ram at 0x%08zx .. 0x%08zx at %#x\n",
+	    hw->base, hw->base+hw->size-1, hw->vbase);
   }
   return rc;
 }

src/include/mapping.h
@@ -101,9 +101,11 @@ void mapping_init(void);
 void mapping_close(void);
 
 void init_hardware_ram(void);
-int map_hardware_ram(char type, int cap);
+int map_hardware_ram(char type);
 int map_hardware_ram_manual(size_t base, dosaddr_t vbase);
-int unmap_hardware_ram(char type, int cap);
+int unmap_hardware_ram(char type);
+int pin_hardware_ram(char type);
+int unpin_hardware_ram(char type);
 int register_hardware_ram(int type, unsigned base, unsigned size);
 unsigned get_hardware_ram(unsigned addr);
 void list_hardware_ram(void (*print)(const char *, ...));

src/plugin/console/vc.c
@@ -257,12 +257,9 @@ static void release_vt(struct sigcontext *scp, siginfo_t *si)
   SIGNAL_save (SIGRELEASE_call, NULL, 0, __func__);
 }
 
-static void unmap_video_ram(int copyback)
+static void unmap_video_ram(void)
 {
-  int cap = MAPPING_VC | MAPPING_LOWMEM;
-
-  if (copyback) cap |= MAPPING_COPYBACK;
-  unmap_hardware_ram('v', cap);
+  unpin_hardware_ram('v');
   scr_state.mapped = 0;
 }
 
@@ -270,19 +267,16 @@ static void map_video_ram(void)
 {
   off_t pbase = VMEM_BASE;
   unsigned int vbase = pbase;
-  int cap = MAPPING_VC | MAPPING_KMEM;
   int err;
 
   if (!config.vga) {
     pbase = phys_text_base;         /* physical page address    */
     vbase = scr_state.virt_address; /* new virtual page address */
-    /* this is used for page switching */
-    cap |= MAPPING_COPYBACK;
   }
 
   g_printf ("mapping %s\n", config.vga ? "GRAPH_BASE" : "PAGE_ADDR");
 
-  err = map_hardware_ram('v', cap);
+  err = pin_hardware_ram('v');
   if (err) {
       error("mmap error in get_video_ram (text)\n");
       return;
@@ -327,7 +321,7 @@ static void get_video_ram (int waitflag)
   }
 
   if (scr_state.mapped)
-    unmap_video_ram(1);
+    unmap_video_ram();
 
   page = vga.display_start / PAGE_SIZE;
   scr_state.pageno = page;
@@ -340,7 +334,7 @@ static void put_video_ram (void)
 {
   if (scr_state.mapped) {
     v_printf ("put_video_ram called\n");
-    unmap_video_ram(!config.vga);
+    unmap_video_ram();
     if (!scr_state.mapped && config.vga) {
       if (dosemu_regs.mem && READ_BYTE(BIOS_VIDEO_MODE) == 3)
 	MEMCPY_2DOS (virt_text_base, dosemu_regs.mem, 32768);

commit 85434678473fb95a258a132cbdea6c25607b70ad

mapping: remove map/unmap_hardware_ram
Commit 16195ac8 caused them to become unused, for suggested in later
use by DPMI 0800/0801. But those would need quite a different
implementation and not include lowmem.

16195ac8 replace that by pin/unpin_hardware_ram, which themselves
were no longer used since e9a0c01a

src/arch/linux/mapping/mapping.c
@@ -186,34 +186,6 @@ static unsigned char *MEM_BASE32x(dosaddr_t a, int base)
 }
 
 #ifdef __linux__
-static dosaddr_t kmem_unmap_single(int cap, int idx)
-{
-  dosaddr_t ret;
-  if (cap & MAPPING_LOWMEM) {
-    int i;
-    for(i = 0; i < MAX_BASES; i++) {
-      void *dst = MEM_BASE32x(kmem_map[idx].dst, i);
-      if (dst == MAP_FAILED)
-        continue;
-      kmem_map[idx].base[i] = mmap(0, kmem_map[idx].len, PROT_NONE,
-	       MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
-      mremap(dst, kmem_map[idx].len,
-	       kmem_map[idx].len, MREMAP_MAYMOVE | MREMAP_FIXED,
-	       kmem_map[idx].base[i]);
-    }
-  } else {
-    kmem_map[idx].base[MEM_BASE] = mmap(0, kmem_map[idx].len, PROT_NONE,
-	       MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
-    mremap(MEM_BASE32(kmem_map[idx].dst), kmem_map[idx].len,
-	       kmem_map[idx].len, MREMAP_MAYMOVE | MREMAP_FIXED,
-	       kmem_map[idx].base[MEM_BASE]);
-  }
-  update_aliasmap(kmem_map[idx].dst, kmem_map[idx].len, NULL);
-  ret = kmem_map[idx].dst;
-  kmem_map[idx].dst = -1;
-  return ret;
-}
-
 // counts number of kmem mappings, only used for assert
 static int kmem_mapped(dosaddr_t addr, int mapsize)
 {
@@ -329,34 +301,6 @@ static void *mmap_mapping_kmem(int cap, dosaddr_t targ, size_t mapsize,
   kmem_map_single(cap, i, targ);
 
   return target;
-}
-
-static void munmap_mapping_kmem(int cap, dosaddr_t addr, size_t mapsize)
-{
-  int i, rc = 0;
-
-  Q__printf("MAPPING: unmap kmem, cap=%s, target=%x, size=%zx\n",
-	cap, addr, mapsize);
-
-  while ((i = map_find(kmem_map, kmem_mappings, addr, mapsize, 1)) != -1) {
-    dosaddr_t old_vbase = kmem_unmap_single(cap, i);
-    if (old_vbase < LOWMEM_SIZE) {
-      rc = alias_mapping(MAPPING_LOWMEM, old_vbase, mapsize,
-	    PROT_READ | PROT_WRITE, LOWMEM(old_vbase));
-    } else {
-      unsigned char *p;
-      p = mmap_mapping(MAPPING_SCRATCH, MEM_BASE32(old_vbase), mapsize,
-          PROT_READ | PROT_WRITE);
-      if (p == MAP_FAILED)
-        rc = -1;
-      else
-	smfree(&main_pool, p);
-    }
-    if (rc == -1) {
-      error("failure unmapping kmem region\n");
-      continue;
-    }
-  }
 }
 #endif
 
@@ -798,40 +742,6 @@ void init_hardware_ram(void)
     if (do_map_hwram(hw) == -1)
       return;
   }
-}
-
-int map_hardware_ram(char type)
-{
-  struct hardware_ram *hw;
-
-  for (hw = hardware_ram; hw != NULL; hw = hw->next) {
-    if (hw->type != type || hw->vbase != -1)
-      continue;
-    if (do_map_hwram(hw) == -1)
-      return -1;
-  }
-  return 0;
-}
-
-int unmap_hardware_ram(char type)
-{
-  struct hardware_ram *hw;
-  int rc = 0;
-
-  for (hw = hardware_ram; hw != NULL; hw = hw->next) {
-  int cap = MAPPING_KMEM;
-    if (hw->type != type || hw->vbase == -1)
-      continue;
-    if (hw->default_vbase != (dosaddr_t)-1)
-      cap |= MAPPING_LOWMEM;
-#ifdef __linux__
-    munmap_mapping_kmem(cap, hw->vbase, hw->size);
-#endif
-    g_printf("unmapped hardware ram at 0x%08zx .. 0x%08zx at %#x\n",
-	    hw->base, hw->base+hw->size-1, hw->vbase);
-    hw->vbase = -1;
-  }
-  return rc;
 }
 
 static int do_register_hwram(int type, unsigned base, unsigned size,

src/include/mapping.h
@@ -118,8 +118,6 @@ void mapping_init(void);
 void mapping_close(void);
 
 void init_hardware_ram(void);
-int map_hardware_ram(char type);
-int unmap_hardware_ram(char type);
 int register_hardware_ram(int type, dosaddr_t base, unsigned size);
 void register_hardware_ram_virtual(int type, unsigned base, unsigned size,
 	dosaddr_t va);