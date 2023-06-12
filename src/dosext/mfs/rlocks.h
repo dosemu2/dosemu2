46a6136ad17b653b3ffae224520c18713118638e
Stas Sergeev <stsp@users.sourceforge.net>
2023-06-12 18:10:48 +0500

    configure: OFD locks are now optional [#506]

--- src/dosext/mfs/rlocks.h
+++ src/dosext/mfs/rlocks.h
@@ -1,10 +1,32 @@
 #ifndef RLOCKS_H
 #define RLOCKS_H
 
+#if HAVE_DECL_F_OFD_SETLK
+
 int lock_file_region(int fd, int lck, long long start,
     unsigned long len, int wr, int mlemu_fd);
 int region_lock_offs(int fd, long long start, unsigned long len,
     int wr, const char *mlemu);
 void region_unlock_offs(int fd);
+
+#else
+
+static inline int lock_file_region(int fd, int lck, long long start,
+    unsigned long len, int wr, int mlemu_fd)
+{
+    return 0;
+}
+
+static inline int region_lock_offs(int fd, long long start, unsigned long len,
+    int wr, const char *mlemu)
+{
+    return len;
+}
+
+static inline void region_unlock_offs(int fd)
+{
+}
+
+#endif
 
 #endif
