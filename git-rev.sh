400213af9c28bf392f8bf84bba45b6c5da182147
Stas Sergeev <stsp@users.sourceforge.net>
2023-06-13 19:53:23 +0500

    git-rev.sh: bsd-compatible touch syntax [#506]

--- git-rev.sh
+++ git-rev.sh
@@ -4,14 +4,14 @@ if [ $# != 2 ]; then
     echo "Usage: $0 <src_dir> <build_dir>" >&2
     exit 1
 fi
-DATE=`git -C $1 log -1 --format=%cd --date=rfc`
+DATE=`git -C $1 log -1 --format=%cd --date=format-local:'%m%d%H%M.%S'`
 if [ $? != 0 ]; then
     echo "Non-git builds deprecated" >&2
     exit 1
 fi
 TSTAMP=$2/.tstamp
-if ! touch --date="$DATE" $TSTAMP 2>/dev/null; then
-    echo "touch doesnt support --date, build may be incomplete" >&2
+if ! touch -t "$DATE" $TSTAMP 2>/dev/null; then
+    echo "touch doesnt support -t, build may be incomplete" >&2
     if [ ! -f "$TSTAMP" ]; then
         touch $TSTAMP
     fi
