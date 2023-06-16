aa6fc1fa639cc21e2926af1372645b700b69eaa7
Stas Sergeev <stsp@users.sourceforge.net>
2023-06-16 15:25:16 +0500

    reintroduce timerfd-based timers again [#506]
    timer_create() is a rather broken API, and SIGEV_THREAD is
    broken up to not being usable at all:
    https://sourceware.org/bugzilla/show_bug.cgi?id=30558
    
    Use timerfd by default, leaving the broken impl for freebsd only.
    This patch re-introduces the timerfd code in a rewritten form:
    it no longer relies on an ioselect machinery. Therefore moved
    to libs.

---
+++ src/base/lib/timer/evtimer.c
@@ -0,0 +1,186 @@
+/*
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+/*
+ * Purpose: event timer API for freebsd.
+ *
+ * Author: stsp
+ *
+ * Note: SIGEV_THREAD is completely broken.
+ * timerfd-based impl should be used whereever possible.
+ * This implementation is provided only as a fall-back for freebsd.
+ *
+ */
+
+#include <assert.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <time.h>
+#include <signal.h>
+#include <time.h>
+#include <sys/time.h>
+#include <pthread.h>
+#ifdef HAVE_LIBBSD
+#include <bsd/sys/time.h>
+#endif
+#include "evtimer.h"
+
+struct evtimer {
+    timer_t tmr;
+    void (*callback)(int ticks, void *);
+    void *arg;
+    clockid_t clk_id;
+    struct timespec start;
+    pthread_mutex_t start_mtx;
+    int blocked;
+    pthread_mutex_t block_mtx;
+    pthread_cond_t block_cnd;
+    int ticks;
+    int in_cbk;
+};
+
+static void evhandler(union sigval sv)
+{
+    int bl;
+    struct evtimer *t = sv.sival_ptr;
+
+    pthread_mutex_lock(&t->block_mtx);
+    bl = t->blocked;
+    t->ticks += timer_getoverrun(t->tmr) + 1;
+    if (!bl)
+        t->in_cbk++;
+    pthread_mutex_unlock(&t->block_mtx);
+    if (!bl) {
+        t->callback(t->ticks, t->arg);
+        pthread_mutex_lock(&t->block_mtx);
+        t->in_cbk--;
+        t->ticks = 0;
+        pthread_mutex_unlock(&t->block_mtx);
+        pthread_cond_signal(&t->block_cnd);
+    }
+}
+
+void *evtimer_create(void (*cbk)(int ticks, void *), void *arg)
+{
+    struct evtimer *t;
+    clockid_t id = CLOCK_MONOTONIC;
+    struct sigevent sev = { .sigev_notify = SIGEV_THREAD,
+                            .sigev_notify_function = evhandler };
+    timer_t tmr;
+    int rc;
+
+    t = malloc(sizeof(*t));
+    assert(t);
+    sev.sigev_value.sival_ptr = t;
+    rc = timer_create(id, &sev, &tmr);
+    assert(rc != -1);
+    t->tmr = tmr;
+    t->callback = cbk;
+    t->arg = arg;
+    t->clk_id = id;
+    t->blocked = 0;
+    t->ticks = 0;
+    t->in_cbk = 0;
+    pthread_mutex_init(&t->start_mtx, NULL);
+    pthread_mutex_init(&t->block_mtx, NULL);
+    pthread_cond_init(&t->block_cnd, NULL);
+    return t;
+}
+
+void evtimer_delete(void *tmr)
+{
+    struct evtimer *t = tmr;
+
+    timer_delete(t->tmr);
+    pthread_mutex_destroy(&t->start_mtx);
+    pthread_mutex_destroy(&t->block_mtx);
+    pthread_cond_destroy(&t->block_cnd);
+    free(t);
+}
+
+void evtimer_set_rel(void *tmr, uint64_t ns, int periodic)
+{
+    struct evtimer *t = tmr;
+    struct itimerspec i = {};
+    struct timespec rel, abs, start;
+
+    rel.tv_sec = ns / NANOSECONDS_PER_SECOND;
+    rel.tv_nsec = ns % NANOSECONDS_PER_SECOND;
+    if (periodic)
+        i.it_interval = rel;
+    clock_gettime(t->clk_id, &start);
+    timespecadd(&start, &rel, &abs);
+    i.it_value = abs;
+    timer_settime(t->tmr, TIMER_ABSTIME, &i, NULL);
+    pthread_mutex_lock(&t->start_mtx);
+    t->start = start;
+    pthread_mutex_unlock(&t->start_mtx);
+}
+
+uint64_t evtimer_gettime(void *tmr)
+{
+    struct evtimer *t = tmr;
+    struct timespec rel, abs;
+
+    clock_gettime(t->clk_id, &abs);
+    pthread_mutex_lock(&t->start_mtx);
+    timespecsub(&abs, &t->start, &rel);
+    pthread_mutex_unlock(&t->start_mtx);
+    return (rel.tv_sec * NANOSECONDS_PER_SECOND + rel.tv_nsec);
+}
+
+void evtimer_stop(void *tmr)
+{
+    struct evtimer *t = tmr;
+    struct itimerspec i = {};
+    struct timespec start;
+
+    timer_settime(t->tmr, 0, &i, NULL);
+    clock_gettime(t->clk_id, &start);
+    pthread_mutex_lock(&t->start_mtx);
+    t->start = start;
+    pthread_mutex_unlock(&t->start_mtx);
+}
+
+void evtimer_block(void *tmr)
+{
+    struct evtimer *t = tmr;
+
+    pthread_mutex_lock(&t->block_mtx);
+    t->blocked++;
+    while (t->in_cbk)
+        pthread_cond_wait(&t->block_cnd, &t->block_mtx);
+    pthread_mutex_unlock(&t->block_mtx);
+}
+
+void evtimer_unblock(void *tmr)
+{
+    struct evtimer *t = tmr;
+    int ticks;
+
+    /* if ticks accumulated, quickly deliver them first */
+    pthread_mutex_lock(&t->block_mtx);
+    ticks = t->ticks;
+    pthread_mutex_unlock(&t->block_mtx);
+    if (ticks)
+        t->callback(ticks, t->arg);
+    /* then unblock */
+    pthread_mutex_lock(&t->block_mtx);
+    t->ticks -= ticks;
+    t->blocked--;
+    pthread_mutex_unlock(&t->block_mtx);
+}
--- src/base/lib/timer/evtimer_fd.c
+++ src/base/lib/timer/evtimer_fd.c
@@ -15,9 +15,9 @@
  */
 
 /*
- * Purpose: event timer API.
+ * Purpose: event timer API on top of timerfd.
  *
- * Author: stsp
+ * Author: @stsp
  *
  */
 
@@ -25,18 +25,19 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdint.h>
+#include <stdio.h>
+#include <sys/timerfd.h>
 #include <time.h>
-#include <signal.h>
-#include <time.h>
-#include <sys/time.h>
 #include <pthread.h>
+#include <poll.h>
 #ifdef HAVE_LIBBSD
 #include <bsd/sys/time.h>
 #endif
+#include "utilities.h"
 #include "evtimer.h"
 
 struct evtimer {
-    timer_t tmr;
+    int fd;
     void (*callback)(int ticks, void *);
     void *arg;
     clockid_t clk_id;
@@ -45,66 +46,102 @@ struct evtimer {
     int blocked;
     pthread_mutex_t block_mtx;
     pthread_cond_t block_cnd;
-    int ticks;
+    pthread_cond_t unblock_cnd;
     int in_cbk;
+    pthread_t thr;
 };
 
-static void evhandler(union sigval sv)
+static void do_callback(struct evtimer *t)
 {
-    int bl;
-    struct evtimer *t = sv.sival_ptr;
+    uint64_t ticks;
+    int rc = read(t->fd, &ticks, sizeof(ticks));
+    if (rc != sizeof(ticks)) {
+        perror("read()");
+        return;
+    }
+    t->callback(ticks, t->arg);
+}
 
-    pthread_mutex_lock(&t->block_mtx);
-    bl = t->blocked;
-    t->ticks += timer_getoverrun(t->tmr) + 1;
-    if (!bl)
+static void *evthread(void *arg)
+{
+    struct evtimer *t = arg;
+
+    while (1) {
+        struct pollfd pf = { .fd = t->fd, .events = POLLIN };
+        int rc = poll(&pf, 1, -1);
+        switch (rc) {
+        case -1:
+            perror("poll()");
+            /* no break */
+        case 0:
+            return NULL;
+        default:
+            if (!(pf.revents & POLLIN)) {
+                error("bad poll return, %i %x\n", rc, pf.revents);
+                continue;
+            }
+            break;
+        }
+
+        pthread_mutex_lock(&t->block_mtx);
+        while (t->blocked)
+            cond_wait(&t->unblock_cnd, &t->block_mtx);
         t->in_cbk++;
-    pthread_mutex_unlock(&t->block_mtx);
-    if (!bl) {
-        t->callback(t->ticks, t->arg);
+        pthread_mutex_unlock(&t->block_mtx);
+
+        do_callback(t);
+
         pthread_mutex_lock(&t->block_mtx);
         t->in_cbk--;
-        t->ticks = 0;
         pthread_mutex_unlock(&t->block_mtx);
         pthread_cond_signal(&t->block_cnd);
     }
+
+    return NULL;
 }
 
 void *evtimer_create(void (*cbk)(int ticks, void *), void *arg)
 {
     struct evtimer *t;
     clockid_t id = CLOCK_MONOTONIC;
-    struct sigevent sev = { .sigev_notify = SIGEV_THREAD,
-                            .sigev_notify_function = evhandler };
-    timer_t tmr;
-    int rc;
+    int fd = timerfd_create(id, TFD_NONBLOCK | TFD_CLOEXEC);
 
+    assert(fd != -1);
     t = malloc(sizeof(*t));
     assert(t);
-    sev.sigev_value.sival_ptr = t;
-    rc = timer_create(id, &sev, &tmr);
-    assert(rc != -1);
-    t->tmr = tmr;
+    t->fd = fd;
     t->callback = cbk;
     t->arg = arg;
     t->clk_id = id;
     t->blocked = 0;
-    t->ticks = 0;
     t->in_cbk = 0;
     pthread_mutex_init(&t->start_mtx, NULL);
     pthread_mutex_init(&t->block_mtx, NULL);
     pthread_cond_init(&t->block_cnd, NULL);
+    pthread_cond_init(&t->unblock_cnd, NULL);
+    pthread_create(&t->thr, NULL, evthread, t);
     return t;
 }
 
 void evtimer_delete(void *tmr)
 {
     struct evtimer *t = tmr;
+    struct itimerspec i = {};
 
-    timer_delete(t->tmr);
+    timerfd_settime(t->fd, 0, &i, NULL);
+    pthread_mutex_lock(&t->block_mtx);
+    t->blocked++;
+    while (t->in_cbk)
+        cond_wait(&t->block_cnd, &t->block_mtx);
+    pthread_mutex_unlock(&t->block_mtx);
+    pthread_cancel(t->thr);
+    pthread_join(t->thr, NULL);
+
+    close(t->fd);
     pthread_mutex_destroy(&t->start_mtx);
     pthread_mutex_destroy(&t->block_mtx);
     pthread_cond_destroy(&t->block_cnd);
+    pthread_cond_destroy(&t->unblock_cnd);
     free(t);
 }
 
@@ -121,7 +158,7 @@ void evtimer_set_rel(void *tmr, uint64_t ns, int periodic)
     clock_gettime(t->clk_id, &start);
     timespecadd(&start, &rel, &abs);
     i.it_value = abs;
-    timer_settime(t->tmr, TIMER_ABSTIME, &i, NULL);
+    timerfd_settime(t->fd, TFD_TIMER_ABSTIME, &i, NULL);
     pthread_mutex_lock(&t->start_mtx);
     t->start = start;
     pthread_mutex_unlock(&t->start_mtx);
@@ -145,7 +182,7 @@ void evtimer_stop(void *tmr)
     struct itimerspec i = {};
     struct timespec start;
 
-    timer_settime(t->tmr, 0, &i, NULL);
+    timerfd_settime(t->fd, 0, &i, NULL);
     clock_gettime(t->clk_id, &start);
     pthread_mutex_lock(&t->start_mtx);
     t->start = start;
@@ -159,24 +196,16 @@ void evtimer_block(void *tmr)
     pthread_mutex_lock(&t->block_mtx);
     t->blocked++;
     while (t->in_cbk)
-        pthread_cond_wait(&t->block_cnd, &t->block_mtx);
+        cond_wait(&t->block_cnd, &t->block_mtx);
     pthread_mutex_unlock(&t->block_mtx);
 }
 
 void evtimer_unblock(void *tmr)
 {
     struct evtimer *t = tmr;
-    int ticks;
 
-    /* if ticks accumulated, quickly deliver them first */
     pthread_mutex_lock(&t->block_mtx);
-    ticks = t->ticks;
-    pthread_mutex_unlock(&t->block_mtx);
-    if (ticks)
-        t->callback(ticks, t->arg);
-    /* then unblock */
-    pthread_mutex_lock(&t->block_mtx);
-    t->ticks -= ticks;
     t->blocked--;
     pthread_mutex_unlock(&t->block_mtx);
+    pthread_cond_signal(&t->unblock_cnd);
 }
