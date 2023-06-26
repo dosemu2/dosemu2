848e30b82ba81fe357e985ec05dde8d383e161d5
Stas Sergeev <stsp@users.sourceforge.net>
2023-06-27 01:27:49 +0500

    munt: honour $_fluid_volume setting
    This amplifies the mt32 volume quite a bit.

--- src/plugin/munt/munt.c
+++ src/plugin/munt/munt.c
@@ -72,6 +72,8 @@ static int midomunt_init(void *arg)
 	goto err;
     }
 
+    mt32emu_set_output_gain(ctx, config.fluid_volume);
+
     sem_init(&syn_sem, 0, 0);
     pthread_create(&syn_thr, NULL, synth_thread, NULL);
 #if defined(HAVE_PTHREAD_SETNAME_NP) && defined(__GLIBC__)
