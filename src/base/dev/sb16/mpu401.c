8c3abf4e6694d9119276f8c424ac0d62c0efc916
Stas Sergeev <stsp@users.sourceforge.net>
2023-07-01 23:29:59 +0500

    mpu401: raise irq for incoming data [#2036]
    ... not only in uart mode.

--- src/base/dev/sb16/mpu401.c
+++ src/base/dev/sb16/mpu401.c
@@ -71,7 +71,7 @@ void mpu401_process(struct mpu401_s *mpu)
     while (midi_get_data_byte(&data)) {
 	put_midi_in_byte(mpu, data);
 #define MPU401_IN_FIFO_TRIGGER 1
-	if (mpu->uart && (get_midi_in_fillup(mpu) == MPU401_IN_FIFO_TRIGGER))
+	if (get_midi_in_fillup(mpu) == MPU401_IN_FIFO_TRIGGER)
 	    mpu->ops->activate_irq(mpu);
     }
 }
