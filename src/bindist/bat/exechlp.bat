c68b80208cb3aa68a762697570d93718749e0b3b
Stas Sergeev <stsp@users.sourceforge.net>
2023-08-08 23:20:47 +0500

    exechlp.bat: allow remaining in a dest dir
    Check DOSEMU_SYS_CMD after reaching the dest dir, not before.
    In that case
    dosemu -K .
    will remain in a newly created drive.

--- src/bindist/bat/exechlp.bat
+++ src/bindist/bat/exechlp.bat
@@ -1,11 +1,11 @@
 @echo off
 system %1 %2 %3
-if "%DOSEMU_SYS_CMD%" == "" goto done
 if not "%DOSEMU_SYS_DRV%" == "" %DOSEMU_SYS_DRV%:
 if ERRORLEVEL 1 exitemu 1
 if not "%DOSEMU_SYS_DIR%" == "" cd %DOSEMU_SYS_DIR%
 if ERRORLEVEL 1 exitemu 1
 if "%DOSEMU_SYS_CMD%" == "exit" goto cont
+if "%DOSEMU_SYS_CMD%" == "" goto done
 call %DOSEMU_SYS_CMD%
 set SHELL_LOADHIGH_DEFAULT=
 :cont
