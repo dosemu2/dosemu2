bd47e5866d85176baf3104d9d1488a01e7b2fb2f
C. Masloch <pushbx@ulukai.org>
2023-08-12 14:36:08 +0200

    bios: set dl to 1 when -B switch is specified

--- src/base/bios/setup.c
+++ src/base/bios/setup.c
@@ -219,7 +219,8 @@ static void bios_setup(void)
     unsigned ptr;
 
     ptr = SEGOFF2LINEAR(BIOSSEG, bios_f000_bootdrive);
-    WRITE_BYTE(ptr, (config.hdiskboot >= 2 || config.hdiskboot == -1) ? 0x80 : 0);
+    WRITE_BYTE(ptr, (config.hdiskboot >= 2 || config.hdiskboot == -1) ? 0x80 :
+      config.hdiskboot);
   }
 
   bios_mem_setup();		/* setup values in BIOS area */
