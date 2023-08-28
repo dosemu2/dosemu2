7fc23ff53f0455c7e7b6ba9a5b2e0bb8ddf1314a
Stas Sergeev <stsp@users.sourceforge.net>
2023-08-28 20:47:23 +0500

    shlock: simpler implementation
    Instead of a single lockfile, use directory with multiple lockfiles.
    Directories has an advantage over files, that they are unremovable
    unless empty. So instead of checking if the lockfile is still used,
    and removing it only if not (which is a non-atomic opration that
    requires the locking on its own), we just try to remove the directory,
    and that attempt succeeds only when there are no more lockers inside.
    
    The creation operation also much simplified: instead of taking a
    dir lock to make sure file is created and locked atomically, we
    can just try to create everything in a loop. As soon as everything
    is created, no one can remove our dir as it has the uniquely-named
    tmpfile within. So uniquely-named tmpfile serves as a mandatory
    lock for a dir.

--- src/base/lib/misc/shlock.c
+++ src/base/lib/misc/shlock.c
@@ -28,8 +28,6 @@
 #include <unistd.h>
 #include <string.h>
 #include <fcntl.h>
-#include <sys/types.h>
-#include <dirent.h>
 #include <assert.h>
 #include "shlock.h"
 
@@ -37,30 +35,18 @@
 #define LOCK_PFX "LCK.."
 
 struct shlck {
+  char *tspec;
   char *fspec;
   char *dir;
   int excl;
   int fd;
 };
 
-static int fd_is_ok(int dir_fd, const char *dspec)
-{
-  int rc = access(dspec, R_OK);
-  if (rc == 0) {
-    struct stat sb1, sb2;
-    fstat(dir_fd, &sb1);
-    stat(dspec, &sb2);
-    if (sb1.st_ino == sb2.st_ino)
-      return 1;
-  }
-  return 0;
-}
-
 void *shlock_open(const char *dir, const char *name, int excl, int block)
 {
   struct shlck *ret;
-  char *fspec, *dspec;
-  int fd, dir_fd, rc;
+  char *fspec, *dspec, *tspec;
+  int fd, tmp_fd, rc;
   int flg = block ? 0 : LOCK_NB;
 
   rc = asprintf(&dspec, LOCK_DIR "/%s", dir);
@@ -68,39 +54,49 @@ void *shlock_open(const char *dir, const char *name, int excl, int block)
     perror("asprintf()");
     return NULL;
   }
-  while (1) {
+  rc = asprintf(&fspec, "%s/%s", dspec, name);
+  if (rc == -1) {
+    perror("asprintf()");
+    goto err_dspec;
+  }
+  rc = asprintf(&tspec, "%s/" LOCK_PFX "%i_XXXXXX", fspec, getpid());
+  if (rc == -1) {
+    perror("asprintf()");
+    goto err_fspec;
+  }
+  tmp_fd = -1;
+  while (tmp_fd == -1) {
     rc = mkdir(dspec, 0775);
     if (rc == -1 && errno != EEXIST) {
       perror("mkdir()");
-      goto err_dspec;
+      goto err_free;
     }
-    dir_fd = open(dspec, O_RDONLY | O_DIRECTORY);
-    if (dir_fd == -1) {
-      perror("open(dir)");
-      goto err_dspec;
-    }
-    /* get directory lock to avoid races */
-    rc = flock(dir_fd, LOCK_EX);
+    rc = mkdir(fspec, 0775);
     if (rc == -1) {
-      perror("flock(dir)");
-      goto err_clodir;
+      switch (errno) {
+        case EEXIST:
+          break;
+        case ENOENT:
+          continue;  // race, someone removed parent dir
+        default:
+          perror("mkdir()");
+          goto err_rmddir;
+      }
     }
-    /* check if someone else removed dir in a process */
-    if (fd_is_ok(dir_fd, dspec))
-      break;
-    /* race happened, retry */
-    close(dir_fd);
+    tmp_fd = mkstemp(tspec);
+    if (tmp_fd == -1) {
+      if (errno == ENOENT)
+        continue;  // race, someone removed parent dir
+      perror("mkstemp()");
+      goto err_rmfdir;
+    }
   }
 
-  rc = asprintf(&fspec, "%s/" LOCK_PFX "%s", dspec, name);
-  if (rc == -1) {
-    perror("asprintf()");
-    goto err_clodir;
-  }
-  fd = open(fspec, O_RDWR | O_CREAT | O_CLOEXEC, 0660);
+  fd = open(fspec, O_RDONLY | O_DIRECTORY);
+  close(tmp_fd);
   if (fd == -1) {
-    perror("open()");
-    goto err_free;
+    perror("open(dir)");
+    goto err_rmt;
   }
   if (excl) {
     rc = flock(fd, LOCK_EX | flg);
@@ -113,10 +109,9 @@ void *shlock_open(const char *dir, const char *name, int excl, int block)
       goto err_close;
     }
   }
-  /* release dir lock */
-  close(dir_fd);
 
   ret = malloc(sizeof(*ret));
+  ret->tspec = tspec;
   ret->fspec = fspec;
   ret->dir = dspec;
   ret->excl = excl;
@@ -125,10 +120,16 @@ void *shlock_open(const char *dir, const char *name, int excl, int block)
 
 err_close:
   close(fd);
+err_rmt:
+  unlink(tspec);
+err_rmfdir:
+  rmdir(fspec);
+err_rmddir:
+  rmdir(dspec);
 err_free:
+  free(tspec);
+err_fspec:
   free(fspec);
-err_clodir:
-  close(dir_fd);
 err_dspec:
   free(dspec);
   return NULL;
@@ -138,49 +139,24 @@ int shlock_close(void *handle)
 {
   struct shlck *s = handle;
   int rc, ret = 0;
-  int dir_fd;
 
-  /* drop file lock first, to avoid AB-BA deadlock with dir lock */
-  rc = flock(s->fd, LOCK_UN);
-  if (rc == -1) {
-    perror("flock(UN)");
-    return -1;
-  }
-  dir_fd = open(s->dir, O_RDONLY | O_DIRECTORY);
-  if (dir_fd == -1) {
-    perror("open(dir)");
-    return -1;
-  }
-  /* grab directory lock */
-  rc = flock(dir_fd, LOCK_EX);
-  if (rc == -1) {
-    perror("flock(dir)");
-    goto err_clodir;
-  }
-  /* if dir or file removed, just close fds */
-  if (!fd_is_ok(dir_fd, s->dir) || !fd_is_ok(s->fd, s->fspec))
-    rc = -1;
+  rc = unlink(s->tspec);
+  if (rc == -1)
+    perror("unlink()");
+  /* close() drops OFD lock */
+  rc = close(s->fd);
+  if (rc == -1)
+    perror("close()");
+  rc = rmdir(s->fspec);  // fails if still not empty
   if (rc == 0)
-    rc = flock(s->fd, LOCK_EX | LOCK_NB);
-  if (rc == 0) {
-    /* we are the last owner, delete the lock file */
-    unlink(s->fspec);
     ret++;
-    /* try deleting dir as well */
-    rc = rmdir(s->dir);
-    if (rc == 0)
-      ret++;
-  }
-  close(s->fd);
-  /* release dir lock */
-  close(dir_fd);
+  rc = rmdir(s->dir);  // fails if still not empty
+  if (rc == 0)
+    ret++;
 
+  free(s->tspec);
   free(s->fspec);
   free(s->dir);
   free(s);
   return ret;
-
-err_clodir:
-  close(dir_fd);
-  return -1;
 }
