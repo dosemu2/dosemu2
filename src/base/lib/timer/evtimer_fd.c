ac1eebc3bae168a9465465627bbe8bfb341f3cf1
Stas Sergeev <stsp2@yandex.ru>
2023-09-12 20:02:19 +0500

    configure: make libbsd optional
    strlcpy() is now provided by glibc:
    https://sourceware.org/git/?p=glibc.git;a=commit;h=454a20c8756c9c1d55419153255fc7692b3d2199

--- src/base/lib/timer/evtimer_fd.c
+++ src/base/lib/timer/evtimer_fd.c
@@ -37,6 +37,18 @@
 #include <poll.h>
 #ifdef HAVE_LIBBSD
 #include <bsd/sys/time.h>
+#else
+#ifndef timespecadd
+#define timespecadd(tsp, usp, vsp)                                      \
+        do {                                                            \
+                (vsp)->tv_sec = (tsp)->tv_sec + (usp)->tv_sec;          \
+                (vsp)->tv_nsec = (tsp)->tv_nsec + (usp)->tv_nsec;       \
+                if ((vsp)->tv_nsec >= 1000000000L) {                    \
+                        (vsp)->tv_sec++;                                \
+                        (vsp)->tv_nsec -= 1000000000L;                  \
+                }                                                       \
+        } while (0)
+#endif
 #endif
 #include "utilities.h"
 #include "evtimer.h"
