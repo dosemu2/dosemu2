6d754c3711e5d2ac3bc70f6bdb52abcd5b5adae1
Stas Sergeev <stsp2@yandex.ru>
2024-01-25 20:43:21 +0500

    dj64: more api work

--- src/plugin/dj64/djdev64.c
+++ src/plugin/dj64/djdev64.c
@@ -18,12 +18,24 @@
 #include <stdio.h>
 #include <djdev64/dj64init.h>
 #include "djdev64.h"
+#include "emu.h"
 #include "dosemu_debug.h"
+#include "utilities.h"
+#include "emudpmi.h"
 #include "dos2linux.h"
 
 static uint8_t *dj64_addr2ptr(uint32_t addr)
 {
     return dosaddr_to_unixaddr(addr);
+}
+
+static uint32_t dj64_ptr2addr(uint8_t *ptr)
+{
+    if (ptr >= MEM_BASE32(config.dpmi_base) &&
+            ptr < MEM_BASE32(config.dpmi_base) + dpmi_mem_size())
+        return DOSADDR_REL(ptr);
+    dosemu_error("bad ptr2addr %p\n", ptr);
+    return -1;
 }
 
 static void dj64_print(int prio, const char *format, va_list ap)
@@ -43,6 +55,7 @@ static void dj64_print(int prio, const char *format, va_list ap)
 
 const struct dj64_api api = {
     .addr2ptr = dj64_addr2ptr,
+    .ptr2addr = dj64_ptr2addr,
     .print = dj64_print,
 };
 
