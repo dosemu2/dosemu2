8de3fef6fef3d0b104deee952a621b29c8833dae
Stas Sergeev <stsp2@yandex.ru>
2024-02-21 16:36:37 +0500

    ci_build.sh: add dj64 ppa in proper place

--- ci_build.sh
+++ ci_build.sh
@@ -30,7 +30,6 @@ git clone --depth 1 --no-single-branch https://github.com/dosemu2/fdpp.git ${LOC
   # Install the build dependancies based FDPP's debian/control file
   sudo add-apt-repository ppa:stsp-0/nasm-segelf
   sudo add-apt-repository ppa:stsp-0/thunk-gen
-  sudo add-apt-repository ppa:stsp-0/dj64
   sudo apt update -q
   mk-build-deps --install --root-cmd sudo
 
@@ -44,6 +43,7 @@ git clone --depth 1 --no-single-branch https://github.com/dosemu2/fdpp.git ${LOC
 )
 
 # Install the build dependancies based Dosemu's debian/control file
+sudo add-apt-repository ppa:stsp-0/dj64
 mk-build-deps --install --root-cmd sudo
 
 if [ "${SUBTYPE}" = "asan" ] ; then
