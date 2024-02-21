2f802caf55fbd1e5b36411dbac5d7a713a397b56
Stas Sergeev <stsp2@yandex.ru>
2024-02-21 16:55:30 +0500

    ci_prereq.sh: split out test-only part

--- ci_prereq.sh
+++ ci_prereq.sh
@@ -1,32 +1,7 @@
 #!/bin/sh
 
-sudo add-apt-repository -y ppa:dosemu2/ppa
-sudo add-apt-repository -y ppa:jwt27/djgpp-toolchain
-sudo add-apt-repository -y ppa:tkchia/build-ia16
-
-sudo apt update -q
-
 sudo apt install -y \
-  acl \
   devscripts \
   equivs \
-  comcom32 \
   git \
-  bash \
-  nasm \
-  python3-cpuinfo \
-  python3-pexpect \
-  mtools \
-  gcc-djgpp \
-  djgpp-dev \
-  qemu-system-common \
-  gdb \
-  valgrind \
-  gcc-ia16-elf \
-  libi86-ia16-elf \
-  libi86-testsuite-ia16-elf \
-  gcc-multilib \
-  dos2unix \
-  bridge-utils \
-  libvirt-daemon \
-  libvirt-daemon-system
+  bash
