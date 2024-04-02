a97d30de20f5dc0b066ee17213c738e4ba604f50
Stas Sergeev <stsp@users.sourceforge.net>
2024-04-02 21:03:31 +0300

    map dos mem after dropping privs [#2181]
    Memory mapping doesn't need extra privs.

--- src/base/core/emu.c
+++ src/base/core/emu.c
@@ -320,7 +320,6 @@ int main(int argc, char **argv, char * const *envp)
     LibpacketInit();            /* initialize network packet interfaces */
 
     mapping_init();		/* initialize mapping drivers */
-    map_memory_space();         /* maps all DOS memory (low, dpmi, xms...) */
 
     if (can_do_root_stuff && !under_root_login) {
         g_printf("dropping root privileges\n");
@@ -328,6 +327,7 @@ int main(int argc, char **argv, char * const *envp)
     }
     priv_drop();
 
+    map_memory_space();         /* maps all DOS memory (low, dpmi, xms...) */
     init_hardware_ram();        /* map the direct hardware ram */
     map_video_bios();           /* map (really: copy) the video bios */
     close_kmem();
