e7f53c4e193b077531e62f404cfccc6310159be3
Stas Sergeev <stsp@users.sourceforge.net>
2024-04-14 00:31:14 +0300

    priv: add GPLv2+ copyrights [#2181]
    The code was so much rewritten that nothing copyrightable is hopefully
    left (except for a few function names).
    All original authors are nevertheless mentioned.

--- src/base/core/priv.c
+++ src/base/core/priv.c
@@ -1,3 +1,29 @@
+/*
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+/*
+ * Purpose: flipping priviledges
+ *
+ * Authors:
+ *   Loosely based on dosemu1's priv.c by Hans Lermen, Eric Biederman
+ *   and Bart Oldeman.
+ *
+ *   Rewritten for dosemu2 by @stsp
+ */
+
 #include <errno.h>
 #include <string.h>
 #include <stdlib.h>
