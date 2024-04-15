4dbfc6514d844a071950217f0572595cd793a067
Andrew Bird <ajb@spheresystems.co.uk>
2024-04-15 19:55:32 +0100

    aspi: fix resource leak (coverity)

--- src/dosext/drivers/aspi.c
+++ src/dosext/drivers/aspi.c
@@ -235,13 +235,18 @@ static int query_device_type(char *devname)
   cdb->sg_hd.reply_len = out_len;
 
   status = write(fd, cdb, in_len);
-  if (status < 0 || status != in_len) return -1;
+  if (status < 0 || status != in_len) {
+    ret = -1;
+    goto done;
+  }
 
   do {
     status = read(fd, si, out_len);
   } while (status == -1 && errno == EINTR);
 
   ret = si->peripheral_device_type;
+
+done:
   free(cdb);
   free(si);
   close(fd);
