421cf2de4dbdcacbd172fce56f3bd603c097ee14
Stas Sergeev <stsp@users.sourceforge.net>
2024-05-27 01:19:52 +0300

    evtimer: fall-backs for libbsd funcs [#575]

--- src/base/lib/timer/evtimer.c
+++ src/base/lib/timer/evtimer.c
@@ -36,6 +36,29 @@
 #include <pthread.h>
 #ifdef HAVE_LIBBSD
 #include <bsd/sys/time.h>
+#else
+#ifndef timespecadd
+#define timespecadd(tsp, usp, vsp)                                      \
+        do {                                                            \
+                (vsp)->tv_sec = (tsp)->tv_sec + (usp)->tv_sec;          \
+                (vsp)->tv_nsec = (tsp)->tv_nsec + (usp)->tv_nsec;       \
+                if ((vsp)->tv_nsec >= 1000000000L) {                    \
+                        (vsp)->tv_sec++;                                \
+                        (vsp)->tv_nsec -= 1000000000L;                  \
+                }                                                       \
+        } while (0)
+#endif
+#ifndef timespecsub
+#define timespecsub(tsp, usp, vsp)                                      \
+        do {                                                            \
+                (vsp)->tv_sec = (tsp)->tv_sec - (usp)->tv_sec;          \
+                (vsp)->tv_nsec = (tsp)->tv_nsec - (usp)->tv_nsec;       \
+                if ((vsp)->tv_nsec < 0) {                               \
+                        (vsp)->tv_sec--;                                \
+                        (vsp)->tv_nsec += 1000000000L;                  \
+                }                                                       \
+        } while (0)
+#endif
 #endif
 #include "evtimer.h"
 
