e4e917f33eb60d74dfb33d2e2d9dcbd8575afd79
Stas Sergeev <stsp@users.sourceforge.net>
2024-09-09 13:29:40 +0300

    ci: try to increase test timeout again

--- test/common_framework.py
+++ test/common_framework.py
@@ -441,7 +441,7 @@ class BaseTestCase(object):
                 f.seek(c[0])
                 f.write(c[2])
 
-    def runDosemu(self, cmd, opts=None, outfile=None, config=None, timeout=5,
+    def runDosemu(self, cmd, opts=None, outfile=None, config=None, timeout=15,
                     eofisok=False, interactions=[]):
         # Note: if debugging is turned on then times increase 10x
         dbin = str(self.dosemu)
