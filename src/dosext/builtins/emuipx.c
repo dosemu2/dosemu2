d4f673a40cd76e8f0d59ddfc60efe6b5a1cca4e8
Stas Sergeev <stsp@users.sourceforge.net>
2025-01-30 21:11:44 +0300

    emuipx: fix resource leak (coverity)

--- src/dosext/builtins/emuipx.c
+++ src/dosext/builtins/emuipx.c
@@ -61,10 +61,10 @@ static int ipx_connect(const char *dest)
   if (len) {
     host = lowmem_alloc(len + 1);
     strcpy(host, s);
-    free(s);
     r.r_es = DOSEMU_LMHEAP_SEG;
     r.r_di = DOSEMU_LMHEAP_OFFS_OF(host);
   }
+  free(s);
   r.r_ax = (DOS_HELPER_IPX_HELPER | (DOS_SUBHELPER_IPX_CONNECT << 8)) & 0xffff;
   com_intr(DOS_HELPER_INT, &r);
   if (host)
