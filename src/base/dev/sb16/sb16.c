65e9adb818a0d0215fe06d656141c21585d1804e
Stas Sergeev <stsp@users.sourceforge.net>
2025-03-04 18:17:07 +0300

    Revert "sb16: pretend busy after every DSP and mixer write"
    This reverts commit 19004ba2491fc6e2670547230a30d63c46ce775b.
    
    SC2000 had initial sound delay anyway, but jill2 feels much better.
    
    Fixes #2408

--- src/base/dev/sb16/sb16.c
+++ src/base/dev/sb16/sb16.c
@@ -742,7 +742,6 @@ static void sb_dsp_write(Bit8u value)
 	S_printf("SB: Accepted byte %#x, index=%i\n", value,
 		 sb.command_idx);
 	sb.command[sb.command_idx++] = value;
-	sb.busy = 1;
     } else {
 	S_printf("SB: ERROR: command buffer overflowed!\n");
     }
@@ -1258,8 +1257,6 @@ static void sb_mixer_write(Bit8u value)
 	sb.mixer_regs[sb.mixer_index] = 0xff;
 	break;
     }
-
-    sb.busy = 1;
 }
 
 static Bit8u sb_mixer_read(void)
