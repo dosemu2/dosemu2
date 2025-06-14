dosemu2/dosemu2

commit 87b84c7c284a71983567f36533a342fc84a74283

vgabios: ported the rest
struct vga is no longer accessed by vbios

src/include/vgaemu.h
@@ -508,7 +508,11 @@ void dirty_all_vga_colors(void);
 int changed_vga_colors(DAC_entry *);
 void vgaemu_adj_cfg(unsigned, unsigned);
 void vgaemu_scroll(int x0, int y0, int x1, int y1, int n, unsigned char attr);
-void vgaemu_put_char(int x, int y, unsigned char c, unsigned char attr);
+void vgaemu_put_char(unsigned char c, unsigned char page, unsigned char attr);
+void vgaemu_repeat_char(unsigned char c, unsigned char page,
+    unsigned char attr, unsigned char count);
+void vgaemu_repeat_char_attr(unsigned char c, unsigned char page,
+    unsigned char attr, unsigned char count);
 void vgaemu_put_pixel(int x, int y, unsigned char page, unsigned char attr);
 unsigned char vgaemu_get_pixel(int x, int y, unsigned char page);
 unsigned char vga_read(unsigned addr);
@@ -520,7 +524,8 @@ void memcpy_dos_to_vga(unsigned dst, unsigned src, size_t len);
 void memcpy_from_vga(void *dst, unsigned src, size_t len);
 void memcpy_dos_from_vga(unsigned dst, unsigned src, size_t len);
 void vga_memcpy(unsigned dst, unsigned src, size_t len);
-void vga_memset(unsigned dst, char val, size_t len);
+void vga_memset(unsigned dst, unsigned char val, size_t len);
+void vga_memsetw(unsigned dst, unsigned short val, size_t len);
 /* for cpuemu: */
 int vga_emu_protect_page(unsigned, int);
 int vga_emu_adjust_protection(unsigned, unsigned);

src/base/bios/int10.c
@@ -308,7 +308,7 @@ void tty_char_out(unsigned char ch, int s, int attr)
 
   default:          /* Printable character */
     if(gfx_mode) {
-      vgaemu_put_char(xpos, ypos, ch, attr);
+      vgaemu_put_char(ch, s, attr);
     }
     else
     {
@@ -762,7 +762,6 @@ int int10(void) /* with dualmon */
   /* some code here is copied from Alan Cox ***************/
   int x, y, co, li;
   unsigned page, page_size, address;
-  u_char c;
   unsigned sm;
 
 #if USE_DUALMON
@@ -936,56 +935,19 @@ int int10(void) /* with dualmon */
        * the difference is that 0xA ignores color for text modes
        */
     case 0x09:		/* write char & attr */
-    case 0x0a:		/* write char */
-      if(HI(ax) == 0x0a && using_text_mode()) {
-        i10_deb(
-          "write char: page %u, char 0x%02x '%c'\n",
-          HI(bx), LO(ax), LO(ax) > ' ' && LO(ax) < 0x7f ? LO(ax) : ' '
-        );
-      }
-      else {
-        i10_deb(
-          "write char: page %u, char 0x%02x '%c', attr 0x%02x\n",
+      i10_deb(
+          "rep char: page %u, char 0x%02x '%c', attr 0x%02x\n",
           HI(bx), LO(ax), LO(ax) > ' ' && LO(ax) < 0x7f ? LO(ax) : ' ', LO(bx)
-        );
-      }
-      if(using_text_mode()) {
-        unsigned sadr;
-        u_short c_attr;
-        int n;
+      );
+      vgaemu_repeat_char_attr(LO(ax), HI(bx), LO(bx), LWORD(ecx));
+      break;
 
-        page = HI(bx);
-        sadr = screen_adr(page)
-	     + (get_bios_cursor_y_position(page) * co
-	        + get_bios_cursor_x_position(page)) * 2;
-        n = LWORD(ecx);
-        c = LO(ax);
-
-        /* XXX - need to make sure this doesn't overrun video memory!
-         * we don't really know how large video memory is yet (I haven't
-         * abstracted things yet) so we'll just leave it for the
-         * Great Video Housecleaning foretold in the TODO file of the
-         * ancients
-         */
-        if(HI(ax) == 9) {		/* use attribute from BL */
-  	 c_attr = c | (LO(bx) << 8);
-	 while(n--) { vga_write_word(sadr, c_attr); sadr += 2; }
-        }
-        else {				/* leave attribute as it is */
-	 while(n--) { vga_write(sadr, c); sadr += 2; }
-        }
-        set_dirty(page);
-        break;
-      }
-      else {
-        unsigned
-          x = get_bios_cursor_x_position(0),
-          y = get_bios_cursor_y_position(0),
-          n = LWORD(ecx);
-
-        while(x < vga.text_width && n--)
-          vgaemu_put_char(x++, y, LO(ax), LO(bx));
-      }
+    case 0x0a:		/* write char */
+      i10_deb(
+          "rep char: page %u, char 0x%02x '%c'\n",
+          HI(bx), LO(ax), LO(ax) > ' ' && LO(ax) < 0x7f ? LO(ax) : ' '
+      );
+      vgaemu_repeat_char(LO(ax), HI(bx), LO(bx), LWORD(ecx));
       break;
 
 

src/env/video/vgaemu.c
@@ -762,7 +762,8 @@ static void Logical_VGA_write(unsigned offset, unsigned char value)
 
 unsigned char vga_read(unsigned addr)
 {
-  if (!vga.inst_emu)
+  if (!vga.inst_emu || addr < vga.mem.bank_base ||
+	addr >= vga.mem.bank_base + vga.mem.bank_len)
     return READ_BYTE(addr);
   return Logical_VGA_read(addr - vga.mem.bank_base);
 }
@@ -776,7 +777,8 @@ unsigned short vga_read_word(unsigned addr)
 
 void vga_write(unsigned addr, unsigned char val)
 {
-  if (!vga.inst_emu) {
+  if (!vga.inst_emu || addr < vga.mem.bank_base ||
+	addr >= vga.mem.bank_base + vga.mem.bank_len) {
     WRITE_BYTE(addr, val);
     return;
   }
@@ -850,7 +852,7 @@ void vga_memcpy(unsigned dst, unsigned src, size_t len)
     vga_write(dst + i, vga_read(src + i));
 }
 
-void vga_memset(unsigned dst, char val, size_t len)
+void vga_memset(unsigned dst, unsigned char val, size_t len)
 {
   int i;
   if (!vga.inst_emu) {
@@ -859,6 +861,21 @@ void vga_memset(unsigned dst, char val, size_t len)
   }
   for (i = 0; i < len; i++)
     vga_write(dst + i, val);
+}
+
+void vga_memsetw(unsigned dst, unsigned short val, size_t len)
+{
+  if (!vga.inst_emu) {
+    while (len--) {
+      WRITE_WORD(dst, val);
+      dst += 2;
+    }
+    return;
+  }
+  while (len--) {
+    vga_write_word(dst, val);
+    dst += 2;
+  }
 }
 
 /*

commit 4daa04c8deae329c856e2f036ecef196cf5374a0

vgabios: fix rep char output
Count was truncated to 1 byte. :(
This is a regression from 87b84c7c2 which is 11 years old now!

Fixes WSCHANGE.EXE of WordStar4.

src/base/bios/vgabios.c
@@ -823,7 +823,7 @@ static void biosfn_write_char_only (Bit8u car,Bit8u page,Bit8u attr,
 }
 
 void vgaemu_repeat_char_attr(unsigned char c, unsigned char page,
-    unsigned char attr, unsigned char count)
+    unsigned char attr, int count)
 {
  vga_msg(
     "vgaemu_repeat_char_attr: page %d, char 0x%02x, attr 0x%02x rep %d\n",
@@ -834,7 +834,7 @@ void vgaemu_repeat_char_attr(unsigned char c, unsigned char page,
 }
 
 void vgaemu_repeat_char(unsigned char c, unsigned char page,
-    unsigned char attr, unsigned char count)
+    unsigned char attr, int count)
 {
  vga_msg(
     "vgaemu_repeat_char: page %d, char 0x%02x, attr 0x%02x rep %d\n",

src/include/vgaemu.h
@@ -510,9 +510,9 @@ void vgaemu_adj_cfg(unsigned, unsigned);
 void vgaemu_scroll(int x0, int y0, int x1, int y1, int n, unsigned char attr);
 void vgaemu_put_char(unsigned char c, unsigned char page, unsigned char attr);
 void vgaemu_repeat_char(unsigned char c, unsigned char page,
-    unsigned char attr, unsigned char count);
+    unsigned char attr, int count);
 void vgaemu_repeat_char_attr(unsigned char c, unsigned char page,
-    unsigned char attr, unsigned char count);
+    unsigned char attr, int count);
 void vgaemu_put_pixel(int x, int y, unsigned char page, unsigned char attr);
 unsigned char vgaemu_get_pixel(int x, int y, unsigned char page);
 unsigned char vga_read(unsigned addr);