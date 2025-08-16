dosemu2/dosemu2

commit 96b0a95cc8797958eca27ef075a98c5f0859b8a4

simx86: move "rep scas"/"rep cmps" vgaemu handling to cpatch.c.
The expensive part of e_vgaemu_fault() is now completely eliminated.

src/base/emu-i386/simx86/cpatch.c
@@ -120,7 +120,7 @@ asmlinkage void rep_movs_stos(struct rep_stack *stack)
 		if (v) {
 			int df = ((EFLAGS & EFLAGS_DF) ? -size:size);
 			e_VgaMovs(&stack->edi, &stack->esi, ecx, df, v);
-			ecx = 0;
+			stack->ecx = 0;
 			goto done;
 		}
 		esi = LINEAR2UNIX(source);
@@ -140,7 +140,7 @@ asmlinkage void rep_movs_stos(struct rep_stack *stack)
 		else source += len;
 		stack->esi = MEM_BASE32(source);
 	}
-	else { /* stos */
+	else if ((op & 0xfe) == 0xaa) { /* stos */
 		unsigned int eax = stack->eax;
 		if (ecx == len) {
 			if (vga_write_access(addr)) {
@@ -176,11 +176,33 @@ asmlinkage void rep_movs_stos(struct rep_stack *stack)
 			else repstos(,l,);
 		}
 	}
+	else if ((op & 0xf6) == 0xa6) { /* cmps/scas */
+		int repmod = (size == 1 ? MBYTE : size == 2 ? DATA16 : 0);
+		AR1.pu = stack->edi;
+		TR1.d = stack->ecx;
+		repmod |= MOVSDST|MREPCOND|(eip[-1]==REPNE? MREPNE:MREP);
+		if ((op & 0xf6) == 0xa6) { /* cmps */
+			repmod |= MOVSSRC;
+			AR2.pu = stack->esi;
+			Gen_sim(O_MOVS_CmpD, repmod);
+			stack->esi = AR2.pu;
+		}
+		else { /* scas */
+			DR1.d = stack->eax;
+			Gen_sim(O_MOVS_ScaD, repmod);
+		}
+		FlagSync_All();
+		stack->edi = AR1.pu;
+		stack->ecx = TR1.d;
+		stack->eflags = (stack->eflags & ~EFLAGS_CC) |
+			(EFLAGS & EFLAGS_CC);
+		goto done;
+	}
 	if (EFLAGS & EFLAGS_DF) addr -= len;
 	else addr += len;
 	stack->edi = MEM_BASE32(addr);
-done:
 	stack->ecx = ecx;
+done:
 	InCompiledCode++;
 	in_cpatch--;
 }
@@ -427,8 +449,9 @@ int Cpatch(sigcontext_t *scp)
 	return 0;
 
     p = eip;
-    if (*p==0xf3 && p[-1] == 0x90 && p[-2] == 0x90) {	// rep movs, rep stos
-	if (debug_level('e')>1) e_printf("### REP movs/stos patch at %p\n",eip);
+    if ((*p==0xf3 || *p==0xf2) && p[-1] == 0x90 && p[-2] == 0x90) {
+	// rep movs, rep stos, rep lods, rep scas, rep cmps
+	if (debug_level('e')>1) e_printf("### REP patch at %p\n",eip);
 	p-=2;
 	G2M(0xff,0x13,p); /* call (%ebx) */
 	_rip -= 2; /* make sure call (%ebx) is performed the first time */

src/base/emu-i386/simx86/codegen-x86.c
@@ -1820,7 +1820,7 @@ shrot0:
 		break;
 	case O_MOVS_LodD:
 		GetDF(Cp);
-		if (mode&(MREP|MREPNE))	{ G1(REP,Cp); }
+		G3M(NOP,NOP,REP,Cp);
 		if (mode&MBYTE)	{ G1(LODSb,Cp); }
 		else {
 			Gen66(mode,Cp);
@@ -1844,6 +1844,7 @@ shrot0:
 		// Pointer to the jecxz distance byte
 		CpTemp = Cp-1;
 		GetDF(Cp);
+		G2M(NOP,NOP,Cp);
 		G1((mode&MREP)?REP:REPNE,Cp);
 		if (mode&MBYTE)	{ G1(SCASb,Cp); }
 		else {
@@ -1886,6 +1887,7 @@ shrot0:
 		// Pointer to the jecxz distance byte
 		CpTemp = Cp-1;
 		GetDF(Cp);
+		G2M(NOP,NOP,Cp);
 		G1((mode&MREP)?REP:REPNE,Cp);
 		if (mode&MBYTE)	{ G1(CMPSb,Cp); }
 		else {

src/base/emu-i386/simx86/sigsegv.c
@@ -234,8 +234,6 @@ int e_vgaemu_fault(sigcontext_t *scp, unsigned page_fault)
   }
 
   if (vga_page < vga.mem.pages) {
-    unsigned char *p;
-    int w16;
     if (!vga.inst_emu) {
       /* Normal: make the display page writeable after marking it dirty */
       dosemu_error("simx86: should not be here\n");
@@ -245,74 +243,11 @@ int e_vgaemu_fault(sigcontext_t *scp, unsigned page_fault)
 
 /**/  e_printf("eVGAEmuFault: trying %08x, a=%08"PRI_RG"\n",*((int *)_rip),_rdi);
 
-    /* try CPatch, and if that fails, the exceptionally expensive route */
+    /* try CPatch, which should not fail */
     if (Cpatch(scp))
       return 1;
-
-    p = (unsigned char *)_rip;
-    if (*p==0x66) w16=1,p++; else w16=0;
-
-    /* Decode the faulting instruction.
-     * Hopefully, since the compiled code contains a well-defined subset
-     * of the many possibilities for writing a memory location, this
-     * decoder can be kept quite small. It is possible, however, that
-     * someone accesses the VGA memory with a shift, or a bit set, and
-     * this will cause the cpuemu to fail.
-     */
-    switch (*p) {
-/*f2*/	case REPNE:
-/*f3*/	case REP: {
-		int repmod;
-		if (p[1]==0x66) w16=1,p++;
-		switch(p[1]) {
-	/*a6*/	case CMPSb:
-		    repmod = MBYTE;
-		    goto REPCMPS_common;
-	/*a7*/	case CMPSw:
-		    repmod = w16 ? DATA16 : 0;
-		REPCMPS_common:
-		    repmod |= MOVSSRC|MOVSDST|MREPCOND|
-		      (p[0]==REPNE? MREPNE:MREP);
-		    AR1.d = _edi;
-		    AR2.d = _esi;
-		    TR1.d = _ecx;
-		    Gen_sim(O_MOVS_CmpD, repmod);
-		    FlagSync_All();
-		    _edi = AR1.d;
-		    _esi = AR2.d;
-		    _ecx = TR1.d;
-		    _eflags = (_eflags & ~EFLAGS_CC) | (EFLAGS & EFLAGS_CC);
-		    break;
-	/*ae*/	case SCASb:
-		    repmod = MBYTE;
-		    goto REPSCAS_common;
-	/*af*/	case SCASw:
-		    repmod = w16 ? DATA16 : 0;
-		REPSCAS_common:
-		    repmod |= MOVSDST|MREPCOND|(p[0]==REPNE? MREPNE:MREP);
-		    AR1.d = _edi;
-		    DR1.d = _eax;
-		    TR1.d = _ecx;
-		    Gen_sim(O_MOVS_ScaD, repmod);
-		    FlagSync_All();
-		    _edi = AR1.d;
-		    _ecx = TR1.d;
-		    _eflags = (_eflags & ~EFLAGS_CC) | (EFLAGS & EFLAGS_CC);
-		    break;
-		default:
-		    goto unimp;
-		}
-		_rip = (long)(p+2); }
-		break;
-	default:
-		goto unimp;
-    }
-/**/  e_printf("eVGAEmuFault: new eip=%08"PRI_RG"\n",_rip);
-    vgaemu_dirty_page(vga_page, 1);
   }
-  return 1;
 
-unimp:
   error("eVGAEmuFault: unimplemented decode instr at %08"PRI_RG": %08x\n",
 	_rip, *((int *)_rip));
   leavedos_from_sig(0x5643);

commit cf9d8950e87cb07a6c51eedb33c2a9f52b0d1f90

Cpatch: fix bug detecting rep cmps instead of rep scas.
Doing op & 0xf6 again would have never reached the rep scas code and did
rep cmps instead. May not be super common to do rep scas on vgaemu memory
then.

Regression from 96b0a95cc (myself, Dec 2015)

src/base/emu-i386/simx86/cpatch.c
@@ -193,7 +193,7 @@ void rep_movs_stos(struct rep_stack *stack)
 		AR1.d = EMUADDR_REL(stack->edi);
 		TR1.d = stack->ecx;
 		repmod |= MOVSDST|MREPCOND|(eip[-1]==REPNE? MREPNE:MREP);
-		if ((op & 0xf6) == 0xa6) { /* cmps */
+		if ((op & 0xfe) == 0xa6) { /* cmps */
 			repmod |= MOVSSRC;
 			AR2.d = EMUADDR_REL(stack->esi);
 			IGen IG = (IGen){.op = O_MOVS_CmpD, .mode = repmod};