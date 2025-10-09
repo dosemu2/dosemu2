dosemu2/dosemu2

commit 5e7510f35368cd7bb9a981cc0b80b91c654b737a

simx86: correct simulated page faults from Sim_helper()
These were still using the P0 from the interpreter, they should
obtain it using FindPC() on the eip/rip from the translated code.

src/base/emu-i386/simx86/codegen-x86.c
@@ -1653,6 +1653,10 @@ shrot0:
 		G2M(0x41,0xb9,Cp); G4(IG->p1,Cp);
 		// push %%rdi // keep mem_ref (2x for alignment)
 		G2M(PUSHdi,PUSHdi,Cp);
+		// call 1f (to push eip)
+		G1(CALLd,Cp); G4(2+4+2+TAILSIZE,Cp);
+		// pop %%rdi // mem_ref
+		G2M(POPdi,POPdi,Cp);
 #else
 		// mov %esp, %ecx // flags
 		G2M(0x89,0xe1,Cp);
@@ -1668,22 +1672,21 @@ shrot0:
 		G1(PUSHax,Cp);
 		// push %%edi // mem_ref
 		G1(PUSHdi,Cp);
-#endif
-		// call Ofs_SimHelper(%%ebx)
-		G3M(0xff,0x53,Ofs_SimHelper(),Cp);
-#ifdef __x86_64__
-		// pop %%rdi // mem_ref
-		G2M(POPdi,POPdi,Cp);
-#else
-		// addl $24,%%esp
+		// call 1f (to push eip)
+		G1(CALLd,Cp); G4(3+4+2+TAILSIZE,Cp);
+		// 2: addl $24,%%esp
 		G3M(0x83,0xc4,24,Cp)
 #endif
 		// cmpl $0x0,Ofs_ERR(%rbx)
 		G4M(0x83,0x7b,Ofs_ERR,0x00,Cp);
 		// jz skip
-		G2M(JE_JZ,TAILSIZE,Cp);
+		G2M(JE_JZ,TAILSIZE+4,Cp);
 		// movl {exit_addr},%%eax; pop %%edx; ret
 		G1(MOViax,Cp); G4(IG->p2,Cp); G2M(POPdx,RET,Cp);
+		// 1: call Ofs_SimHelper(%%ebx)
+		G3M(0xff,0x53,Ofs_SimHelper(),Cp);
+		G1(RET,Cp);
+		// skip:
 
 	case O_MOVS_SetA: {
 		/* use edi for loads unless MOVSDST or REP is set */

src/base/emu-i386/simx86/codegen-sim.c
@@ -2128,7 +2128,7 @@ static unsigned int Gen_sim(const IGen *IG, dosaddr_t mem_ref)
 	case O_SIM: {
 		uint32_t flags = FlagSync_All();
 		DR1.d = Sim_helper(mem_ref, DR1.d, mode,
-				   &flags, IG->p0, IG->p1);
+				   &flags, IG->p0, IG->p1, currentIG);
 		FlagSync_RFL(flags);
 		if (TheCPU.err)
 			P0 = IG->p2;
@@ -2878,14 +2878,12 @@ static void emu_pagefault_handler(dosaddr_t addr, int err, uint32_t op, int len)
 	TheCPU.err = EXCP0E_PAGE;
 	TheCPU.scp_err = err;
 	TheCPU.cr[2] = addr;
-	if (currentIG) {
-		unsigned int P0 = FindPC(currentIG);
-		TheCPU.eip = P0 - LONG_CS;
+	assert (currentIG);
+	TheCPU.eip = FindPC(currentIG) - LONG_CS;
+	if (CONFIG_CPUSIM)
 		EFLAGS = (EFLAGS & ~EFLAGS_CC) | FlagSync_All();
-		longjmp(jmp_env, 2);
-	} else
-		/* for faulting sim_read/write directly from interp.c */
-		longjmp(jmp_env, 1);
+	// Sim_helper can call from JIT, with synced EFLAGS
+	longjmp(jmp_env, 1);
 }
 
 uint8_t sim_read_byte(dosaddr_t x)
@@ -2947,8 +2945,13 @@ static __inline__ void SetCPU_WL(int m, signed char o, unsigned long v)
  * returns data (could be modified), so compiled code can write it back
  */
 unsigned int Sim_helper(unsigned int mem_ref, unsigned int data, int mode,
-			uint32_t *flags, unsigned int opc, unsigned int arg)
+			uint32_t *flags, unsigned int opc, unsigned int arg,
+			unsigned char *eip)
 {
+	/* needed in emu_pagefault_handler */
+	currentIG = eip;
+	EFLAGS = (EFLAGS & ~EFLAGS_CC) | (*flags & EFLAGS_CC);
+
 	unsigned int temp;
 	switch (opc) {
 /*9c*/	case PUSHF:    { /* flag handling for VME-case only,
@@ -3081,7 +3084,7 @@ unsigned int Sim_helper(unsigned int mem_ref, unsigned int data, int mode,
 			    if (debug_level('e')>1)
 				e_printf("Popped flags %08x->{r=%08x v=%08x}\n",temp,EFLAGS,get_FLAGS(EFLAGS));
 			}
-			*flags = EFLAGS & EFLAGS_CC;
+			*flags = (EFLAGS & EFLAGS_CC) | (*flags & ~EFLAGS_CC);
 			} break;
 /*9d*/	case POPF: {
 			/* GPF handled in interpreter */
@@ -3157,7 +3160,7 @@ stack_return_from_vm86:
 				TheCPU.err = (is_tf ? EXCP01_SSTP : EXCP_STISIGNAL);
 			    }
 			}
-			*flags = EFLAGS & EFLAGS_CC;
+			*flags = (EFLAGS & EFLAGS_CC) | (*flags & ~EFLAGS_CC);
 			} break;
 /*fa*/	case CLI:
 			/* only for PVI/VME IOPL<3 CLI, not used presently */

src/base/emu-i386/simx86/emu86.h
@@ -766,7 +766,8 @@ void prejit_sync(void);
 void init_emu_npu(void);
 
 unsigned int Sim_helper(unsigned int mem_ref, unsigned int data, int mode,
-			uint32_t *flags, unsigned int opc, unsigned int arg);
+			uint32_t *flags, unsigned int opc, unsigned int arg,
+			unsigned char *eip);
 
 void e_VgaMovs(dosaddr_t edi, dosaddr_t esi, unsigned int rep,
 	       int dp, unsigned int access);

src/base/emu-i386/simx86/interp.c
@@ -567,14 +567,12 @@ static TNode *interp_post(unsigned int PC, unsigned int Interp_LONG_CS,
 
 static unsigned int _Interp86(unsigned int PC)
 {
-	volatile unsigned int P0 = PC; /* volatile because of setjmp */
-	int val;
 	TNode *G;
 
-	if (PROTMODE() && (val = setjmp(jmp_env))) {
+	if (PROTMODE() && setjmp(jmp_env)) {
 		/* long jump to here from simulated page fault
-		   val == 2 comes from Gen_Sim, with different cs:eip */
-		return val == 2 ? LONG_CS + TheCPU.eip : P0;
+		   via Gen_sim or Sim_helper, with different cs:eip */
+		return LONG_CS + TheCPU.eip;
 	}
 
 	CEmuStat &= ~(CeS_TRAP|CeS_STI);
@@ -589,7 +587,6 @@ static unsigned int _Interp86(unsigned int PC)
 		if (TheCPU.err)
 			return PC;
 		do {
-			P0 = PC;
 			PC = InterpOne(PC, LONG_CS, TheCPU.mode);
 			G = interp_post(PC, LONG_CS, TheCPU.mode, 1);
 		} while (!G);

commit c556e232247e7fffd1437b38d5180d6ae7d365ed

simx86: fix stack alignment for x86-64 Sim_helper call
Regression from 5e7510f, which caused debug printfs to hang in
Sim_helper().

src/base/emu-i386/simx86/codegen-x86.c
@@ -1653,12 +1653,12 @@ shrot0:
 		G2M(0x41,0xb8,Cp); G4(IG->p0,Cp);
 		// mov $arg, %%r9d
 		G2M(0x41,0xb9,Cp); G4(IG->p1,Cp);
-		// push %%rdi // keep mem_ref (2x for alignment)
-		G2M(PUSHdi,PUSHdi,Cp);
+		// push %%rdi // keep mem_ref
+		G1(PUSHdi,Cp);
 		// call 1f (to push eip)
-		G1(CALLd,Cp); G4(2+4+2+TAILSIZE,Cp);
+		G1(CALLd,Cp); G4(1+4+2+TAILSIZE,Cp);
 		// pop %%rdi // mem_ref
-		G2M(POPdi,POPdi,Cp);
+		G1(POPdi,Cp);
 #else
 		// mov %esp, %ecx // flags
 		G2M(0x89,0xe1,Cp);