5329bbfe180bfc7c6cb282833c95f6a685cc53e1
Stas Sergeev <stsp2@yandex.ru>
2025-10-09 17:17:50 +0300

    sdl3: remove w/a https://bugzilla.libsdl.org/show_bug.cgi?id=5341
    This bug is long ago fixed.
    Work-around however now causes a very small window.
    It seems, GetWindowSize() is not updated in time.

--- src/plugin/sdl3/sdl.c
+++ src/plugin/sdl3/sdl.c
@@ -985,26 +985,10 @@ static void SDL_change_mode(int x_res, int y_res, int w_x_res, int w_y_res)
   desired_win_height = w_y_res;
 
   flags = SDL_GetWindowFlags(window);
-  if (!(flags & (SDL_WINDOW_MAXIMIZED | SDL_WINDOW_FULLSCREEN))) {
-    int nw_x_res, nw_y_res;
+  if (!(flags & (SDL_WINDOW_MAXIMIZED | SDL_WINDOW_FULLSCREEN)))
     SDL_SetWindowSize(window, w_x_res, w_y_res);
-    /* work around SDL bug:
-     * https://bugzilla.libsdl.org/show_bug.cgi?id=5341
-     */
-    SDL_GetWindowSize(window, &nw_x_res, &nw_y_res);
-    if (nw_x_res != w_x_res) {
-      error("X res changed: %i -> %i\n", w_x_res, nw_x_res);
-      w_x_res = nw_x_res;
-    }
-    if (nw_y_res != w_y_res) {
-      error("Y res changed: %i -> %i\n", w_y_res, nw_y_res);
-      w_y_res = nw_y_res;
-    }
-    /* set window size again to avoid crash, huh? */
-    SDL_SetWindowSize(window, w_x_res, w_y_res);
-  } else if (!config.X_fixed_aspect || is_text) {
+  else if (!config.X_fixed_aspect || is_text)
     SDL_GetWindowSize(window, &w_x_res, &w_y_res);
-  }
   if (!is_text) {
     if (config.X_fixed_aspect)
       SDL_SetRenderLogicalPresentation(renderer, w_x_res, w_y_res,
