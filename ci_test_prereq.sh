c42ce7622827ef90ac2c8522c3d4e23cd2c97d08
Andrew Bird <ajb@spheresystems.co.uk>
2025-10-14 20:50:18 +0100

    CI: We no longer test on Ubuntu 20.04
    Since Github Actions removed the Ubuntu 20.04 runner, remove
    the workarounds that we needed for it.

--- ci_test_prereq.sh
+++ ci_test_prereq.sh
@@ -5,11 +5,7 @@ set -e
 sudo add-apt-repository -y ppa:jwt27/djgpp-toolchain
 sudo add-apt-repository -y ppa:stsp-0/gcc-ia16
 if [ "${RUNTYPE}" = "packaged" ] ; then
-  if ! $(grep -q '^Ubuntu 20' /etc/issue) ; then
-    sudo add-apt-repository -y -c main -c main/debug ppa:dosemu2/ppa
-  else
-    sudo add-apt-repository -y ppa:dosemu2/ppa
-  fi
+  sudo add-apt-repository -y -c main -c main/debug ppa:dosemu2/ppa
 fi
 
 sudo apt update -q
@@ -36,21 +32,16 @@ sudo apt install -y \
   libvirt-daemon \
   libvirt-daemon-system
 
-if ! $(grep -q '^Ubuntu 20' /etc/issue) ; then
-  sudo apt install -y \
-    dj64-dbgsym \
-    djdev64-dbgsym
-fi
+sudo apt install -y \
+  dj64-dbgsym \
+  djdev64-dbgsym
 
 if [ "${RUNTYPE}" = "packaged" ] ; then
   sudo apt install -y \
     dosemu2 \
-    fdpp
-  if ! $(grep -q '^Ubuntu 20' /etc/issue) ; then
-    sudo apt install -y \
-      dosemu2-dbgsym \
-      fdpp-dbgsym
-  fi
+    dosemu2-dbgsym \
+    fdpp \
+    fdpp-dbgsym
 fi
 
 # Install the FAT mount helper
