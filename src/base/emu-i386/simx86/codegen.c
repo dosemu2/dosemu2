dosemu2/dosemu2

commit a63048b59177137946a75c21bf74cc5f166b8fa0

simx86: move forever loop exit to execution loop
Since at translation time we don't know IF yet, we can only tag
a candidate node.

src/base/emu-i386/simx86/interp.c
@@ -279,9 +279,9 @@ static unsigned int JumpGen(unsigned int P2, unsigned int Interp_LONG_CS,
 	}
 	/* no break */
 	case JMPsid: case JMPd:   /* uncond jmp */
-		if (dsp == 0 && !(EFLAGS & (VIF|IF|TF))) {	// eb fe
-		    dbug_printf("!Forever loop!\n");
-		    leavedos_main(0xebfe);
+		if (dsp == 0) {      // eb fe
+		    dbug_printf("!Possible forever loop!\n");
+		    InstrMeta[CurrIMeta].flags |= F_SLFJ;
 		}
 		if (dsp <= 0) mode |= CKSIGN;
 		Gen(JMP_LINK, mode, j_t);
@@ -439,6 +439,11 @@ static unsigned int FindExecCode(unsigned int PC)
 		if (G->flags & F_PREJ)
 			PrejitNodesExecd++;
 #endif
+		/* checking for infinite loops, flagged in JumpGen() */
+		if ((G->flags & F_SLFJ) && !(EFLAGS & (VIF|IF|TF))) {
+			error("!Forever loop!\n");
+			leavedos_main(0xebfe);
+		}
 		if (G->seqlen == 0) {
 			error("CPU-EMU: Zero-len code node?\n");
 			break;

src/base/emu-i386/simx86/codegen.h
@@ -197,6 +197,7 @@
 #define F_PREJ	0x0010	// generated by any prejit thread
 #define F_SPRJ	0x0020	// generated by speculative prejit thread
 #define F_LEAV	0x0040	// leave instremu after executing this node
+#define F_SLFJ	0x0080	// self-jump (forever loop)
 
 /////////////////////////////////////////////////////////////////////////////
 

commit 64778f740aa982e95dbe0af4f4b727de723fd3f6

simx86: fix forever loop detection
Regression from a63048b591.

Some nodes could be misidentified as forever loops since they
were linked and backrefs inherited the flags. Also after DoExec_fast
we looked at the wrong node.

So never link to a node with a forever loop, and put the exit logic
in Exec_post makes it use the right node.

src/base/emu-i386/simx86/codegen.c
@@ -584,7 +584,7 @@ static unsigned int Exec_pre(unsigned char *ecpu)
 	return flg;
 }
 
-static void Exec_post(unsigned long flg, unsigned int mem_ref)
+static void Exec_post(unsigned long flg, unsigned int mem_ref, const TNode *G)
 {
 	EFLAGS = (EFLAGS & ~EFLAGS_CC) | (flg &	EFLAGS_CC);
 	TheCPU.mem_ref = mem_ref;
@@ -592,6 +592,11 @@ static void Exec_post(unsigned long flg, unsigned int mem_ref)
 	if (TheCPU.err == EXCP_STISHADOW) {
 		CEmuStat |= CeS_STI;
 		TheCPU.err = 0;
+	}
+	/* checking for infinite loops, flagged in JumpGen() */
+	if (G && (G->flags & F_SLFJ) && !(EFLAGS & (VIF|IF|TF))) {
+		error("!Forever loop!\n");
+		leavedos_main(0xebfe);
 	}
 }
 
@@ -621,7 +626,7 @@ unsigned int DoExec(TNode *G)
 #endif
 	flg = Exec_pre(ecpu);
 	ePC = Exec(&mem_ref, &flg, ecpu, SeqStart, seqflg);
-	Exec_post(flg, mem_ref);
+	Exec_post(flg, mem_ref, G);
 
 #ifdef SKIP_EMU_VBIOS
 	if ((jcs&0xf000)==config.vbios_seg && !TheCPU.err)
@@ -723,9 +728,9 @@ unsigned int DoExec_fast(TNode *G)
 			TheCPU.err = EXCP_GOBACK;
 #endif
 	} while (!TheCPU.err && (G=FindTree(ePC)) &&
-		 GoodNode(G) && !(G->flags & (F_FPOP|F_INHI)));
+		 GoodNode(G) && !(G->flags & (F_FPOP|F_INHI|F_SLFJ)));
 
-	Exec_post(flg, mem_ref);
+	Exec_post(flg, mem_ref, G);
 	return ePC;
 }
 

src/base/emu-i386/simx86/interp.c
@@ -440,11 +440,6 @@ static unsigned int FindExecCode(unsigned int PC)
 		if (G->flags & F_PREJ)
 			PrejitNodesExecd++;
 #endif
-		/* checking for infinite loops, flagged in JumpGen() */
-		if ((G->flags & F_SLFJ) && !(EFLAGS & (VIF|IF|TF))) {
-			error("!Forever loop!\n");
-			leavedos_main(0xebfe);
-		}
 		if (G->seqlen == 0) {
 			error("CPU-EMU: Zero-len code node?\n");
 			break;

src/base/emu-i386/simx86/trees.c
@@ -742,8 +742,9 @@ static void linknode(TNode *LG, TNode *G, linkdesc *L, unsigned target_type)
 {
 	backref *B;
 
-	// points to current node?
-	if (L->target!=G->key || !(LG->unlinked_jmp_targets & target_type))
+	// points to current node, which can't be a forever loop?
+	if (L->target!=G->key || !(LG->unlinked_jmp_targets & target_type) ||
+	    (G->flags & F_SLFJ))
 		return;
 
 	if (L->ref!=0) {