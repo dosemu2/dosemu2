dosemu2/dosemu2

commit d18f5fa3e8f1a2995ee1b571675d9556aed470a6

simx86: move protected mode RETlisp to Sim_helper
To not set TheCPU.eip too early much like for IRET/RETl

src/base/emu-i386/simx86/interp.c
@@ -1689,13 +1689,16 @@ intop3b:		{ int op = ArOpsFR[D_MO(opc)];
 /*ca*/	case RETlisp: {	/* restartable */
 			/* pop from stack without adjusting esp before A_SR_* */
 			int dr = (signed short)FetchW(PC+1);
-			Gen(O_POP1, _mode);
-			Gen(O_POP2, _mode, Ofs_EIP);
-			Gen(O_POP2, _mode|MNOREG|MRETISP, dr);
-			if (REALADDR())
+			if (REALADDR()) {
+				Gen(O_POP1, _mode);
+				Gen(O_POP2, _mode, Ofs_EIP);
+				Gen(O_POP2, _mode|MNOREG|MRETISP, dr);
 				AddrGen(A_SR_SH4, _mode, Ofs_CS, Ofs_XCS);
-			else
-				AddrGen(A_SR_PROT, _mode, Ofs_CS, P0);
+				Gen(O_POP3, _mode);
+			}
+			else {
+				Gen(O_SIM, _mode, opc, dr, P0);
+			}
 			Gen(O_POP3, _mode);
 			PC = JumpGen(PC, Interp_LONG_CS, _mode, opc, 3);
 			if (debug_level('e')>2)

src/base/emu-i386/simx86/codegen-sim.c
@@ -3093,6 +3093,7 @@ static unsigned int _Sim_helper(unsigned int mem_ref, unsigned int data, int mod
 					    inum, _AX, _CS, _IP);
 			break;
 		       }
+/*ca*/	case RETlisp:
 /*cb*/	case RETl:
 /*cf*/	case IRET: {	/* restartable */
 			if (!REALADDR()) {
@@ -3107,6 +3108,10 @@ static unsigned int _Sim_helper(unsigned int mem_ref, unsigned int data, int mod
 					eip = POPl(sp);
 					cs = POPl(sp);
 					if (opc == IRET) data = POPl(sp);
+				}
+				if (opc == RETlisp) {
+					sp += arg;
+					sp &= TheCPU.StackMask;
 				}
 				rESP = sp | (rESP&~TheCPU.StackMask);
 				if (SetSegProt_helper(cs, Ofs_CS) < 0)

commit 18348639db43a1bdcb58455f04ff9beddc16dad3

simx86: remove O_POP3 left behind for RETlisp
Regression from d18f5fa3e

src/base/emu-i386/simx86/interp.c
@@ -1699,7 +1699,6 @@ intop3b:		{ int op = ArOpsFR[D_MO(opc)];
 			else {
 				Gen(O_SIM, _mode, opc, dr, P0);
 			}
-			Gen(O_POP3, _mode);
 			PC = JumpGen(PC, Interp_LONG_CS, _mode, opc, 3);
 			if (debug_level('e')>2)
 				e_printf("RET_%d: ret=%08x\n",dr,TheCPU.eip);