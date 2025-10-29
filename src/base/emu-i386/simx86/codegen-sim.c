dosemu2/dosemu2

commit 16726e71c2174a53ec0f7357f2effab7764e00f6

simx86: handle protected mode lret in Sim_helper
similar to IRET, to avoid setting TheCPU.eip too early

src/base/emu-i386/simx86/codegen-sim.c
@@ -3093,6 +3093,7 @@ static unsigned int _Sim_helper(unsigned int mem_ref, unsigned int data, int mod
 					    inum, _AX, _CS, _IP);
 			break;
 		       }
+/*cb*/	case RETl:
 /*cf*/	case IRET: {	/* restartable */
 			if (!REALADDR()) {
 				unsigned int cs, eip;
@@ -3100,12 +3101,12 @@ static unsigned int _Sim_helper(unsigned int mem_ref, unsigned int data, int mod
 				if (mode&DATA16) {
 					eip = POPw(sp);
 					cs = POPw(sp);
-					data = POPw(sp);
+					if (opc == IRET) data = POPw(sp);
 				}
 				else {
 					eip = POPl(sp);
 					cs = POPl(sp);
-					data = POPl(sp);
+					if (opc == IRET) data = POPl(sp);
 				}
 				rESP = sp | (rESP&~TheCPU.StackMask);
 				if (SetSegProt_helper(cs, Ofs_CS) < 0)
@@ -3114,6 +3115,7 @@ static unsigned int _Sim_helper(unsigned int mem_ref, unsigned int data, int mod
 				   page fault: we return to the main loop after */
 				TheCPU.eip = eip;
 			}
+			if (opc != IRET) break;
 			/* non-segment GPF handled in interpreter */
 			assert(!(V86MODE() && IOPL!=3 && !(TheCPU.cr[4] & CR4_VME)));
 			temp = data;

src/base/emu-i386/simx86/interp.c
@@ -1765,14 +1765,16 @@ intop3b:		{ int op = ArOpsFR[D_MO(opc)];
 
 /*cb*/	case RETl:
 			/* pop from stack without adjusting esp before A_SR_* */
-			Gen(O_POP1, _mode);
-			Gen(O_POP2, _mode, Ofs_EIP);
-			Gen(O_POP2, _mode|MNOREG);
-			if (REALADDR())
+			if (REALADDR()) {
+				Gen(O_POP1, _mode);
+				Gen(O_POP2, _mode, Ofs_EIP);
+				Gen(O_POP2, _mode|MNOREG);
 				AddrGen(A_SR_SH4, _mode, Ofs_CS, Ofs_XCS);
-			else
-				AddrGen(A_SR_PROT, _mode, Ofs_CS, P0);
-			Gen(O_POP3, _mode);
+				Gen(O_POP3, _mode);
+			}
+			else {
+				Gen(O_SIM, _mode, opc, 0, P0);
+			}
 			PC = JumpGen(PC, Interp_LONG_CS, _mode, opc, 1);
 			break;
 

commit 40bb516224244430211007484e49dd6e7f416432

simx86: fix Sim_helper regressions in 16726e7 and a6405384
First one set ESP before the fault, it should be the other
way around, and cause a GPF in BC31, because the stack was
wrong after the first fault. Second one was calling SetSegProt
for real mode segments which I noticed looking at that code.

Fixes #2659

src/base/emu-i386/simx86/codegen-sim.c
@@ -3142,14 +3142,16 @@ static unsigned int _Sim_helper(unsigned int mem_ref, unsigned int data, int mod
 					sp += arg;
 					sp &= TheCPU.StackMask;
 				}
-				rESP = sp | (rESP&~TheCPU.StackMask);
 				if (REALADDR()) {
 					TheCPU.cs = cs;
 					LONG_CS = cs << 4;
 				}
-				SetSegProt(Ofs_CS, cs);
-				if (TheCPU.err)
-					break;
+				else {
+					SetSegProt(Ofs_CS, cs);
+					if (TheCPU.err)
+						break;
+				}
+				rESP = sp | (rESP&~TheCPU.StackMask);
 				/* eax used by JMP_INDIRECT */
 				data = eip;
 			}