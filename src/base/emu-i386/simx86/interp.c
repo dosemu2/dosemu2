7a26eca2bfc6356fa39de7ad614002786ed9dcbe
Bart Oldeman <bartoldeman@users.noreply.github.com>
2025-10-30 17:54:03 -0400

    simx86: STI does not inhibit single stepping with TF set
    Unlike POPss/MOVss
    
    See also
    https://gitlab.com/qemu-project/qemu/-/commit/1e94ddc685443
    
    I verified this in real mode. FreeDOS debug displays STI
    together with the next instruction when tracing but actually
    does not skip over it.

--- src/base/emu-i386/simx86/interp.c
+++ src/base/emu-i386/simx86/interp.c
@@ -2123,7 +2123,8 @@ repag0:
 			Gen(O_SIM, _mode, opc, 0, PC);
 			/* real mode inhibits after STI as well but
 			   we've always relied on trapping behaviour with vm86 */
-			if (!V86MODE() && !(_mode & MINHI)) {
+			/* note that TF does not inhibit after STI! */
+			if (!V86MODE() && !(_mode & (MTRAP|MINHI))) {
 				PC = InterpOne(PC, Interp_LONG_CS, ocs,
 					       basemode|MINHI|MSSTP);
 				/* check signals immediately after the
