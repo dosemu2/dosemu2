dosemu2/dosemu2

commit 6be02db05e528faa3cb8ce4c6d3f684fdb2bdb57

simx86: for "push segreg", always use 16-bit moves
But adjust ESP by 4 in 32-bit mode.

This matches real CPUs:
https://www.felixcloutier.com/x86/push
"if the operand size is 32-bits, either a zero-extended value
is pushed on the stack or the segment selector is written
on the stack using a 16-bit move. For the last case, all
recent Intel Core and Intel Atom processors perform a 16-bit
move, leaving the upper portion of the stack location
unmodified."

src/base/emu-i386/simx86/codegen-x86.c
@@ -1104,7 +1104,7 @@ shrot0:
 			0x8b,0x73,Ofs_XSS,
 			// movl Ofs_ESP(%%ebx),%%ecx
 			0x8b,0x4b,Ofs_ESP,
-			// leal -2(%%ecx),%%ecx
+/*06*/			// leal -2(%%ecx),%%ecx
 			0x8d,0x49,0xfe,
 			// 16-bit stack seg w/underflow (RM)
 			// andl StackMask(%%ebx),%%ecx
@@ -1154,9 +1154,14 @@ shrot0:
 			0x89,0x4b,Ofs_ESP
 		};
 		const unsigned char *p; int sz;
-		if (mode&DATA16) p=pseq16,sz=sizeof(pseq16);
+		if (mode&(SEGREG|DATA16)) p=pseq16,sz=sizeof(pseq16);
 			else p=pseq32,sz=sizeof(pseq32);
 		GNX(Cp, p, sz);
+		if ((mode & (SEGREG|DATA16)) == SEGREG) {
+			// use pseq16 but -4 for 32-bit segreg
+			// mirroring Core Intel CPUs and newer
+			Cp[-sz+8] = 0xfc;
+		}
 		} break;
 
 /* PUSH derived (sub-)sequences: */
@@ -1172,10 +1177,10 @@ shrot0:
 
 	case O_PUSH2: {		/* register push only */
 		const unsigned char pseq16[] = {
-			// movl offs(%%ebx),%%ax
-/*00*/			0x66,0x8b,0x43,0x00,
+			// movw offs(%%ebx),%%ax (with 66 prefix)
+/*00*/			0x8b,0x43,0x00,
 			// leal -2(%%ecx),%%ecx
-			0x8d,0x49,0xfe,
+/*03*/			0x8d,0x49,0xfe,
 			// andl StackMask(%%ebx),%%ecx
 			0x23,0x4b,Ofs_STACKM,
 			// leal (%%esi,%%ecx,1),%%edx
@@ -1184,8 +1189,8 @@ shrot0:
 			0x66,0x89,0x04,0x2a,
 		};
 		const unsigned char pseq32[] = {
-			// nop; movl offs(%%ebx),%%eax
-/*00*/			0x90,0x8b,0x43,0x00,
+			// movl offs(%%ebx),%%eax
+/*00*/			0x8b,0x43,0x00,
 			// leal -4(%%ecx),%%ecx
 			0x8d,0x49,0xfc,
 			// andl StackMask(%%ebx),%%ecx
@@ -1198,14 +1203,14 @@ shrot0:
 		const unsigned char *p;
 		unsigned char *q;
 		int sz;
-		if (mode&DATA16) p=pseq16,sz=sizeof(pseq16);
+		if (mode&(SEGREG|DATA16)) G1(0x66,Cp),p=pseq16,sz=sizeof(pseq16);
 			else p=pseq32,sz=sizeof(pseq32);
 		q=Cp; GNX(Cp, p, sz);
-		q[3] = IG->p0;
+		q[2] = IG->p0;
 		if ((mode & (SEGREG|DATA16)) == SEGREG) {
-			// change to movzx for 32bit segreg
-			q[0] = 0x0f;
-			q[1] = 0xb7;
+			// use pseq16 but -4 for 32-bit segreg
+			// mirroring recent Intel CPUs
+			q[5] = 0xfc;
 		}
 		} break;
 

src/base/emu-i386/simx86/codegen-sim.c
@@ -1928,25 +1928,23 @@ static unsigned int Gen_sim(IGen *IG, unsigned int *pmem_ref,
 		break;
 
 	case O_PUSH: {
-		wkreg SR1, AR2;
+		dosaddr_t esp, addr;
 		unsigned long stackm = CPULONG(Ofs_STACKM);
 		GTRACE0("O_PUSH");
-		if (mode & DATA16) {
-			AR2.d = CPULONG(Ofs_XSS);
-			SR1.d = CPULONG(Ofs_ESP) - 2;
-			SR1.d &= stackm;
-			sim_write_word(AR2.d + SR1.d, DR1.w.l);
-		}
-		else {
-			AR2.d = CPULONG(Ofs_XSS);
-			SR1.d = CPULONG(Ofs_ESP) - 4;
-			SR1.d &= stackm;
-			sim_write_dword(AR2.d + SR1.d, DR1.d);
-		}
+		if (mode & DATA16)
+			esp = CPULONG(Ofs_ESP) - 2;
+		else
+			esp = CPULONG(Ofs_ESP) - 4;
+		esp &= stackm;
+		addr = CPULONG(Ofs_XSS) + esp;
+		if (mode & (SEGREG|DATA16))
+			sim_write_word(addr, DR1.w.l);
+		else
+			sim_write_dword(addr, DR1.d);
 #ifdef KEEP_ESP	/* keep high 16-bits of ESP in small-stack mode */
-		SR1.d |= (CPULONG(Ofs_ESP) & ~stackm);
+		esp |= (CPULONG(Ofs_ESP) & ~stackm);
 #endif
-		CPULONG(Ofs_ESP) = SR1.d;
+		CPULONG(Ofs_ESP) = esp;
 		if (debug_level('e')>3) dbug_printf("(V) %08x\n",DR1.d);
 		} break;
 
@@ -1957,23 +1955,20 @@ static unsigned int Gen_sim(IGen *IG, unsigned int *pmem_ref,
 		break;
 
 	case O_PUSH2: {
-		wkreg AR2;
+		dosaddr_t addr;
 		unsigned int o = IG->p0;
 		unsigned long stackm = CPULONG(Ofs_STACKM);
 		GTRACE1("O_PUSH2",o);
-		AR2.d = CPULONG(Ofs_XSS);
-		if (mode & DATA16) {
-			DR1.w.l = CPUWORD(o);
+		if (mode & DATA16)
 			SR1.d -= 2;
-			SR1.d &= stackm;
-			sim_write_word(AR2.d + SR1.d, DR1.w.l);
-		}
-		else {
-			DR1.d = (mode & SEGREG) ? CPUWORD(o) : CPULONG(o);
+		else
 			SR1.d -= 4;
-			SR1.d &= stackm;
-			sim_write_dword(AR2.d + SR1.d, DR1.d);
-		}
+		SR1.d &= stackm;
+		addr = CPULONG(Ofs_XSS) + SR1.d;
+		if (mode & (SEGREG|DATA16))
+			sim_write_word(addr, CPUWORD(o));
+		else
+			sim_write_dword(addr, CPULONG(o));
 		if (debug_level('e')>3) dbug_printf("(V) %08x\n",DR1.d);
 		} break;
 
@@ -2981,6 +2976,10 @@ static __inline__ void SetCPU_WL(int m, signed char o, unsigned long v)
 	sp -= 2; sp &= TheCPU.StackMask; \
 	sim_write_word(LONG_SS + sp, w); })
 
+#define PUSHwl(sp,w) ({ \
+	sp -= 4; sp &= TheCPU.StackMask; \
+	sim_write_word(LONG_SS + sp, w); })
+
 #define PUSHl(sp,l) ({ \
 	sp -= 4; sp &= TheCPU.StackMask; \
 	sim_write_dword(LONG_SS + sp, l); })
@@ -3115,7 +3114,7 @@ static unsigned int _Sim_helper(unsigned int mem_ref, unsigned int data, int mod
 				PUSHw(sp,oldeip);
 			}
 			else {
-				PUSHl(sp,oldcs);
+				PUSHwl(sp,oldcs);
 				PUSHl(sp,oldeip);
 			}
 			if (debug_level('e')>2) {

src/base/emu-i386/simx86/interp.c
@@ -298,8 +298,8 @@ static unsigned int JumpGen(unsigned int P2, unsigned int Interp_LONG_CS,
 		unsigned short jcs = FetchW(P2 + pskip - 2);
 		if (REALADDR()) {
 		    /* ok, first push old cs:eip */
-		    Gen(L_REG, mode, Ofs_CS);
-		    Gen(O_PUSH, mode);
+		    Gen(L_REG, mode|DATA16, Ofs_CS);
+		    Gen(O_PUSH, mode|SEGREG);
 		    Gen(O_PUSHI, mode, d_nt);
 		    Gen(L_IMM_R1, mode&~DATA16, jcs);
 		    AddrGen(A_SR_SH4, mode, Ofs_CS, Ofs_XCS);
@@ -1013,7 +1013,7 @@ intop3b:		{ int op = ArOpsFR[D_MO(opc)];
 			/* optimized multiple register push */
 			while (1) {
 			    int op;
-			    if (opc > 8 && !(m & DATA16)) // 32-bit segreg
+			    if (opc > 8)
 				m |= SEGREG;
 			    Gen(O_PUSH2, m, R1Tab_l[opc-1]);
 			    PC++;
@@ -1039,9 +1039,9 @@ intop3b:		{ int op = ArOpsFR[D_MO(opc)];
 			Gen(O_PUSH3, m); } else
 #endif
 			{
-			if (opc > 8 && !(_mode & DATA16)) { // 32-bit segreg
+			if (opc > 8) {
+			    _mode |= SEGREG;
 			    Gen(L_REG, _mode|DATA16, R1Tab_l[opc-1]);
-			    Gen(L_ZXAX, _mode);
 			} else
 			    Gen(L_REG, _mode, R1Tab_l[opc-1]);
 			Gen(O_PUSH, _mode); PC++;
@@ -1724,6 +1724,7 @@ intop3b:		{ int op = ArOpsFR[D_MO(opc)];
 			fprintf(aLog,"%08x:\t\tint %02x\n", P0, inum);
 #endif
 			if (V86MODE() && (TheCPU.cr[4] & CR4_VME)) {
+				_mode |= DATA16|ADDR16; // no operand size prefix
 				if (IOPL == 3) {
 					Gen(O_INT, _mode, inum, P0);
 					Gen(O_PUSH2F, _mode);
@@ -2193,8 +2194,8 @@ repag0:
 					    /* first push old cs:eip */
 					    oip = PC + len - Interp_LONG_CS;
 					    if (REALADDR()) {
-						Gen(L_REG, _mode, Ofs_CS);
-						Gen(O_PUSH, _mode);
+						Gen(L_REG, _mode|DATA16, Ofs_CS);
+						Gen(O_PUSH, _mode|SEGREG);
 						Gen(O_PUSHI, _mode, oip);
 					    }
 					    else {
@@ -2480,9 +2481,7 @@ repag0:
 ///
 			case 0xa0: /* PUSHfs */
 				Gen(L_REG, _mode|DATA16, Ofs_FS);
-				if (!(_mode & DATA16)) // 32-bit segreg padding
-				    Gen(L_ZXAX, _mode);
-				Gen(O_PUSH, _mode); PC+=2;
+				Gen(O_PUSH, _mode|SEGREG); PC+=2;
 				break;
 			case 0xa1: /* POPfs */
 				if (REALADDR()) {
@@ -2575,9 +2574,7 @@ repag0:
 ///
 			case 0xa8: /* PUSHgs */
 				Gen(L_REG, _mode|DATA16, Ofs_GS);
-				if (!(_mode & DATA16)) // 32-bit segreg padding
-				    Gen(L_ZXAX, _mode);
-				Gen(O_PUSH, _mode); PC+=2;
+				Gen(O_PUSH, _mode|SEGREG); PC+=2;
 				break;
 			case 0xa9: /* POPgs */
 				if (REALADDR()) {

commit 44790f9ade0e2645487e5bb4aa267329f062daeb

simx86: fix codegen-x86 O_PUSH2 for cpatch multipush code
CPatch has some tricky code to deal with multiple cases
of O_PUSH2 where the length of 16-bit and 32-bit sequences
until the actual write are the same. Easiest to restore
the nop then.

Regression from 6be02db05

src/base/emu-i386/simx86/codegen-x86.c
@@ -1165,10 +1165,10 @@ shrot0:
 
 	case O_PUSH2: {		/* register push only */
 		const unsigned char pseq16[] = {
-			// movw offs(%%ebx),%%ax (with 66 prefix)
-/*00*/			0x8b,0x43,0x00,
+			// movw offs(%%ebx),%%ax
+/*00*/			0x66,0x8b,0x43,0x00,
 			// leal -2(%%ecx),%%ecx
-/*03*/			0x8d,0x49,0xfe,
+/*04*/			0x8d,0x49,0xfe,
 			// andl StackMask(%%ebx),%%ecx
 			0x23,0x4b,Ofs_STACKM,
 			// leal (%%esi,%%ecx,1),%%edx
@@ -1178,7 +1178,7 @@ shrot0:
 		};
 		const unsigned char pseq32[] = {
 			// movl offs(%%ebx),%%eax
-/*00*/			0x8b,0x43,0x00,
+/*00*/			0x90,0x8b,0x43,0x00,
 			// leal -4(%%ecx),%%ecx
 			0x8d,0x49,0xfc,
 			// andl StackMask(%%ebx),%%ecx
@@ -1191,14 +1191,14 @@ shrot0:
 		const unsigned char *p;
 		unsigned char *q;
 		int sz;
-		if (mode&(SEGREG|DATA16)) G1(0x66,Cp),p=pseq16,sz=sizeof(pseq16);
+		if (mode&(SEGREG|DATA16)) p=pseq16,sz=sizeof(pseq16);
 			else p=pseq32,sz=sizeof(pseq32);
 		q=Cp; GNX(Cp, p, sz);
-		q[2] = IG->p0;
+		q[3] = IG->p0;
 		if ((mode & (SEGREG|DATA16)) == SEGREG) {
 			// use pseq16 but -4 for 32-bit segreg
 			// mirroring recent Intel CPUs
-			q[5] = 0xfc;
+			q[6] = 0xfc;
 		}
 		} break;
 