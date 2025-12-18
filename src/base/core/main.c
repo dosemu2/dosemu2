3bbe4fff9b939b2d3a08a057e2a3e9fc1b99ab18
Stas Sergeev <stsp2@yandex.ru>
2025-12-19 00:48:21 +0200

    emscripten: add some nodefs support [#575]
    Now it goes up to processing global.conf and stops there as the
    shell commands are unavailable.

--- src/base/core/main.c
+++ src/base/core/main.c
@@ -1,6 +1,18 @@
+#ifdef __EMSCRIPTEN__
+#include <emscripten.h>
+#endif
 #include "dosemu2/emu.h"
 
 int main(int argc, char **argv, char * const *envp)
 {
+#ifdef __EMSCRIPTEN__
+    EM_ASM({
+        FS.mkdir('/usr');
+        FS.mount(NODEFS, { root: '/usr' }, '/usr');
+        FS.mkdir(process.env.HOME);
+        FS.mount(NODEFS, { root: process.env.HOME }, process.env.HOME);
+        FS.mount(NODEFS, { root: process.env.HOME }, '/home/web_user');
+    });
+#endif
     return dosemu2_emulate(argc, argv, envp);
 }
