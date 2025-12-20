cdb33ecf7a80852b63aba571db5cc29f257a9e09
Stas Sergeev <stsp2@yandex.ru>
2025-12-20 13:20:47 +0200

    emscripten: support -sPROXY_TO_PTHREAD [#575]
    This required switching from nodefs to wasmfs, and also to
    change the signature of "main" to the more portable variant.

--- src/base/core/main.c
+++ src/base/core/main.c
@@ -1,18 +1,34 @@
 #ifdef __EMSCRIPTEN__
 #include <emscripten.h>
+#include <emscripten/wasmfs.h>
+#include <stdlib.h>
+#include <assert.h>
 #endif
 #include "dosemu2/emu.h"
 
-int main(int argc, char **argv, char * const *envp)
+extern char **environ;
+
+int main(int argc, char **argv)
 {
 #ifdef __EMSCRIPTEN__
-    EM_ASM({
-        FS.mkdir('/usr');
-        FS.mount(NODEFS, { root: '/usr' }, '/usr');
-        FS.mkdir(process.env.HOME);
-        FS.mount(NODEFS, { root: process.env.HOME }, process.env.HOME);
-        FS.mount(NODEFS, { root: process.env.HOME }, '/home/web_user');
-    });
+    const char *h = emscripten_run_script_string("process.env.HOME");
+    const char *h2 = getenv("HOME");
+    backend_t home, usr;
+    int err;
+
+    assert(h && h2);
+    home = wasmfs_create_node_backend(h);
+    assert(home);
+    usr = wasmfs_create_node_backend("/usr");
+    assert(usr);
+    err = wasmfs_create_directory("/usr", 0500, usr);
+    assert(!err);
+    err = mkdir("/home", 0755);
+    assert(!err);
+    err = wasmfs_create_directory(h, 0700, home);
+    assert(!err);
+    err = wasmfs_create_directory(h2, 0700, home);
+    assert(!err);
 #endif
-    return dosemu2_emulate(argc, argv, envp);
+    return dosemu2_emulate(argc, argv, environ);
 }
