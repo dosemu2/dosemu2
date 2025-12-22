dbc71a8f4656dd798fe43c19328995c362208f2e
Stas Sergeev <stsp2@yandex.ru>
2025-12-22 14:37:02 +0200

    cpu: add CWD-based IGNNE implementation [#2680]
    This however implements the "unrecommended" (by Intel) design,
    which may theoretically be not properly supported by some DOS
    program, in which case IGNNE may never get untriggered, preventing
    further FPU exceptions from happening.
    Or the program may save CWD too early (before untriggering IGNNE)
    and "observe the differences" (get a wrong exception mask).
    We'll need to find such program though, and evaluate its "importantness".
    
    This is basically a forward-port of #2687
    
    See #2724 which still works.

--- src/base/emu-i386/cpu.c
+++ src/base/emu-i386/cpu.c
@@ -280,6 +280,7 @@ static void fpu_reset(void)
 }
 
 static int fpu_ignne;
+static int fpu_orig_mask;
 
 static Bit8u fpu_io_read(ioport_t port, void *arg)
 {
@@ -288,16 +289,27 @@ static Bit8u fpu_io_read(ioport_t port, void *arg)
 
 static void fpu_io_write(ioport_t port, Bit8u val, void *arg)
 {
+  int old_ignne = fpu_ignne;
+  int mask = old_ignne ? fpu_orig_mask : (vm86_fpu_state.cwd & 0x7f);
+
   switch (port) {
   case 0xf0:
-    /* We need to check if the FPU exception is pending, and set IGNNE
-     * if it is, before untriggering an IRQ. But we don't (and probably
-     * can't) always emulate IGNNE properly. So the trick is to do fnclex in
-     * bios.S, then untrigger IRQ unconditionally. */
-    pic_untrigger(13); /* done by default via int75 handler in bios.S */
-    /* DJGPP's int75 pm handler for example does not use fnclex;
-       but we can force it in return_from_hw_int() in dpmi.c, so
-       this only makes sense in DPMI, as this variable is not checked in RM */
+    pic_untrigger(13);
+    if (_CPU_VM_CURRENT() == CPUVM_KVM)
+      kvm_get_fpu();
+    /* don't trust bit7 (ES) as it may be suppressed by our fake IGNNE,
+     * which is actually an exception mask in CWD */
+    fpu_ignne = !!(vm86_fpu_state.swd & 0x7f & ~mask);
+    /* Note: we emuate the "unrecommended" (by Intel) design where the
+     * untriggering of IGNNE requires an extra write to 0xf0 after fnclex */
+    if (fpu_ignne) {
+      if (!old_ignne)
+        fpu_orig_mask = mask;
+      vm86_fpu_state.cwd |= 0x7f;
+    } else if (old_ignne) {
+      vm86_fpu_state.cwd &= ~0x7f;
+      vm86_fpu_state.cwd |= mask;
+    }
     break;
   case 0xf1:
     fpu_reset();
@@ -306,13 +318,16 @@ static void fpu_io_write(ioport_t port, Bit8u val, void *arg)
   case 0xf2:
     if (fpu_ignne) {
       fpu_ignne = 0;
+      vm86_fpu_state.cwd &= ~0x7f;
+      vm86_fpu_state.cwd |= mask;
       /* fnclex */
       vm86_fpu_state.swd &= 0x7f00;
-      if (_CPU_VM_CURRENT() == CPUVM_KVM)
-        kvm_update_fpu();
     }
     break;
   }
+
+  if (_CPU_VM_CURRENT() == CPUVM_KVM)
+    kvm_update_fpu();
 }
 
 static int fpu_is_masked(void)
