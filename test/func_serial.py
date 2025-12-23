bb9ef9349759b036de84eb655269d7f20e3b8e99
Andrew Bird <ajb@spheresystems.co.uk>
2025-12-23 12:02:04 +0000

    CI: Add simple serial test read from echo

---
+++ test/func_serial.py
@@ -0,0 +1,16 @@
+from common_framework import DOSEMU_CONF_DEFAULT
+
+
+def serial_simple_read_echo(self):
+    config = DOSEMU_CONF_DEFAULT
+    config += """\
+$_com1 = "exec '/usr/bin/echo -n -e hello_world\\\\n\\\\x1a'"
+"""
+    self.mkfile("testit.bat", """\
+type com1
+rem end
+""", newline="\r\n")
+
+    results = self.runDosemu("testit.bat", config=config)
+
+    self.assertIn('hello_world', results)
