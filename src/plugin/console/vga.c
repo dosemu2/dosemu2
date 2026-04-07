dosemu2/dosemu2

commit 9743f13744196a12c2024ba82324bc42cf0bb8e3

console: fix save and restore (ctrl-alt-fx) for hardware VESA
Made sure vga_initialized (now separate from video_initialized, used
by vgaemu) is set, and add another page of lowmem heap to allow extra
space for ext regs and avoid OOM.

src/plugin/console/vga.c
@@ -738,6 +738,7 @@ static int vga_post_init(void)
 
   config.vga = 1;
   set_vc_screen_page();
+  vga_initialized = 1;
   return 0;
 }
 

src/base/core/lowmem.c
@@ -29,6 +29,7 @@
 #include "utilities.h"
 #include "emudpmi.h"
 #include "lowmem.h"
+#include "video.h"
 
 static smpool mp;
 unsigned char *dosemu_lmheap_base;
@@ -162,20 +163,21 @@ void rm_stack_leave(void)
 		set_TF();
 }
 
-#define LMHEAP_OFF 0x9000
-#define LMHEAP_SIZE 0x5000
+#define LMHEAP_OFF 0x8000
+#define LMHEAP_SIZE 0x6000
 
-static uint16_t lmheap_add(void)
+static uint16_t lmheap_sub(void)
 {
-	return (config.dos_up == 2 ? FDPP_LMHEAP_ADD : 0);
+	return (config.dos_up == 2 ? FDPP_LMHEAP_ADD : 0) +
+		(!(config.vga && config.chipset == VESA) ? VBE_LMHEAP_ADD : 0);
 }
 
 uint16_t lmheap_off(void)
 {
-	return LMHEAP_OFF + lmheap_add();
+	return LMHEAP_OFF + lmheap_sub();
 }
 
 uint16_t lmheap_size(void)
 {
-	return LMHEAP_SIZE - lmheap_add();
+	return LMHEAP_SIZE - lmheap_sub();
 }

src/include/memory.h
@@ -158,6 +158,7 @@ u_short INT_OFF(u_char i);
 uint16_t lmheap_off(void);
 uint16_t lmheap_size(void);
 #define FDPP_LMHEAP_ADD (1024 * 6)
+#define VBE_LMHEAP_ADD (1024 * 4)
 
 /* memcheck memory conflict finder definitions */
 int  memcheck_addtype(unsigned char map_char, const char *name);

src/plugin/console/vc.c
@@ -143,7 +143,7 @@ set_dos_video (void)
 
   /* jesx */
   /* After all that fun up there, get permissions and save/restore states */
-  if (video_initialized)
+  if (vga_initialized)
     {
       v_printf ("Acquiring vt, restoring dosemu_regs\n");
       get_perm ();
@@ -159,7 +159,7 @@ static void set_linux_video (void)
 
   /* jesx */
   /* After all that fun  rel perms and save/restore states */
-  if (video_initialized)
+  if (vga_initialized)
     {
       v_printf ("Storing dosemu_regs, Releasing vt mode=%02x\n", READ_BYTE(0x449));
       dosemu_regs.video_mode = READ_BYTE(0x449);

src/plugin/console/vga.h
@@ -18,5 +18,7 @@ void restore_vga_state(struct video_save_struct *save_regs);
 int emu_video_retrace_on(void);
 int emu_video_retrace_off(void);
 
+extern int vga_initialized;
+
 #endif
 /* End of video/vga.h */

commit 975131e142954e6d765739171270ef36354ae7e0

console: fix vga_initialized variable
Silly regression from 9743f1374, would compile but not load the
console plugin any more.

src/plugin/console/vga.c
@@ -39,6 +39,7 @@
 static int vga_init(void);
 static int vga_post_init(void);
 static struct video_system *Video_console;
+int vga_initialized;
 
 /* Here are the REGS values for valid dos int10 call */
 