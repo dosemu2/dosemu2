ba4e2acdb8a503cb73229beed3ce2b637ae304c9
Stas Sergeev <stsp2@yandex.ru>
2026-04-07 23:08:14 +0300

    vgaemu.h: fix wrong comment [#2817]
    graph_size is 0x10000, not 0x20000.

--- src/include/vgaemu.h
+++ src/include/vgaemu.h
@@ -273,7 +273,7 @@ typedef struct {
   unsigned bank_base;			/* banked base in bytes */
   unsigned bank_len;			/* banked length in bytes */
   unsigned graph_base;                  /* graphics base, normally 0xa0000 */
-  unsigned graph_size;                  /* graphics size, normally 0x20000 */
+  unsigned graph_size;                  /* graphics size, normally 0x10000 */
   dosaddr_t lfb_base;			/* base address for lfb, 0 if no lfb */
   unsigned lfb_base_page;		/* lfb base page, 0 -> no lfb support */
   vga_mapping_type map[VGAEMU_MAX_MAPPINGS];	/* all the mappings */
