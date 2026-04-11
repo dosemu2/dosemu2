908e6fd3edb1b16107f98de1cc51a4a9083aa947
Andrew Bird <ajb@spheresystems.co.uk>
2026-04-11 18:33:53 +0100

    CI: Add IVEC to test_dosdebug

--- test/test_dosdebug.py
+++ test/test_dosdebug.py
@@ -43,6 +43,25 @@ echo 'd b800:0f40 256' | {self.dosdebug}
         tstr = ' '.join(tlist)
         self.assertIn("43 07 3A 07 5C 07 3E 07", tstr)  # C.:.\.>.
 
+    def test_ivec_at_startup(self):
+        """Use the IVEC command to show the interrupt vectors at startup"""
+
+        dbg_script = f"""\
+#!/bin/bash
+set -e
+echo 'ivec' | {self.dosdebug}
+"""
+
+        dos_results, dbg_results = self.runDosemuWithDosdebug(
+                CmdState.WaitAtPromptForSentinal,
+                dbg_script,
+                dostimeout=30, dbgtimeout=60)
+
+        self.assertRegex(dbg_results, r"  33  [0-9A-Z]{4}:[0-9A-Z]{4}\(MOUSE_INT33_OFF\)")
+        self.assertRegex(dbg_results, r"  41  [0-9A-Z]{4}:[0-9A-Z]{4}\(HD_parameter_table0\)")
+        self.assertRegex(dbg_results, r"  46  [0-9A-Z]{4}:[0-9A-Z]{4}\(HD_parameter_table1\)")
+        self.assertRegex(dbg_results, r"  61  [0-9A-Z]{4}:[0-9A-Z]{4}\(TCPDRV_OFF\)")
+
 
 # The DOS variants we want get included here
 FRDOS130TestCase = frdos130(OurTestCase, {})
