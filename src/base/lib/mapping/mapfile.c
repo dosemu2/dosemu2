dosemu2/dosemu2

commit adff2a5f82953e2177e1c34a8978ffb7caf46d3c

mapping: integrate SHM mappings with SW ptbl [#2772]
This patch integrates SHM mappings with mapfile subsystem to
make it "aliasable". Then alias_mapping_pa() is used to set up
SW ptbls.

src/base/lib/mapping/mapfile.c
@@ -224,25 +224,17 @@ static void close_mapping_file(int cap)
   }
 }
 
-static void *alloc_mapping_file(int cap, size_t mapsize, void *target)
+static void *alloc_tail(int fd, size_t mapsize, int prot, void *target)
 {
-  int i, fixed = 0, prot = PROT_READ | PROT_WRITE;
+  int i, fixed = 0;
   struct file_mapping *p;
-  int fd, rc;
+  int rc = ftruncate(fd, mapsize);
+  assert(rc != -1);
 
-  Q__printf("MAPPING: alloc, cap=%s, mapsize=%zx\n", cap, mapsize);
   for (i = 0, p = file_mappings; i < MAX_FILE_MAPPINGS; i++, p++)
     if (p->size == 0)
       break;
   assert(i < MAX_FILE_MAPPINGS);
-  fd = mfops->open();
-  if (fd < 0) {
-    error("mapping: open() failed\n");
-    return MAP_FAILED;
-  }
-  rc = ftruncate(fd, mapsize);
-  assert(rc != -1);
-
   if (target != (void *)-1)
     fixed = MAP_FIXED;
   else
@@ -258,6 +250,33 @@ static void *alloc_mapping_file(int cap, size_t mapsize, void *target)
   p->fd = fd;
   p->prot = prot;
   return target;
+}
+
+static void *alloc_mapping_file(int cap, size_t mapsize, void *target)
+{
+  int prot = PROT_READ | PROT_WRITE;
+  int fd;
+
+  Q__printf("MAPPING: alloc, cap=%s, mapsize=%zx\n", cap, mapsize);
+  fd = mfops->open();
+  if (fd < 0) {
+    error("mapping: open() failed\n");
+    return MAP_FAILED;
+  }
+  return alloc_tail(fd, mapsize, prot, target);
+}
+
+static void *attach_mapping_file(int fd, size_t mapsize, int prot)
+{
+  return alloc_tail(fd, mapsize, prot, (void *)-1);
+}
+
+static void detach_mapping_file(void *target)
+{
+  struct file_mapping *p = find_file_mapping(target);
+
+  munmap(p->addr, p->size);
+  p->size = 0;
 }
 
 static void free_mapping_file(int cap, void *addr, size_t mapsize)
@@ -323,7 +342,9 @@ struct mappingdrivers mappingdriver_shm = {
   alloc_mapping_file,
   free_mapping_file,
   resize_mapping_file,
-  alias_mapping_file
+  alias_mapping_file,
+  attach_mapping_file,
+  detach_mapping_file,
 };
 #endif
 
@@ -337,7 +358,9 @@ struct mappingdrivers mappingdriver_mshm = {
   alloc_mapping_file,
   free_mapping_file,
   resize_mapping_file,
-  alias_mapping_file
+  alias_mapping_file,
+  attach_mapping_file,
+  detach_mapping_file,
 };
 #endif
 #endif
@@ -350,5 +373,7 @@ struct mappingdrivers mappingdriver_file = {
   alloc_mapping_file,
   free_mapping_file,
   resize_mapping_file,
-  alias_mapping_file
+  alias_mapping_file,
+  attach_mapping_file,
+  detach_mapping_file,
 };

src/base/lib/mapping/mapping.c
@@ -70,6 +70,7 @@ struct hardware_ram {
   int type;
   struct aliasmap_s *aliasmap;
   struct hardware_ram *next;
+  void *shm;
 };
 
 #ifdef __linux__
@@ -692,7 +693,7 @@ char *decode_mapping_cap(int cap)
     if (cap & MAPPING_VGAEMU) p += sprintf(p, " VGAEMU");
     if (cap & MAPPING_VIDEO) p += sprintf(p, " VIDEO");
     if (cap & MAPPING_VC) p += sprintf(p, " VC");
-    if (cap & MAPPING_HGC) p += sprintf(p, " HGC");
+    if (cap & MAPPING_SHM) p += sprintf(p, " SHM");
     if (cap & MAPPING_HMA) p += sprintf(p, " HMA");
     if (cap & MAPPING_SHARED) p += sprintf(p, " SHARED");
     if (cap & MAPPING_INIT_HWRAM) p += sprintf(p, " INIT_HWRAM");
@@ -991,6 +992,7 @@ static int do_register_hwram(int type, unsigned base, unsigned size,
   hw->size = size;
   hw->type = type;
   hw->aliasmap = alloc_aliasmap(size);
+  hw->shm = NULL;
   hw->next = hardware_ram;
   hardware_ram = hw;
   if (base >= LOWMEM_SIZE || type == 'h')
@@ -1042,7 +1044,9 @@ int unregister_hardware_ram_virtual(unsigned base)
       else
         hardware_ram = hw->next;
       if (hw->base + hw->size > ALIAS_SIZE)
-	free(hw->aliasmap);
+        free(hw->aliasmap);
+      if (hw->shm)
+        mappingdriver->detach(hw->shm);
       free(hw);
       return 0;
     }
@@ -1303,6 +1307,8 @@ int alias_mapping_pa(int cap, unsigned addr, size_t mapsize, int protect,
   }
   hwram_update_aliasmap(hw, addr, mapsize, source);
   hwram_mprotect_aliasmap(hw, addr, mapsize, protect);
+  if (cap & MAPPING_SHM)
+    hw->shm = source;
   return 0;
 }
 
@@ -1378,23 +1384,18 @@ static int madvise_mapping(dosaddr_t targ, size_t length, int flags)
 }
 #endif
 
-void *mmap_shm_mapping(dosaddr_t targ, size_t length, int prot, int fd)
+void *mmap_shm_mapping(unsigned targ, size_t length, int prot, int fd)
 {
-  int i, err;
+  int err;
   void *addr, *ret;
   int flags = MAP_SHARED | MAP_FIXED;
 
-  for (i = 0; i < MAX_BASES; i++) {
-    int prot2 = prot;
-    addr = MEM_BASE32x(targ, i);
-    if (addr == MAP_FAILED)
-      continue;
-    if (mem_bases[i].nx)
-      prot2 &= ~PROT_EXEC;
-    ret = mmap(addr, length, prot2, flags, fd, 0);
-    if (ret != addr)
-      return MAP_FAILED;
-  }
+  ret = mappingdriver->attach(fd, length, prot);
+  if (ret == MAP_FAILED)
+    return MAP_FAILED;
+  err = alias_mapping_pa(MAPPING_SHM, targ, length, prot, ret);
+  if (err)
+    return MAP_FAILED;
   addr = MEM_BASE32(targ);
   if ((unsigned char *)addr < mem_bases[MEM_BASE].base ||
       (unsigned char *)addr + length > mem_bases[MEM_BASE].base +

src/base/lib/mapping/mapping.h
@@ -64,7 +64,7 @@
 #define MAPPING_VGAEMU		0x000008
 #define MAPPING_VIDEO		0x000010
 #define MAPPING_VC		MAPPING_VIDEO
-#define MAPPING_HGC		0x000020
+#define MAPPING_SHM		0x000020
 #define MAPPING_HMA		0x000040
 #define MAPPING_SHARED		0x000080
 #define MAPPING_INIT_HWRAM	0x000100
@@ -115,7 +115,7 @@ int restore_mapping_pa(unsigned int addr, size_t mapsize);
 int mcommit_mapping(dosaddr_t targ, size_t size);
 int mcommit(void *addr, size_t size);
 /* below wrapper is needed only for remoting the mapping subsystem */
-void *mmap_shm_mapping(dosaddr_t targ, size_t length, int prot, int fd);
+void *mmap_shm_mapping(unsigned targ, size_t length, int prot, int fd);
 int mapping_is_mapped(void *addr);
 int mapping_is_mapped_pa(unsigned int addr, int mapsize);
 
@@ -135,6 +135,8 @@ void mapping_register_hook(const struct mapping_hook *hook);
 typedef int open_mapping_type(int cap);
 typedef void close_mapping_type(int cap);
 typedef void *alloc_mapping_type(int cap, size_t mapsize, void *target);
+typedef void *attach_mapping_type(int fd, size_t mapsize, int prot);
+typedef void detach_mapping_type(void *target);
 typedef void free_mapping_type(int cap, void *addr, size_t mapsize);
 typedef void *resize_mapping_type(int cap, void *addr, size_t oldsize, size_t newsize);
 typedef void *alias_mapping_type(int cap, void *target, size_t mapsize, int protect, void *source);
@@ -148,6 +150,8 @@ struct mappingdrivers {
   free_mapping_type *free;
   resize_mapping_type *resize;
   alias_mapping_type *alias;
+  attach_mapping_type *attach;
+  detach_mapping_type *detach;
 };
 char *decode_mapping_cap(int cap);
 

commit 20062a96643022bf335d103016bdc4c0219dd7bd

mapping: dont ftruncate on fd attach [#2902]
It breaks debuglink.
Regression from adff2a5f8

src/base/lib/mapping/mapfile.c
@@ -230,10 +230,6 @@ static void *alloc_tail(int fd, size_t mapsize, int prot, void *target)
 {
   int i, fixed = 0;
   struct file_mapping *p;
-  if (fd != -1) {
-    int rc = ftruncate(fd, mapsize);
-    assert(rc != -1);
-  }
 
   for (i = 0, p = file_mappings; i < MAX_FILE_MAPPINGS; i++, p++)
     if (p->size == 0)
@@ -268,7 +264,7 @@ static void *alloc_tail(int fd, size_t mapsize, int prot, void *target)
 static void *alloc_mapping_file(int cap, size_t mapsize, void *target)
 {
   int prot = PROT_READ | PROT_WRITE;
-  int fd;
+  int fd, rc;
 
   Q__printf("MAPPING: alloc, cap=%s, mapsize=%zx\n", cap, mapsize);
   fd = mfops->open();
@@ -276,6 +272,8 @@ static void *alloc_mapping_file(int cap, size_t mapsize, void *target)
     error("mapping: open() failed\n");
     return MAP_FAILED;
   }
+  rc = ftruncate(fd, mapsize);
+  assert(rc != -1);
   return alloc_tail(fd, mapsize, prot, target);
 }
 