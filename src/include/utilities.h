2cb404816a422b42cfe0d193b8dab6ae37e30666
Stas Sergeev <stsp2@yandex.ru>
2026-07-06 16:12:52 +0300

    utilities: ECHO undeclared on solaris

--- src/include/utilities.h
+++ src/include/utilities.h
@@ -210,7 +210,7 @@ static inline void cfmakeraw(struct termios *termios_p)
 {
     termios_p->c_iflag &= ~(IGNBRK|BRKINT|PARMRK|ISTRIP|INLCR|IGNCR|ICRNL|IXON);
     termios_p->c_oflag &= ~OPOST;
-    termios_p->c_lflag &= ~(ECHO|ECHONL|ICANON|ISIG|IEXTEN);
+    termios_p->c_lflag &= ~(ECHOE|ECHONL|ICANON|ISIG|IEXTEN);
     termios_p->c_cflag &= ~(CSIZE|PARENB);
     termios_p->c_cflag |= CS8;
 }
